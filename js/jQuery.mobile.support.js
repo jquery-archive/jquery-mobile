0479ff52397f29d841e29736c75a7e1b52421c68
Scott González <scott.gonzalez@gmail.com>
2010-09-17 14:19:53 -0400

    Refactored $.media and moved to jQuery.mobile.support.js

--- js/jQuery.mobile.support.js
+++ js/jQuery.mobile.support.js
@@ -42,3 +42,23 @@ $.extend( $.support, {
 	
 	fakeBody.remove();
 })();
+
+// test whether a CSS media type or query applies
+$.media = (function() {
+	// TODO: use window.matchMedia once at least one UA implements it
+	var cache = {},
+		testDiv = $( "<div id='jquery-mediatest'>" ),
+		fakeBody = $( "<body>" ).append( testDiv );
+	
+	return function( query ) {
+		if ( !( query in cache ) ) {
+			var styleBlock = $( "<style type='text/css'>" +
+				"@media " + query + "{#jquery-mediatest{position:absolute;}}" +
+				"</style>" );
+			$html.prepend( fakeBody ).prepend( styleBlock );
+			cache[ query ] = testDiv.css( "position" ) === "absolute";
+			fakeBody.add( styleBlock ).remove();
+		}
+		return cache[ query ];
+	};
+})();
