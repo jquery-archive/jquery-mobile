2e9c5914e6c56b02ee29f21af7f2232bdc54c583
scottjehl <scott@scottjehl.com>
2010-09-18 09:53:00 -0400

    split CSS files out into individual components

--- css/manifest.php
+++ css/manifest.php
@@ -1,7 +1,20 @@
 <?
 $elements = array(
-	'theme.css',
-	'structure.css'
+	'jQuery.mobile.theme.css',
+	'jQuery.mobile.core.css',
+	'jQuery.mobile.transitions.css',
+	'jQuery.mobile.grids.css',
+	'jQuery.mobile.headerfooter.css',
+	'jQuery.mobile.globalnav.css',
+	'jQuery.mobile.button.css',
+	'jQuery.mobile.collapsible.css',
+	'jQuery.mobile.controlgroup.css',
+	'jQuery.mobile.dialog.css',
+	'jQuery.mobile.forms.checkboxradio.css',
+	'jQuery.mobile.forms.fieldcontain.css',
+	'jQuery.mobile.forms.select.css',
+	'jQuery.mobile.forms.textinput.css',
+	'jQuery.mobile.listview.css'
 );
 
 ?>
