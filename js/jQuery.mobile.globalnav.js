08f1ec9bf80cd765f0c176faf02308c5a16e3312
scottjehl <scott@scottjehl.com>
2010-09-19 10:51:21 -0400

    rewrite of globalnav plugin. Anything beyond 3 links auto-generates a "more..." button and creates a fully navigable, scrollable page with all the links in a grid view.

--- js/jQuery.mobile.globalnav.js
+++ js/jQuery.mobile.globalnav.js
@@ -10,24 +10,53 @@ $.fn.globalnav = function(settings){
 		if($(this).find('.ui-globalnav').length){ return; }
 		
 		//remove any other globalnav currently present
-		$('.ui-globalnav').remove();	
+		$('[data-role="globalnav"]:has(.ui-globalnav)').remove();	
 		
 		var o = $.extend({
-			fixedAs: 'footer'
+			fixedAs: 'footer',
+			moreText: 'More...',
+			thisId: $(this).parents('.ui-page').attr('id') + '-nav'
 		},settings);
 		
-		//wrap it with footer classes
-		var $globalnav = $(this);
-			$globalnav
-				.wrapInner('<div class="ui-globalnav ui-bar-a"></div>')
-				.children(0)
-				.addClass(o.fixedAs == 'footer' ? 'ui-footer' : 'ui-header');
+		$(this)
+			.attr('id', o.thisId)
+			.wrapInner('<div class="ui-bar-a ' + (o.fixedAs == 'footer' ? 'ui-footer' : 'ui-header') + '"><div class="ui-globalnav"></div></div>');
 		
-		//set up the nav tabs widths (currently evenly divided widths, to be improved later)
+		//wrap it with footer classes
+		var $globalnav = $(this).find('.ui-globalnav'),
+			numTabs = $globalnav.find('li').length;
+			
+			$globalnav
+				.find('ul')
+				.grid({grid: numTabs > 2 ? 'b' : 'a'})		
+		
+		if(numTabs > 3 ){
+			var moreId = o.thisId + '-more',
+				$navToggle = $('<a href="#'+ moreId +'" data-transition="slideup">' + o.moreText + '</a>'),
+				$truncatedLis = $globalnav.find('li:gt(2)'),
+				$newPage = $('<div id="'+ moreId +'" class="ui-page ui-globalnav-expanded ui-body-a"><div class="ui-header"><h1>' + o.moreText + '</h1></div><div class="ui-content"></div></div>'),
+				$newPageContent =  $globalnav.find('ul').clone(); 
+				
+				$newPageContent
+					.find('a')
+					.buttonMarkup({iconPos: 'top', theme: 'b', icon: 'arrow-r'})
+				
+				$newPage.append( $newPageContent ).appendTo('body');
+			
+			$navToggle
+				.wrap('<div class="ui-globalnav-toggle"></div>')
+				.parent()
+				.appendTo($globalnav);
+			
+			$navToggle.buttonMarkup({corners: false, iconPos: 'top', icon: 'arrow-r'});	
+				
+			$globalnav.addClass('ui-globalnav-collapsed');
+		
+			$truncatedLis.addClass('ui-globalnav-truncate');
+		}	
+		
 		$globalnav
-			.find('ul')
-			.grid({grid: $globalnav.find('li').length > 2 ? 'b' : 'a'})		
-			.find('a')
+			.find('ul a')
 			.buttonMarkup({corners: false, iconPos: 'top', icon: 'arrow-u'})
 			.bind('tap',function(){
 				//NOTE: we'll need to find a way to highlight an active tab at load as well
@@ -35,10 +64,9 @@ $.fn.globalnav = function(settings){
 				$(this).addClass('ui-btn-active');
 			});
 		
-		$globalnav
+		$(this)
 			.appendTo('body')
 			.fixHeaderFooter();	
-
 	});
 };	
 })(jQuery);
