6f10fe2459999d24c9907b3273205f4210c9d911
Scott González <scott.gonzalez@gmail.com>
2010-10-07 10:50:10 -0400

    Renamed files from jQuery.* to jquery.*. Fixes #33.

--- css/manifest.php
+++ css/manifest.php
@@ -1,21 +1,21 @@
 <?
 $elements = array(
-	'jQuery.mobile.theme.css',
-	'jQuery.mobile.core.css',
-	'jQuery.mobile.transitions.css',
-	'jQuery.mobile.grids.css',
-	'jQuery.mobile.headerfooter.css',
-	'jQuery.mobile.navlist.css',
-	'jQuery.mobile.button.css',
-	'jQuery.mobile.collapsible.css',
-	'jQuery.mobile.controlgroup.css',
-	'jQuery.mobile.dialog.css',
-	'jQuery.mobile.forms.checkboxradio.css',
-	'jQuery.mobile.forms.fieldcontain.css',
-	'jQuery.mobile.forms.select.css',
-	'jQuery.mobile.forms.textinput.css',
-	'jQuery.mobile.listview.css',
-	'jQuery.mobile.forms.slider.css'
+	'jquery.mobile.theme.css',
+	'jquery.mobile.core.css',
+	'jquery.mobile.transitions.css',
+	'jquery.mobile.grids.css',
+	'jquery.mobile.headerfooter.css',
+	'jquery.mobile.navlist.css',
+	'jquery.mobile.button.css',
+	'jquery.mobile.collapsible.css',
+	'jquery.mobile.controlgroup.css',
+	'jquery.mobile.dialog.css',
+	'jquery.mobile.forms.checkboxradio.css',
+	'jquery.mobile.forms.fieldcontain.css',
+	'jquery.mobile.forms.select.css',
+	'jquery.mobile.forms.textinput.css',
+	'jquery.mobile.listview.css',
+	'jquery.mobile.forms.slider.css'
 );
 
 ?>
