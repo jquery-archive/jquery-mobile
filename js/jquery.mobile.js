930430ab2ea19c37fb324d8879cc9227bc5a9578
scottjehl <scott@scottjehl.com>
2010-10-12 14:27:48 -0400

    removed the event logger from core. Fixes #159

--- js/jquery.mobile.js
+++ js/jquery.mobile.js
@@ -356,11 +356,6 @@
 		
 		//update orientation 
 		$html.addClass( jQuery.event.special.orientationchange.orientation( $window ) );
-		
-		//some debug stuff for the events pages
-		jQuery('body').bind('scrollstart scrollstop swipe swipeleft swiperight tap taphold turn',function(e){
-			jQuery('#eventlogger').prepend('<div>Event fired: '+ e.type +'</div>');
-		});
 	});
 	
 	$window.load(hideBrowserChrome);
