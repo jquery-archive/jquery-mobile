47e35832a9933097ce05c0404c9ef53d495e0bd3
scottjehl <scott@scottjehl.com>
2010-10-13 17:25:14 -0400

    Added two properties to support: - boxShadow (tests that the property is defined and window.blackberry is false, as BB's shadows have no feathering) - scrollTop (tests that the property is defined and that a webos property or two isn't defined, as Palm always reports 0)
    Added an existence test for CSS properties, which is used for cssPseudoElement and boxShadow).

--- js/jquery.mobile.support.js
+++ js/jquery.mobile.support.js
@@ -25,7 +25,22 @@ $.media = (function() {
 	};
 })();
 
-var fakeBody = $( "<body>" ).prependTo( "html" );
+var fakeBody = $( "<body>" ).prependTo( "html" ),
+	fbCSS = fakeBody[0].style,
+	vendors = ['webkit','moz','o'],
+	webos = window.palmGetResource || window.PalmServiceBridge, //only used to rule out scrollTop 
+	bb = window.blackberry; //only used to rule out box shadow, as it's filled opaque on BB
+
+//thx Modernizr
+function propExists( prop ){
+	var uc_prop = prop.charAt(0).toUpperCase() + prop.substr(1),
+		props   = (prop + ' ' + vendors.join(uc_prop + ' ') + uc_prop).split(' ');
+	for(var v in props){
+		if( fbCSS[ v ] !== undefined ){
+			return true;
+		}
+	}
+};
 
 $.extend( $.support, {
 	orientation: "orientation" in window,
@@ -33,7 +48,9 @@ $.extend( $.support, {
 	WebKitAnimationEvent: typeof WebKitTransitionEvent === "object",
 	pushState: !!history.pushState,
 	mediaquery: $.media('only all'),
-	cssPseudoElement: fakeBody[0].style.content !== undefined
+	cssPseudoElement: !!propExists('content'),
+	boxShadow: !!propExists('boxShadow') && !bb,
+	scrollTop: ("pageXOffset" in window || "scrollTop" in document.documentElement || "scrollTop" in fakeBody[0]) && !webos
 });
 
 fakeBody.remove();
