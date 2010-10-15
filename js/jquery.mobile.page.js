193a4004766f370bab77a251bcbe482005ec1ce6
scottjehl <scott@scottjehl.com>
2010-10-15 18:25:54 -0400

    changed logic for auto-adding back btns. Hoping this Fixes #143

--- js/jquery.mobile.page.js
+++ js/jquery.mobile.page.js
@@ -50,7 +50,7 @@ $.widget( "mobile.page", $.mobile.widget, {
 				}
 				
 				//auto-add back btn on pages beyond first view
-				if ( $.mobile.addBackBtn && role === "header" && $.mobile.urlStack.length > 1 && !leftbtn && !$this.data( "noBackBtn" ) ) {
+				if ( $.mobile.addBackBtn && role === "header" && ($.mobile.urlStack.length > 1 || $('.ui-page').length > 1) && !leftbtn && !$this.data( "noBackBtn" ) ) {
 					$( "<a href='#' class='ui-btn-left' data-icon='arrow-l'>Back</a>" )
 						.tap(function() {
 							history.back();
