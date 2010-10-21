ef3fd116dca8be147bac6057ff84e21895c25380
scottjehl <scott@scottjehl.com>
2010-10-21 16:29:05 -0400

    refactored to use widget factory and not be tracked via hash/history. Fixes #176

--- js/jquery.mobile.dialog.js
+++ js/jquery.mobile.dialog.js
@@ -4,27 +4,32 @@
 * Dual licensed under the MIT (MIT-LICENSE.txt) and GPL (GPL-LICENSE.txt) licenses.
 * Note: Code is in draft form and is subject to change 
 */
-(function($){
-$.fn.dialog = function(options){
-	return $(this).each(function(){		
-		$(this)
+(function ( $ ) {
+$.widget( "mobile.dialog", $.mobile.widget, {
+	options: {},
+	_create: function(){	
+		var $el = this.element,
+			$closeBtn = $('<a href="#" data-icon="delete" data-iconpos="notext">Close</a>')
+							.click(function(){
+								$.changePage([$el, $.activePage], undefined, true );
+								return false;
+							});
+	
+		this.element
+			.bind("pageshow",function(){
+				return false;
+			})
 			//add ARIA role
 			.attr("role","dialog")
 			.addClass('ui-page ui-dialog ui-body-a')
 			.find('[data-role=header]')
 			.addClass('ui-corner-top ui-overlay-shadow')
-				.find('.ui-btn-left').tap(function(){
-					$.changePage()
-					return false;
-				})
-				.click(function(){
-					return false;
-				})
-				.end()
+				.prepend( $closeBtn )
 			.end()
 			.find('.ui-content,[data-role=footer]')
 				.last()
 				.addClass('ui-corner-bottom ui-overlay-shadow');
-	});
-};
-})(jQuery);
+
+	}
+});
+})( jQuery );
