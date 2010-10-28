6fb1d2a6100a45cd5be3bf5e30994f61c61c3282
scottjehl <scott@scottjehl.com>
2010-10-28 09:58:00 +0800

    rewrote navbar with widget factory

--- js/jquery.mobile.navbar.js
+++ js/jquery.mobile.navbar.js
@@ -4,40 +4,36 @@
 * Dual licensed under the MIT (MIT-LICENSE.txt) and GPL (GPL-LICENSE.txt) licenses.
 * Note: Code is in draft form and is subject to change 
 */
-(function($){
-$.fn.navbar = function(settings){
-	return $(this).each(function(){ 
-
-		var o = $.extend({
-			iconpos: $(this).data('iconpos') || 'top',
-			transition: $(this).data('transition') || 'slideup'
-		},settings);
-		
-		//wrap it with footer classes
-		var $navbar = $(this).addClass('ui-navbar'),
-			numTabs = $navbar.find('li').length,
-			moreIcon = $navbar.find('a[data-icon]').length ? 'arrow-r' : null;
-			
-			if( moreIcon == null ){ 
-				o.iconpos = null; 
-				$navbar.add( $navbar.children(0) ).addClass('ui-navbar-noicons');
-			}
-			
-			$navbar
-				//add ARIA role
-				.attr("role","navigation")
-				.find('ul')
-				.grid({grid: numTabs > 2 ? 'b' : 'a'});		
+(function ( $ ) {
+$.widget( "mobile.navbar", $.mobile.widget, {
+	options: {
+		iconpos: 'top'
+	},
+	_create: function(){
+		var $navbar = this.element,
+			$navbtns = $navbar.find("a"),
+			iconpos = $navbtns.filter('icon').length ? this.options.iconpos : undefined;
 		
 		$navbar
-			.find('ul a')
-			.buttonMarkup({corners: false, shadow:false, iconpos: o.iconpos})
-			.bind('click',function(){
-				//NOTE: we'll need to find a way to highlight an active tab at load as well
-				$navbar.find('.ui-btn-active').removeClass('ui-btn-active');
-				$(this).addClass('ui-btn-active');
+			.addClass('ui-navbar')
+			.attr("role","navigation")
+			.find("ul")
+				.grid({grid: $navbtns.length > 2 ? "b" : "a"});		
+		
+		if( !iconpos ){ 
+			$navbar.addClass("ui-navbar-noicons");
+		}
+		
+		$navbtns
+			.buttonMarkup({
+				corners:	false, 
+				shadow:		false, 
+				iconpos:	iconpos
 			});
-
-	});
-};	
-})(jQuery);
+		
+		$navbar.delegate("a", "click",function(event){
+			$navbtns.removeClass("ui-btn-active");
+		});	
+	}
+});
+})( jQuery );
