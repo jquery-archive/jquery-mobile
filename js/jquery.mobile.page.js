9ff1a1be6d75037c09bed8576c03bc186bcd48ac
scottjehl <scott@scottjehl.com>
2010-11-01 22:43:51 -0400

    allow header, content, and footer to inherit theming from the page element, if unspecified on the elements themselves. Fixes #282

--- js/jquery.mobile.page.js
+++ js/jquery.mobile.page.js
@@ -25,7 +25,7 @@ jQuery.widget( "mobile.page", jQuery.mobile.widget, {
 		var $dataEls = $elem.find( "[data-role]" ).andSelf().each(function() {
 			var $this = jQuery( this ),
 				role = $this.data( "role" ),
-				theme = $this.data( "theme" );
+				theme = $this.data( "theme" ) || $this.parent('[data-role=page]').data( "theme" );
 			
 			//apply theming and markup modifications to page,header,content,footer
 			if ( role === "header" || role === "footer" ) {
