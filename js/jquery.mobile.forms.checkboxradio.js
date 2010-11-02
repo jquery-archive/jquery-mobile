e66997b7940d7cf1d795096db79f9b1cce5580d5
Matt Lins <mattlins@gmail.com>
2010-11-02 13:31:09 -0500

    Use existing inputtype var

--- js/jquery.mobile.forms.checkboxradio.js
+++ js/jquery.mobile.forms.checkboxradio.js
@@ -55,7 +55,7 @@ $.widget( "mobile.checkboxradio", $.mobile.widget, {
 			.bind({
 
 				click: function() {
-					jQuery( "input[name='" + input.attr( "name" ) + "'][type='" + input.attr( "type" ) + "']" ).checkboxradio( "refresh" );
+					jQuery( "input[name='" + input.attr( "name" ) + "'][type='" + inputtype + "']" ).checkboxradio( "refresh" );
 				},
 
 				focus: function() { 
