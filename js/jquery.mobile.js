34283c04ed8ae59f75faaca91349bc20d1a95800
scottjehl <scott@scottjehl.com>
2010-11-04 11:24:13 -0400

    changed hashchange args to use array

--- js/jquery.mobile.js
+++ js/jquery.mobile.js
@@ -422,10 +422,10 @@
 		
 		// needs to be bound at domready (for IE6)
 		// find or load content, make it active
-		$window.bind( "hashchange", function(e, extras) {
+		$window.bind( "hashchange", function(e, manuallyTriggered) {
 			if( !hashListener ){ return; } 
 			var to = location.hash,
-				transition = (extras && extras.manuallyTriggered) ? false : undefined;
+				transition = manuallyTriggered ? false : undefined;
 				
 			// either we've backed up to the root page url
 			// or it's the first page load with no hash present
@@ -436,7 +436,7 @@
 			}
 			//there's no hash, the active page is not the start page, and it's not manually triggered hashchange
 			// > probably backed out to the first page visited
-			else if( $.activePage.length && !$startPage.is( $.activePage ) && !(extras && extras.manuallyTriggered) ) {
+			else if( $.activePage.length && !$startPage.is( $.activePage ) && !manuallyTriggered ) {
 				changePage( $startPage, transition, true );
 			}
 			else{
@@ -554,7 +554,7 @@
 		$pages.page();
 		
 		//trigger a new hashchange, hash or not
-		$window.trigger( "hashchange", { manuallyTriggered: true } );
+		$window.trigger( "hashchange", [ true ] );
 		
 		//update orientation 
 		$html.addClass( jQuery.event.special.orientationchange.orientation( $window ) );
