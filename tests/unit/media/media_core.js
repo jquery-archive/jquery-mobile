5f59a3908bed57498f93f10fb55c9497774d56ba
John Bender <john.m.bender@gmail.com>
2010-11-18 23:38:54 -0800

    added negative case for basic absolute position test

--- tests/unit/media/media_core.js
+++ tests/unit/media/media_core.js
@@ -6,16 +6,22 @@
 	test( "media check returns true when the position is absolute", function(){
 		expect( 1 );
 		$.fn.css = function(){ return "absolute"; };
-		same($.mobile.media('screen'), true);
+		same($.mobile.media('screen 1'), true);
+	});
+
+	test( "media check returns false when the position is not absolute", function(){
+		expect( 1 );
+		$.fn.css = function(){ return "not absolute"; };
+		same($.mobile.media('screen 2'), false);
 	});
 
 	test( "media check is cached", function(){
 		expect( 2 );
 
 		$.fn.css = function(){ return "absolute"; };
-		same($.mobile.media('screen'), true);
+		same($.mobile.media('screen 3'), true);
 
 		$.fn.css = function(){ return "not absolute"; };
-		same($.mobile.media('screen'), true);
+		same($.mobile.media('screen 3'), true);
 	});
 })(jQuery);
