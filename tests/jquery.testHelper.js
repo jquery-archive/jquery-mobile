e071a9da0c6d4a8aa78cae1ee8b869b4ad902592
John Bender <john.m.bender@gmail.com>
2010-11-22 08:27:31 -0800

    added reload lib helper, moved support core over

--- tests/jquery.testHelper.js
+++ tests/jquery.testHelper.js
@@ -6,7 +6,7 @@
 	$.testHelper = {
 		excludeFileProtocol: function(callback){
 			var message = "Tests require script reload and cannot be run via file: protocol";
-			//NOTE alert tester that running the file locally will not work for these tests
+
 			if ( location.protocol == "file:" ) {
 				test(message, function(){
 					ok(false, message);
@@ -14,6 +14,21 @@
 			} else {
 				callback();
 			}
+		},
+
+		reloadCounts: {},
+
+		reloadLib: function(libName){
+			var lib = $("script[src$=" + libName + "]").clone(),
+					src = lib.attr('src');
+
+			if(this.reloadCounts[libName] === undefined) {
+				this.reloadCounts[libName] = 0;
+			}
+
+			//NOTE append "cache breaker" to force reload
+			lib.attr('src', src + "?" + this.reloadCounts[libName]++);
+			$("body").append(lib);
 		}
 	};
 })(jQuery);
