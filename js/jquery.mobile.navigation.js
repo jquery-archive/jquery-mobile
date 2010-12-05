bacf915e7e70dfc53022b4a720963ba4db5082ca
kosmas58 <kosmas.schuetz@gmx.com>
2010-12-05 10:19:36 +0100

    1.0a2-134
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -146,6 +146,11 @@
 			duplicateCachedPage = null,
 			back = (back !== undefined) ? back : ( urlStack.length > 1 && urlStack[ urlStack.length - 2 ].url === url ),
 			transition = (transition !== undefined) ? transition : $.mobile.defaultTransition;
+			
+		//If we are trying to transition to the same page that we are currently on ignore the request.
+		if(urlStack.length > 1 && url === urlStack[urlStack.length -1].url) {
+			return;
+		}
 		
 		if( $.type(to) === "object" && to.url ){
 			url = to.url,
@@ -161,6 +166,9 @@
 			
 		//reset base to pathname for new request
 		if(base){ base.reset(); }
+		
+		//kill the keyboard
+		$( window.document.activeElement ).add(':focus').blur();
 			
 		// if the new href is the same as the previous one
 		if ( back ) {
@@ -174,9 +182,6 @@
 		
 		//function for transitioning between two existing pages
 		function transitionPages() {
-				
-			//kill the keyboard
-			$( window.document.activeElement ).blur();
 			
 			//get current scroll distance
 			var currScroll = $window.scrollTop();
@@ -448,12 +453,12 @@
 			
 		//if to is defined, use it
 		if ( to ){
-			$.mobile.changePage( to, transition);
+			$.mobile.changePage( to, transition, undefined, false);
 		}
 		//there's no hash, the active page is not the start page, and it's not manually triggered hashchange
 		//we probably backed out to the first page visited
 		else if( $.mobile.activePage.length && $.mobile.startPage[0] !== $.mobile.activePage[0] && !triggered ) {
-			$.mobile.changePage( $.mobile.startPage, transition, true );
+			$.mobile.changePage( $.mobile.startPage, transition, true, false );
 		}
 		//probably the first page - show it
 		else{
