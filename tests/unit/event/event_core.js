cb2f3ef4bcc4ed461649163f1d72b9a6831c7053
kosmas58 <kosmas.schuetz@gmx.com>
2010-12-08 18:27:20 +0100

    1.0a2-142
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- tests/unit/event/event_core.js
+++ tests/unit/event/event_core.js
@@ -4,6 +4,7 @@
 
 (function( $ ) {
 	var libName = "jquery.mobile.event.js",
+			absFn = Math.abs,
 			events = ("touchstart touchmove touchend orientationchange tap taphold " +
 								"swipe swipeleft swiperight scrollstart scrollstop").split( " " );
 
@@ -14,6 +15,9 @@
 			});
 
 			$($.event.special.scrollstart).unbind("scrollstart");
+
+			//NOTE unmock abs
+			Math.abs = absFn;
 		}
 	});
 
@@ -104,11 +108,15 @@
 		}, 50);
 	});
 
+	var forceTouchSupport = function(){
+		$.support.touch = true;
+		$.testHelper.reloadLib(libName);
+	};
+
 	test( "long press fires tap hold after 750 ms", function(){
 		var taphold = false;
-		$.support.touch = true;
 
-		$.testHelper.reloadLib(libName);
+		forceTouchSupport();
 
 		$($.event.special.tap).bind("taphold", function(){
 			taphold = true;
@@ -120,29 +128,135 @@
 		setTimeout(function(){
 			ok(taphold);
 			start();
-	  }, 751);
+		}, 751);
 	});
+
+	//NOTE used to simulate movement when checked
+	//TODO find a better way ...
+	var mockAbs = function(value){
+		Math.abs = function(){
+			return value;
+		};
+	};
 
 	test( "touchmove pushes back taphold", function(){
 		var taphold = false;
-		$.support.touch = true;
 
-		$.testHelper.reloadLib(libName);
+		forceTouchSupport();
+		mockAbs(100);
 
+		//NOTE record taphold event
 		$($.event.special.tap).bind("taphold", function(){
 			taphold = true;
 		});
 
+		//NOTE start the touch events
 		$($.event.special.tap).trigger("touchstart");
 
+		//NOTE fire touchmove to push back taphold
 		setTimeout(function(){
 			$($.event.special.tap).trigger("touchmove");
 		}, 100);
 
+		//NOTE verify that the taphold hasn't been fired
+		//		 with the normal timing
 		stop();
+		setTimeout(function(){
+			ok(!taphold);
+
+			//NOTE start and stop required to fire other events?
+			start();
+			stop();
+		}, 751);
+
+		//NOTE verify that the taphold hasn't been fired
+		//		 with the normal timing
 		setTimeout(function(){
 			ok(taphold);
 			start();
-	  }, 851);
+		}, 851);
+	});
+
+	test( "tap event fired without movement", function(){
+		var tap = false;
+
+		forceTouchSupport();
+
+		//NOTE record the tap event
+		$($.event.special.tap).bind("tap", function(){
+			start();
+			tap = true;
+		});
+
+		$($.event.special.tap).trigger("touchstart");
+		$($.event.special.tap).trigger("touchend");
+		stop();
+
+		ok(tap);
+	});
+
+	test( "tap event not fired when there is movement", function(){
+		var tap = false;
+		forceTouchSupport();
+
+		//NOTE record tap event
+		$($.event.special.tap).bind("tap", function(){
+			tap = true;
+		});
+
+		//NOTE make sure movement is recorded
+		mockAbs(100);
+
+		//NOTE start and move right away
+		$($.event.special.tap).trigger("touchstart");
+		$($.event.special.tap).trigger("touchmove");
+
+		//NOTE end touch sequence after 20 ms
+		setTimeout(function(){
+			$($.event.special.tap).trigger("touchend");
+		}, 20);
+
+		ok(!tap);
+	});
+
+	var swipeTimedTest = function(timeout, expected){
+		var swipe = false;
+
+		forceTouchSupport();
+
+		$($.event.special.swipe).bind('swipe', function(){
+			swipe = true;
+			start();
+		});
+
+		//NOTE bypass the trigger source check
+		$.Event.prototype.originalEvent = {
+			touches: false
+		};
+
+		$($.event.special.swipe).trigger("touchstart");
+
+		//NOTE make sure the coordinates are calculated within range
+		//     to be registered as a swipe
+		mockAbs(35);
+
+		setTimeout(function(){
+			$($.event.special.swipe).trigger("touchmove");
+			$($.event.special.swipe).trigger("touchend");
+		}, timeout);
+
+		stop();
+		setTimeout(function(){
+			same(swipe, expected);
+			start();
+		}, timeout + 10);
+	};
+
+	test( "swipe fired when coords change in less than a second", function(){
+		swipeTimedTest(10, true);
+	});
+
+	test( "swipe not fired when coords change takes more than a second", function(){
+		swipeTimedTest(1000, false);
 	});
 })(jQuery);
