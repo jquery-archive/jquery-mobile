970f46c5e08ba583452d5ac310c7b1a43383427e
John Bender <john.m.bender@gmail.com>
2010-12-08 00:12:05 -0800

    header and footer links will now remain unstyled, Fixes #524

---
+++ tests/unit/page/page_core.js
@@ -0,0 +1,24 @@
+/*
+ * mobile page unit tests
+ */
+
+(function( $ ) {
+	module('jquery.mobile.page.js');
+
+
+	test( "nested header anchors aren't altered", function(){
+		ok(!$('.ui-header > div > a').hasClass('ui-btn'));
+	});
+
+	test( "nested footer anchors aren't altered", function(){
+		ok(!$('.ui-footer > div > a').hasClass('ui-btn'));
+	});
+
+	test( "unnested footer anchors are styled", function(){
+		ok($('.ui-footer > a').hasClass('ui-btn'));
+	});
+
+	test( "unnested footer anchors are styled", function(){
+		ok($('.ui-footer > a').hasClass('ui-btn'));
+	});
+})(jQuery);
