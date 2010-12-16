458a56839b686f372765f85961c10bbcf5d38c7a
jzaefferer <joern.zaefferer@gmail.com>
2010-12-16 10:34:23 +0100

    Updated unit tests: Drop document-ready block, use #qunit-fixture instead of #main and drop extra styles now provided by QUnit.

--- tests/unit/navigation/navigation_core.js
+++ tests/unit/navigation/navigation_core.js
@@ -1,48 +1,46 @@
 /*
  * mobile navigation unit tests
  */
-(function( $ ) {
-	var perspective = "ui-mobile-viewport-perspective",
-			transitioning = "ui-mobile-viewport-transitioning",
-	    animationCompleteFn = $.fn.animationComplete,
+var perspective = "ui-mobile-viewport-perspective",
+		transitioning = "ui-mobile-viewport-transitioning",
+    animationCompleteFn = $.fn.animationComplete,
 
-			removeClasses = function(){
-				$("body").removeClass([perspective, transitioning].join(" "));
-			};
+		removeClasses = function(){
+			$("body").removeClass([perspective, transitioning].join(" "));
+		};
 
-	module('jquery.mobile.navigation.js', {
-		teardown: function(){
-			// unmock animation complete
-			$.fn.animationComplete = animationCompleteFn;
+module('jquery.mobile.navigation.js', {
+	teardown: function(){
+		// unmock animation complete
+		$.fn.animationComplete = animationCompleteFn;
 
-			removeClasses();
-		}});
+		removeClasses();
+	}});
 
-	test( "changePage applys perspective class to mobile viewport for flip", function(){
-		//stub to prevent class removal
-		$.fn.animationComplete = function(){};
+test( "changePage applys perspective class to mobile viewport for flip", function(){
+	//stub to prevent class removal
+	$.fn.animationComplete = function(){};
 
-		$("#foo > a").click();
+	$("#foo > a").click();
 
-		ok($("body").hasClass(perspective), "has perspective class");
-	});
+	ok($("body").hasClass(perspective), "has perspective class");
+});
 
 
-	test( "changePage applys does not apply perspective class to mobile viewport for transitions other than flip", function(){
-		//stub to prevent class removal
-		$.fn.animationComplete = function(){};
+test( "changePage applys does not apply perspective class to mobile viewport for transitions other than flip", function(){
+	//stub to prevent class removal
+	$.fn.animationComplete = function(){};
 
-		$("#bar > a").click();
+	$("#bar > a").click();
 
-		ok(!$("body").hasClass(perspective), "doesn't have perspective class");
-	});
+	ok(!$("body").hasClass(perspective), "doesn't have perspective class");
+});
 
-	test( "changePage applys transition class to mobile viewport for default transition", function(){
-		//stub to prevent class removal
-		$.fn.animationComplete = function(){};
+test( "changePage applys transition class to mobile viewport for default transition", function(){
+	//stub to prevent class removal
+	$.fn.animationComplete = function(){};
 
-		$("#baz > a").click();
+	$("#baz > a").click();
 
-		ok($("body").hasClass(transitioning), "has transitioning class");
-	});
-})(jQuery);
+	ok($("body").hasClass(transitioning), "has transitioning class");
+});
