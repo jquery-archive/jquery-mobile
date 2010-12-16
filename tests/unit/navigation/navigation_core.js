1497e605d32582120da5669bd0d4ade0c66457f5
John Bender <john.m.bender@gmail.com>
2010-12-15 23:33:06 -0800

    test fixes and addition for proper back transitions

--- tests/unit/navigation/navigation_core.js
+++ tests/unit/navigation/navigation_core.js
@@ -1,48 +1,47 @@
 /*
  * mobile navigation unit tests
  */
+// TODO move tests to navigation_transitions.js
 var perspective = "ui-mobile-viewport-perspective",
 		transitioning = "ui-mobile-viewport-transitioning",
-    animationCompleteFn = $.fn.animationComplete,
+	  animationCompleteFn = $.fn.animationComplete,
 
-		removeClasses = function(){
+		removeBodyClasses = function(){
 			$("body").removeClass([perspective, transitioning].join(" "));
+		},
+
+		removePageTransClasses = function(){
+			$("[data-role='page']").removeClass("in out fade slide flip reverse");
 		};
 
 module('jquery.mobile.navigation.js', {
+	setup: function(){
+		//stub to prevent class removal
+		$.fn.animationComplete = function(){};
+	},
+
 	teardown: function(){
 		// unmock animation complete
 		$.fn.animationComplete = animationCompleteFn;
-		removeClasses();
-		// Fix test layout issues from page styling needed for webkit transitions
-		$("body").attr('style', 'margin: 8px;');
-	}
+
+		// required cleanup from animation complete mocking
+		removeBodyClasses();
+  }
 });
 
-
 test( "changePage applys perspective class to mobile viewport for flip", function(){
-	//stub to prevent class removal
-	$.fn.animationComplete = function(){};
-
 	$("#foo > a").click();
 
 	ok($("body").hasClass(perspective), "has perspective class");
 });
 
-
 test( "changePage does not apply perspective class to mobile viewport for transitions other than flip", function(){
-	//stub to prevent class removal
-	$.fn.animationComplete = function(){};
-
 	$("#bar > a").click();
 
 	ok(!$("body").hasClass(perspective), "doesn't have perspective class");
 });
 
 test( "changePage applys transition class to mobile viewport for default transition", function(){
-	//stub to prevent class removal
-	$.fn.animationComplete = function(){};
-
 	$("#baz > a").click();
 
 	ok($("body").hasClass(transitioning), "has transitioning class");
@@ -61,10 +60,38 @@ test( "explicit transition preferred for page navigation reversal (ie back)", fu
 	}, 600);
 
 	setTimeout(function(){
+		//guarantee that we check only the newest changes
+		removePageTransClasses();
+
 		$("#fade-trans > a").click();
 
-		ok($("#fade-trans").hasClass("in fade reverse"),
-			 "has fade class and back reverse");
+		ok($("#flip-trans").hasClass("fade"),
+			 "has fade class");
+
+		start();
+	}, 900);
+});
+
+test( "previous transition used when not set and going back through url stack", function(){
+
+	stop();
+	setTimeout(function(){
+		$("#no-trans > a").click();
+	}, 300);
+
+	setTimeout(function(){
+		$("#pop-trans > a").click();
+	}, 600);
+
+
+	setTimeout(function(){
+		//guarantee that we check only the newest changes
+		removePageTransClasses();
+
+		$("#no-trans > a").click();
+
+		ok($("#pop-trans").hasClass("pop"),
+			 "has slide class from previous trans");
 
 		start();
 	}, 900);
