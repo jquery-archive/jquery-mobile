8cd15f22f3aa42643564fa44f99e49f6e72a1900
Jesse Streb <jstreb@brightcove.com>
2010-12-22 14:10:31 -0500

    Added client side integration tests in order to avoid regressionsin the future.  Currently, I have just added some tests for the list views and there are clearly some more permutations to try but wanted to get this in order to get feedback from the community before building more tests.  The two reasons I chose to do this client side rather then in selenium is that there is less friction since there is no new technology dependency and the tests can be run on the various devices.

---
+++ submit.php
@@ -0,0 +1,5 @@
+<?php
+
+	 header( 'Location: http://localhost:8888/test-error.html' )
+
+?>
---
+++ tests/integration/helpers/spec_helpers.js
@@ -0,0 +1,49 @@
+/*function basicListViewHTML() {
+	var html = '<ul data-role="listview" data-theme="g">\
+								<li><a href="acura.html">Acura</a></li>\
+								<li><a href="audi.html">Audi</a></li>\
+								<li><a href="bmw.html">BMW</a></li>\
+							</ul>';
+							
+	return html;			
+}*/
+var listViewHelper = {};
+
+(function ($, lvh, undefined ) {
+	
+	var current_page
+		, pageShowFired = false;
+	
+
+ 	lvh.resetForPage = function(new_page) {
+		//remove any old event listeners from the current page if defined
+		if(current_page)
+			current_page.die('pageshow', lvh.pageshowHandler);
+			
+		pageShowFired = false;
+		current_page = new_page;
+		current_page.live('pageshow', lvh.pageshowHandler);
+	}
+	
+	lvh.currentPage = function() {
+		return current_page;
+	}
+	
+	lvh.transitionComplete = function() {
+		return pageShowFired;
+	}
+	
+	lvh.pageshowHandler = function() {
+		pageShowFired = true;
+	}
+	
+	lvh.showResultsWhenComplete = function() {
+		if($('.jasmine_reporter .running').length == 0) {
+			$('.ui-page-active').css('display', 'none');
+		} else {
+			setTimeout(lvh.showResultsWhenComplete, 500);
+		}
+	}
+	
+	setTimeout(lvh.showResultsWhenComplete, 500);
+})(jQuery, listViewHelper)
