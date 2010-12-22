4dcb65fc7ace75817e0d24859a474716c1efb510
Louy Alakkad <louy08@gmail.com>
2010-12-22 07:53:07 -0800

    fixing spaces

--- js/jquery.mobile.forms.checkboxradio.js
+++ js/jquery.mobile.forms.checkboxradio.js
@@ -18,11 +18,11 @@ $.widget( "mobile.checkboxradio", $.mobile.widget, {
 
 		if ( inputtype != "checkbox" && inputtype != "radio" ) { return; }
 
-                // If there's no selected theme...
+		// If there's no selected theme...
 		if( !this.options.theme ) {
 			this.options.theme = this.element.data( "theme" );
 		}
-		
+
 		label
 			.buttonMarkup({
 				theme: this.options.theme,
