8a70cdb3de80de3dea507007840be766142b32d0
Kin Blas <jblas@adobe.com>
2011-01-11 09:34:50 -0800

    Initial patch from pull 672 had incorrect check for backbtn data value. Changed it from $this.data("backbtn") === false to $this.data("backbtn") !== false to match the original logic that was in place prior to the patch.
    Original patch missed a nobackbtn reference in jquery.mobile.forms.select.js.

--- js/jquery.mobile.page.js
+++ js/jquery.mobile.page.js
@@ -76,7 +76,7 @@ $.widget( "mobile.page", $.mobile.widget, {
 				// auto-add back btn on pages beyond first view
 				if ( o.addBackBtn && role === "header" &&
 						($.mobile.urlStack.length > 1 || $(".ui-page").length > 1) &&
-						!leftbtn && $this.data( "backBtn" ) === false ) {
+						!leftbtn && $this.data( "backbtn" ) !== false ) {
 
 					$( "<a href='#' class='ui-btn-left' data-icon='arrow-l'>"+ o.backBtnText +"</a>" )
 						.click(function() {
