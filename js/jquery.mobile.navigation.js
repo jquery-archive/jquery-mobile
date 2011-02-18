b6b7aebe147395ea7f9fdc12f042aab81a8c66bd
scottjehl <scott@scottjehl.com>
2011-02-18 16:46:28 -0500

    null hrefs should just preventDefault and return, rather than returning false. Fixes #1118

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -659,7 +659,8 @@
 
 		if( url === "#" ){
 			//for links created purely for interaction - ignore
-			return false;
+			event.preventDefault();
+			return;
 		}
 
 		$activeClickedLink = $this.closest( ".ui-btn" ).addClass( $.mobile.activeBtnClass );
