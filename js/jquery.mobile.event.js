b0a667ae44af722c4f5e8433e2a1e42d8441cb41
Kin Blas <jblas@adobe.com>
2011-03-11 16:12:58 -0800

    Fixed typo in my last checkin for converting tap to virtual mouse events. It was preventing tap from actually firing so things like the cancel button link in a search textfield  was being followed.

--- js/jquery.mobile.event.js
+++ js/jquery.mobile.event.js
@@ -90,7 +90,7 @@ $.event.special.tap = {
 					/* ONLY trigger a 'tap' event if the start target is
 					 * the same as the stop target.
 					 */
-					if ( !origTarget == event.target ) {
+					if ( origTarget == event.target ) {
 						triggerCustomEvent( thisObject, "tap", event );
 					}
 				}
