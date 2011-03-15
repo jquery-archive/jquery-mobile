42c1271963281a68178f2f97524bb75669703393
John Resig <jeresig@gmail.com>
2011-03-15 15:08:02 -0400

    Adding in a new :jqdata() selector for filtering data- attributes within the proper jQuery Mobile namespace.

--- js/jquery.mobile.core.js
+++ js/jquery.mobile.core.js
@@ -236,6 +236,17 @@
 		}
 	});
 
+	// Monkey-patching Sizzle to filter the :jqdata selector
+	var oldFind = jQuery.find;
+
+	jQuery.find = function( selector, context, ret ) {
+		selector = selector.replace(/:jqdata\((.*)\)/g, "[data-" + (jQuery.mobile.ns || "") + "$1]");
+
+		return oldFind.call( this, selector, context, ret );
+	};
+
+	jQuery.extend( jQuery.find, oldFind );
+
 	//dom-ready inits
 	$($.mobile.initializePage);
 
