6138a57e67dd0de662290f4b98518eb6b0806952
John Resig <jeresig@gmail.com>
2011-03-15 15:05:22 -0400

    Revert "Adding in a new :jqdata() selector for filtering data- attributes within the proper jQuery Mobile namespace."
    This reverts commit da24d2779637a6ee01542b1e34b764a88023d1dd.

--- js/jquery.mobile.core.js
+++ js/jquery.mobile.core.js
@@ -109,15 +109,4 @@
 			}, 150 );
 		}
 	});
-
-	// Monkey-patching Sizzle to filter the :jqdata selector
-	var oldFind = jQuery.find;
-
-	jQuery.find = function( selector, context, ret ) {
-		selector = selector.replace(/:jqdata\((.*)\)/g, "[data-" + (jQuery.mobile.ns || "") + "$1]");
-
-		return oldFind.call( this, selector, context, ret );
-	};
-
-	jQuery.extend( jQuery.find, oldFind );
 })( jQuery, this );
