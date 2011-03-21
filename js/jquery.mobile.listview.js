dd71c6f0b413817c911f5d58705e04c8c607f3b1
Felipe Brahm <felipebrahm@gmail.com>
2011-03-21 16:00:52 -0700

    Bug fix: only apply thumb style if img tag is on the first level. This fixes the problem when you want multiple images on each li element, like a 5 star rating for each element for example. Now to add multiple images you just have to put them in a div.

--- js/jquery.mobile.listview.js
+++ js/jquery.mobile.listview.js
@@ -128,7 +128,7 @@ $.widget( "mobile.listview", $.mobile.widget, {
 
 		item.find( "p, dl" ).addClass( "ui-li-desc" );
 
-		$list.find( "li" ).find( "img:eq(0)" ).addClass( "ui-li-thumb" ).each(function() {
+		$list.find( "li" ).children( "img:eq(0)" ).addClass( "ui-li-thumb" ).each(function() {
 			$( this ).closest( "li" )
 				.addClass( $(this).is( ".ui-li-icon" ) ? "ui-li-has-icon" : "ui-li-has-thumb" );
 		});
