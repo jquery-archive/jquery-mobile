aa58b2a7e676c27043e3189f10a38f46e589bbfc
Kin Blas <jblas@adobe.com>
2011-03-25 18:00:58 -0700

    Added back blocking based on ancestry. Looks like we need to block on both proximity and ancestry to work around touch and mouse target differences.

--- js/jquery.mobile.vmouse.js
+++ js/jquery.mobile.vmouse.js
@@ -280,7 +280,7 @@ function handleTouchEnd(event)
 			// touch. This means we need to rely on coordinates for blocking
 			// any click that is generated.
 			var t = getNativeEvent(event).changedTouches[0];
-			clickBlockList.push({ x: t.clientX, y: t.clientY });
+			clickBlockList.push({ touchID: lastTouchID, x: t.clientX, y: t.clientY });
 
 			// Prevent any mouse events that follow from triggering
 			// virtual event notifications.
@@ -395,13 +395,48 @@ if (eventCaptureSupported){
 			var x = e.clientX,
 				y = e.clientY,
 				threshold = $.vmouse.clickDistanceThreshold;
-			for (var i = 0; i < cnt; i++) {
-				var o = clickBlockList[i];
-				if (Math.abs(o.x - x) < threshold && Math.abs(o.y - y) < threshold){
-					e.preventDefault();
-					e.stopPropagation();
-					return;
+
+			// The idea here is to run through the clickBlockList to see if
+			// the current click event is in the proximity of one of our
+			// vclick events that had preventDefault() called on it. If we find
+			// one, then we block the click.
+			//
+			// Why do we have to rely on proximity?
+			//
+			// Because the target of the touch event that triggered the vclick
+			// can be different from the target of the click event synthesized
+			// by the browser. The target of a mouse/click event that is syntehsized
+			// from a touch event seems to be implementation specific. For example,
+			// some browsers will fire mouse/click events for a link that is near
+			// a touch event, even though the target of the touchstart/touchend event
+			// says the user touched outside the link. Also, it seems that with most
+			// browsers, the target of the mouse/click event is not calculated until the
+			// time it is dispatched, so if you replace an element that you touched
+			// with another element, the target of the mouse/click will be the new
+			// element underneath that point.
+			//
+			// Aside from proximity, we also check to see if the target and any
+			// of its ancestors were the ones that blocked a click. This is necessary
+			// because of the strange mouse/click target calculation done in the
+			// Android 2.1 browser, where if you click on an element, and there is a
+			// mouse/click handler on one of its ancestors, the target will be the
+			// innermost child of the touched element, even if that child is no where
+			// near the point of touch.
+			
+			var ele = target;
+			while (ele) {
+				for (var i = 0; i < cnt; i++) {
+					var o = clickBlockList[i],
+						touchID = 0;
+					if ((ele === target && Math.abs(o.x - x) < threshold && Math.abs(o.y - y) < threshold) || $(ele).data(touchTargetPropertyName) === o.touchID){
+						// XXX: We may want to consider removing matches from the block list
+						//      instead of waiting for the reset timer to fire.
+						e.preventDefault();
+						e.stopPropagation();
+						return;
+					}
 				}
+				ele = ele.parentNode;
 			}
 		}
 	}, true);
