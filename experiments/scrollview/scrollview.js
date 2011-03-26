2add396bb28e04885bbb2490bb2b51271a10149e
scottjehl <scott@scottjehl.com>
2011-03-26 10:14:48 -0400

    moved some selectors over to :jqmData()

--- experiments/scrollview/scrollview.js
+++ experiments/scrollview/scrollview.js
@@ -10,7 +10,7 @@ function ResizePageContentHeight(page)
 	$content.height(wh - (hh + fh) - (pt + pb));
 }
 
-$("[data-"+ $.mobile.ns +"role=page]").live("pageshow", function(event) {
+$(":jqmData(role='page')").live("pageshow", function(event) {
 	var $page = $(this);
 
 	// For the demos that use this script, we want the content area of each
@@ -22,7 +22,7 @@ $("[data-"+ $.mobile.ns +"role=page]").live("pageshow", function(event) {
 	// into the jqm page processing code when scrollview support is "official"
 	// instead of "experimental".
 
-	$page.find("[data-"+ $.mobile.ns +"scroll]:not(.ui-scrollview-clip)").each(function(){
+	$page.find(":jqmData(scroll):not(.ui-scrollview-clip)").each(function(){
 		var $this = $(this);
 		// XXX: Remove this check for ui-scrolllistview once we've
 		//      integrated list divider support into the main scrollview class.
