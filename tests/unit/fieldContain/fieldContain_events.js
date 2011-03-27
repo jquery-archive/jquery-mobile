d66cd439f0d0bc7f82e3f6a1ac96501b4665d137
scottjehl <scott@scottjehl.com>
2011-03-27 12:46:51 -0400

    removed a console log

--- tests/unit/fieldContain/fieldContain_events.js
+++ tests/unit/fieldContain/fieldContain_events.js
@@ -4,9 +4,7 @@
 (function($){
 	module('jquery.mobile.fieldContain.js');
 
-	test( "Field container contains appropriate css styles", function(){
-		console.log($('#test-fieldcontain'))
-	
+	test( "Field container contains appropriate css styles", function(){	
 		ok($('#test-fieldcontain').hasClass('ui-field-contain ui-body ui-br'), 'A fieldcontain element must contain styles "ui-field-contain ui-body ui-br"');
 	});
 })(jQuery);
