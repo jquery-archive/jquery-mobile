c3854fe8ca83708d37f175cd2a9eb0d300cfeddb
John Bender <john.m.bender@gmail.com>
2011-03-26 17:27:33 -0700

    added simple helper to rerun qunit tests

--- tests/jquery.testHelper.js
+++ tests/jquery.testHelper.js
@@ -34,6 +34,17 @@
 			$("body").append(lib);
 		},
 
+		rerunQunit: function(){
+			var self = this;
+			QUnit.init();
+			$("script:not([src*='.\/'])").each(function(i, elem){
+				var src = elem.src.split("/");
+				console.log(elem.src);
+				self.reloadLib(src[src.length - 1]);
+			});
+			QUnit.start();
+		},
+
 		alterExtend: function(extraExtension){
 			var extendFn = $.extend;
 
