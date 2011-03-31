3cf524136abefc514da34a16dc0d8d3144a117c9
Brian Antonelli <brian.antonelli@gmail.com>
2011-03-31 09:02:11 -0700

    data not being passed during beforehide and beforeshow events

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -355,9 +355,9 @@
 				//set as data for returning to that spot
 				from.data( "lastScroll", currScroll);
 				//trigger before show/hide events
-				from.data( "page" )._trigger( "beforehide", { nextPage: to } );
+				from.data( "page" )._trigger( "beforehide", null, { reverse: typeof reverse !== "undefined", nextPage: to } );
 			}	
-			to.data( "page" )._trigger( "beforeshow", { prevPage: from || $("") } );
+			to.data( "page" )._trigger( "beforeshow", null, { reverse: typeof reverse !== "undefined", prevPage: from || $("") } );
 
 			function loadComplete(){
 
