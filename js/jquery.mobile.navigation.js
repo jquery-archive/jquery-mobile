8cd0d2c6790f55718eb95cf428a3cd6f64c58d23
Kin Blas <jblas@adobe.com>
2011-04-01 13:37:18 -0700

    I'm not sure how this happened, but the dataType fix for Issue 991 disappeared between  commits 61005944 and 7503f62a.
    Adding it back in to fix Issue 1349 - File urls still not working in alpha 4 as shipped.

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -551,6 +551,7 @@
 				url: fileUrl,
 				type: type,
 				data: data,
+				dataType: "html",
 				success: function( html ) {
 					//pre-parse html to check for a data-url,
 					//use it as the new fileUrl, base path, etc
