b0e3d5fbf106abb61ace6afd52ae72d0691a1ab4
John Bender <john.m.bender@gmail.com>
2011-04-19 23:26:31 -0700

    select test cleanup, added test to check that the dialog hash key is removed when a large select menu item is clicked Fixes #1454

--- tests/unit/select/select_core.js
+++ tests/unit/select/select_core.js
@@ -5,7 +5,7 @@
 (function($){
 	var libName = "jquery.mobile.forms.select.js";
 
-	$(document).bind('mobileinit', function(){
+	$(document).bind("mobileinit", function(){
 		$.mobile.selectmenu.prototype.options.nativeMenu = false;
 	});
 
@@ -13,16 +13,35 @@
 		teardown: function(){ location.hash = ""; }
 	});
 
-
 	asyncTest( "custom select menu always renders screen from the left", function(){
 		expect( 1 );
 		var select = $("ul#select-offscreen-menu");
 
-		$('#select-offscreen-container a').trigger("click");
+		$("#select-offscreen-container a").trigger("click");
 
 		setTimeout(function(){
 			ok(select.offset().left >= 30);
 			start();
 		}, 1000);
+	});
+
+	asyncTest( "selecting an item from a dialog sized custom select menu leaves no dialog hash key", function(){
+		var dialogHashKey = "ui-state=dialog";
+
+		$.testHelper.sequence([
+			function(){
+				$("#select-choice-many-container-hash-check a").trigger("click");
+			},
+
+			function(){
+				ok(location.hash.indexOf(dialogHashKey) > -1);
+				$(".ui-page-active li").trigger("click");
+			},
+
+			function(){
+				ok(location.hash.indexOf(dialogHashKey) == -1);
+				start();
+			}
+		], 500);
 	});
 })(jQuery);
