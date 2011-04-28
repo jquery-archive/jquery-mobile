9b885874266f45910564f3f713e2d80b28ff990a
John Bender <john.m.bender@gmail.com>
2011-04-27 23:07:58 -0700

    completed doc relative tests

--- tests/unit/navigation/navigation_paths.js
+++ tests/unit/navigation/navigation_paths.js
@@ -2,20 +2,18 @@
  * mobile navigation path unit tests
  */
 (function($){
-	module("jquery.mobile.navigation.js");
-
-	var testPageLoad = function(testPageSelector, expectedTextValue){
+	var testPageLoad = function(testPageAnchorSelector, expectedTextValue){
 		expect( 1 );
 
 		$.testHelper.sequence([
 			// open our test page
 			function(){
-				$.testHelper.openPage(testPageSelector);
+				$.testHelper.openPage("#pathing-tests");
 			},
 
 			// navigate to the linked page
 			function(){
-				$(".ui-page-active a").click();
+				$( ".ui-page-active a" + testPageAnchorSelector ).click();
 			},
 
 			// verify that the page has changed and the expected text value is present
@@ -26,11 +24,29 @@
 		], 800);
 	};
 
-	asyncTest( "document relative file reference no nesting", function(){
+	module("document relative paths");
+
+ 	asyncTest( "file reference no nesting", function(){
 		testPageLoad("#doc-rel-file-ref-no-nest", "doc rel file ref no nest");
 	});
 
-	asyncTest( "document relative file reference with nesting", function(){
+	asyncTest( "file reference with nesting", function(){
 		testPageLoad("#doc-rel-file-ref-nested", "doc rel file ref nested");
+	});
+
+	asyncTest( "file reference with double nesting", function(){
+		testPageLoad("#doc-rel-file-ref-double-nested", "doc rel file ref double nested");
+	});
+
+	asyncTest( "dir refrence with nesting", function(){
+		testPageLoad("#doc-rel-dir-ref-nested-dir", "doc rel dir ref nested dir");
+	});
+
+	asyncTest( "file refrence with parent dir", function(){
+		testPageLoad("#doc-rel-file-ref-parent", "doc rel dir ref parent");
+	});
+
+	asyncTest( "dir refrence with parent dir", function(){
+		testPageLoad("#doc-rel-dir-ref-parent", "doc rel dir ref parent");
 	});
 })(jQuery);
