3b3b34b9c154b84d5f8c30a3c9eb430bb41904d3
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2011-05-20 08:34:13 +1200

    Better fix for Issue #1551 (Local images with full URLs broken in Firefox 3.6)
    Change replacement URL to use path.clean( path.origin ) instead of path.get( fileUrl ) so that generated URLs are not broken (basing them off fileUrl breaks them if the page being viewed is in a different directory to path.origin).

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -604,7 +604,7 @@
 
 					//rewrite src and href attrs to use a base url
 					if( !$.support.dynamicBaseTag ) {
-						var newPath = path.get( fileUrl );
+						var newPath = path.clean( path.origin );
 						to.find( "[src], link[href], a[rel='external'], :jqmData(ajax='false'), a[target]" ).each( function() {
 							var attrs = [ "href", "src", "action" ],
 								thisAttr = undefined,
