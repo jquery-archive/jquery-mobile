2c81dcef82c4b9902effe2466bdf993f0fee6211
Ghislain Seguin <ghislain@jivesoftware.com>
2011-05-24 17:50:48 -0700

    Issue #1617 Handle case where page has an id (url), we want to use that as a key in the listCountPerPage dictionary

--- js/jquery.mobile.listview.js
+++ js/jquery.mobile.listview.js
@@ -197,24 +197,25 @@ $.widget( "mobile.listview", $.mobile.widget, {
 	_createSubPages: function() {
 		var parentList = this.element,
 			parentPage = parentList.closest( ".ui-page" ),
-			parentId = parentPage.jqmData( "url" ),
+			parentUrl = parentPage.jqmData( "url" ),
+			parentId  = parentUrl || parentPage[0][$.expando],
 			o = this.options,
 			dns = "data-" + $.mobile.ns,
 			self = this,
 			persistentFooterID = parentPage.find( ":jqmData(role='footer')" ).jqmData( "id" );
 
-		if (typeof(listCountPerPage[parentPage[0][$.expando]]) === 'undefined') {
-            listCountPerPage[parentPage[0][$.expando]] = -1;
+		if (typeof(listCountPerPage[parentId]) === 'undefined') {
+            listCountPerPage[parentId] = -1;
         }
 
 		$( parentList.find( "li>ul, li>ol" ).toArray().reverse() ).each(function( i ) {
 			var list = $( this ),
-				listId = list.attr("id") || ++listCountPerPage[parentPage[0][$.expando]],
+				listId = list.attr("id") || ++listCountPerPage[parentId],
 				parent = list.parent(),
 				nodeEls = $( list.prevAll().toArray().reverse() ),
 				nodeEls = nodeEls.length ? nodeEls : $( "<span>" + $.trim(parent.contents()[ 0 ].nodeValue) + "</span>" ),
 				title = nodeEls.first().text(),//url limits to first 30 chars of text
-				id = (parentId ? (parentId + "&") : "") + $.mobile.subPageUrlKey + "=" + listId + "-" + i,
+				id = (parentUrl ? (parentUrl + "&") : "") + $.mobile.subPageUrlKey + "=" + listId + "-" + i,
 				theme = list.jqmData( "theme" ) || o.theme,
 				countTheme = list.jqmData( "counttheme" ) || parentList.jqmData( "counttheme" ) || o.countTheme,
 				newPage = list.detach()
