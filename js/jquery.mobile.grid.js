b780c8eb6d29d3182228461e043e704199b318dc
Eddie Monge <EMAIL>
2011-06-09 00:19:49 -0700

    grid rework

--- js/jquery.mobile.grid.js
+++ js/jquery.mobile.grid.js
@@ -8,43 +8,32 @@
 $.fn.grid = function(options){
 	return this.each(function(){
 		var o = $.extend({
-			grid: null
-		},options);
-	
-			
-		var $kids = $(this).children(),
-			gridCols = {solo:1, a:2, b:3, c:4, d:5},
-			grid = o.grid,
-			iterator;
-			
-			if( !grid ){
-				if( $kids.length <= 5 ){
-					for(var letter in gridCols){
-						if(gridCols[letter] == $kids.length){ grid = letter; }
-					}
-				}
-				else{
-					grid = 'a';
-				}
-			}
-			iterator = gridCols[grid];
-			
-		$(this).addClass('ui-grid-' + grid);
-	
+				grid: null 
+			}, options),
+			$this = $(this),
+			$kids = $this.children(),
+			kidlen = $kids.length,
+			gridCols = ['solo', 'a', 'b', 'c', 'd'],
+			grid = o.grid || ( kidlen < 6 ? gridCols[ kidlen - 1 ] : 'a' ),
+			iterator = kidlen < 6 ? kidlen : 2 ;
+
+		$this.addClass('ui-grid-' + grid);
+
+		// iterator 0 or 1
 		$kids.filter(':nth-child(' + iterator + 'n+1)').addClass('ui-block-a');
-		if(iterator > 1){	
+		if ( iterator > 1 ) {
 			$kids.filter(':nth-child(' + iterator + 'n+2)').addClass('ui-block-b');
 		}	
-		if(iterator > 2){	
-			$kids.filter(':nth-child(3n+3)').addClass('ui-block-c');
+		if ( iterator > 2 ) {
+			$kids.filter(':nth-child(' + iterator + 'n+3)').addClass('ui-block-c');
 		}	
-		if(iterator > 3){	
-			$kids.filter(':nth-child(4n+4)').addClass('ui-block-d');
+		if ( iterator > 3 ) {
+			$kids.filter(':nth-child(' + iterator + 'n+4)').addClass('ui-block-d');
 		}	
-		if(iterator > 4){	
-			$kids.filter(':nth-child(5n+5)').addClass('ui-block-e');
+		if ( iterator > 4 ) {
+			$kids.filter(':nth-child(' + iterator + 'n+5)').addClass('ui-block-e');
 		}
-				
-	});	
+
+	});
 };
 })(jQuery);
