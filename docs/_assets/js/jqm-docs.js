1e5441fd76c207d8078a20ae3854177c13e6befb
scottjehl <scott@scottjehl.com>
2011-06-19 21:58:12 -0400

    reworked the docs to better accommodate wide screens using a simple responsive layout. Tweaks forthcoming!

--- docs/_assets/js/jqm-docs.js
+++ docs/_assets/js/jqm-docs.js
@@ -1,6 +1,12 @@
 //set up the theme switcher on the homepage
 $('div').live('pagecreate',function(event){
 	if( !$(this).is('.ui-dialog')){ 
+		var appendEl = $(this).find('.ui-footer');
+		
+		if( !appendEl.length ){
+			appendEl = $(this).find('.ui-content');
+		}
+		
 		$('<a href="#themeswitcher" data-'+ $.mobile.ns +'rel="dialog" data-'+ $.mobile.ns +'transition="pop">Switch theme</a>')
 			.buttonMarkup({
 				'icon':'gear',
@@ -8,11 +14,18 @@ $('div').live('pagecreate',function(event){
 				'shadow': false,
 				'theme': 'd'
 			})
-			.appendTo( $(this).find('.ui-content') )
+			.appendTo( appendEl )
 			.wrap('<div class="jqm-themeswitcher">')
 			.bind("vclick", function(){
 				$.themeswitcher();
 			});
 	}	
 	event.stopPropagation();
+});
+
+//collapse page navs after use
+$(function(){
+	$('body').delegate('.content-secondary .ui-collapsible-content', 'vclick',  function(){
+		$(this).trigger("collapse")
+	});
 });
