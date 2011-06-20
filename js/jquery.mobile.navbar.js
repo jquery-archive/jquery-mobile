41781802cf720eb8eb742b15c6183bde2a950ecd
Praveen <pravinkmrr@gmail.com>
2011-06-20 03:14:02 -0700

    Navbar: data-iconpos is ignored #1797 Issue fixed

--- js/jquery.mobile.navbar.js
+++ js/jquery.mobile.navbar.js
@@ -1,3 +1,4 @@
+/*
 * jQuery Mobile Framework : "navbar" plugin
 * Copyright (c) jQuery Project
 * Dual licensed under the MIT or GPL Version 2 licenses.
@@ -6,35 +7,40 @@
 (function($, undefined ) {
 $.widget( "mobile.navbar", $.mobile.widget, {
 	options: {
-		iconpos:"top",
+		iconpos: 'top',
 		grid: null
 	},
 	_create: function(){
-		var icon=this.element.find("a").attr("data-iconpos").length ? this.element.find("a").attr("data-iconpos"):this.options.iconpos;
 		var $navbar = this.element,
-			$navbtns = $navbar.find("a"),
-			iconpos = $navbtns.filter( ":jqmData(icon)").length ? icon : undefined;
-			
-		$navbar
+			$navbtns = $navbar.find("a")
+			i=0,
+			j=0;
+			$navbar
 			.addClass('ui-navbar')
 			.attr("role","navigation")
 			.find("ul")
-				.grid({grid: this.options.grid });		
-		
-		if( !iconpos){ 
-			$navbar.addClass("ui-navbar-noicons");
-		}
-		if(icon=="notext") {
-			iconpos="top"
-			$navbtns = $navbar.find("a").text("");
-		}		
-		$navbtns
+			.grid({grid: this.options.grid });	
+			$navbtns.each(function(){
+			var icon= $(this).attr("data-iconpos") ? $(this).attr("data-iconpos") : "top",
+			iconpos =$(this).filter( ":jqmData(icon)").length ? icon: undefined,
+			notext=$(this).filter(":jqmData(iconpos='notext')");
+			if( !iconpos ){ 
+				i++;
+			}
+			if(iconpos=="notext") {
+				iconpos="top";
+				notext.text("");
+			}
+			$(this)
 			.buttonMarkup({
 				corners:	false, 
 				shadow:		false, 
 				iconpos:	iconpos
 			});
-		
+			j++;
+		});
+		if(i==j)
+			$navbar.addClass("ui-navbar-noicons");
 		$navbar.delegate("a", "vclick",function(event){
 			$navbtns.not( ".ui-state-persist" ).removeClass( $.mobile.activeBtnClass );
 			$( this ).addClass( $.mobile.activeBtnClass );
