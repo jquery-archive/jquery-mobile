6b1343d9467bee95d65f741195c5961ebc9ce4d8
Ghislain Seguin <ghislain@gmail.com>
2011-06-24 20:49:16 -0700

    Added test to catch previous breakage caused by buttons having class 'ui-btn-corner-all'

--- tests/unit/controlgroup/controlgroup_core.js
+++ tests/unit/controlgroup/controlgroup_core.js
@@ -2,11 +2,9 @@
  * mobile checkboxradio unit tests
  */
 (function($){
-	module( 'vertical controlgroup', {
+	module( 'vertical controlgroup, no refresh' , {
 		setup: function() {
 			this.vcontrolgroup = $( "#vertical-controlgroup" );
-			this.vcontrolgroup.find( ".ui-btn" ).show();
-			this.vcontrolgroup.controlgroup("refresh");
 		}
 	});
 
@@ -15,12 +13,21 @@
 			middlebuttons = buttons.filter(function(index) { return index > 0 && index < (length-1)}),
 			length = buttons.length;
 
+		ok( !buttons.hasClass( "ui-btn-corner-all" ), "no button should have class 'ui-btn-corner-all'");
 		ok( buttons.first().hasClass( "ui-corner-top" ), "first button should have class 'ui-corner-top'" );
 		ok( !middlebuttons.hasClass( "ui-corner-top" ), "middle buttons should not have class 'ui-corner-top'" );
 		ok( !middlebuttons.hasClass( "ui-corner-bottom" ), "middle buttons should not have class 'ui-corner-bottom'" );
 		ok( buttons.last().hasClass( "ui-corner-bottom"), "last button should have class 'ui-corner-bottom'" );
 	});
 
+//	module( 'vertical controlgroup, refresh', {
+//		setup: function() {
+//			this.vcontrolgroup = $( "#vertical-controlgroup" );
+//			this.vcontrolgroup.find( ".ui-btn" ).show();
+//			this.vcontrolgroup.controlgroup("refresh");
+//		}
+//	});
+//
 //	test( "vertical controlgroup after first button was hidden", function() {
 //		//https://github.com/jquery/jquery-mobile/issues/1929
 //
@@ -37,7 +44,7 @@
 //		ok( !middlebuttons.hasClass( "ui-corner-bottom" ), "middle buttons should not have class 'ui-corner-bottom'" );
 //		ok( buttons.last().hasClass( "ui-corner-bottom"), "last visible button should have class 'ui-corner-bottom'" );
 //	});
-
+//
 //	test( "vertical controlgroup after last button was hidden", function() {
 //		//https://github.com/jquery/jquery-mobile/issues/1929
 //
@@ -55,11 +62,9 @@
 //		ok( buttons.last().hasClass( "ui-corner-bottom"), "last visible button should have class 'ui-corner-bottom'" );
 //	});
 
-	module( 'horizontal controlgroup', {
+	module( 'horizontal controlgroup, no refresh', {
 		setup: function() {
 			this.hcontrolgroup = $( "#horizontal-controlgroup" );
-			this.hcontrolgroup.find( ".ui-btn" ).show();
-			this.hcontrolgroup.controlgroup("refresh");
 		}
 	});
 
@@ -68,12 +73,21 @@
 			middlebuttons = buttons.filter(function(index) { return index > 0 && index < (length-1)}),
 			length = buttons.length;
 
+		ok( !buttons.hasClass( "ui-btn-corner-all" ), "no button should have class 'ui-btn-corner-all'");
 		ok( buttons.first().hasClass( "ui-corner-left" ), "first button should have class 'ui-corner-left'" );
 		ok( !middlebuttons.hasClass( "ui-corner-left" ), "middle buttons should not have class 'ui-corner-left'" );
 		ok( !middlebuttons.hasClass( "ui-corner-right" ), "middle buttons should not have class 'ui-corner-right'" );
 		ok( buttons.last().hasClass( "ui-corner-right"), "last button should have class 'ui-corner-right'" );
 	});
 
+//	module( 'horizontal controlgroup, refresh', {
+//		setup: function() {
+//			this.hcontrolgroup = $( "#horizontal-controlgroup" );
+//			this.hcontrolgroup.find( ".ui-btn" ).show();
+//			this.hcontrolgroup.controlgroup("refresh");
+//		}
+//	});
+//
 //	test( "horizontal controlgroup after first button was hidden", function() {
 //		//We hide the first button and refresh
 //		this.hcontrolgroup.find( ".ui-btn" ).first().hide();
@@ -88,7 +102,7 @@
 //		ok( !middlebuttons.hasClass( "ui-corner-right" ), "middle buttons should not have class 'ui-corner-right'" );
 //		ok( buttons.last().hasClass( "ui-corner-right"), "last visible button should have class 'ui-corner-right'" );
 //	});
-
+//
 //	test( "horizontal controlgroup after last button was hidden", function() {
 //		//We hide the last button and refresh
 //		this.hcontrolgroup.find( ".ui-btn" ).last().hide();
