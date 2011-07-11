8e5b0110c5d7bb016d3965f7fc0c710ad651572d
Eddie Monge <eddie@eddiemonge.com>
2011-07-11 00:23:32 -0400

    bring up to core styling guide

--- js/jquery.mobile.grid.js
+++ js/jquery.mobile.grid.js
@@ -10,28 +10,32 @@ $.fn.grid = function(options) {
 		var o = $.extend({
 				grid: null 
 			}, options),
-			$this = $(this),
+			$this = $( this ),
 			$kids = $this.children(),
 			kidlen = $kids.length,
-			gridCols = ['solo', 'a', 'b', 'c', 'd'],
-			grid = o.grid || ( kidlen < 6 ? gridCols[ kidlen - 1 ] : 'a' ),
+			gridCols = [ "solo", "a", "b", "c", "d" ],
+			grid = o.grid || ( kidlen < 6 ? gridCols[ kidlen - 1 ] : "a" ),
 			iterator = kidlen < 6 ? kidlen : 2 ;
 
-		$this.addClass('ui-grid-' + grid);
+		$this.addClass( "ui-grid-" + grid );
 
 		// iterator 0 or 1
-		$kids.filter(':nth-child(' + iterator + 'n+1)').addClass('ui-block-a');
+		$kids.filter( ":nth-child(" + iterator + "n+1)" ).addClass( "ui-block-a" );
+
 		if ( iterator > 1 ) {
-			$kids.filter(':nth-child(' + iterator + 'n+2)').addClass('ui-block-b');
+			$kids.filter( ":nth-child(" + iterator + "n+2)" ).addClass( "ui-block-b" );
 		}	
+
 		if ( iterator > 2 ) {
-			$kids.filter(':nth-child(' + iterator + 'n+3)').addClass('ui-block-c');
+			$kids.filter( ":nth-child(" + iterator + "n+3)" ).addClass( "ui-block-c" );
 		}	
+
 		if ( iterator > 3 ) {
-			$kids.filter(':nth-child(' + iterator + 'n+4)').addClass('ui-block-d');
+			$kids.filter( ":nth-child(" + iterator + "n+4)" ).addClass( "ui-block-d" );
 		}	
+
 		if ( iterator > 4 ) {
-			$kids.filter(':nth-child(' + iterator + 'n+5)').addClass('ui-block-e');
+			$kids.filter( ":nth-child(" + iterator + "n+5)" ).addClass( "ui-block-e" );
 		}
 
 	});
