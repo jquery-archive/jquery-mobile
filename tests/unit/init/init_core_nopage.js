a93cdbcea026144b4f52c13c02e6f853d621305c
scottjehl <scott@scottjehl.com>
2011-07-17 16:29:05 -0400

    changed up the unit test so that it didn't load the page in an iframe, which seemed to mess things up on the test server.

---
+++ tests/unit/init/init_core_nopage.js
@@ -0,0 +1,12 @@
+/*
+ * mobile init tests
+ */
+(function($){
+
+		
+		test( "page element is generated when not present in initial markup", function(){
+			ok( $( ".ui-page" ).length, 1 );
+		});
+
+
+})(jQuery);
