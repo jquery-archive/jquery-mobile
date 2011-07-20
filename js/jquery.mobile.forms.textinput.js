4d66aa6397cf0e1ab8d1ee208e40011bfc9f32bf
Scott Jehl <scott@scottjehl.com>
2011-07-20 15:29:08 -0700

    added input types url and tel to auto-init.

--- js/jquery.mobile.forms.textinput.js
+++ js/jquery.mobile.forms.textinput.js
@@ -9,7 +9,7 @@
 
 //auto self-init widgets
 $( document ).bind( "pagecreate enhance", function( e ){
-	$( "input[type='text'], input[type='search'], input[type='number'], input[type='password'], input[type='email'], textarea", e.target )
+	$( "input[type='text'], input[type='search'], input[type='number'], input[type='password'], input[type='email'], input[type='url'], input[type='tel'], textarea", e.target )
 		.not( ":jqmData(role='none'), :jqmData(role='nojs')" )
 		.textinput();
 });
