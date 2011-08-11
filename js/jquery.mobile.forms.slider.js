8c0d22c6c16ad0a27c7b85fdf0b83ccea973c57b
jrowny <jrowny@gmail.com>
2011-08-11 17:37:58 -0400

    add slider step support from jQuery UI

--- js/jquery.mobile.forms.slider.js
+++ js/jquery.mobile.forms.slider.js
@@ -224,8 +224,9 @@ $.widget( "mobile.slider", $.mobile.widget, {
 		var control = this.element, percent,
 			cType = control[0].nodeName.toLowerCase(),
 			min = cType === "input" ? parseFloat( control.attr( "min" ) ) : 0,
-			max = cType === "input" ? parseFloat( control.attr( "max" ) ) : control.find( "option" ).length - 1;
-
+			max = cType === "input" ? parseFloat( control.attr( "max" ) ) : control.find( "option" ).length - 1,
+			step = (cType === "input" && parseFloat(control.attr("step"))>0) ? parseFloat(control.attr("step")) : 1; 
+			
 		if ( typeof val === "object" ) {
 			var data = val,
 				// a slight tolerance helped get to the ends of the slider
