c41c65a1cfd391df19544114a3a71ad546a3b02b
John Bender <john.m.bender@gmail.com>
2011-08-10 17:07:34 -0700

    quote consistency

--- tests/unit/dialog/dialog_events.js
+++ tests/unit/dialog/dialog_events.js
@@ -17,12 +17,12 @@
 				var fooDialog;
 				ok(/&ui-state=dialog/.test(location.hash), "ui-state=dialog =~ location.hash", "dialog open");
 
-				fooDialog = $('#foo-dialog');
+				fooDialog = $("#foo-dialog");
 				// Assert dialog theme inheritance (issue 1375):
-				ok(fooDialog.hasClass('ui-body-b'), 'Expected explicit theme ui-body-b');
-				ok(fooDialog.find( ":jqmData(role=header)" ).hasClass('ui-bar-a'), 'Expected header to inherit from $.mobile.page.prototype.options.headerTheme');
-				ok(fooDialog.find( ":jqmData(role=content)" ).hasClass('ui-body-d'), 'Expect content to inherit from $.mobile.page.prototype.options.contentTheme');
-				ok(fooDialog.find( ":jqmData(role=footer)" ).hasClass('ui-bar-a'), 'Expected footer to inherit from $.mobile.page.prototype.options.footerTheme');
+				ok(fooDialog.hasClass("ui-body-b"), "Expected explicit theme ui-body-b");
+				ok(fooDialog.find( ":jqmData(role=header)" ).hasClass("ui-bar-a"), "Expected header to inherit from $.mobile.page.prototype.options.headerTheme");
+				ok(fooDialog.find( ":jqmData(role=content)" ).hasClass("ui-body-d"), "Expect content to inherit from $.mobile.page.prototype.options.contentTheme");
+				ok(fooDialog.find( ":jqmData(role=footer)" ).hasClass("ui-bar-a"), "Expected footer to inherit from $.mobile.page.prototype.options.footerTheme");
 
 				// close the dialog
 				$(".ui-dialog").dialog("close");
