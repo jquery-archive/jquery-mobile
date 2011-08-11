6ebaf991a831b155e54d240e3fd1e880064a8ca9
John Bender <john.m.bender@gmail.com>
2011-08-10 17:06:56 -0700

    moved dialog test to pageSequence helper, and small cleanup

--- tests/unit/dialog/dialog_events.js
+++ tests/unit/dialog/dialog_events.js
@@ -7,25 +7,31 @@
 	asyncTest( "dialog hash is added when the dialog is opened and removed when closed", function(){
 		expect( 6 );
 
-		//bring up the dialog
-		$("a[href='#foo-dialog']").click();
+		$.testHelper.pageSequence([
+			function(){
+				//bring up the dialog
+				$("#foo-dialog-link").click();
+			},
 
-		setTimeout(function(){
-			ok(/&ui-state=dialog/.test(location.hash), "ui-state=dialog =~ location.hash");
-			
-			// Assert dialog theme inheritance (issue 1375):
-			ok($('#foo-dialog').hasClass('ui-body-b'), 'Expected explicit theme ui-body-b');
-			ok($('#foo-dialog').find( ":jqmData(role=header)" ).hasClass('ui-bar-a'), 'Expected header to inherit from $.mobile.page.prototype.options.headerTheme');
-			ok($('#foo-dialog').find( ":jqmData(role=content)" ).hasClass('ui-body-d'), 'Expect content to inherit from $.mobile.page.prototype.options.contentTheme');
-			ok($('#foo-dialog').find( ":jqmData(role=footer)" ).hasClass('ui-bar-a'), 'Expected footer to inherit from $.mobile.page.prototype.options.footerTheme');
-			
-			// close the dialog
-			$(".ui-dialog").dialog("close");
-		}, 500);
+			function(){
+				var fooDialog;
+				ok(/&ui-state=dialog/.test(location.hash), "ui-state=dialog =~ location.hash", "dialog open");
 
-		setTimeout(function(){
-			ok(!/&ui-state=dialog/.test(location.hash), "ui-state=dialog !~ location.hash");
-			start();
-		}, 1000);
+				fooDialog = $('#foo-dialog');
+				// Assert dialog theme inheritance (issue 1375):
+				ok(fooDialog.hasClass('ui-body-b'), 'Expected explicit theme ui-body-b');
+				ok(fooDialog.find( ":jqmData(role=header)" ).hasClass('ui-bar-a'), 'Expected header to inherit from $.mobile.page.prototype.options.headerTheme');
+				ok(fooDialog.find( ":jqmData(role=content)" ).hasClass('ui-body-d'), 'Expect content to inherit from $.mobile.page.prototype.options.contentTheme');
+				ok(fooDialog.find( ":jqmData(role=footer)" ).hasClass('ui-bar-a'), 'Expected footer to inherit from $.mobile.page.prototype.options.footerTheme');
+
+				// close the dialog
+				$(".ui-dialog").dialog("close");
+			},
+
+			function(){
+				ok(!/&ui-state=dialog/.test(location.hash), "ui-state=dialog !~ location.hash");
+				start();
+			}
+		]);
 	});
 })(jQuery);
