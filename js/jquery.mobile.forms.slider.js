a6e922a54afba35564fed4146447e534fc4078dd
jrowny <jrowny@gmail.com>
2011-08-12 10:06:24 -0400

    slider step, remove round clean up code a bit

--- js/jquery.mobile.forms.slider.js
+++ js/jquery.mobile.forms.slider.js
@@ -225,7 +225,7 @@ $.widget( "mobile.slider", $.mobile.widget, {
 			cType = control[0].nodeName.toLowerCase(),
 			min = cType === "input" ? parseFloat( control.attr( "min" ) ) : 0,
 			max = cType === "input" ? parseFloat( control.attr( "max" ) ) : control.find( "option" ).length - 1,
-			step = (cType === "input" && parseFloat(control.attr("step"))>0) ? parseFloat(control.attr("step")) : 1; 
+			step = (cType === "input" && parseFloat( control.attr( "step" ) ) > 0) ? parseFloat(control.attr("step")) : 1; 
 			
 		if ( typeof val === "object" ) {
 			var data = val,
@@ -256,17 +256,17 @@ $.widget( "mobile.slider", $.mobile.widget, {
 			percent = 100;
 		}
 
-		var newval = Math.round( ( percent / 100 ) * ( max - min ) ) + min;
+		var newval = ( percent / 100 ) * ( max - min ) + min;
 
 		//from jQuery UI slider, the following source will round to the neraest step
-		var valModStep = (newval - min) % step;	
+		var valModStep = ( newval - min ) % step;	
 		var alignValue = newval - valModStep;	
 
-		if ( Math.abs(valModStep) * 2 >= step ) {
+		if ( Math.abs( valModStep ) * 2 >= step ) {
 			alignValue += ( valModStep > 0 ) ? step : ( -step );
 		}
 		// Since JavaScript has problems with large floats, round
-		// the final value to 5 digits after the decimal point (see jQuery UI: #4124)
+		// the final value to 5 digits after the decimal point (see jQueryUI: #4124)
 		newval = parseFloat( alignValue.toFixed(5) );
 
 		if ( newval < min ) {
