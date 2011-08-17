eba4e7eeb4f281220687a87973797c4011f7702a
John Bender <john.m.bender@gmail.com>
2011-08-16 17:06:55 -0700

    switched path helper

--- js/jquery.mobile.navigation.pushstate.js
+++ js/jquery.mobile.navigation.pushstate.js
@@ -30,7 +30,7 @@
 
 			// only replaceState when pushState support is present and
 			// the hash doesn't represent an embeded page
-			if( $.support.pushState && !$.mobile.path.isEmbeddedPage(location.hash) ) {
+			if( $.support.pushState && $.mobile.path.isPath(location.hash) ) {
 				var hash = location.hash || "#" + self.initialFilePath,
 					href = hash.replace( "#", "" );
 
