00e56c15606bb87cdc65e76551d8e9c16b9a0f2d
gseguin <ghislain@gmail.com>
2011-08-24 13:55:15 -0700

    Added test for ui-li-has presence related to issue #779

--- tests/unit/listview/listview_core.js
+++ tests/unit/listview/listview_core.js
@@ -53,6 +53,31 @@
 		]);
 	});
 
+	asyncTest( "Presence of ui-li-has- classes", function(){
+		$.testHelper.pageSequence( [
+			function() {
+				$.testHelper.openPage( "#ui-li-has-test" );
+			},
+
+			function() {
+				var page = $( ".ui-page-active" ),
+					items = page.find( "li" );
+
+				ok(  items.eq( 0 ).hasClass( "ui-li-has-count"), "First LI should have ui-li-has-count class" );
+				ok(  items.eq( 0 ).hasClass( "ui-li-has-arrow"), "First LI should have ui-li-has-arrow class" );
+				ok( !items.eq( 1 ).hasClass( "ui-li-has-count"), "Second LI should NOT have ui-li-has-count class" );
+				ok(  items.eq( 1 ).hasClass( "ui-li-has-arrow"), "Second LI should have ui-li-has-arrow class" );
+				ok( !items.eq( 2 ).hasClass( "ui-li-has-count"), "Third LI should NOT have ui-li-has-count class" );
+				ok( !items.eq( 2 ).hasClass( "ui-li-has-arrow"), "Third LI should NOT have ui-li-has-arrow class" );
+				ok(  items.eq( 3 ).hasClass( "ui-li-has-count"), "Fourth LI should have ui-li-has-count class" );
+				ok( !items.eq( 3 ).hasClass( "ui-li-has-arrow"), "Fourth LI should NOT have ui-li-has-arrow class" );
+				ok( !items.eq( 4 ).hasClass( "ui-li-has-count"), "Fifth LI should NOT have ui-li-has-count class" );
+				ok( !items.eq( 4 ).hasClass( "ui-li-has-arrow"), "Fifth LI should NOT have ui-li-has-arrow class" );
+				start();
+			}
+		]);
+	});
+
 	module('Nested List Test');
 
 	asyncTest( "Changes page to nested list test and enhances", function() {
