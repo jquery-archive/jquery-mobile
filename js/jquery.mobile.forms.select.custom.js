07edada0dd034b0d03eab53199311ff2adc10bac
John Bender <john.m.bender@gmail.com>
2011-08-29 10:09:22 -0700

    added todo to centralize page dom removal and ref counting Fixes #2329

--- js/jquery.mobile.forms.select.custom.js
+++ js/jquery.mobile.forms.select.custom.js
@@ -245,6 +245,9 @@
 				var self = this;
 
 				if ( self.menuType == "page" ) {
+					// TODO centralize page removal binding / handling in the page plugin.
+					// Suggestion from @jblas to do refcounting
+					//
 					// rebind the page remove that was unbound in the open function
 					// to allow for the parent page removal from actions other than the use
 					// of a dialog sized custom select
