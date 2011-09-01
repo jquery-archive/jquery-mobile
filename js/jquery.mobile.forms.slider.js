5333ab7bf7e95acd6ce4ef98ef392e45ae8172f6
Kin Blas <jblas@adobe.com>
2011-09-01 15:42:30 -0700

    I noticed that the code in the mouseup for the toggle switch was using a CSS3 transition, but calling the animationComplete() function which waits for an animationend event which will never come. This means we bind a new animationComplete() handler every time the toggle switch is clicked.
    I removed the binding for the handler completely since it doesn't hurt to just leave the transition in place. I also removed the code that was setting the position of the slider since it really should only be set within the mouseup if the user did not change the switch setting (toggle).

--- js/jquery.mobile.forms.slider.js
+++ js/jquery.mobile.forms.slider.js
@@ -140,16 +140,9 @@ $.widget( "mobile.slider", $.mobile.widget, {
 
 						if ( !self.userModified ) {
 							//tap occurred, but value didn't change. flip it!
+							handle.addClass( "ui-slider-handle-snapping" );
 							self.refresh( !self.beforeStart ? 1 : 0 );
 						}
-						var curval = val();
-						var snapped = Math.round( curval / ( max - min ) * 100 );
-						handle
-							.addClass( "ui-slider-handle-snapping" )
-							.css( "left", snapped + "%" )
-							.animationComplete( function() {
-								handle.removeClass( "ui-slider-handle-snapping" );
-							});
 					}
 					return false;
 				}
