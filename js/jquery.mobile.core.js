43c5037bd89a722c4885c0991d5ad23b444ceb08
gseguin <ghislain@gmail.com>
2011-09-16 09:10:08 -0700

    Changed behavior of .jqmData() only when called with no argument. It now returns undefined as per discussion on #jquerymobile-dev

--- js/jquery.mobile.core.js
+++ js/jquery.mobile.core.js
@@ -128,7 +128,11 @@
 	// Mobile version of data and removeData and hasData methods
 	// ensures all data is set and retrieved using jQuery Mobile's data namespace
 	$.fn.jqmData = function( prop, value ) {
-		return this.data( prop ? $.mobile.nsNormalize( prop ) : prop, value );
+		var result;
+		if ( typeof prop != "undefined" ) {
+			result = this.data( prop ? $.mobile.nsNormalize( prop ) : prop, value );
+		}
+		return result;
 	};
 
 	$.jqmData = function( elem, prop, value ) {
