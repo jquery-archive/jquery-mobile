a20bb72a2945baa23eef84799acf0060860ae741
John Bender <john.m.bender@gmail.com>
2011-09-26 14:00:17 -0700

    whitespace in events

--- js/jquery.mobile.event.js
+++ js/jquery.mobile.event.js
@@ -118,11 +118,11 @@ $.event.special.tap = {
 // also handles swipeleft, swiperight
 $.event.special.swipe = {
 	scrollSupressionThreshold: 10, // More than this horizontal displacement, and we will suppress scrolling.
-	
+
 	durationThreshold: 1000, // More time than this, and it isn't a swipe.
-	
+
 	horizontalDistanceThreshold: 30,  // Swipe horizontal displacement must be more than this.
-	
+
 	verticalDistanceThreshold: 75,  // Swipe vertical displacement must be less than this.
 
 	setup: function() {
