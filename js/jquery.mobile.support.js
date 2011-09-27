ff9720281b25c68c779cd71955089408249fc734
toddparker <todd@filamentgroup.com>
2011-09-27 12:05:37 -0400

    Added another check for orientation change event for Android 2.1

--- js/jquery.mobile.support.js
+++ js/jquery.mobile.support.js
@@ -64,7 +64,7 @@ $.mobile.browser.ie = (function() {
 
 
 $.extend( $.support, {
-	orientation: "orientation" in window,
+	orientation: "orientation" in window && "onorientationchange" in window,
 	touch: "ontouchend" in document,
 	cssTransitions: "WebKitTransitionEvent" in window,
 	pushState: "pushState" in history && "replaceState" in history,
