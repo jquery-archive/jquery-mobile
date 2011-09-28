22c2fff7dea09f00b6b7953ef66b355026a5a253
Ryan Neufeld <ryan@neufeldmail.com>
2011-09-28 11:15:41 -0700

    Bug 1609; detect if list view is multi line and add additional class to accomodate extra content

--- js/jquery.mobile.listview.js
+++ js/jquery.mobile.listview.js
@@ -52,6 +52,11 @@ $.widget( "mobile.listview", $.mobile.widget, {
 				var $this = $(this);
 				$this.prependTo( $this.parent() ); //shift aside to front for css float
 			});
+
+		if ( item.find( "h1, h2, h3, h4, h5, p, dl" ).length > 1 ) {
+			item.addClass( "ui-li-multiline" );
+		}
+
 	},
 
 	_removeCorners: function( li, which ) {
