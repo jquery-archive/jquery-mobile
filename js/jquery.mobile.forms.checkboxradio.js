aa16aec3586b00a6d07ef1eea5b501631876d329
John Bender <john.m.bender@gmail.com>
2011-10-10 13:49:55 -0700

    move checkbox radio to enhanceWithin

--- js/jquery.mobile.forms.checkboxradio.js
+++ js/jquery.mobile.forms.checkboxradio.js
@@ -186,9 +186,7 @@ $.widget( "mobile.checkboxradio", $.mobile.widget, {
 
 //auto self-init widgets
 $( document ).bind( "pagecreate create", function( e ){
-	$( $.mobile.checkboxradio.prototype.options.initSelector, e.target )
-		.not( ":jqmData(role='none'), :jqmData(role='nojs')" )
-		.checkboxradio();
+	$.mobile.checkboxradio.prototype.enhanceWithin( e.target );
 });
 
 })( jQuery );
