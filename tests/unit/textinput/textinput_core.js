18737f4940445dbee5428a8fc5fb94311f94952c
John Bender <john.m.bender@gmail.com>
2011-10-10 13:36:55 -0700

    test to verify that textinputs are kept native where necessary

--- tests/unit/textinput/textinput_core.js
+++ tests/unit/textinput/textinput_core.js
@@ -7,4 +7,11 @@
 	test( "inputs without type specified are enhanced", function(){
 		ok( $( "#typeless-input" ).hasClass( "ui-input-text" ) );
 	});
+
+	$.mobile.page.prototype.options.keepNative = "textarea.should-be-native";
+
+  // not testing the positive case here since's it's obviously tested elsewhere
+	test( "textarea in the keepNative set shouldn't be enhanced", function() {
+    ok( !$("textarea.should-be-native").is("ui-input-text") );
+	});
 })(jQuery);
