5150114c133d5cb03c6add96e2ccba37cec613d5
John Bender <john.m.bender@gmail.com>
2011-10-11 11:25:28 -0700

    test page init textarea autogrow

--- tests/unit/textinput/textinput_core.js
+++ tests/unit/textinput/textinput_core.js
@@ -10,8 +10,18 @@
 
 	$.mobile.page.prototype.options.keepNative = "textarea.should-be-native";
 
-  // not testing the positive case here since's it's obviously tested elsewhere
+	// not testing the positive case here since's it's obviously tested elsewhere
 	test( "textarea in the keepNative set shouldn't be enhanced", function() {
-    ok( !$("textarea.should-be-native").is("ui-input-text") );
+		ok( !$("textarea.should-be-native").is("ui-input-text") );
+	});
+
+	asyncTest( "textarea should autogrow on document ready", function() {
+		var test = $( "#init-autogrow" );
+
+		setTimeout(function() {
+			ok( $( "#reference-autogrow" )[0].clientHeight < test[0].clientHeight, "the height is greater than the reference text area with no content" );
+			ok( test[0].clientHeight > 100, "autogrow text area's height is greater than any style padding");
+			start();
+		}, 400);
 	});
 })(jQuery);
