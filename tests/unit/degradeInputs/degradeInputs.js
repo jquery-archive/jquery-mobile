862f58e90cc75782a1b18aa165706a648a46eaa7
Gabriele Rabbiosi <gabriele.rabbiosi@gmail.com>
2011-10-14 01:02:37 +0200

    refactored degradeInputs tests

--- tests/unit/degradeInputs/degradeInputs.js
+++ tests/unit/degradeInputs/degradeInputs.js
@@ -4,93 +4,26 @@
 
 (function($){
     
-
-	module('jquery.mobile.slider.js', {
-	    setup: function() {
-	        $('#test-container').html("");
-	    }
-	});
+    module('jquery.mobile.slider.js');
 	
 	test('keepNative elements should not be degraded', function() {
         same($('input#not-to-be-degraded').attr("type"), "range");
 	});
 	
-	test('input type color should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="color" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "color");
-    });
+	test('should degrade input type to a different type, as specified in page options', function(){
+        var degradeInputs = $.mobile.page.prototype.options.degradeInputs;
     
-    test('input type date should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="date" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "date");
-    });
+        expect( degradeInputs.length );
     
-    test('input type datetime should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="datetime" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "datetime");
-    });
-    
-    test('input type datetime-local should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="datetime-local" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "datetime-local");
-    });
-    
-    test('input type email should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="email" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "email");
-    });
-    
-    test('input type month should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="month" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "month");
-    });
-    
-    test('input type number should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="number" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "number");
-    });
-
-    test('input type range should degrade to number when "create" event is triggered', function(){
-        $('#test-container').html('<input type="range" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "number");
-    });
-    
-    test('input type search should degrade to text when "create" event is triggered', function(){
-        $('#test-container').html('<input type="search" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "text");
-    });
-    
-    test('input type tel should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="tel" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "tel");
-    });
-    
-    test('input type time should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="time" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "time");
-    });
-    
-    test('input type url should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="url" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "url");
-    });
-    
-    test('input type week should not degrade when "create" event is triggered', function(){
-        $('#test-container').html('<input type="week" />').trigger("create");
-        
-        same($('#test-container input').attr("type"), "week");
+        $.each(degradeInputs, function( oldType, newType ) {
+            if (newType === false) {
+                newType = oldType;
+            }
+            
+            $('#test-container').html('<input type="' + oldType + '" />').trigger("create");
+            
+            same($('#test-container input').attr("type"), newType);
+        });
     });
 	
 })(jQuery);
