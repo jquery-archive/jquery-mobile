13a0c1508f3011cda0ede3d8f3c34414420a6306
John Bender <john.m.bender@gmail.com>
2011-11-01 10:45:14 -0700

    Revert "fixed js refs and whitespace in samples Fixes #2904"
    This reverts commit c748072bacbf544f4f80171dd2ba5d4699c5e31c.

--- docs/forms/forms-sample-response.php
+++ docs/forms/forms-sample-response.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html> 
+<html> 
 	<head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>jQuery Mobile Docs - Sample form response</title>
-	<link rel="stylesheet"  href="../../css/themes/default/" />
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<title>jQuery Mobile Docs - Sample form response</title> 
+	<link rel="stylesheet"  href="../../css/themes/default/" />  
 	<link rel="stylesheet" href="../_assets/css/jqm-docs.css"/>
-	<script src="../../../js/jquery.js"></script>
-	<script src="../../../experiments/themeswitcher/jquery.mobile.themeswitcher.js"></script>
+	<script src="../../js/jquery.js"></script>
+	<script src="../../experiments/themeswitcher/jquery.mobile.themeswitcher.js"></script>
 	<script src="../_assets/js/jqm-docs.js"></script>
-	<script src="../../../js/"></script>
-</head>
-<body>
+	<script src="../../js/"></script>
+</head> 
+<body> 
 
 	<div data-role="page" class="type-interior">
 
@@ -22,9 +22,9 @@
 
 	<div data-role="content" data-theme="c">
 		<div class="content-primary">
-
+		
 		<form action="index.html" method="get">
-
+			
 			<h2>You Chose:</h2>
 
 			<div class="ui-body ui-body-d ui-corner-all">
@@ -32,23 +32,23 @@
 			<?php
 				echo "<p> " . $_REQUEST['shipping'] . "</p>";
 			?>
-
+			
 			</div>
-
+			
 			<a href="forms-sample.html" data-role="button" data-theme="b" data-icon="arrow-l">Change shipping method</a>
 
 	</form>
-
-	</div><!--/content-primary -->
-
+	
+	</div><!--/content-primary -->		
+	
 	<div class="content-secondary">
-
+		
 		<div data-role="collapsible" data-collapsed="true" data-theme="b" data-content-theme="d">
-
+			
 				<h3>More in this section</h3>
-
+				
 				<ul data-role="listview" data-theme="c" data-dividertheme="d">
-
+				
 					<li data-role="list-divider">Form elements</li>
 					<li><a href="docs-forms.html">Form basics</a></li>
 					<li><a href="forms-all.html">Form element gallery</a></li>
@@ -62,18 +62,18 @@
 					<li><a href="forms-themes.html">Theming forms</a></li>
 					<li><a href="forms-all-native.html">Native form elements</a></li>
 					<li data-theme="a"><a href="forms-sample.html">Submitting forms</a></li>
-
-
+					
+	
 				</ul>
 		</div>
-	</div>
+	</div>		
 
 </div><!-- /content -->
 
 <div data-role="footer" class="footer-docs" data-theme="c">
 		<p>&copy; 2011 The jQuery Project</p>
 </div>
-
+	
 </div><!-- /page -->
 
 </body>
