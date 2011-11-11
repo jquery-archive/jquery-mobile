ced1f865a2d31ca626f81c7f9200e6ce166eaafc
John Bender <john.m.bender@gmail.com>
2011-11-11 11:49:31 -0800

    whitespace and a comment on setting buttonPlaceholder to undefined

--- js/jquery.mobile.forms.button.js
+++ js/jquery.mobile.forms.button.js
@@ -46,15 +46,17 @@ $.widget( "mobile.button", $.mobile.widget, {
 					// Add hidden input if it doesn’t already exist.
 					if( $buttonPlaceholder === undefined ) {
 						$buttonPlaceholder = $( "<input>", {
-									type: "hidden",
-									name: $el.attr( "name" ),
-									value: $el.attr( "value" )
-								})
-								.insertBefore( $el );
+							type: "hidden",
+							name: $el.attr( "name" ),
+							value: $el.attr( "value" )
+						}).insertBefore( $el );
 
 						// Bind to doc to remove after submit handling
 						$( document ).one("submit", function(){
 							$buttonPlaceholder.remove();
+
+							// reset the local var so that the hidden input
+							// will be re-added on subsequent clicks
 							$buttonPlaceholder = undefined;
 						});
 					}
