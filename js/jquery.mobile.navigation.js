098eed9c3b6ff53749fa67e95e550361565f9b4c
jblas@adobe.com <jblas@adobe.com>
2011-11-15 16:17:14 -0800

    Fix for #2826 - an error occurred with click event on svg element using "live" or "delegate"
    - Modified findClosestLink() to check the ele.nodeName type before trying to access it.

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -1191,7 +1191,14 @@
 	function findClosestLink( ele )
 	{
 		while ( ele ) {
-			if ( ele.nodeName.toLowerCase() == "a" ) {
+			// Look for the closest element with a nodeName of "a".
+			// Note that we are checking if we have a valid nodeName
+			// before attempting to access it. This is because the
+			// node we get called with could have originated from within
+			// an embedded SVG document where some symbol instance elements
+			// don't have nodeName defined on them, or strings are of type
+			// SVGAnimatedString.
+			if ( ( typeof ele.nodeName === "string" ) && ele.nodeName.toLowerCase() == "a" ) {
 				break;
 			}
 			ele = ele.parentNode;
