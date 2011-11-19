960488c0a3d11dc0614d9c0fbdf725f546ffa936
Eddie Monge <eddie@eddiemonge.com>
2011-11-19 12:21:21 -0800

    Fix for issue #2308

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -1228,8 +1228,11 @@
 		//bind to form submit events, handle with Ajax
 		$( document ).delegate( "form", "submit", function( event ) {
 			var $this = $( this );
+			//Don't use ajax if ajax is turned off or if its a form with a file upload
 			if( !$.mobile.ajaxEnabled ||
-				$this.is( ":jqmData(ajax='false')" ) ) {
+				$this.is( ":jqmData(ajax='false')" ) ||
+				$this.attr( "enctype" ) === "multipart/form-data" ||
+				$this.find( "input[type='file']" ).length > 0 ) {
 					return;
 				}
 
