f49fe2082be283c648fc40ac5c7980ef7f3fd581
John Bender <john.m.bender@gmail.com>
2011-11-22 13:15:20 -0800

    record enhancement time and pageprocessing, both of which ignore the network load time of the jqm js

--- tests/speed/stats/startup.js
+++ tests/speed/stats/startup.js
@@ -4,11 +4,23 @@
 	Perf.setCurrentRev();
 	Perf.pageLoadStart = Date.now();
 
+	$( document ).bind( "mobileinit", function() {
+		Perf.pageProcessingStart = Date.now();
+	});
+
 	$listPage.live( "pagebeforecreate", function() {
 		if( firstCounter == 0 ) {
 			Perf.pageCreateStart = Date.now();
 			firstCounter++;
 		}
+	});
+
+	$listPage.live( "pagecreate", function( event ) {
+		if( event.target !== $("#list-page")[0] ){
+			return;
+		}
+
+		Perf.pageEnhanceStart = Date.now();
 	});
 
 	$listPage.live( "pageinit", function( event ) {
@@ -20,10 +32,26 @@
 
 		Perf.pageLoadEnd = Date.now();
 
-		// report the time taken for a full app boot
+		// report the time taken for a full app boot includes the time for
+		// the jquery mobile js to load
 		Perf.report({
 			datapoint: "fullboot",
 			value: Perf.pageLoadEnd - Perf.pageLoadStart
+		});
+
+		// report the time taken for the first page to be enhanced
+		// pagecreate -> pageinit
+		Perf.report({
+			datapoint: "enhancement",
+			value: Perf.pageLoadEnd - Perf.pageEnhanceStart
+		});
+
+		// report the time taken for the page to process. does *not*
+		// inclue the load time for js
+		// mobileinit -> pageinit
+		Perf.report({
+			datapoint: "pageprocessing",
+			value: Perf.pageLoadEnd - Perf.pageProcessingStart
 		});
 
 		// record the time taken to load and enhance the page
