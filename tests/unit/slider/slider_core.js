b4f945f73b34240e333d541900b3c34819b01812
John Bender <john.m.bender@gmail.com>
2011-11-23 13:23:31 -0800

    test steps in refresh

--- tests/unit/slider/slider_core.js
+++ tests/unit/slider/slider_core.js
@@ -8,4 +8,17 @@
 	test( "slider elements in the keepNative set shouldn't be enhanced", function() {
 		same( $("input.should-be-native").siblings("div.ui-slider").length, 0 );
 	});
+
+	test( "refresh should force val to nearest step", function() {
+		var slider = $( "#step-slider" ),
+			step = parseInt(slider.attr( "step" ), 10);
+
+		slider.val( step + 1 );
+
+		slider.slider( 'refresh' );
+
+		ok( step > 1, "the step is greater than one" );
+		ok( slider.val() > 0, "the value has been altered" );
+		same( slider.val() % step, 0, "value has 'snapped' to a step" );
+	});
 })( jQuery );
