8975f5b9a07c20c52e73b5dbe8236a01416ec6cd
toddparker <todd@filamentgroup.com>
2011-11-28 15:03:12 -0500

    Added initial-scale to the meta tags

--- docs/pages/dynamic-samples/category.php
+++ docs/pages/dynamic-samples/category.php
@@ -113,7 +113,7 @@ if( $_SERVER[ "HTTP_X_REQUESTED_WITH" ] && $_SERVER[ "HTTP_X_REQUESTED_WITH" ] =
 <html>
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Vehicles</title>
 <link rel="stylesheet"  href="../../../css/themes/default/">
 <script src="../../../js/jquery.js"></script>
