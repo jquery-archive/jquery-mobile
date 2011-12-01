63da13b8f5bafcf44afacf55d2ecb43dc161844e
Ghislain Seguin <ghislain@gmail.com>
2011-12-01 13:03:06 -0800

    Removed ref to order Added dir exclusion regex for build

--- js/app.build.js
+++ js/app.build.js
@@ -3,13 +3,19 @@
 	baseUrl: "js/",
 	dir: "../dist",
 
-	optimize: "none",
+	//optimize: "none",
 
 	modules: [
 	     {
 	         name: "jquery.mobile",
-		     include: [ "almond" ],
-		     exclude: [ "jquery", "order" ]
+		     exclude: [ "jquery" ]
 	     },
-	]
+	],
+
+	wrap: {
+	    start: "(function() {",
+	    end: '\nif ( typeof define === "function" && define.amd ) {\ndefine( "jquery.mobile", [], function () { return jQuery.mobile; } );\n}\n}());'
+	},
+
+	dirExclusionRegExp: /^build|^compiled|^external|^tests/
 })
