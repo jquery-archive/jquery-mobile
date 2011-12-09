def90bf230583206f75a61c60c790cbefd67c8b1
John Bender <john.m.bender@gmail.com>
2011-12-09 12:51:38 -0800

    whitespace in degradeInputs tests

--- tests/unit/degradeInputs/degradeInputs.js
+++ tests/unit/degradeInputs/degradeInputs.js
@@ -3,35 +3,33 @@
  */
 
 (function($){
-    
-    module('jquery.mobile.slider.js');
-	
+	module('jquery.mobile.slider.js');
+
 	test('keepNative elements should not be degraded', function() {
-        same($('input#not-to-be-degraded').attr("type"), "range");
+		same($('input#not-to-be-degraded').attr("type"), "range");
 	});
-	
+
 	test('should degrade input type to a different type, as specified in page options', function(){
-        var degradeInputs = $.mobile.page.prototype.options.degradeInputs;
-    
-        expect( degradeInputs.length );
- 
-        // Initialize dialog page
-        $.mobile.changePage($('#dialog'));
-        $.mobile.changePage($('#page'));
+		var degradeInputs = $.mobile.page.prototype.options.degradeInputs;
 
-        $.each(degradeInputs, function( oldType, newType ) {
-            if (newType === false) {
-                newType = oldType;
-            }
-            
-            $('#page-test-container').html('<input type="' + oldType + '" />').trigger("create");
-            
-            same($('#page-test-container input').attr("type"), newType);
+		expect( degradeInputs.length );
 
-            $('#dialog-test-container').html('<input type="' + oldType + '" />').trigger("create");
+		// Initialize dialog page
+		$.mobile.changePage($('#dialog'));
+		$.mobile.changePage($('#page'));
 
-            same($('#dialog-test-container input').attr("type"), newType);
-        });
-    });
-	
+		$.each(degradeInputs, function( oldType, newType ) {
+			if (newType === false) {
+				newType = oldType;
+			}
+
+			$('#page-test-container').html('<input type="' + oldType + '" />').trigger("create");
+
+			same($('#page-test-container input').attr("type"), newType);
+
+			$('#dialog-test-container').html('<input type="' + oldType + '" />').trigger("create");
+
+			same($('#dialog-test-container input').attr("type"), newType);
+		});
+	});
 })(jQuery);
