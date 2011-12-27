8293af5e68ecfc336406c8b012b558f11f5f4f63
sgrebnov <sergeyg@bsquare.com>
2011-12-27 15:24:54 +0400

    speed tests for checkbox, slider and custom select menu

---
+++ tests/speed/slider.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>jQuery Mobile Docs - Slider</title>
+	<link rel="stylesheet"  href="../../css/themes/default/" />
+	<script src="../../js/jquery.js"></script>
+	<script type="text/javascript" src="stats/perf.js"></script>
+	<script type="text/javascript" src="stats/startup.js"></script>
+	<script src="../../js/"></script>
+</head>
+<body>
+
+<div data-role="page" id="list-page">
+
+	<div  data-role="header">
+		<h1>Slider</h1>
+	</div><!-- /header -->
+
+	<div  data-role="content" data-filter="true">
+
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+		
+		<select data-role="slider">
+			<option value="off">Off</option>
+			<option value="on">On</option>
+		</select>
+			
+	</div><!-- /content -->
+</div><!-- /page -->
+
+</body>
+</html>
