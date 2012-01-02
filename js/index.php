72fe72f772606a0f9855e41a1bfcbf5744c7f086
scottjehl <scott@scottjehl.com>
2012-01-02 18:38:21 +0700

    added new filename to make and combiner files

--- js/index.php
+++ js/index.php
@@ -18,7 +18,7 @@ $files = array(
 	'jquery.mobile.navigation.js',
 	'jquery.mobile.navigation.pushstate.js',
 	'jquery.mobile.transition.js',
-	'jquery.mobile.transition.fadeoutin.js',
+	'jquery.mobile.transition.outin.js',
 	'jquery.mobile.degradeInputs.js',
 	'jquery.mobile.dialog.js',
 	'jquery.mobile.page.sections.js',
