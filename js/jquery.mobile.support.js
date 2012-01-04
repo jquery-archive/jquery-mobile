1e8a7c520ec512d008f3268eccc7462c1d8176e3
scottjehl <scott@scottjehl.com>
2012-01-04 16:23:26 +0700

    Added a feature test for css 3d transforms, which will allow us to conditionally apply the flip transition (AKA cartwheel, where unsupported).

--- js/jquery.mobile.support.js
+++ js/jquery.mobile.support.js
@@ -27,6 +27,12 @@ function propExists( prop ) {
 	}
 }
 
+// Thanks to Modernizr src for this test idea
+function transform3dTest() {
+	var prop = "transform-3d";
+	return $.mobile.media( "(-" + vendors.join( "-" + prop + "),(-" ) + prop + "),(" + prop + ")" );
+}
+
 // Test for dynamic-updating base tag support ( allows us to avoid href,src attr rewriting )
 function baseTagTest() {
 	var fauxBase = location.protocol + "//" + location.host + location.pathname + "ui-dir/",
@@ -76,6 +82,7 @@ $.extend( $.support, {
 	mediaquery: $.mobile.media( "only all" ),
 	cssPseudoElement: !!propExists( "content" ),
 	touchOverflow: !!propExists( "overflowScrolling" ),
+	cssTransform3d: transform3dTest(),
 	boxShadow: !!propExists( "boxShadow" ) && !bb,
 	scrollTop: ( "pageXOffset" in window || "scrollTop" in document.documentElement || "scrollTop" in fakeBody[ 0 ] ) && !webos && !operamini,
 	dynamicBaseTag: baseTagTest()
