fe32fd5b178734eec5f461aa51e8192cfcb00607
John Bender <john.m.bender@gmail.com>
2012-01-04 11:38:32 -0800

    ensure defaultTransitionHandler is defined after the navigation has been loaded

--- js/jquery.mobile.transition.js
+++ js/jquery.mobile.transition.js
@@ -3,7 +3,10 @@
 */
 
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
-define( [ "jquery.mobile.core" ], function() {
+// TODO the dependency defined here for transitions is to make sure
+// that the defaultTransitionHandler is defined _after_ navigation has been defined
+// This requires a rework/rethinking
+define( [ "jquery.mobile.core", "order!jquery.mobile.navigation" ], function() {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, window, undefined ) {
 
