43b5537d5b1231406eb8b4bdb19f20436b805c19
scottjehl <scott@scottjehl.com>
2012-01-09 21:09:12 +0700

    added an experimental 3D transition, "flow"

--- css/structure/index.php
+++ css/structure/index.php
@@ -9,6 +9,7 @@ $files = array_merge($files, array(
 	'../../structure/jquery.mobile.transitions.slidedown.css',
 	'../../structure/jquery.mobile.transitions.slideup.css',
 	'../../structure/jquery.mobile.transitions.flip.css',
+	'../../structure/jquery.mobile.transitions.flow.css',
 	'../../structure/jquery.mobile.grids.css',
 	'../../structure/jquery.mobile.headerfooter.css',
 	'../../structure/jquery.mobile.navbar.css',
