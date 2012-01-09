f2e4aa525f85b9d1cb8c3da5460d391f94393270
scottjehl <scott@scottjehl.com>
2012-01-09 22:51:35 +0700

    added fallback transition for flow

---
+++ js/jquery.mobile.transition.flow.js
@@ -0,0 +1,9 @@
+/*
+* fallback transition for flow in non-3D supporting browsers (which tend to handle complex transitions poorly in general
+*/
+
+(function( $, window, undefined ) {
+
+$.mobile.transitionFallbacks.flow = "fade";
+
+})( jQuery, this );
