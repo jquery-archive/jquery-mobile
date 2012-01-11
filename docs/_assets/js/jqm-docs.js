071b65f00db8c9e380a139593167afec5e5635c7
Mat Marquis <mat@matmarquis.com>
2012-01-11 14:53:09 -0500

    Removed themeswitcher in docs.

--- docs/_assets/js/jqm-docs.js
+++ docs/_assets/js/jqm-docs.js
@@ -1,33 +1,6 @@
 //set up the theme switcher on the homepage
 
 define( [ "jquery.mobile" ], function() {
-$('div').live('pagecreate',function(event){
-	if( !$(this).is('.ui-dialog')){
-		var appendEl = $(this).find('.ui-footer:last');
-
-		if( !appendEl.length ){
-			appendEl = $(this).find('.ui-content');
-		}
-
-		if( appendEl.is("[data-position]") ){
-			return;
-		}
-
-		$('<a href="#themeswitcher" data-'+ $.mobile.ns +'rel="dialog" data-'+ $.mobile.ns +'transition="pop">Switch theme</a>')
-			.buttonMarkup({
-				'icon':'gear',
-				'inline': true,
-				'shadow': false,
-				'theme': 'd'
-			})
-			.appendTo( appendEl )
-			.wrap('<div class="jqm-themeswitcher">')
-			.bind( "vclick", function(){
-				$.themeswitcher();
-			});
-	}
-
-});
 
 //collapse page navs after use
 $(function(){
