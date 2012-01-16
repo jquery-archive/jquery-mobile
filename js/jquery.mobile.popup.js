39dd1c4d397a15e8cdcc7627c1a51019124d6ec4
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-01-16 17:09:30 +0200

    [popup] Getting rid of the z-index stuff - using the z-index value from .ui-selectmenu

--- js/jquery.mobile.popup.js
+++ js/jquery.mobile.popup.js
@@ -202,21 +202,7 @@ $.widget("mobile.popup", $.mobile.widget, {
 			var self = this,
 			    coords = this._placementCoords(
 			    	(undefined === x ? window.innerWidth  / 2 : x),
-			    	(undefined === y ? window.innerHeight / 2 : y)),
-				zIndexMax = 0;
-
-            $(document)
-                .find("*")
-                .each(function() {
-                    var el = $(this),
-                        zIndex = parseInt(el.css("z-index"));
-
-                    if (!(el.is(self._ui.container) || el.is(self._ui.screen) || isNaN(zIndex)))
-                        zIndexMax = Math.max(zIndexMax, zIndex);
-                });
-
-            this._ui.screen.css("z-index", (zIndexMax + 1));
-            this._ui.container.css("z-index", (zIndexMax + 2));
+			    	(undefined === y ? window.innerHeight / 2 : y));
 
 			this._ui.screen
 				.height($(document).height())
