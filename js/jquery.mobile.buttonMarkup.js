7a7882288668d250f7b80c635674aefbc878e8ba
Josh Dean <joshua.lee.dean@gmail.com>
2012-01-16 18:23:14 -0600

    Prevent inputs with unnecessary data-role button from being double enhanced

--- js/jquery.mobile.buttonMarkup.js
+++ js/jquery.mobile.buttonMarkup.js
@@ -29,6 +29,9 @@ $.fn.buttonMarkup = function( options ) {
 			buttonText = document.createElement( o.wrapperEls ),
 			buttonIcon = o.icon ? document.createElement( "span" ) : null;
 
+		// if so, prevent double enhancement, and continue with rest of the elements.
+		if( e.tagName === "INPUT" && e.getAttribute( "data-role" ) === "button" ) continue;
+
 		// if this is a button, check if it's been enhanced and, if not, use the right function
 		if( e.tagName === "BUTTON" ) {
 	 	 	if ( !$( e.parentNode ).hasClass( "ui-btn" ) ) $( e ).button();
