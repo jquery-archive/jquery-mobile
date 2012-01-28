b15815d5d81db02e7947a0d1b42b26f0b66cce08
Ghislain Seguin <ghislain@gmail.com>
2012-01-27 23:27:13 -0800

    Ensure module paths are relative

--- js/jquery.mobile.docs.js
+++ js/jquery.mobile.docs.js
@@ -2,5 +2,5 @@
 //>>description: jQuery Mobile documentation JS
 //>>label: Docs
 
-define( [ "order!jquery.mobile", "order!../docs/_assets/js/jqm-docs" ] );
+define( [ "./order!./jquery.mobile", "./order!../docs/_assets/js/jqm-docs" ] );
 //>>excludeEnd("jqmBuildExclude");
