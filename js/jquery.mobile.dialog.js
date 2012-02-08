7acf03d6737dcda793f3195aadaec0d79a9a0ff3
John Bender <john.m.bender@gmail.com>
2012-02-08 10:21:18 -0800

    move dialog to enhanceWithin

--- js/jquery.mobile.dialog.js
+++ js/jquery.mobile.dialog.js
@@ -89,7 +89,7 @@ $.widget( "mobile.dialog", $.mobile.widget, {
 
 //auto self-init widgets
 $( document ).delegate( $.mobile.dialog.prototype.options.initSelector, "pagecreate", function(){
-	$( this ).dialog();
+	$.mobile.dialog.prototype.enhanceWithin( this );
 });
 
 })( jQuery, this );
