cbe432016e8bfce952dfb979b4c77f20da9e0473
Mat Marquis <mat@matmarquis.com>
2012-03-02 19:00:50 -0500

    Fixes #1459 — Clicking on a search input’s “clear” button now returns focus to the search input.

--- js/jquery.mobile.forms.textinput.js
+++ js/jquery.mobile.forms.textinput.js
@@ -49,9 +49,11 @@ $.widget( "mobile.textinput", $.mobile.widget, {
 
 			focusedEl = input.wrap( "<div class='ui-input-search ui-shadow-inset ui-btn-corner-all ui-btn-shadow ui-icon-searchfield" + themeclass + miniclass + "'></div>" ).parent();
 			clearbtn = $( "<a href='#' class='ui-input-clear' title='clear text'>clear text</a>" )
-				.tap(function( event ) {
-					input.val( "" ).focus();
-					input.trigger( "change" );
+				.bind('click', function( event ) {
+					input
+						.val( "" )
+						.focus()
+						.trigger( "change" );
 					clearbtn.addClass( "ui-input-clear-hidden" );
 					event.preventDefault();
 				})
