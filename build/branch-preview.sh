315e281340f3556d6f02aa5b1aeea05e0a51c4b8
John Bender <john.m.bender@gmail.com>
2012-03-06 14:14:08 -0800

    quote directory refs to avoid issues with spaces

--- build/branch-preview.sh
+++ build/branch-preview.sh
@@ -14,15 +14,15 @@ function log {
 }
 
 # Make the output directory if it doesnt exist
-mkdir -p $output
+mkdir -p "$output"
 
 branches=`git ls-remote --heads origin | cut -f2 -s | sed 's@refs/heads/@@'`
 existing_branch_dirs=`ls -l --full-time $output | grep "^d" | awk '{ print $9 }' | xargs`
 
-echo "<html><head><title>jQm Branches Preview</title></head><body>" > $index_page
-echo "<h1>jQuery Mobile Branches Live Previews</h1><hr />" >> $index_page
-echo "<span class='date'>Updated: $(date)</span>" >> $index_page
-echo "<ul>" >> $index_page
+echo "<html><head><title>jQm Branches Preview</title></head><body>" > "$index_page"
+echo "<h1>jQuery Mobile Branches Live Previews</h1><hr />" >> "$index_page"
+echo "<span class='date'>Updated: $(date)</span>" >> "$index_page"
+echo "<ul>" >> "$index_page"
 # Loop through the array to export each branch
 for branch in $branches; do
 
@@ -37,19 +37,19 @@ for branch in $branches; do
   # $branch = escapeshellarg($branch);
 
   log "checking out $branch into $output/$branch/"
-  git checkout-index -a -f --prefix=$output/$branch/
+  git checkout-index -a -f --prefix="$output/$branch/"
 
   # Manipulate the commit message
-  echo "<li>Branch: <a href='$branch/index.html'>$branch</a></li>" >> $index_page
+  echo "<li>Branch: <a href='$branch/index.html'>$branch</a></li>" >> "$index_page"
 done
 
 # close out the list
-echo "</ul>" >> $index_page
+echo "</ul>" >> "$index_page"
 
 # close out the index file
-echo "</body></html>" >> $index_page
+echo "</body></html>" >> "$index_page"
 
 for dir in $existing_branch_dirs; do
   log "removing old branch from $output: $dir"
-  rm -r $output/$dir
+  rm -r "$output/$dir"
 done
