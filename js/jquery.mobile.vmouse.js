9856ba36805dcc3b2decddf02e2da969c5bc815c
jblas@adobe.com <jblas@adobe.com>
2012-03-14 11:09:33 -0700

    Fix for issue #3815 - vclick events don't have pageX/pageY defined on them
    - Modified createVirtualEvent() so that it uses the mouseHook properties if the original event type starts with "mouse" or "click". The original regex was only looking for "mouse".

--- js/jquery.mobile.vmouse.js
+++ js/jquery.mobile.vmouse.js
@@ -68,7 +68,7 @@ function createVirtualEvent( event, eventType ) {
 
 	// addresses separation of $.event.props in to $.event.mouseHook.props and Issue 3280
 	// https://github.com/jquery/jquery-mobile/issues/3280
-	if ( t.search(/mouse/) >-1 ) {
+	if ( t.search( /^(mouse|click)/ ) > -1 ) {
 		props = mouseEventProps;
 	}
 
