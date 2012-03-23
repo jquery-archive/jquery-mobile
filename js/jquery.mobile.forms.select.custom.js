b2bb230269843ef054bfc1d0df3af067f0dd93a5
John Bender <john.m.bender@gmail.com>
2012-03-23 15:50:10 -0700

    misplaced semi, whitespace, comment correction

--- js/jquery.mobile.forms.select.custom.js
+++ js/jquery.mobile.forms.select.custom.js
@@ -54,7 +54,7 @@ define( [
 			headerTitle = $( "<h1>", {
 				"class": "ui-title"
 			}).appendTo( header ),
-			
+
 			menuPageContent,
 			menuPageClose,
 			headerClose;
@@ -116,7 +116,7 @@ define( [
 						$( e.target )
 							.attr( "tabindex", "0" )
 							.trigger( "vmouseover" );
-						
+
 					})
 					.bind( "focusout", function( e ){
 						$( e.target )
@@ -162,7 +162,7 @@ define( [
 							// up or left arrow keys
 						 case 38:
 							prev = li.prev().not( ".ui-selectmenu-placeholder" );
-							
+
 							if( prev.is( ".ui-li-divider" ) ) {
 								prev = prev.prev();
 							}
@@ -182,7 +182,7 @@ define( [
 							// down or right arrow keys
 						 case 40:
 							next = li.next();
-							
+
 							if( next.is( ".ui-li-divider" ) ) {
 								next = next.next();
 							}
@@ -236,7 +236,7 @@ define( [
 				});
 
 				// Close button on small overlays
-				if( self.isMultiple ){ 
+				if( self.isMultiple ){
 					self.headerClose.click( function() {
 						if ( self.menuType == "overlay" ) {
 							self.close();
@@ -354,11 +354,11 @@ define( [
 				}
 
 				if ( menuHeight > screenHeight - 80 || !$.support.scrollTop ) {
-					
-					self.menuPage.appendTo( $.mobile.pageContainer ).page();					
+
+					self.menuPage.appendTo( $.mobile.pageContainer ).page();
 					self.menuPageContent = menuPage.find( ".ui-content" );
 					self.menuPageClose = menuPage.find( ".ui-header a" );
-					
+
 					// prevent the parent page from being removed from the DOM,
 					// otherwise the results of selecting a list item in the dialog
 					// fall into a black hole
@@ -445,30 +445,30 @@ define( [
 				self.list.empty().filter( ".ui-listview" ).listview( "destroy" );
 
 				var $options = self.select.find("option"),
-					numOptions = $options.length,                      
-					select = this.select[ 0 ],                         
-					dataPrefix = 'data-' + $.mobile.ns,                 
-					dataIndexAttr = dataPrefix + 'option-index', 
+					numOptions = $options.length,
+					select = this.select[ 0 ],
+					dataPrefix = 'data-' + $.mobile.ns,
+					dataIndexAttr = dataPrefix + 'option-index',
 					dataIconAttr = dataPrefix + 'icon',
 					dataRoleAttr = dataPrefix + 'role',
 					fragment = document.createDocumentFragment(),
 					optGroup;
-									
-				for (var i = 0; i < numOptions;i++){				
+
+				for (var i = 0; i < numOptions;i++){
 					var option = $options[i],
 						$option = $(option),
 						parent = option.parentNode,
-						text = $option.text(),			
-						anchor  = document.createElement('a');
-						classes = [];				
-					
-					anchor.setAttribute('href','#');							
-					anchor.appendChild(document.createTextNode(text));	
-					
-					// Are we inside an optgroup?									
+						text = $option.text(),
+						anchor  = document.createElement('a'),
+						classes = [];
+
+					anchor.setAttribute('href','#');
+					anchor.appendChild(document.createTextNode(text));
+
+					// Are we inside an optgroup?
 					if (parent !== select && parent.nodeName.toLowerCase() === "optgroup"){
 						var optLabel = parent.getAttribute('label');
-						if ( optLabel != optGroup) {						
+						if ( optLabel != optGroup) {
 							var divider = document.createElement('li');
 							divider.setAttribute(dataRoleAttr,'list-divider');
 							divider.setAttribute('role','option');
@@ -489,19 +489,19 @@ define( [
 						}
 					}
 
-					var item = document.createElement('li');															
+					var item = document.createElement('li');
 					if ( option.disabled ) {
 						classes.push( "ui-disabled" );
 						item.setAttribute('aria-disabled',true);
 					}
 					item.setAttribute(dataIndexAttr,i);
-					item.setAttribute(dataIconAttr,dataIcon);					
+					item.setAttribute(dataIconAttr,dataIcon);
 					item.className = classes.join(" ");
 					item.setAttribute('role','option');
 					anchor.setAttribute('tabindex','-1');
-					item.appendChild(anchor);					
+					item.appendChild(anchor);
 					fragment.appendChild(item);
-				}	
+				}
 
 				self.list[0].appendChild(fragment);
 
@@ -531,7 +531,7 @@ define( [
 		});
 	};
 
-	// issue #3894 - core doesn't trigger events to disabled delegates
+	// issue #3894 - core doesn't triggered events on disabled delegates
 	$( document ).bind( "selectmenubeforecreate", function( event ){
 		var selectmenuWidget = $( event.target ).data( "selectmenu" );
 
