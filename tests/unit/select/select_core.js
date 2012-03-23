3862dc52949d25d4258a9b577a6a961fd68427f4
John Bender <john.m.bender@gmail.com>
2012-03-23 16:27:09 -0700

    test that disabed non native selects are enhanced

--- tests/unit/select/select_core.js
+++ tests/unit/select/select_core.js
@@ -377,4 +377,11 @@
 			start
 		]);
 	});
+
+	test( "a disabled custom select should still be enhanced as custom", function() {
+		$("#select-disabled-enhancetest").selectmenu("enable").siblings("a").click();
+
+		var menu = $(".ui-selectmenu").not( ".ui-selectmenu-hidden" );
+		ok( menu.text().indexOf("disabled enhance test") > -1, "the right select is showing" );
+	});
 })(jQuery);
