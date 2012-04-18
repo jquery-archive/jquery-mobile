49155c214939b29449de770b911cc540877eb862
Ghislain Seguin <ghislain@gmail.com>
2012-04-18 15:59:38 -0700

    Revert "Merge pull request #4073 from gabrielschulhof/fix-dialog-double-hash"
    This reverts commit 379ba6d87fe3df3b7ff8cf1868bf04eb94332ba2.

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -1041,7 +1041,7 @@ define( [
 			// be an empty string. Moving the undefined -> empty string back into
 			// urlHistory.addNew seemed imprudent given undefined better represents
 			// the url state
-			url = ( active.url || "" ).replace(dialogHashKey, "") + dialogHashKey;
+			url = ( active.url || "" ) + dialogHashKey;
 		}
 
 		// Set the location hash.
