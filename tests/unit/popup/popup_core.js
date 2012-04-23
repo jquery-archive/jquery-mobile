4ee8aef0d012e22fbb76acaa2b91e0281d1b60ae
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-04-23 22:04:03 +0300

    [popup unit tests] Some basic tests

---
+++ tests/unit/popup/popup_core.js
@@ -0,0 +1,39 @@
+/*
+ * mobile popup unit tests
+ */
+(function($){
+
+	test( "Popup div is associated with a popup widget", function() {
+		ok( $("#test-popup" ).data( "popup" ) );
+	});
+
+	test( "Popup div parent has class ui-popup-container", function() {
+		ok( $("#test-popup" ).parent().hasClass( "ui-popup-container" ) );
+	});
+
+	test( "Popup div grandparent is the page", function() {
+		ok( $("#test-popup" ).parent().parent().hasClass( "ui-page" ) );
+	});
+
+	test( "Popup div is preceded by its screen", function() {
+		ok(
+			$( "#test-popup" ).parent().prev().hasClass( "ui-selectmenu-screen" ) &&
+			$( "#test-popup" ).parent().prev().hasClass( "ui-popup-screen" ) );
+	});
+
+	asyncTest( "Popup opens and closes", function() {
+		$( "#test-popup" ).popup( "open" );
+		setTimeout(function() {
+			ok( $( "#test-popup" ).parent().hasClass( "in" ), "Open popup container has class 'in'" );
+			ok( !$( "#test-popup" ).parent().prev().hasClass( "ui-screen-hidden" ), "Open popup screen is not hidden" );
+			ok( $( "#test-popup" ).parent().attr( "class" ).match( /( |^)ui-body-[a-z]( |$)/ ), "Open popup has a valid overlay theme" );
+			$( "#test-popup" ).popup( "close" );
+			setTimeout(function() {
+				ok( !$( "#test-popup" ).parent().hasClass( "in" ), "Closed popup container does not have class 'in'" );
+				ok( $( "#test-popup" ).parent().prev().hasClass( "ui-screen-hidden" ), "Closed popup screen is hidden" );
+				start();
+			}, 1000);
+		}, 1000);
+	});
+
+})( jQuery );
