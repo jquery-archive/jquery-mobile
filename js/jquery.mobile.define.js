4a20ca276c6ed69297534d4949509c910b11dfe1
Ghislain Seguin <ghislain@gmail.com>
2012-06-07 15:05:30 -0700

    Removed builder metadata as this should not even appear in the builder page

--- js/jquery.mobile.define.js
+++ js/jquery.mobile.define.js
@@ -1,9 +1,3 @@
-//>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
-//>>description: Creates the define method on window, for async loading
-//>>label: Define
-//>>group: exclude
-//>>excludeEnd("jqmBuildExclude");
-
 // creates the define method on window, only used where async loading
 // is not desired in the docs and experiments
 window.define = function(){
