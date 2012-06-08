36acd089630f7e55cea44ebd86e718329e731026
Ghislain Seguin <ghislain@gmail.com>
2012-06-08 15:01:08 -0700

    Fix for #4504

--- js/jquery.mobile.page.sections.js
+++ js/jquery.mobile.page.sections.js
@@ -68,7 +68,7 @@ $( document ).bind( "pagecreate", function( e ) {
 				$page.jqmData( "url" ) !== $.mobile.path.stripHash( location.hash ) &&
 				!leftbtn ) {
 
-				backBtn = $( "<a href='#' class='ui-btn-left' data-"+ $.mobile.ns +"rel='back' data-"+ $.mobile.ns +"icon='arrow-l'>"+ o.backBtnText +"</a>" )
+				backBtn = $( "<a href='javascript:void(0);' class='ui-btn-left' data-"+ $.mobile.ns +"rel='back' data-"+ $.mobile.ns +"icon='arrow-l'>"+ o.backBtnText +"</a>" )
 					// If theme is provided, override default inheritance
 					.attr( "data-"+ $.mobile.ns +"theme", o.backBtnTheme || thisTheme )
 					.prependTo( $this );
