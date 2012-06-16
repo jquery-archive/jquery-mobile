d89a919fb0f5e902934d0179b73baacb0cc4fc2f
Jasper de Groot <mail@ugomobi.com>
2012-06-16 16:35:19 +0200

    Merge pull request #4462 from Diveboard/master
    slide flickering within a ios app webview (i.e. phonegap container)

--- js/jquery.mobile.transition.js
+++ js/jquery.mobile.transition.js
@@ -70,11 +70,14 @@ var createHandler = function( sequential ){
 				
 				startIn();
 			},
-			
-			startIn = function(){	
-			
-				$to.addClass( $.mobile.activePageClass );				
-			
+
+			startIn = function(){
+
+				//prevent flickering in phonegap container
+				$to.css("z-index", -10);
+
+				$to.addClass( $.mobile.activePageClass + toPreClass );
+
 				// Send focus to page as it is now display: block
 				$.mobile.focusPage( $to );
 
@@ -82,7 +85,10 @@ var createHandler = function( sequential ){
 				$to.height( screenHeight + toScroll );
 				
 				scrollPage();
-				
+
+				//restores visibility of the new page
+				$to.css("z-index", "");
+
 				if( !none ){
 					$to.animationComplete( doneIn );
 				}
