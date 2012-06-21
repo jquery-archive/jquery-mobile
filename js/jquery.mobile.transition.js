470706892449902c6236daef472377d709440185
Jasper de Groot <mail@ugomobi.com>
2012-06-21 21:33:21 +0200

    Merge pull request #4462 from Diveboard/master

--- js/jquery.mobile.transition.js
+++ js/jquery.mobile.transition.js
@@ -71,11 +71,14 @@ var createHandler = function( sequential ){
 				
 				startIn();
 			},
-			
-			startIn = function(){	
-			
-				$to.addClass( $.mobile.activePageClass + toPreClass );				
-			
+
+			startIn = function(){
+
+				//prevent flickering in phonegap container
+				$to.css("z-index", -10);
+
+				$to.addClass( $.mobile.activePageClass + toPreClass );
+
 				// Send focus to page as it is now display: block
 				$.mobile.focusPage( $to );
 
@@ -84,6 +87,9 @@ var createHandler = function( sequential ){
 				
 				scrollPage();
 				
+				//restores visibility of the new page
+				$to.css("z-index", "");
+
 				if( !none ){
 					$to.animationComplete( doneIn );
 				}
