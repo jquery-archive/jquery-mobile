7aabc88e0fc7e4977d54eac2631ddb31fb4a669b
Jasper de Groot <mail@ugomobi.com>
2012-06-26 23:37:46 +0200

    Popup map with close button example

--- docs/_assets/js/jqm-docs.js
+++ docs/_assets/js/jqm-docs.js
@@ -70,3 +70,19 @@ if ( location.protocol.substr(0,4)  === 'file' ||
     });
   });
 }
+
+// popup Map example
+function initialize() {
+	var myLatlng = new google.maps.LatLng(51.520838,-0.140261);
+	var myOptions = {
+		zoom: 15,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	var marker = new google.maps.Marker({
+		position: myLatlng, 
+		map: map,
+		title:"Where I work!"
+	});  
+}
