edf9c70c8daa343bdcbd50f5e5890f0abd69475c
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-07-15 14:45:44 +0300

    [custom select menu unit tests] More verbose messages in test to hunt down build failure

--- tests/unit/select/select_core.js
+++ tests/unit/select/select_core.js
@@ -191,7 +191,7 @@
 			},
 
 			function() {
-				ok( menu.find( ":focus" ).length > 0, "item in open select menu has focus" );
+				same( menu.find( ":focus" ).length, 1, "item in open select menu (" + menu.length + ") has focus" );
 				select.selectmenu( "close" );
 			},
 
