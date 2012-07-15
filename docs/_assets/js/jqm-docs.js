07a30fb0138f34a880ca7c9c9427319d1f976eda
Jasper de Groot <mail@ugomobi.com>
2012-07-16 01:56:11 +0200

    Docs (popup): Improved settings for photo and video examples.

--- docs/_assets/js/jqm-docs.js
+++ docs/_assets/js/jqm-docs.js
@@ -76,7 +76,7 @@ $( document ).on( "pageinit", function() {
 	$( "#popupPhotoPortrait, #popupPhotoLandscape" ).on({
 		popupbeforeopen: function( event ) {
 			var inner = $( window ).height() - 62 + "px";
-			$( ".ui-popup > img" ).css( "height", inner );
+			$( ".popphoto" ).css( "max-height", inner );
 		}
 	});
 });
