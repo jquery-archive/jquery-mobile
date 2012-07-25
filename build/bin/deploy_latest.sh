89f39e063f1b8fee8f61d2206b69c2925d39e96b
John Bender <john.m.bender@gmail.com>
2012-07-25 10:30:03 -0700

    handle directories again

--- build/bin/deploy_latest.sh
+++ build/bin/deploy_latest.sh
@@ -1,4 +1,4 @@
 source build/bin/config.sh
 
 # Push the latest git version to the CDN. This is done on a post commit hook
-cp $OUTPUT/* /var/www/html/code.jquery.com/mobile/latest/
+cp -r $OUTPUT/* /var/www/html/code.jquery.com/mobile/latest/
