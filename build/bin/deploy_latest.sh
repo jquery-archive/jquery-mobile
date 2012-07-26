b1c3163572eb2c5aaaa32e66d17154a1485c83c8
John Bender <john.m.bender@gmail.com>
2012-07-26 12:05:37 -0700

    revert refresh until the issues are sorted out

--- build/bin/deploy_latest.sh
+++ build/bin/deploy_latest.sh
@@ -2,7 +2,3 @@ source build/bin/config.sh
 
 # Push the latest git version to the CDN. This is done on a post commit hook
 cp -r $OUTPUT/* /var/www/html/code.jquery.com/mobile/latest/
-
-for file in `ls $OUTPUT/jquery.mobile*`; do
-  curl -s "http://origin.code.jquery.com/mobile/latest/$file";
-done
