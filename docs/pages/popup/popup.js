a8ab9838dbd0ee2d75b75cbf21ef9ef810522466
Jasper de Groot <mail@ugomobi.com>
2012-07-31 14:10:39 +0200

    Docs (popup): update examples page

--- docs/pages/popup/popup.js
+++ docs/pages/popup/popup.js
@@ -1,81 +1,84 @@
 // popup examples
 $( document ).on( "pageinit", function() {
 
-	// scale images	
 	$( ".photopopup" ).on({
 		popupbeforeposition: function() {
-			var inner = $( window ).height() - 60 + "px";
-			$( "img, .photopopup" ).css( "max-height", inner );
+			var maxHeight = $( window ).height() - 60 + "px";
+			$( ".photopopup img" ).css( "max-height", maxHeight );
 		}
 	});
 
-	// set the size of iframes
-	$( "iframe" )
-		.prop( "width", 0 )
-		.prop( "height", "auto" );
-		$( "#popupMap iframe" ).contents().find( "#map_canvas" ).css( { "width" : 0, "height" : 0 } );
-			
-	function sizes( iframewidth, iframeheight, padding, border ) {
-		var sw = $( window ).width() - 30,
-			sh = $( window ).height() - 30,
-			ip = 2 * padding,
-			ib = 2 * border,
-			iw = iframewidth + ip + ib,
-			ih = iframeheight + ip + ib,
-			h, w, width, height;
+	function scale( width, height, padding, border ) {
+		var scrWidth = $( window ).width() - 30,
+			scrHeight = $( window ).height() - 30,
+			ifrPadding = 2 * padding,
+			ifrBorder = 2 * border,
+			ifrWidth = width + ifrPadding + ifrBorder,
+			ifrHeight = height + ifrPadding + ifrBorder,
+			h, w;
 
-		if ( iw < sw && ih < sh ) {
-			w = iw;
-			h = ih;
-		} else if ( ( iw / sw ) > ( ih / sh ) ) {
-			w = sw;
-			h = ( sw / iw ) * ih;
+		if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
+			w = ifrWidth;
+			h = ifrHeight;
+		} else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
+			w = scrWidth;
+			h = ( scrWidth / ifrWidth ) * ifrHeight;
 		} else {
-			h = sh;
-			w = ( sh / ih ) * iw;
+			h = scrHeight;
+			w = ( scrHeight / ifrHeight ) * ifrWidth;
 		}
 		
-		width = w - ( ip + ib );
-		height = h - ( ip + ib );
-		
 		return {
-			'width': width,
-			'height': height
+			'width': w - ( ifrPadding + ifrBorder ),
+			'height': h - ( ifrPadding + ifrBorder )
 		};
 	};
-	
+
+	$( ".ui-popup iframe" )
+		.prop( "width", 0 )
+		.prop( "height", "auto" );
+	 
+	$( "#popupVideo" ).on({
+		popupbeforeposition: function() {
+			// call our custom function scale() to get the width and height 
+			var size = scale( 497, 298, 15, 1 ),
+				w = size.width,
+				h = size.height;
+
+			$( "#popupVideo iframe" )
+				.prop( "width", w )
+				.prop( "height", h );
+		},
+		popupafterclose: function() {
+			$( "#popupVideo iframe" )
+				.prop( "width", 0 )
+				.prop( "height", 0 );	
+		}
+	});
+
+	$( "#popupMap iframe" ).contents().find( "#map_canvas" )
+		.css( { "width" : 0, "height" : 0 } );
+	 		 
 	$( "#popupMap" ).on({
 		popupbeforeposition: function() {
-			var size = sizes(480, 320, 0, 1),
+			var size = scale( 480, 320, 0, 1 ),
 				w = size.width,
 				h = size.height;
 
 			$( "#popupMap iframe" )
 				.prop( "width", w )
 				.prop( "height", h );
-			$( "#popupMap iframe" ).contents().find( "#map_canvas" ).css( { "width": w, "height" : h } );
+					 
+			$( "#popupMap iframe" ).contents().find( "#map_canvas" )
+				.css( { "width": w, "height" : h } );
 		},
 		popupafterclose: function() {
-			$("#popupMap iframe")
+			$( "#popupMap iframe" )
 				.prop( "width", 0 )
 				.prop( "height", 0 );
-			$( "#popupMap iframe" ).contents().find( "#map_canvas" ).css( { "width": 0, "height" : 0 } );
-		}
-	});
-	$( "#popupVideo" ).on({
-		popupbeforeposition: function() {
-			var size = sizes(497, 298, 15, 1),
-				w = size.width,
-				h = size.height;
-				
-			$( "#popupVideo iframe" )
-				.prop( "width", w )
-				.prop( "height", h );
-		},
-		popupafterclose: function() {
-			$("#popupVideo iframe")
-				.prop( "width", 0 )
-				.prop( "height", 0 );	
+					 
+			$( "#popupMap iframe" ).contents().find( "#map_canvas" )
+				.css( { "width": 0, "height" : 0 } );
 		}
 	});
 });
