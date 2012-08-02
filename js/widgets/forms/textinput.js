779dd6cfc9fe52817cabf0ec9c133c8a8ff54d3f
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-08-02 22:54:17 +0300

    Revert "[textinput] Move _keyup function into the widget prototype and attach it with ._on() to ensure removal upon destroy()"
    This reverts commit 34220031ce30e80a47e7fd49a22f4bfac6ec0733.

--- js/widgets/forms/textinput.js
+++ js/widgets/forms/textinput.js
@@ -105,23 +105,33 @@ $.widget( "mobile.textinput", $.mobile.widget, {
 
 		// Autogrow
 		if ( input.is( "textarea" ) ) {
-			var keyupTimeoutBuffer = 100,
+			var extraLineHeight = 15,
+				keyupTimeoutBuffer = 100,
 				keyupTimeout;
+
+			this._keyup = function() {
+				var scrollHeight = input[ 0 ].scrollHeight,
+					clientHeight = input[ 0 ].clientHeight;
+
+				if ( clientHeight < scrollHeight ) {
+					input.height(scrollHeight + extraLineHeight);
+				}
+			};
 
 			input.keyup(function() {
 				clearTimeout( keyupTimeout );
-				keyupTimeout = setTimeout( $.proxy( self, "_keyup" ), keyupTimeoutBuffer );
+				keyupTimeout = setTimeout( self._keyup, keyupTimeoutBuffer );
 			});
 
 			// binding to pagechange here ensures that for pages loaded via
 			// ajax the height is recalculated without user input
-			$( document ).one( "pagechange", $.proxy( this, "_keyup" ) );
+			$( document ).one( "pagechange", this._keyup );
 
 			// Issue 509: the browser is not providing scrollHeight properly until the styles load
 			if ( $.trim( input.val() ) ) {
 				// bind to the window load to make sure the height is calculated based on BOTH
 				// the DOM and CSS
-				this._on( window, { load: "_keyup" } );
+				$( window ).load( this._keyup );
 			}
 		}
 		if ( input.attr( "disabled" ) ) {
@@ -129,15 +139,8 @@ $.widget( "mobile.textinput", $.mobile.widget, {
 		}
 	},
 
-	// Used with a textarea to recalculate its height
-	_keyup: function() {
-		var scrollHeight = this.element[ 0 ].scrollHeight,
-			clientHeight = this.element[ 0 ].clientHeight,
-			extraLineHeight = 15;
-
-		if ( clientHeight < scrollHeight ) {
-			this.element.height( scrollHeight + extraLineHeight );
-		}
+	_destroy: function() {
+		$( window ).unbind( "load", this._keyup );
 	},
 
 	disable: function() {
