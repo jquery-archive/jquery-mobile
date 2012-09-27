0c038b73a4233e6a06a2d98f95117943cf034b4c
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-09-27 20:10:18 +0300

    [sequence tests] Moving to detailedEventSequence

--- tests/unit/navigation/sequence-tests/sequence_tests.js
+++ tests/unit/navigation/sequence-tests/sequence_tests.js
@@ -5,13 +5,26 @@
 	asyncTest( "Returning from a dialog results in the page from which it opened", function() {
 		expect( 2 );
 
-		$.testHelper.pageSequence([
+		$.testHelper.detailedEventCascade([
+			function() {
+			},
+			{
+				navigate: { src: $( document ), event: "navigate.returningFromADialogStep0" }
+			},
 			function() {
 				$( "#openBasicDialog" ).click();
+			},
+			{
+				navigate: { src: $( document ), event: "hashchange.returningFromADialogStep1" },
+				pagechange: { src: $.mobile.pageContainer, event: "pagechange.returningFromADialogStep1" }
 			},
 			function() {
 				ok( $.mobile.activePage.attr( "id" ) === "basicDialog", "Basic dialog has opened" );
 				$( "a", $.mobile.activePage[ 0 ] ).click();
+			},
+			{
+				navigate: { src: $( document ), event: "hashchange.returningFromADialogStep2" },
+				pagechange: { src: $.mobile.pageContainer, event: "pagechange.returningFromADialogStep2" }
 			},
 			function() {
 				ok( $.mobile.activePage.attr( "id" ) === "basicTestPage", "Active page is original page" );
