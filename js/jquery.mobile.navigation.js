fd8680a6cc7f7a94f548e8bbf99dcbca21fac9a9
John Bender <john.m.bender@gmail.com>
2012-10-12 15:09:35 -0700

    disable base tag reset on stale dialog url

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -778,11 +778,6 @@ define( [
 			}
 		}
 
-		// Reset base to the default document base.
-		if ( $.support.dynamicBaseTag ) {
-			base.reset();
-		}
-
 		// If the page we are interested in is already in the DOM,
 		// and the caller did not indicate that we should force a
 		// reload of the file, we are done. Otherwise, track the
