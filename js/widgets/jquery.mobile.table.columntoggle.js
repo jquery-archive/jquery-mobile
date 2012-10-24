85e8a32c2704e4169c269da7d7daae4950619335
Scott Jehl <scott@scottjehl.com>
2012-10-24 15:42:26 -0500

    woops that should've been ===

--- js/widgets/jquery.mobile.table.columntoggle.js
+++ js/widgets/jquery.mobile.table.columntoggle.js
@@ -27,57 +27,57 @@ $( document ).delegate( ":jqmData(role='table')", "tablecreate", function() {
       self = $table.jqmData( "table" ),
       o = self.options;
 
-   if( o.mode !== "columntoggle" ){
+   if( o.mode === "columntoggle" ){
+      return;
+   }
 
-      var id = ( $table.attr( "id" ) || self.options.classes.popup ) + "-popup", //TODO BETTER FALLBACK ID HERE
-         $menuButton = $( "<a href='#" + id + "' class='" + o.classes.btn + "' data-rel='popup'>" + o.columnBtnText + "</a>" ),
-         $popup = $( "<div data-role='popup' data-role='fieldcontain' class='" + o.classes.popup + "' id='" + id + "'></div>"),
-         $menu = $("<fieldset data-role='controlgroup'></fieldset>").appendTo( $popup );
+   var id = ( $table.attr( "id" ) || self.options.classes.popup ) + "-popup", //TODO BETTER FALLBACK ID HERE
+      $menuButton = $( "<a href='#" + id + "' class='" + o.classes.btn + "' data-rel='popup'>" + o.columnBtnText + "</a>" ),
+      $popup = $( "<div data-role='popup' data-role='fieldcontain' class='" + o.classes.popup + "' id='" + id + "'></div>"),
+      $menu = $("<fieldset data-role='controlgroup'></fieldset>").appendTo( $popup );
 
-      // create the hide/show toggles
-      self._headers.each(function(i){
+   // create the hide/show toggles
+   self._headers.each(function(i){
 
-         var priority = $( this ).jqmData( "priority" ),
-            $cells = $( this ).add( $( this ).jqmData( "cells" ) );
+      var priority = $( this ).jqmData( "priority" ),
+         $cells = $( this ).add( $( this ).jqmData( "cells" ) );
 
-         if( priority !== "persist" ){
+      if( priority !== "persist" ){
 
-            $cells.addClass( o.classes.priorityPrefix + priority );
+         $cells.addClass( o.classes.priorityPrefix + priority );
 
-            $("<label><input type='checkbox' checked />" + $( this ).text() + "</label>" )
-               .appendTo( $menu )
-               .children( 0 )
-               .jqmData( "cells", $cells )
-               .checkboxradio();
-         }
-      });
+         $("<label><input type='checkbox' checked />" + $( this ).text() + "</label>" )
+            .appendTo( $menu )
+            .children( 0 )
+            .jqmData( "cells", $cells )
+            .checkboxradio();
+      }
+   });
 
-      $menuButton
-         .insertBefore( $table )
-         .buttonMarkup();
+   $menuButton
+      .insertBefore( $table )
+      .buttonMarkup();
 
-      $popup
-         .insertBefore( $table )
-         .popup();
+   $popup
+      .insertBefore( $table )
+      .popup();
 
-      // bind change event listeners to inputs - TODO: move to a private method?
-      $menu.on( "change", "input", function( e ){
-         if( this.checked ){
-            $( this ).jqmData( "cells" ).removeClass( "ui-table-cell-hidden" ).addClass( "ui-table-cell-visible" );
-         }
-         else {
-            $( this ).jqmData( "cells" ).removeClass( "ui-table-cell-visible" ).addClass( "ui-table-cell-hidden" );
-         }
+   // bind change event listeners to inputs - TODO: move to a private method?
+   $menu.on( "change", "input", function( e ){
+      if( this.checked ){
+         $( this ).jqmData( "cells" ).removeClass( "ui-table-cell-hidden" ).addClass( "ui-table-cell-visible" );
+      }
+      else {
+         $( this ).jqmData( "cells" ).removeClass( "ui-table-cell-visible" ).addClass( "ui-table-cell-hidden" );
+      }
+   } );
+
+   $( window ).on( "throttledresize", function(){
+      $menu.find( "input" ).each( function(){
+            this.checked = $( this ).jqmData( "cells" ).eq(0).is( ":visible" );
+            $( this ).checkboxradio( "refresh" );
       } );
-
-      $( window ).on( "throttledresize", function(){
-         $menu.find( "input" ).each( function(){
-               this.checked = $( this ).jqmData( "cells" ).eq(0).is( ":visible" );
-               $( this ).checkboxradio( "refresh" );
-         } );
-      });
-
-   } // if mode !== toggle
+   });
 
 });
 
