0c449121278c3711d376da97aa78e80663d58752
Jeffrey Lembeck <jlembeck@gmail.com>
2012-10-25 10:23:22 -0700

    Changed urlParseRE to ignore space at beginning. This is expected behavior in browsers. This used to result in pages changing to "%20destination.html" instead of the now "destination.html". Fixes issue #4882

--- tests/unit/navigation/navigation_core.js
+++ tests/unit/navigation/navigation_core.js
@@ -535,4 +535,18 @@
 			}
 		]);
 	});
+
+	asyncTest( "external page is accessed correctly even if it has a space in the url", function(){
+		$.testHelper.pageSequence([
+			function(){
+				$.mobile.changePage( " external.html" );
+			},
+
+			function(){
+				ok( $.mobile.activePage.attr( "id" ), "external-test", "the correct page is loaded" );
+				start();
+			}
+		]);
+	});
+	
 })(jQuery);
