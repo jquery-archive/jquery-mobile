967a8b9e1105603119cdfebdb4e69b268f73c27f
Jasper de Groot <mail@ugomobi.com>
2012-11-30 17:39:58 +0100

    Range: coding standards

--- js/widgets/forms/range.js
+++ js/widgets/forms/range.js
@@ -23,23 +23,25 @@ define( [ "jquery", "../../jquery.mobile.core", "../../jquery.mobile.widget", ".
 
 		_create: function() {
 			var inputFirst,inputs,sliders, inputLast, sliderFirst, sliderLast, sliderLastWidth, sliderFirstWidth, label,
-			self = this,
-			$el = self.element,
-			o = this.options;
+				self = this,
+				$el = self.element,
+				o = this.options;
+			
 			inputFirst = $el.find( "input:first" );
 			inputLast = $el.find( "input:last" );
 			inputFirst.addClass( "ui-range-first" );
 			inputLast.addClass( "ui-range-last" );
 			label = $el.find( "label" );
-			$el.append("<div class=\"ui-range-sliders\" />");
-			sliders = $el.find(".ui-range-sliders");
+			$el.append( "<div class=\"ui-range-sliders\" />" );
+			sliders = $el.find( ".ui-range-sliders" );
 			
 			if( o.mini ){
-				label.addClass("ui-mini");
+				label.addClass( "ui-mini" );
 				inputFirst.addClass( "ui-mini" );
 				inputLast.addClass( "ui-mini" );
-				self.element.addClass("ui-mini");
+				self.element.addClass( "ui-mini" );
 			}
+			
 			$el.find( "input" ).slider({
 				theme: o.theme,
 				trackTheme: o.trackTheme,
@@ -47,17 +49,20 @@ define( [ "jquery", "../../jquery.mobile.core", "../../jquery.mobile.widget", ".
 				mini: o.mini,
 				highlight: o.highlight
 			}).slider( "refresh" );
+			
 			$el.addClass( "ui-range" );
 			sliderFirst = $el.find( ".ui-slider:first" ).addClass( "ui-slider-first" );
 			sliderLast = $el.find( ".ui-slider:last" ).addClass( "ui-slider-last" );
-			sliderFirst.appendTo(sliders);
+			sliderFirst.appendTo( sliders );
+			
 			if( o.split ) {
 				$el.addClass( "ui-range-split" );
 			}
-			inputFirst.after("<span class=\"ui-range-dash\">&nbsp;-&nbsp;</span>");
 			
-			sliderLast.appendTo(sliders);
+			inputFirst.after( "<span class=\"ui-range-dash\">&nbsp;-&nbsp;</span>" );
+			sliderLast.appendTo( sliders );
 			label.addClass( "ui-slider" );
+			
 			$.extend( self, {
 				inputFirst: inputFirst,
 				inputLast: inputLast,
@@ -75,33 +80,40 @@ define( [ "jquery", "../../jquery.mobile.core", "../../jquery.mobile.widget", ".
 
 		_bindChangeEvents: function() {
 			this._on( {
-				"change input": function( event ){
+				"change input": function( event ) {
 					var min = parseFloat( this.inputFirst.val(), 10 ),
 						max = parseFloat( this.inputLast.val(), 10 ),
-						first = $(event.target).hasClass("ui-range-first");
-						if( min > max )  {
-							$( event.target ).val(first ? max: min).slider("refresh");
-						}
-						if(first){
-							this.sliderFirstWidth = this.sliderFirst.find( ".ui-slider-bg" ).width();
-						} else {
-							this.sliderLastWidth = this.sliderLast.find( ".ui-slider-bg" ).width();
-						}
-						if( min !== max || !first ) this._updateHighlight();
+						first = $( event.target ).hasClass( "ui-range-first" );
+						
+					if( min > max )  {
+						$( event.target ).val( first ? max: min ).slider( "refresh" );
+					}
+					
+					if( first ) {
+						this.sliderFirstWidth = this.sliderFirst.find( ".ui-slider-bg" ).width();
+					} else {
+						this.sliderLastWidth = this.sliderLast.find( ".ui-slider-bg" ).width();
+					}
+					
+					if( min !== max || !first ) {
+						this._updateHighlight();
+					}
 				}
 			});
 		},
 
 		_bindResize: function() {
 			var self = this,delay;
-			this._on( this.element.closest(".ui-page") , {
-				"pageshow": function(){
+			
+			this._on( this.element.closest( ".ui-page" ), {
+				"pageshow": function() {
 					self.sliderFirstWidth = self.sliderFirst.find( ".ui-slider-bg" ).width();
 					self.sliderLastWidth = self.sliderLast.find( ".ui-slider-bg" ).width();
 				}
 			});
-			this._on( window , {
-				"throttledresize": function(event){
+			
+			this._on( window, {
+				"throttledresize": function( event ) {
 					self.sliderLastWidth = self.sliderLast.find( ".ui-slider-bg" ).width();
 				}
 			});
@@ -110,8 +122,7 @@ define( [ "jquery", "../../jquery.mobile.core", "../../jquery.mobile.widget", ".
 		_bindToggle: function() {
 			this._on( this.element.find( ".ui-slider-handle" ), {
 				"vclick": function( event ){
-					var first = $(event.target).closest( ".ui-slider" ).is( ":first-child" );
-					console.log(first);
+					var first = $( event.target ).closest( ".ui-slider" ).is( ":first-child" );
 					this.sliderFirst.css( "z-index", first ? 1 : "" );
 				}
 			});
@@ -120,10 +131,11 @@ define( [ "jquery", "../../jquery.mobile.core", "../../jquery.mobile.widget", ".
 		_updateHighlight: function() {
 			var newWidth = this.sliderLastWidth - this.sliderFirstWidth,
 				tWidth = this.sliderLast.width();
-				this.element.find( ".ui-slider-bg" ).css({
-					"margin-left": this.sliderFirstWidth / tWidth * 100 + "%",
-					"width": (newWidth) / tWidth * 100 + "%"
-				});
+				
+			this.element.find( ".ui-slider-bg" ).css({
+				"margin-left": this.sliderFirstWidth / tWidth * 100 + "%",
+				"width": ( newWidth ) / tWidth * 100 + "%"
+			});
 		}
 
 	});
