4c3493f10bed91914218a1d6c9fff907e2a59a83
Jeffrey Lembeck <jlembeck@gmail.com>
2012-12-10 10:07:45 -0800

    Fixing a few js bugs

--- js/widgets/panel.js
+++ js/widgets/panel.js
@@ -13,7 +13,8 @@ $.widget( "mobile.panel", $.mobile.widget, {
 	options: {
 		classes: {
 			panel: "ui-panel",
-			contentWrap: "ui-panel-content-wrap"
+			panelOpen: "ui-panel-open",
+			modal: "ui-panel-dismiss"
 		},
 		theme: null,
 		position: "left",
@@ -21,266 +22,169 @@ $.widget( "mobile.panel", $.mobile.widget, {
 		display: "reveal",
 		initSelector: ":jqmData(role='panel')"
 	},
-	_handleLink: function( roleType , callback ){
-		var elId = this.element.attr( "id" ),
-			self = this;
+
+	_panelID: null,
+	_closeLink: null,
+	_page: null,
+	_modal: null,
+
+	_create: function() {
+		var self = this,
+			$el = self.element;
+
+		// expose some private props to other methods
+		self._panelID = $el.attr( "id" );
+		self._closeLink = $el.find( ":jqmData(rel='close')" );
+		self._page = $el.closest( ":jqmData(role='page')" );
+
+		self._addPanelClasses();
+		self._bindCloseEvents();
+		self._bindLinkListeners();
+		self._bindPageEvents();
+
+		if( self.options.dismissible ){
+			self._createModal();
+		}
+
+		self._trigger( "create" );
+	},
+
+	_createModal: function( options ){
+		this._modal = $( "<div class='" + this.options.classes.modal +"' data-panelid='" + this._panelID + "'></div>" )
+			.on( "vclick" , this.close );
+	},
+
+	_addPanelClasses: function(){
+		var $el = this.element,
+			o = this.options,
+			panelClasses = o.classes.panel;
+
+		if( o.theme ){
+			panelClasses += " ui-body-" + o.theme;
+		}
+
+		if( $.support.cssTransform3d ){
+			panelClasses += " ui-panel-transforms";
+		} else {
+			panelClasses +=  " ui-panel-positioning";
+		}
+
+		$el.addClass( panelClasses );
+	},
+
+	_bindCloseEvents: function(){
+		var self = this;
+		self._closeLink.on( "vclick" , function( e ){
+			e.preventDefault();
+			self.close();
+			return false;
+		});
+	},
+
+	_bindLinkListeners: function( roleType , callback ){
+		var self = this;
+
 		$( document ).on( "vclick" , "a", function( e ) {
-			var $link = $( this ),
-				id = $link.attr( "href" ).split( "#" )[1];
-			if( elId === id ){
+			if( this.href.split( "#" )[ 1 ] === self._panelID ){
+				var $link = $( this );
 				e.preventDefault();
-				callback.call( self , $link , id );
+				self.toggle();
 				$link.addClass( $.mobile.activeBtnClass );
+
+				self.element.bind( "open close", function(){
+					$link.removeClass( $.mobile.activeBtnClass );
+				});
+
 				return false;
 			}
 		});
 	},
-	_blockPage: function( options ){
-		var deferred = $.Deferred();
-		var $div = $( "<div>" ),
-			$panel = this,
-			$el = this.element,
-			slideDir = options.position === "left" ? "right" : "left",
-			clickable = options.dismissible,
-			klass = "ui-panel-dismiss",
-			$page = $el.closest( ":jqmData(role='page')" ),
-			$contentsWrap = $page.find( "." + options.classes.contentWrap ),
-			responsiveClasses;
 
-		setTimeout(function(){
-			if( clickable ){
-				responsiveClasses = $panel.element[0].className.match(/ui-responsive-?\w*/g) || [];
-				for( var j = 0, len = responsiveClasses.length; j < len; j++ ){
-					$contentsWrap.addClass( responsiveClasses[ j ] );
-				}
-				$div.addClass( "ui-panel-dismiss-overlay" )
-					.css( "height" , $.mobile.activePage.height() )
-					.css( slideDir , 0 )
-					.attr( "id" , "page-block" )
-					.addClass( klass )
-					.appendTo( $.mobile.activePage );
-				$div.bind( "vclick" , function(){
-					$panel.close();
-				});
-			}
-			deferred.resolve( options );
-		}, 0); // TODO get rid of setTimeout 0 hacks
-		return deferred.promise();
-	},
-	_create: function() {
-		var o = this.options,
-			$el = this.element,
-			$closeLink = $el.find( "[data-rel=close]" ),
-			$page = $el.closest( ":jqmData(role='page')" );
-
-		$el.addClass( o.classes.panel );
-		if( $page.find( "." + o.classes.contentWrap ).length === 0 ){
-			$page.find( ".ui-header, .ui-content, .ui-footer" ).wrapAll( '<div class="' + o.classes.contentWrap + '" />' );
-		}
-		if( o.theme ){
-			$el.addClass( "ui-body-" + o.theme );
-		}
-
-		$page.addClass( $.support.cssTransform3d ? "ui-panel-transforms" : "ui-panel-positioning" );
-
-		this._handleLink( "panel" , function( $link , id ){
-			var options = $.extend( {} , this.options ),
-				op = {
-					position: $link.jqmData( "position" ),
-					dismissible: $link.jqmData( "dismissible" ),
-					display: $link.jqmData( "display" )
-				};
-			for( var i in op ){
-				if( op.hasOwnProperty( i ) && typeof op[ i ] !== "undefined" ){
-					options[ i ] = op[ i ];
-				}
-			}
-			$( "#" + id ).panel( "toggle" , {
-				position: options.position,//left right
-				dismissible: options.dismissible,//true or false
-				display: options.display,// overlay or push
-				link: $link
-			});
-		});
-		$closeLink.on( "vclick" , function( e ){
-			e.preventDefault();
-			$el.panel( "close" );
-			return false;
-		});
-
-		$page.on( "swipe" , function( e ){
-			$( ".ui-panel-active" ).panel( "close" );
-		});
-
-		this._trigger( "create" );
-	},
-	_position: function( options ){
-		var deferred = $.Deferred();
-		var o = options,
-			klass = o.classes.panel,
-			$el = this.element;
-		setTimeout(function(){
-			$el.addClass( "hidden" )
-				.addClass( klass + "-position-" + o.position )
-				.addClass( klass + "-dismissible-" + o.dismissible )
-				.addClass( klass + "-display-" + o.display )
-				.jqmData( "position" , o.position )
-				.jqmData( "display" , o.display )
-				.jqmData( "dismissible" , o.dismissible )
-				.removeClass( "hidden" );
-				deferred.resolve( options );
-		}, 0); // TODO get rid of setTimeout 0 hacks
-		return deferred.promise();
-	},
-	_openPanel: function( options ){
-		var deferred = $.Deferred();
-		var o = options,
-			$el = this.element,
-			klass = o.classes.panel,
-			$page = $el.closest( ":jqmData(role='page')" ),
-			$contentsWrap = $page.find( "." + o.classes.contentWrap ),
-			_triggerAndResolve = function(){
-				deferred.resolve( options );
-				$page.addClass( "ui-panel-open" );
-			};
-		if( o.display === "reveal" ){
-			$contentsWrap.one( "webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd" , _triggerAndResolve );
-		} else {
-			$el.one( "webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd" , _triggerAndResolve );
-		}
-		setTimeout(function(){
-			$el.addClass( klass + "-active" );
-			if( o.display === "reveal" || o.display === "push" ){
-				$contentsWrap.addClass( "panel-shift-" + o.position );
-			}
-			if( o.display === "push" ){
-				$contentsWrap.addClass( "panel-push" );
-			}
-			$( ".ui-page-active" ).addClass( "ui-panel-body-scroll-block" );
-		}, 0);//TODO setTimout hacks
-		return deferred.promise();
-	},
-	_destroy: function(){},
-	open: function( options ){
+	_bindPageEvents: function(){
 		var self = this;
-		var deferred = $.Deferred();
-		var o = $.extend( {} , this.options ),
-			klass = o.classes.panel,
-			$el = this.element,
-			$page = $el.closest( ":jqmData(role='page')" ),
-			$contentsWrap = $page.find( "." + o.classes.contentWrap ),
-			responsiveClasses;
 
-		for( var i in options ){
-			if( options.hasOwnProperty( i ) ){
-				o[ i ] = options[ i ];
-			}
-		}
-
-		responsiveClasses = $el[0].className.match(/ui-responsive-?\w*/g) || [];
-		for( var j = 0, len = responsiveClasses.length; j < len; j++ ){
-			$contentsWrap.addClass( responsiveClasses[ j ] );
-		}
-
-		this._position( o )
-		.then( function( o ){
-			return self._openPanel( o );
-		})
-		.then( function( o ){
-			return self._blockPage( o );
-		})
-		.then( function( o ){
-			self._trigger( "open" , "open" , { link: o.link } );
-			deferred.resolve( o );
-		});
-		return deferred.promise();
-	},
-	close: function( options , pagehide ){
-		var deferred = $.Deferred();
-		var o = $.extend( {} , this.options ),
-			klass = o.classes.panel,
-			$el = this.element,
-			position = $el.jqmData( "position" ),
-			display = $el.jqmData( "display" ),
-			dismissible = $el.jqmData( "dismissible" ),
-			$page = $el.closest( ":jqmData(role='page')" ),
-			$contentsWrap = $page.find( "." + o.classes.contentWrap ),
-			_closePanel = function(){
-				var responsiveClasses = $el[0].className.match(/ui-responsive-?\w*/g) || [];
-
-				$el.removeClass( klass + "-position-" + position )
-					.removeClass( klass + "-display-" + display )
-					.removeClass( klass + "-dismissible-" + dismissible );
-				for( var j = 0, len = responsiveClasses.length; j < len; j++ ){
-					$contentsWrap.removeClass( responsiveClasses[ j ] );
+		self._page
+			// on swipe, close the panel (should swipe open too?)
+			.on( "swipe" , function( e ){
+				self.close();
+			})
+			// Close immediately if another panel on the page opens
+			.on( "pagebeforeopen", function( e ){
+				if( self._open && e.target !== self.element ){
+					self.close( true );
 				}
-				$el.data( "mobile-panel" )._trigger( "close" , "close" , { link: o.link } );
-				deferred.resolve( o );
-			};
-		$page.removeClass( "ui-panel-open" );
-		for( var i in options ){
-			if( options.hasOwnProperty( i ) ){
-				o[ i ] = options[ i ];
-			}
-		}
-		
-		if( !pagehide ){
-			if( display === "reveal" ){
-				$contentsWrap.one( "webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd" , _closePanel );
-			} else {
-				$el.one( "webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd" , _closePanel );
-			}
-		}
-
-		$el.removeClass( klass + "-active" );
-		$( "#page-block" ).remove();
-		$( "." + o.classes.contentWrap ).removeClass( "panel-shift-" + position )
-			.removeClass( "panel-push" );
-		$( ".ui-page-active" ).removeClass( "ui-panel-body-scroll-block" );
-		return deferred.promise();
-	},
-	toggle: function( options ){
-		var $el = this.element,
-			active = $( ".ui-panel-active" ),
-			self = this;
-		if( active.length > 0 &&
-				( active.jqmData( "position") === options.position ) &&
-				( active.attr( "id" ) === $el.attr( "id" ) ) &&
-				( active.jqmData( "display" ) === options.display ) ){
-			return active.panel( "close" , options );
-		} else if ( active.length > 0 ){
-			active.panel( "close" , options )
-			.then( function( options ){
-				self.open( options );
+			})
+			// clean up open panels after page hide
+			.on(  "pagehide", self.close )
+			// on escape, close? might need to have a target check too...
+			.on( "keyup", function() {
+				if( e.keyCode === 27 && self._open ){
+					self.close();
+				}
 			});
-		} else {
-			return this.open( options );
-		}
 	},
-	refresh: function(){
-	}
-});
 
-$( document ).bind( "panelopen panelclose" , function( e , data ){
-	var $link = data.link, $parent;
-	if( $link ){
-		$parent = $link.parent().parent();
-		if ($parent.hasClass("ui-li")) {
-			$link = $parent.parent();
+	// state storage of open or closed
+	_open: false,
+
+	open: function( options ){
+		var o = this.options,
+			cb = function(){
+				this.element.addClass( o.classes.panelOpen );
+			};
+		this._trigger( "beforeopen" );
+
+		if ( $.support.cssTransitions ) {
+			this.element.one( this._transitionEndEvents , cb );
+		} else{
+			setTimeout( cb , 0 );
 		}
-		$link.removeClass( $.mobile.activeBtnClass );
+
+		this._modal.addClass( this._getOpenClasses( o.classes.modal ) );
+		this.element.addClass( this._getOpenClasses( o.classes.panel ) );
+
+		this._open = true;
+		this._trigger( "open" );
+	},
+
+	close: function( immediate ){
+		var o = this.options;
+		this._trigger( "beforeclose" );
+
+		this.element.removeClass( o.classes.panelOpen + " " + this._getOpenClasses( o.classes.panel ) );
+		this._modal.addClass( this._getOpenClasses( o.classes.modal ) );
+
+		this._open = false;
+		this._trigger( "close" );
+	},
+
+	toggle: function( options ){
+		this[ this._open ? "close" : "open" ]();
+	},
+
+	// cached string of classes used to last open the panel
+	_lastOpenClasses: null,
+
+	// return string of classes needed for opening panel
+	_getOpenClasses: function( joiner ){
+		var o = this.options;
+		this._lastOpenClasses = [
+				"",
+				"-position-" + o.position,
+				"-dismissible-" + o.dismissible,
+				"-display-" + o.display
+			].join( " " + joiner );
+
+		return this._lastOpenClasses;
+	},
+
+	_transitionEndEvents: "webkitTransitionEnd oTransitionEnd otransitionend transitionend msTransitionEnd",
+
+	destroy: function(){
+		// unbind events, remove generated elements, remove classes, remove data
 	}
 });
 
-$( document ).bind( "pagebeforehide" , function( e , data ){
-	$( ".ui-panel-active" ).panel( "close" , {} , true );
-});
-
-$(document).keyup(function(e) {
-	if( e.keyCode === 27 ){
-		$( ".ui-panel-active" ).panel( "close" );
-	}
-});
 
 //auto self-init widgets
 $( document ).bind( "pagecreate create", function( e ) {
