b3166c936b94201beb8a180d6af6ebc3e1cccfb9
Alexander Schmitz <arschmitz@gmail.com>
2012-12-12 11:07:38 -0500

    added fixedToolbar.workarounds to mobile.js

--- js/jquery.mobile.js
+++ js/jquery.mobile.js
@@ -29,6 +29,7 @@ define([
 	'./widgets/controlgroup',
 	'./jquery.mobile.links',
 	'./widgets/fixedToolbar',
+	'./widgets/fixedToolbar.workarounds'
 	'./widgets/popup',
 	'./widgets/table',
 	'./widgets/table.columntoggle',
