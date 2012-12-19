3571bedf83b97e6cf0ceb16ac3e36ee9e6dfcec8
John Bender <john.m.bender@gmail.com>
2012-12-19 13:43:19 -0800

    switch live to on

--- tests/unit/widget/widget_init.js
+++ tests/unit/widget/widget_init.js
@@ -6,7 +6,7 @@
 
 	module( 'jquery.mobile.widget.js' );
 
-	$( "#foo" ).live( 'pageinit', function(){
+	$( document ).on( 'pageinit', "#foo", function(){
 		// ordering sensitive here, the value has to be set after the call
 		// so that if the widget factory says that its not yet initialized,
 		// which is an exception, the value won't be set
