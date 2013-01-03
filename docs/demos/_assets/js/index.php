b2ee3c3bd78e206a8445e7f4ec8f6015ed6da101
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-01-03 20:43:49 +0200

    Revert "Demos: changed demo script src because combining scripts with PHP resulted in wrong src in the build."
    This reverts commit aded72456967c347521ec972693dfe6e3f391848.

--- docs/demos/_assets/js/index.php
+++ docs/demos/_assets/js/index.php
@@ -1,5 +1,4 @@
 <?php
-// This file is not in use because script src ../_assets/js/ results in ../_assets/js/jquery.mobile.js in the build.
 $type = 'text/javascript';
 $files = array(
 	'jqm-demos.js',
