cd1e031448abc22c69434f088579544dff3e27c3
Jasper de Groot <mail@ugomobi.com>
2013-01-16 22:46:03 +0100

    Panel: Fixed wrong name for position fixed support test. This fixes "unfixing" the panel when panel content is longer than screen height.

--- js/widgets/panel.js
+++ js/widgets/panel.js
@@ -179,13 +179,13 @@ $.widget( "mobile.panel", $.mobile.widget, {
 	},
 
 	_unfixPanel: function() {
-		if ( !!this.options.positionFixed && $.support.positionFixed ) {
+		if ( !!this.options.positionFixed && $.support.fixedPosition ) {
 			this.element.removeClass( this.options.classes.panelFixed );
 		}
 	},
 
 	_fixPanel: function() {
-		if ( !!this.options.positionFixed && $.support.positionFixed ) {
+		if ( !!this.options.positionFixed && $.support.fixedPosition ) {
 			this.element.addClass( this.options.classes.panelFixed );
 		}
 	},
