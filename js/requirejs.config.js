9cbd48b094988572f531385130372b8b9c5aa158
Ghislain Seguin <ghislain@gmail.com>
2013-01-17 17:02:43 -0700

    Moved RequireJS plugins to requirejs/plugins (cherry picked from commit 870d4e2b603dfccc6afcb26119e824ba23f048b9)

---
+++ js/requirejs.config.js
@@ -0,0 +1,9 @@
+requirejs.config({
+	"paths": {
+		// requireJS plugins
+		"text": "../external/requirejs/plugins/text",
+		"depend": "../external/requirejs/plugins/depend",
+		"json": "../external/requirejs/plugins/json"
+	}
+
+});
