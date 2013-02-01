fab06bffbc8d8ddf1d332428d8ae8186f74e5601
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-02-01 12:45:57 +0200

    Slider tooltip: New extension to render a tooltip above the slider which moves with the button, displaying the value along the way.
    Adapted from https://github.com/web-ui-fw/web-ui-fw/tree/e9ac5c21d2f75b5a68acb2c9d9e7f90494cf0512/src/widgets/slider/

--- js/index.php
+++ js/index.php
@@ -52,6 +52,7 @@ $files = array(
 	'widgets/forms/checkboxradio.js',
 	'widgets/forms/button.js',
 	'widgets/forms/slider.js',
+	'widgets/forms/slider.tooltip.js',
 	'widgets/forms/rangeslider.js',
 	'widgets/forms/textinput.js',
 	'widgets/forms/select.custom.js',
