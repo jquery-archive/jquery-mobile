0d3de3ebd7c48c9be71a75a4c97866661ebd7430
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-02-01 23:13:34 +0200

    Slider tooltip: Move to demos

--- js/index.php
+++ js/index.php
@@ -52,7 +52,6 @@ $files = array(
 	'widgets/forms/checkboxradio.js',
 	'widgets/forms/button.js',
 	'widgets/forms/slider.js',
-	'widgets/forms/slider.tooltip.js',
 	'widgets/forms/rangeslider.js',
 	'widgets/forms/textinput.js',
 	'widgets/forms/select.custom.js',
