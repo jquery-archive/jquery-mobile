f9cd8abdf6f9b599f0e9099ef684bd11a420d3da
John Chacko <poonkave@gmail.com>
2013-02-06 10:42:03 +0000

    Update js/jquery.mobile.navigation.js
    function releasePageTransitionLock sets isPageTransitioning  to false;

--- js/jquery.mobile.navigation.js
+++ js/jquery.mobile.navigation.js
@@ -738,8 +738,7 @@ define( [
 					newPage.data( 'absUrl', triggerData.absUrl );
 					$.mobile.changePage( newPage, options );
 				})
-				.fail(function( url, options ) {
-					isPageTransitioning = false;
+				.fail(function( url, options ){
 
 					//clear out the active button state
 					removeActiveLinkClass( true );
