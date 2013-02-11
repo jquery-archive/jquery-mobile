66043136d55befca46d8c40a8bbca26f55d62252
John Bender <john.m.bender@gmail.com>
2013-02-11 15:59:59 -0800

    kitchen sink test name

--- tests/perf/kitchen-sink/perf.js
+++ tests/perf/kitchen-sink/perf.js
@@ -2,7 +2,7 @@ require.config({ baseUrl: "../../../js" });
 
 // Require both the generic suite and the library to be tested for performance
 require( ["../suite.js", "jquery.mobile"], function( suite ) {
-	suite.add( "full widget enchancement", function() {
+	suite.add( "kitchen sink page enchancement", function() {
 		suite.fixtures.reset();
 		suite.fixtures.trigger( "pagecreate" );
 	});
