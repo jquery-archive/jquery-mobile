e47d39887ea75ca3774544ce641676b2195caf93
Jasper de Groot <mail@ugomobi.com>
2013-02-15 01:46:42 +0100

    Copied demos folder to root.

---
+++ demos/_assets/js/jqm-demos.js
@@ -0,0 +1,66 @@
+// Turn off AJAX for local file browsing
+if ( location.protocol.substr(0,4)  === 'file' ||
+     location.protocol.substr(0,11) === '*-extension' ||
+     location.protocol.substr(0,6)  === 'widget' ) {
+
+	// Start with links with only the trailing slash and that aren't external links
+	var fixLinks = function() {
+		$( "a[href$='/'], a[href='.'], a[href='..']" ).not( "[rel='external']" ).each( function() {
+			this.href = $( this ).attr( "href" ).replace( /\/$/, "" ) + "/index.html";
+		});
+	};
+
+	// Fix the links for the initial page
+	$( fixLinks );
+
+	// Fix the links for subsequent ajax page loads
+	$( document ).on( 'pagecreate', fixLinks );
+
+	// Check to see if ajax can be used. This does a quick ajax request and blocks the page until its done
+	$.ajax({
+		url: '.',
+		async: false,
+		isLocal: true
+	}).error(function() {
+		// Ajax doesn't work so turn it off
+		$( document ).on( "mobileinit", function() {
+			$.mobile.ajaxEnabled = false;
+			
+			var message = $( '<div>' , {
+				'class': "ui-footer ui-bar-e",
+				style: "overflow: auto; padding:10px 15px;",
+				'data-ajax-warning': true
+			});
+			
+			message
+			.append( "<h3>Note: Navigation may not work if viewed locally</h3>" )
+			.append( "<p>The AJAX-based navigation used throughout the jQuery Mobile docs may need to be viewed on a web server to work in certain browsers. If you see an error message when you click a link, try a different browser or <a href='https://github.com/jquery/jquery-mobile/wiki/Downloadable-Docs-Help'>view help</a>.</p>" );
+			
+			$( document ).on( "pagecreate", function( event ) {
+				$( event.target ).append( message );
+			});
+		});
+	});
+}
+
+
+// display the version of jQM
+$( document ).on( "pageinit", function() {
+	var version = $.mobile.version || "dev",
+		words = version.split( "-" ),
+		ver = words[0],
+		str = (words[1] || "Final"),
+		html = "Version " + ver;
+
+	if( str.indexOf( "rc" ) == -1 ){
+		str = str.charAt( 0 ).toUpperCase() + str.slice( 1 );
+	} else {
+		str = str.toUpperCase().replace(".", "");
+	}
+
+	if ( $.mobile.version && str ) {
+		html += " " + str;
+	}
+
+	$( "p.jqm-version" ).html( html );
+});
---
+++ demos/examples/redirect/redirect-target.html
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+  <title>Redirect Target</title>
+  <meta charset=utf-8 />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet"  href="http://code.jquery.com/mobile/latest/jquery.mobile.css" /> 
+  <script src="http://code.jquery.com/jquery-1.8.3.js"></script> 
+  <script src="http://code.jquery.com/mobile/latest/jquery.mobile.js"></script> 
+	
+</head>
+<body>
+<div data-role="page">
+	<div data-role="header">
+		<h1>Redirect Target</h1>
+	</div>
+	<div data-role="content">
+		<p>Content</p>
+		<tt>
+		</tt>
+	</div>
+
+</div>
+</body>
+</html>
