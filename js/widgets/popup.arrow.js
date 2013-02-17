ffd514045e8235ad28633d3a90cac1c09aac7a6e
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-02-17 02:10:32 +0200

    Popup arrow: Take into account border width.

--- js/widgets/popup.arrow.js
+++ js/widgets/popup.arrow.js
@@ -83,27 +83,31 @@ $.widget( "mobile.popup", $.mobile.popup, {
 			return best;
 		}
 
-		desiredForArrow[ p.fst ] = desired[ p.fst ] + ( s.arHalf[ p.oDimKey ] + s.menuHalf[ p.oDimKey ] ) * p.offsetFactor;
+		desiredForArrow[ p.fst ] = desired[ p.fst ] +
+			( s.arHalf[ p.oDimKey ] + s.menuHalf[ p.oDimKey ] ) * p.offsetFactor -
+			s.contentBox[ p.fst ] + ( s.clampInfo.menuSize[ p.oDimKey ] - s.contentBox[ p.oDimKey ] ) * p.arrowOffsetFactor;
 		desiredForArrow[ p.snd ] = desired[ p.snd ];
 
 		result = s.result || this._calculateFinalLocation( desiredForArrow, s.clampInfo );
 		r = { x: result.left, y: result.top };
 
-		tip[ p.fst ] = r[ p.fst ] + p.tipOffset;
+		tip[ p.fst ] = r[ p.fst ] + s.contentBox[ p.fst ] + p.tipOffset;
 		tip[ p.snd ] = Math.max( result[ p.prop ] + s.guideOffset[ p.prop ] + s.arHalf[ p.dimKey ],
 			Math.min( result[ p.prop ] + s.guideOffset[ p.prop ] + s.guideDims[ p.dimKey ] - s.arHalf[ p.dimKey ],
 				desired[ p.snd ] ) );
 
 		diff = Math.abs( desired.x - tip.x ) + Math.abs( desired.y - tip.y );
 		if ( !best || diff < best.diff ) {
-			best = { dir: dir, diff: diff, result: result, posProp: p.prop, posVal: tip[ p.snd ] - s.arHalf[ p.dimKey ] - result[ p.prop ] };
+			// Convert tip offset to coordinates inside the popup
+			tip[ p.snd ] -= s.arHalf[ p.dimKey ] + result[ p.prop ] + s.contentBox[ p.snd ];
+			best = { dir: dir, diff: diff, result: result, posProp: p.prop, posVal: tip[ p.snd ] };
 		}
 
 		return best;
 	},
 
 	_getPlacementState: function( clamp ) {
-		var offset,
+		var offset, gdOffset,
 			ar = this._ui.arrow,
 			state = {
 				clampInfo: this._clampPopupWidth( !clamp ),
@@ -113,6 +117,17 @@ $.widget( "mobile.popup", $.mobile.popup, {
 			};
 
 		offset = this.element.offset();
+
+		ar.gd.css( { left: 0, top: 0, right: 0, bottom: 0 } );
+		gdOffset = ar.gd.offset();
+		state.contentBox = {
+			x: gdOffset.left - offset.left,
+			y: gdOffset.top - offset.top,
+			cx: ar.gd.width(),
+			cy: ar.gd.height()
+		};
+		ar.gd.removeAttr( "style" );
+
 		// The arrow box moves between guideOffset and guideOffset + guideDims - arFull
 		state.guideOffset = { left: state.guideOffset.left - offset.left, top: state.guideOffset.top - offset.top };
 		state.arHalf = { cx: state.arFull.cx / 2, cy: state.arFull.cy / 2 };
@@ -122,7 +137,7 @@ $.widget( "mobile.popup", $.mobile.popup, {
 	},
 
 	_placementCoords: function( desired ) {
-		var state, menuFull, best, params, bgOffset, diff,
+		var state, best, params, bgOffset, elOffset, diff,
 			bgRef = {},
 			ar = this._ui.arrow ;
 
@@ -133,13 +148,12 @@ $.widget( "mobile.popup", $.mobile.popup, {
 		ar.arEls.show();
 
 		state = this._getPlacementState( true );
-		menuFull = state.clampInfo.menuSize;
 
 		params = {
-			"l": { fst: "x", snd: "y", prop: "top", dimKey: "cy", oDimKey: "cx", offsetFactor: 1, tipOffset: -( state.arHalf.cx ) },
-			"r": { fst: "x", snd: "y", prop: "top", dimKey: "cy", oDimKey: "cx", offsetFactor: -1, tipOffset: menuFull.cx + state.arHalf.cx },
-			"b": { fst: "y", snd: "x", prop: "left", dimKey: "cx", oDimKey: "cy", offsetFactor: -1, tipOffset: menuFull.cy + state.arHalf.cy },
-			"t": { fst: "y", snd: "x", prop: "left", dimKey: "cx", oDimKey: "cy", offsetFactor: 1, tipOffset: -( state.arHalf.cx ) }
+			"l": { fst: "x", snd: "y", prop: "top", dimKey: "cy", oDimKey: "cx", offsetFactor: 1, tipOffset:  -state.arHalf.cx, arrowOffsetFactor: 0 },
+			"r": { fst: "x", snd: "y", prop: "top", dimKey: "cy", oDimKey: "cx", offsetFactor: -1, tipOffset: state.arHalf.cx + state.contentBox.cx, arrowOffsetFactor: 1 },
+			"b": { fst: "y", snd: "x", prop: "left", dimKey: "cx", oDimKey: "cy", offsetFactor: -1, tipOffset: state.arHalf.cy + state.contentBox.cy, arrowOffsetFactor: 1 },
+			"t": { fst: "y", snd: "x", prop: "left", dimKey: "cx", oDimKey: "cy", offsetFactor: 1, tipOffset: -state.arHalf.cy, arrowOffsetFactor: 0 }
 		};
 
 		// Try each side specified in the options to see on which one the arrow
@@ -163,11 +177,15 @@ $.widget( "mobile.popup", $.mobile.popup, {
 
 		// Do not move/size the background div on IE, because we use the arrow div for background as well.
 		if ( !ieHack ) {
+			elOffset = this.element.offset();
 			bgRef[ params[ best.dir ].fst ] = ar.ct.offset();
-			bgRef[ params[ best.dir ].snd ] = this.element.offset();
+			bgRef[ params[ best.dir ].snd ] = {
+				left: elOffset.left + state.contentBox.x,
+				top: elOffset.top + state.contentBox.y
+			};
 			bgOffset = ar.bg
 				.removeAttr( "style" )
-				.css( ( "cx" === params[ best.dir ].dimKey ? "width" : "height" ), menuFull[ params[ best.dir ].dimKey ] )
+				.css( ( "cx" === params[ best.dir ].dimKey ? "width" : "height" ), state.contentBox[ params[ best.dir ].dimKey ] )
 				.offset();
 			diff = { dx: bgRef.x.left - bgOffset.left, dy: bgRef.y.top - bgOffset.top };
 			ar.bg.css( { left: txFactor * diff.dy + txFactor * diff.dx, top: txFactor * diff.dy - txFactor * diff.dx } );
