eec3a43a5bcad116dcc4e057e77c7c339f000dee
Jasper de Groot <mail@ugomobi.com>
2013-02-18 17:48:44 +0100

    Demos: alphabetical order demos menu

--- docs/nav-examples.php
+++ docs/nav-examples.php
@@ -1,3 +1,8 @@
+<li data-role="list-divider">Collapsibles</li>
+
+<li data-section="Demo Showcase" data-filtertext="slider tooltip extension input range sliders forms"><a href="examples/collapsible/dynamic-collapsible.php" data-ajax="false">Dynamic collapsible</a></li>
+
+
 <li data-role="list-divider">Listviews</li>
 
 <li data-section="Demo Showcase" data-filtertext="grid listview responsive grids responsive listviews lists ul"><a href="examples/listviews/grid-listview.php" data-ajax="false">Grid Listview</a></li>
@@ -5,6 +10,12 @@
 <li data-section="Demo Showcase" data-filtertext="autodividers linkbars listviews lists ul"><a href="examples/listviews/autodividers-linkbar.php" data-ajax="false">Autodividers Linkbar</a></li>
 
 <li data-section="Demo Showcase" data-filtertext="autodividers selector autodividersselector listviews lists ul ol"><a href="examples/listviews/autodividers-selector.php" data-ajax="false">Autodividers Selector</a></li>
+
+<li data-role="list-divider">Navigation</li>
+
+<li data-section="Demo Showcase" data-filtertext="backbone and require.js example navigation router"><a href="examples/backbone-require/index.php" data-ajax="false">Backbone and Require.js example</a></li>
+
+<li data-section="Demo Showcase" data-filtertext="server redirection with server-side support php"><a href="examples/redirect/index.php" data-ajax="false">Redirection with server-side support</a></li>
 
 <li data-role="list-divider">Panels</li>
 
@@ -30,22 +41,12 @@
 
 <li data-section="Demo Showcase" data-filtertext="responsive tables table column toggle demo: phone comparison"><a href="examples/tables/phone-compare.php" data-ajax="false">Column toggle demo: Phone comparison</a></li>
 
-<li data-role="list-divider">Navigation</li>
+<li data-role="list-divider">Sliders</li>
 
-<li data-section="Demo Showcase" data-filtertext="backbone and require.js example navigation router"><a href="examples/backbone-require/index.php" data-ajax="false">Backbone and Require.js example</a></li>
-
-<li data-section="Demo Showcase" data-filtertext="server redirection with server-side support php"><a href="examples/redirect/index.php" data-ajax="false">Redirection with server-side support</a></li>
+<li data-section="Demo Showcase" data-filtertext="slider tooltip extension input range sliders forms"><a href="examples/slider/tooltip.php" data-ajax="false">Tooltip extension</a></li>
 
 <li data-role="list-divider">Swipe</li>
 
 <li data-section="Demo Showcase" data-filtertext="swipe to navigate page transitions navigation"><a href="examples/swipe/swipe-page.php" data-ajax="false">Swipe to navigate</a></li>
 
 <li data-section="Demo Showcase" data-filtertext="swipe to delete list item listviews lists ul ol"><a href="examples/swipe/swipe-list.php" data-ajax="false">Swipe to delete list item</a></li>
-
-<li data-role="list-divider">Sliders</li>
-
-<li data-section="Demo Showcase" data-filtertext="slider tooltip extension input range sliders forms"><a href="examples/slider/tooltip.php" data-ajax="false">Tooltip extension</a></li>
-
-<li data-role="list-divider">Collapsibles</li>
-
-<li data-section="Demo Showcase" data-filtertext="slider tooltip extension input range sliders forms"><a href="examples/collapsible/dynamic-collapsible.php" data-ajax="false">Dynamic collapsible</a></li>
