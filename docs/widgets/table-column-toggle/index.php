9c8f547221c11f282f601d727c7837bc674753c2
toddparker <todd@filamentgroup.com>
2013-02-17 23:35:45 -0500

    Demos: Linked logo to the homepage

--- docs/widgets/table-column-toggle/index.php
+++ docs/widgets/table-column-toggle/index.php
@@ -16,10 +16,10 @@
 <div data-role="page" class="jqm-demos" data-quicklinks="true">
 
 	<div data-role="header" class="jqm-header">
-		<h1 class="jqm-logo"><img src="../../_assets/img/jquery-logo.png" alt="jQuery Mobile Framework"></h1>
+		<h1 class="jqm-logo"><a href="../../../"><img src="../../_assets/img/jquery-logo.png" alt="jQuery Mobile Framework"></a></h1>
 		<a href="#panel-nav" class="jqm-navmenu-link" data-icon="bars" data-iconpos="notext">Navigation</a>
 		<a href="#" class="jqm-search-link" data-icon="search" data-iconpos="notext">Search</a>
-		<?php include( '../../search.php' ); ?>
+        <?php include( '../../search.php' ); ?>
 	</div><!-- /header -->
 	
 	<div data-role="content" class="jqm-content">
