64a290313ddd89b3ed6c7692e85b8fcf5f718c79
Todd Parker <todd@filamentgroup.com>
2013-02-18 11:03:53 -0500

    Demos: Adjusted font for slider handle labels, fixed broken css link

--- docs/examples/slider/tooltip.php
+++ docs/examples/slider/tooltip.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="../../_assets/css/jqm-demos.css">
 	<link rel="shortcut icon" href="../../_assets/favicon.ico">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-	<link rel="stylesheet" href="slider/slider.tooltip.css" id="tooltipStyle">
+	<link rel="stylesheet" href="slider.tooltip.css" id="tooltipStyle">
 	<script src="../../../js/jquery.js"></script>
 	<script src="../../_assets/js/"></script>
 	<script src="../../../js/"></script>
