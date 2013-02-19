0265bdc264815fc34c99842de60101feaf3bec8e
John Bender <john.m.bender@gmail.com>
2013-02-19 14:51:24 -0800

    force global and search includes first

--- build/bin/docs.sh
+++ build/bin/docs.sh
@@ -7,7 +7,7 @@ cp compiled/*.js tmp/demos/js
 cp js/jquery.js tmp/demos/js
 # ... Copy html files
 cp index.php tmp/demos
-cp -r docs tmp/demos
+cp -r docs tmp/demos/
 # ... Copy css and images
 cp compiled/*.css tmp/demos/css/themes/$THEME
 cp -r compiled/images tmp/demos/css/themes/$THEME
@@ -33,38 +33,47 @@ find tmp/demos \( -name '*.php' ! -wholename "*redirect*" \)| while read file; d
   mv $file "$rename"
 done
 
+# NOTE SPECIAL CASE
 # fix the index of the redirect sample
 mv tmp/demos/docs/examples/redirect/index.php tmp/demos/docs/examples/redirect/index.html
 
-# find all the files with php includes
-# TODO sort out demos/search.php first
-for file in $(grep -lr "\?php include(" tmp/demos/); do
-  file_dir=$(echo $file | sed "s/\/[^\/]*.html$/\//")
+function inline {
+  for file in $1; do
+	file_dir=$(echo $file | sed "s/\/[^\/]*.html$/\//")
 
   # remove the php leaving only a list of relative html file paths
-  includes=$(grep -h "\?php include(" $file | sed "s/<?php include( '//" | sed "s/' ); ?>//g" | sed "s/\s*//")
+	includes=$(grep -h "\?php include(" $file | sed "s/<?php include( '//" | sed "s/' ); ?>//g" | sed "s/\s*//")
 
   # for each relative path resolve it and replac the corresponding php
   # include in the file with the resolved files contents
-  for include_relative in $includes; do
+	for include_relative in $includes; do
 	# the html extension file include
-	html_include=$(echo $include_relative | sed "s/.php/.html/")
+	  html_include=$(echo $include_relative | sed "s/.php/.html/")
 
 	# the fully resolved path, NOTE readlink should be standard
-	full_file_path=$(readlink -m $file_dir$html_include)
+	  full_file_path=$(readlink -m $file_dir$html_include)
 
 	# construct the include script replace, removing the path delimiters
 	# and the double dot refs to prevent sed from borking
-	include_string="<?php include( '.*$(echo $include_relative | sed "s@\([a-zA-Z\-\0-9.]*/\)*@@g")' ); ?>"
+	  include_string="<?php include( '.*$(echo $include_relative | sed "s@\([a-zA-Z\-\0-9.]*/\)*@@g")' ); ?>"
 
 	# replace the include string with the resolved file contents
-    sed -i$SED_INPLACE_EXT "/$include_string/{
+      sed -i$SED_INPLACE_EXT "/$include_string/{
       s/$include_string//g
       r $full_file_path
     }" $file
 
-    find tmp/demos -name "*$SED_INPLACE_EXT" -exec rm {} \;
+      find tmp/demos -name "*$SED_INPLACE_EXT" -exec rm {} \;
+	done
   done
+}
+
+# find all the files with php includes, do the work on includes that will
+# be inlined into other files first
+# NOTE SPECIAL CASE
+inline "tmp/demos/docs/global-nav.html tmp/demos/docs/search.html"
+for file in $(grep -lr "\?php include(" tmp/demos/); do
+  inline $file
 done
 
 # replace link references from php with references to html
