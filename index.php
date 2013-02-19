12d9a579ee057d814b4cd8007755c86d37fd2f4f
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-02-19 23:44:26 +0200

    Docs: Front page: Whitespace removed.

--- index.php
+++ index.php
@@ -20,18 +20,17 @@
 		<a href="#" class="jqm-search-link ui-btn-right" data-icon="search" data-iconpos="notext">Search</a>
         <?php include( 'docs/search.php' ); ?>
 	</div><!-- /header -->
-	
+
 	<div data-role="content" class="jqm-content">
-		
+
 		<div class="jqm-home-welcome">
 		<h2>Demo center: <span class="jqm-version"></span></h2>
 		
 		<p class="jqm-intro">jQuery Mobile is a touch-optimized HTML5 UI framework designed to make sites and apps that are accessible on all popular smartphone, tablet and desktop devices.</p>
-        
+
         <p class="jqm-intro"><small>For technical info,&nbsp;visit the API documentation.</small></p>
 		<a href="http://api.jquerymobile.com" class="jqm-button ui-icon-alt" data-ajax="false" data-role="button" data-inline="true" data-mini="true" data-icon="arrow-r" data-iconpos="right">jQuery Mobile API Site </a> 
 		</div>
-		
 
 		<ul data-role="listview" data-inset="true" data-theme="d" data-icon="false" data-filter-placeholder="Search..." class="jqm-list jqm-home-list">
             <li><a href="docs/intro/"><h2>Introduction</h2><p>New to jQuery Mobile? Start here.</p></a></li>
@@ -39,22 +38,22 @@
             <li><a href="docs/faq/"><h2>Questions &amp; Answers</h2><p>Common issues and questions, explained.</p></a></li>
 			<li data-section="Widgets" data-filtertext="responsive web design rwd adaptive PE accessible mobile breakpoints media query"><a href="docs/intro/rwd.php"><h2>Going Responsive</h2><p>How to use RWD with jQuery Mobile</p></a></li>
 		</ul>
-		
+
 		<h2 class="jqm-home-widget">Widget reference</h2>
-		
+
 		<p class="jqm-intro">Test drive every component in the library, and easily build pages by copying and pasting the markup configuration you need.</p>
-        
+
 		<ul data-role="listview" data-inset="true" data-filter="true" data-theme="d" data-divider-theme="d" data-icon="false" data-filter-placeholder="Search widgets..." data-global-nav="docs" class="jqm-list">
         	<?php include( 'docs/nav-widgets.php' ); ?>
         </ul>
 
 	</div><!-- /content -->
-	
+
 	<div data-role="footer" class="jqm-footer">
 		<p class="jqm-version"></p>
 		<p>Copyright 2013 The jQuery Foundation</p>
 	</div><!-- /jqm-footer -->
-	
+
 </div><!-- /page -->
 </body>
 </html>
