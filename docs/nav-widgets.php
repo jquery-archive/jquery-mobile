6f26a1caeb146f4310242166ff3c20da6dfd54c8
Todd Parker <todd@filamentgroup.com>
2013-02-19 19:02:58 -0500

    Demos: Fortified widget filter text

--- docs/nav-widgets.php
+++ docs/nav-widgets.php
@@ -1,61 +1,61 @@
-<li data-section="Widgets" data-filtertext="accordions collapsible sets content formatting"><a href="widgets/accordions/">Accordion</a></li>
+<li data-section="Widgets" data-filtertext="accordions collapsible sets content formatting grouped inset mini"><a href="widgets/accordions/">Accordion</a></li>
 
-<li data-section="Widgets" data-filtertext="ajax navigation"><a href="widgets/navigation/">AJAX Navigation</a></li>
+<li data-section="Widgets" data-filtertext="ajax navigation navigate event method"><a href="widgets/navigation/" data-ajax="false">AJAX Navigation</a></li>
 
-<li data-section="Widgets" data-filtertext="autocomplete filter reveal listviews"><a href="widgets/autocomplete/" data-ajax="false">Autocomplete</a></li>
+<li data-section="Widgets" data-filtertext="autocomplete filter reveal listviews remote listviewbeforefilter placeholder"><a href="widgets/autocomplete/" data-ajax="false">Autocomplete</a></li>
 
-<li data-section="Widgets" data-filtertext="buttons inputs forms"><a href="widgets/buttons/" data-ajax="false">Buttons</a></li>
+<li data-section="Widgets" data-filtertext="buttons inputs forms inline theme grouped icons mini disabled"><a href="widgets/buttons/" data-ajax="false">Buttons</a></li>
 
-<li data-section="Widgets" data-filtertext="checkboxes checkboxradio inputs forms"><a href="widgets/checkbox/">Checkboxes</a></li>
+<li data-section="Widgets" data-filtertext="checkboxes checkboxradio inputs forms mini disabled"><a href="widgets/checkbox/">Checkboxes</a></li>
 
 <li data-section="Widgets" data-filtertext="collapsibles content formatting"><a href="widgets/collapsibles/" data-ajax="false">Collapsibles</a></li>
 
-<li data-section="Widgets" data-filtertext="controlgroups selects checkboxradio buttons"><a href="widgets/controlgroups/">Controlgroup</a></li>
+<li data-section="Widgets" data-filtertext="controlgroups selects checkboxradio buttons horizontal vertical"><a href="widgets/controlgroups/">Controlgroup</a></li>
 
 <li data-section="Widgets" data-filtertext="dialogs modal popups"><a href="widgets/dialog/">Dialogs</a></li>
 
-<li data-section="Widgets" data-filtertext="fixed toolbars headers footers sections"><a href="widgets/headers/bars-fixed.php">Fixed toolbars</a></li>
+<li data-section="Widgets" data-filtertext="fixed toolbars headers footers sections fullscreen"><a href="widgets/headers/bars-fixed.php">Fixed toolbars</a></li>
 
-<li data-section="Widgets" data-filtertext="flip toggle switch flip switch toggle slider selects forms"><a href="widgets/sliders/switch.php" data-ajax="false">Flip switch toggle</a></li>
+<li data-section="Widgets" data-filtertext="flip toggle switch binary slider selects forms disabled"><a href="widgets/sliders/switch.php" data-ajax="false">Flip switch toggle</a></li>
 
 <li data-section="Widgets" data-filtertext="footer toolbars footers sections"><a href="widgets/footers/">Footer toolbar</a></li>
 
-<li data-section="Widgets" data-filtertext="form elements inputs forms fieldcontains field containers"><a href="widgets/forms/">Form elements</a></li>
+<li data-section="Widgets" data-filtertext="forms inputs slider button range toggle switch label disabled accessible fieldcontains elements"><a href="widgets/forms/">Form elements</a></li>
 
-<li data-section="Widgets" data-filtertext="grids grid blocks content formatting rwd responsive"><a href="widgets/grids/">Grids</a></li>
+<li data-section="Widgets" data-filtertext="grids columns blocks content formatting rwd responsive"><a href="widgets/grids/">Grids</a></li>
 
-<li data-section="Widgets" data-filtertext="header toolbars headers sections"><a href="widgets/headers/">Header toolbar</a></li>
+<li data-section="Widgets" data-filtertext="header toolbars fixed fullscreen sections"><a href="widgets/headers/">Header toolbar</a></li>
 
-<li data-section="Widgets" data-filtertext="icons buttons"><a href="widgets/icons/">Icon</a></li>
+<li data-section="Widgets" data-filtertext="icons buttons disc position"><a href="widgets/icons/">Icon</a></li>
 
-<li data-section="Widgets" data-filtertext="links linking navigation ajax"><a href="widgets/links/">Links</a></li>
+<li data-section="Widgets" data-filtertext="links navigation ajax prefetch cache"><a href="widgets/links/">Links</a></li>
 
-<li data-section="Widgets" data-filtertext="listviews lists ul ol"><a href="widgets/listviews/" data-ajax="false">Listviews</a></li>
+<li data-section="Widgets" data-filtertext="listviews thumbnails icons nested split button collapsible ul ol"><a href="widgets/listviews/" data-ajax="false">Listviews</a></li>
 
-<li data-section="Widgets" data-filtertext="loader overlay loading pages"><a href="widgets/loader/">Loader overlay</a></li>
+<li data-section="Widgets" data-filtertext="ajax loader overlay spinner pages"><a href="widgets/loader/">Loader overlay</a></li>
 
-<li data-section="Widgets" data-filtertext="navbars navmenu toolbars"><a href="widgets/navbar/" data-ajax="false">Navbar</a></li>
+<li data-section="Widgets" data-filtertext="navbars navmenu toolbars links icons footer header"><a href="widgets/navbar/" data-ajax="false">Navbar</a></li>
 
-<li data-section="Widgets" data-filtertext="navbar, persistent navbars persistent header persistent footer persistent navmenu"><a href="widgets/navbar/footer-persist-a.php">Navbar, persistent</a></li>
+<li data-section="Widgets" data-filtertext="navbars persistent header footer links navmenu"><a href="widgets/navbar/footer-persist-a.php">Navbar, persistent</a></li>
 
-<li data-section="Widgets" data-filtertext="pages single page templates multipage templates multi-page templates"><a href="widgets/pages/">Pages</a></li>
+<li data-section="Widgets" data-filtertext="pages single multipage templates ajax nav"><a href="widgets/pages/">Pages</a></li>
 
-<li data-section="Widgets" data-filtertext="panels slide panels sliding panels rwd responsive"><a href="widgets/panels/">Panels <span class="ui-li-count">New</span></a></li>
+<li data-section="Widgets" data-filtertext="panels sliding nav modal transition display reveal overlay push rwd responsive"><a href="widgets/panels/">Panels <span class="ui-li-count">New</span></a></li>
 
-<li data-section="Widgets" data-filtertext="popup dialog modal"><a href="widgets/popup/">Popup</a></li>
+<li data-section="Widgets" data-filtertext="popup dialog modal transition tooltip lightbox form overlay screen flip pop fade transition"><a href="widgets/popup/">Popup</a></li>
 
-<li data-section="Widgets" data-filtertext="radiobuttons radio buttons checkboxradio inputs forms"><a href="widgets/radiobuttons/">Radio buttons</a></li>
+<li data-section="Widgets" data-filtertext="radiobuttons checkboxradio inputs forms disabled grouped"><a href="widgets/radiobuttons/">Radio buttons</a></li>
 
-<li data-section="Widgets" data-filtertext="selectmenus custom select menus native selects multiple selects forms"><a href="widgets/selects/">Select</a></li>
+<li data-section="Widgets" data-filtertext="selectmenus custom native multiple optgroup disabled forms"><a href="widgets/selects/">Select</a></li>
 
-<li data-section="Widgets" data-filtertext="slider, single sliders range inputs forms"><a href="widgets/sliders/" data-ajax="false">Slider, single</a></li>
+<li data-section="Widgets" data-filtertext="slider, single sliders range inputs forms disabled"><a href="widgets/sliders/" data-ajax="false">Slider, single</a></li>
 
-<li data-section="Widgets" data-filtertext="slider, dual range sliders rangesliders inputs forms"><a href="widgets/sliders/rangeslider.php" data-ajax="false">Slider, dual range <span class="ui-li-count">New</span></a></li>
+<li data-section="Widgets" data-filtertext="slider, dual range sliders rangesliders inputs forms disabled"><a href="widgets/sliders/rangeslider.php" data-ajax="false">Slider, dual range <span class="ui-li-count">New</span></a></li>
 
-<li data-section="Widgets" data-filtertext="table, column toggle tables responsive tables rwd responsive"><a href="widgets/table-column-toggle/">Table, column toggle <span class="ui-li-count">New</span></a></li>
+<li data-section="Widgets" data-filtertext="tables column th td toggle responsive tables rwd hide show tabular"><a href="widgets/table-column-toggle/">Table, column toggle <span class="ui-li-count">New</span></a></li>
 
-<li data-section="Widgets" data-filtertext="table, reflow tables responsive tables rwd responsive"><a href="widgets/table-reflow/">Table, reflow <span class="ui-li-count">New</span></a></li>
+<li data-section="Widgets" data-filtertext="tables reflow th td responsive rwd columns tabular"><a href="widgets/table-reflow/">Table, reflow <span class="ui-li-count">New</span></a></li>
 
-<li data-section="Widgets" data-filtertext="text inputs & textarea textinputs forms"><a href="widgets/textinputs/">Text inputs & textarea</a></li>
+<li data-section="Widgets" data-filtertext="text inputs textarea numeric email tel file date time month clear pattern placeholder forms"><a href="widgets/textinputs/">Text inputs & textarea</a></li>
 
-<li data-section="Widgets" data-filtertext="page transitions pages navigation" data-ajax="false"><a href="widgets/transitions/" data-ajax="false">Transitions</a></li>
+<li data-section="Widgets" data-filtertext="page transitions animated pages ajax navigation flip slide fade pop" data-ajax="false"><a href="widgets/transitions/" data-ajax="false">Transitions</a></li>
