574793b75ff5277c1312fc2678c89620ad6a22f1
Jasper de Groot <mail@ugomobi.com>
2013-03-11 09:25:38 +0100

    Demos: fixed broken home links.

--- demos/global-nav.php
+++ demos/global-nav.php
@@ -1,7 +1,7 @@
     <div data-role="panel" class="jqm-nav-panel jqm-navmenu-panel" data-position="left" data-display="reveal" data-theme="c">
         <ul data-role="listview" data-theme="d" data-divider-theme="d" data-icon="false" data-global-nav="demos" class="jqm-list">
             <li data-role="list-divider">jQuery Mobile Demos</li>
-			<li><a href="/">Home</a></li>
+			<li><a href="./">Home</a></li>
             <li><a href="intro/">Introduction</a></li>
             <li><a href="examples/">Demo Showcase</a></li>
             <li><a href="faq/">Questions & Answers</a></li>
