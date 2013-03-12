700aa8cbd2663dfa1a8416469b6db726dc4b6426
Ghislain Seguin <ghislain@gmail.com>
2013-03-12 14:03:04 -0600

    Build: Added possibility to set jqueries using environment variable JQUERIES

--- Gruntfile.js
+++ Gruntfile.js
@@ -363,7 +363,7 @@ module.exports = function( grunt ) {
 							onePath = "",
 							uniquePaths = [],
 							versionedPaths = [],
-							jQueries = grunt.util._.without( ( grunt.option( "jqueries" ) || "" ).split( "," ), "" );
+							jQueries = grunt.util._.without( ( grunt.option( "jqueries" ) || process.env.JQUERIES || "" ).split( "," ), "" );
 
 						if ( suites.length ) {
 							patterns = [];
