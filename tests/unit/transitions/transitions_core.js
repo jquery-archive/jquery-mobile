817b22deb8c4f60626c70eaa3d770e75686f40cd
John Bender <john.m.bender@gmail.com>
2013-03-29 15:37:43 -0700

    split modules by method name, begin doneIn tests

--- tests/unit/transitions/transitions_core.js
+++ tests/unit/transitions/transitions_core.js
@@ -2,18 +2,9 @@
  * Transitions unit tests
  */
 (function( $ ){
-	var instance;
+	var instance, proto;
 
-	module( "Transition", {
-		setup: function() {
-			instance = new $.mobile.Transition();
-		},
-
-		teardown: function() {
-			window.scrollTo( 0 );
-			$( "body" ).height( $(window).height() );
-		}
-	});
+	module( "Transition cleanFrom" );
 
 	test( "cleanFrom removes transition classes", function(){
 		var $from = $("<div>"),
@@ -27,17 +18,51 @@
 		ok( !$from.hasClass("foo") );
 	});
 
-	test( "scrollPage moves the page to the toScroll position", function() {
+	module( "Transition scrollPage", {
+		setup: function() {
+			instance = new $.mobile.Transition();
+		},
+
+		teardown: function() {
+			window.scrollTo( 0 );
+			$( "body" ).height( $(window).height() );
+		}
+	});
+
+	test( "moves the page to the toScroll position", function() {
 		$( "body" ).height( 5000 );
 		instance.toScroll = 100;
 		instance.scrollPage();
 		equal( $(window).scrollTop(), 100, "page has been scrolled" );
 	});
 
-	test( "scrollPage disables scrollstart for a short duration", function() {
+	test( "disables scrollstart for a short duration", function() {
 		instance.toScroll = 0;
 		instance.scrollPage();
 		equal( $.event.special.scrollstart.enabled, false, "scrollstart is disabled" );
+	});
+
+	module( "Transition doneIn", {
+		setup: function() {
+			proto = $.extend({}, $.mobile.Transition.prototype);
+			$.mobile.Transition.prototype.toggleViewportClass = $.noop;
+		},
+
+		teardown: function() {
+			$.mobile.Transition.prototype = proto;
+		}
+	});
+
+	test( "doneIn removes classes from the destination 'page'", function() {
+		var $to = $("<div>"),
+			transition = new $.mobile.Transition( "foo", false, $to, $());
+
+		$to.addClass( "out in reverse foo" );
+		transition.doneIn();
+		ok( !$to.hasClass("out") );
+		ok( !$to.hasClass("in") );
+		ok( !$to.hasClass("reverse") );
+		ok( !$to.hasClass("foo") );
 	});
 
 })( jQuery );
