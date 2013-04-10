2c87df44a8ff74306d129d44f1f4bd149bb1e227
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-04-11 00:34:16 +0300

    Controlgroup: Centralize option setting.

--- js/widgets/controlgroup.js
+++ js/widgets/controlgroup.js
@@ -8,7 +8,6 @@
 define( [ "jquery",
 	"../jquery.mobile.buttonMarkup",
 	"./addFirstLastClasses",
-	"./optionDemultiplexer",
 	"../jquery.mobile.widget" ], function( jQuery ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, undefined ) {
@@ -26,50 +25,49 @@ define( [ "jquery",
 		_create: function() {
 			var $el = this.element,
 				inner = $( "<div class='ui-controlgroup-controls'></div>" ),
-				grouplegend = $el.children( "legend" ),
-				o = this.options;
+				grouplegend = $el.children( "legend" );
 
 			// Apply the proto
-			$el.wrapInner( inner );
+			$el.wrapInner( inner ).addClass( "ui-controlgroup" );
 			if ( grouplegend.length ) {
 				$( "<div role='heading' class='ui-controlgroup-label'></div>" ).append( grouplegend ).insertBefore( $el.children( 0 ) );
 			}
-			$el.addClass( "ui-controlgroup" );
 
 			$.extend( this, {
 				_initialRefresh: true
 			});
 
-			// This duplicates the code from the various option setters below for
-			// better performance. It must be kept in sync with those setters.
-			$el
-				.addClass( "ui-controlgroup-" + o.type )
-				.toggleClass( "ui-corner-all", o.corners )
-				.toggleClass( "ui-shadow", o.shadow )
-				.toggleClass( "ui-mini", o.mini );
+			this._setOptions( this.options );
+
 		},
 
 		_init: function() {
 			this.refresh();
 		},
 
-		_setType: function( value ) {
-			this.element
-				.removeClass( "ui-controlgroup-horizontal ui-controlgroup-vertical" )
-				.addClass( "ui-controlgroup-" + value );
-			this.refresh();
-		},
+		_setOptions: function( o ) {
+			var $el = this.element;
 
-		_setCorners: function( value ) {
-			this.element.toggleClass( "ui-corner-all", value );
-		},
+			if ( o.type !== undefined ) {
+				$el
+					.removeClass( "ui-controlgroup-horizontal ui-controlgroup-vertical" )
+					.addClass( "ui-controlgroup-" + o.type );
+				this.refresh();
+			}
 
-		_setShadow: function( value ) {
-			this.element.toggleClass( "ui-shadow", value );
-		},
+			if ( o.corners !== undefined ) {
+				$el.toggleClass( "ui-corner-all", o.corners );
+			}
 
-		_setMini: function( value ) {
-			this.element.toggleClass( "ui-mini", value );
+			if ( o.shadow !== undefined ) {
+				$el.toggleClass( "ui-shadow", o.shadow );
+			}
+
+			if ( o.mini !== undefined ) {
+				$el.toggleClass( "ui-mini", o.mini );
+			}
+
+			return this._super( o );
 		},
 
 		container: function() {
@@ -77,15 +75,16 @@ define( [ "jquery",
 		},
 
 		refresh: function() {
-			var els = this.element.find( ".ui-btn" ).not( ".ui-slider-handle" ),
+			var $el = this.element,
+				els = $el.find( ".ui-btn" ).not( ".ui-slider-handle" ),
 				create = this._initialRefresh;
 			if ( $.mobile.checkboxradio ) {
-				this.element.find( ":mobile-checkboxradio" ).checkboxradio( "refresh" );
+				$el.find( ":mobile-checkboxradio" ).checkboxradio( "refresh" );
 			}
 			this._addFirstLastClasses( els, this.options.excludeInvisible ? this._getVisibles( els, create ) : els, create );
 			this._initialRefresh = false;
 		}
-	}, $.mobile.behaviors.addFirstLastClasses, $.mobile.behaviors.optionDemultiplexer ) );
+	}, $.mobile.behaviors.addFirstLastClasses ) );
 
 	// TODO: Implement a mechanism to allow widgets to become enhanced in the
 	// correct order when their correct enhancement depends on other widgets in
