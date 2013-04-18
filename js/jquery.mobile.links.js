8bc9b1b018f3e8e04a88e1cba2de7a834093dea3
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-04-18 10:11:26 +0300

    links: Needs to depend on core, because it uses .jqmEnhanceable() and the :jqmData() pseudoselector defined in core. (cherry picked from commit 67582c3c48061670fae5c6a88c0d3426973eac88)
    Conflicts:
    	js/jquery.mobile.links.js

--- js/jquery.mobile.links.js
+++ js/jquery.mobile.links.js
@@ -4,7 +4,7 @@
 //>>group: Utilities
 
 
-define( [ "jquery" ], function( jQuery ) {
+define( [ "jquery", "jquery.mobile.core" ], function( jQuery ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, undefined ) {
 
