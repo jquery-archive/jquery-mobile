9d2b1f9799a6b1cd18cc11ab1f6dc2c00744d009
John Bender <john.m.bender@gmail.com>
2013-04-23 11:11:04 -0700

    test that the hash state is used when the original event is a hashchange

--- tests/unit/content/content_core.js
+++ tests/unit/content/content_core.js
@@ -2,20 +2,38 @@
  * Content Widget Tests
  */
 (function($){
-	module("Content Widget");
+	var mockEvent;
+
+	module("Content Widget _filterNavigateEvents", {
+		setup: function() {
+			mockEvent = $.Event( "mock" );
+			mockEvent.originalEvent = $.Event( "hashchange" );
+		}
+	});
 
 	test( "rejects navigate events where the origina event's default is prevented", function(){
-		var mockEvent = $.Event( "mock" );
+		expect( 1 );
 
-		mockEvent.originalEvent = $.Event( "childmock" );
 		mockEvent.originalEvent.isDefaultPrevented = function() {
 			ok( true, "request for default status" );
 			return true;
 		};
 
-		$.mobile.content.prototype._filterNavigateEvents( mockEvent, {} );
 		$.mobile.content.prototype._handleHashChange = function() {
 			throw "should not be called when the original event's default is prevented";
 		};
+
+		$.mobile.content.prototype._filterNavigateEvents( mockEvent, {} );
+	});
+
+	test( "uses the hash in the state when the original event is a hashchange", function() {
+		expect( 2 );
+
+		$.mobile.content.prototype._handleHashChange = function( url, state ) {
+			equal( url, "foo", "the url is the hash stored in the state" );
+			equal( state.hash, url, "hash stored in the state is used as the url" );
+		};
+
+		$.mobile.content.prototype._filterNavigateEvents( mockEvent, { state: { hash: "foo" }} );
 	});
 })(jQuery);
