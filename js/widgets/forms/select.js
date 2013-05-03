d813a545b6bcb6ac7919ec6ceaf2d303fdc0151f
Jasper de Groot <mail@ugomobi.com>
2013-05-03 22:05:01 +0200

    Select: Use ui-fill-x class for the count bubble and use same theme swatch as the select button.

--- js/widgets/forms/select.js
+++ js/widgets/forms/select.js
@@ -131,7 +131,7 @@ $.widget( "mobile.selectmenu", $.mobile.widget, $.extend( {
 		// Add counter for multi selects
 		if ( this.isMultiple ) {
 			this.buttonCount = $( "<span>" )
-				.addClass( "ui-li-count ui-btn-a ui-corner-all" )
+				.addClass( "ui-li-count ui-corner-all ui-fill-" + ( options.theme || "a" ) )
 				.hide()
 				.appendTo( button.addClass( "ui-li-has-count" ) );
 		}
