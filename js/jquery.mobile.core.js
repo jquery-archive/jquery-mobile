c57dfa3d3e33b782d776b9454db49abc59d9358b
Alexander Schmitz <arschmitz@gmail.com>
2013-05-09 08:13:01 -0400

    Core: Update key codes. fix incorrect ones and update to match UI.

--- js/jquery.mobile.core.js
+++ js/jquery.mobile.core.js
@@ -97,40 +97,23 @@ define( [ "jquery", "./jquery.mobile.ns", "json!../package.json" ], function( jQ
 		window: $( window ),
 		document: $( document ),
 
-		// TODO might be useful upstream in jquery itself ?
 		keyCode: {
-			ALT: 18,
 			BACKSPACE: 8,
-			CAPS_LOCK: 20,
 			COMMA: 188,
-			COMMAND: 91,
-			COMMAND_LEFT: 91, // COMMAND
-			COMMAND_RIGHT: 93,
-			CONTROL: 17,
 			DELETE: 46,
 			DOWN: 40,
 			END: 35,
 			ENTER: 13,
 			ESCAPE: 27,
 			HOME: 36,
-			INSERT: 45,
 			LEFT: 37,
-			MENU: 93, // COMMAND_RIGHT
-			NUMPAD_ADD: 107,
-			NUMPAD_DECIMAL: 110,
-			NUMPAD_DIVIDE: 111,
-			NUMPAD_ENTER: 108,
-			NUMPAD_MULTIPLY: 106,
-			NUMPAD_SUBTRACT: 109,
 			PAGE_DOWN: 34,
 			PAGE_UP: 33,
 			PERIOD: 190,
 			RIGHT: 39,
-			SHIFT: 16,
 			SPACE: 32,
 			TAB: 9,
-			UP: 38,
-			WINDOWS: 91 // COMMAND
+			UP: 38
 		},
 
 		// Place to store various widget extensions
