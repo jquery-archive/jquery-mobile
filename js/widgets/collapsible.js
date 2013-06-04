856505571fd484976814d85280f26d139b752915
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-06-04 16:16:40 +0300

    Collapsible: Needs to depend on core, because it uses $.mobile.getAttribute.

--- js/widgets/collapsible.js
+++ js/widgets/collapsible.js
@@ -5,7 +5,12 @@
 //>>css.structure: ../css/structure/jquery.mobile.collapsible.css
 //>>css.theme: ../css/themes/default/jquery.mobile.theme.css
 
-define( [ "jquery", "../jquery.mobile.widget", "../jquery.mobile.buttonMarkup", "../jquery.mobile.registry" ], function( jQuery ) {
+define( [
+	"jquery",
+	"../jquery.mobile.core",
+	"../jquery.mobile.widget",
+	"../jquery.mobile.buttonMarkup",
+	"../jquery.mobile.registry" ], function( jQuery ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, undefined ) {
 
