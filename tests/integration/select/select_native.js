d5566ba393883feea60fcf022e2901f4dcf187bf
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-06-06 10:13:11 +0300

    Integration tests: Select: Removing test for the presence of ui-btn-down-c.

--- tests/integration/select/select_native.js
+++ tests/integration/select/select_native.js
@@ -20,31 +20,6 @@
 		setAndCheck( 'standard' );
 	});
 
-	asyncTest( "selecting a value removes the related buttons down state", function(){
-		var select = $( "#native-select-choice-few" );
-
-		$.testHelper.sequence([
-			function() {
-				// click the native menu parent button
-				select.parent().trigger( 'vmousedown' );
-			},
-
-			function() {
-				ok( select.parent().hasClass("ui-btn-down-c"), "button down class added" );
-			},
-
-			function() {
-				// trigger a change on the select
-				select.trigger( "change" );
-			},
-
-			function() {
-				ok( !select.parent().hasClass("ui-btn-down-c"), "button down class removed" );
-				start();
-			}
-		], 300);
-	});
-
 	// issue https://github.com/jquery/jquery-mobile/issues/2410
 	test( "adding options and refreshing a custom select defaults the text", function() {
 		var select = $( "#custom-refresh" ),
