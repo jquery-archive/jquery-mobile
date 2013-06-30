04061d0a3c6e9d22dbec3376e06299518381c54a
Jasper de Groot <mail@ugomobi.com>
2013-06-30 11:37:59 +0200

    Moved Grunticon preview and loader.txt to demo center.

--- demos/index.php
+++ demos/index.php
@@ -59,7 +59,7 @@
 			<li data-role="list-divider">Theming</li>
 			<li><a href="test/theming/" data-ajax="false">Theme inheritance</a></li>
 			<li data-role="list-divider">Icons</li>
-			<li><a href="test/icons/font-icons.php" data-ajax="false">Font icons</a></li>
+			<li><a href="test/icons/index.html" data-ajax="false">Grunticon - SVG icons with fallback</a></li>
         </ul>
 
 	</div><!-- /content -->
