56c15c70df9651c011c2f41b5bba054b47142ba9
Jasper de Groot <mail@ugomobi.com>
2013-06-30 11:29:10 +0200

    Updated default icons from arrow to carat.

--- js/widgets/forms/select.js
+++ js/widgets/forms/select.js
@@ -13,7 +13,7 @@ $.widget( "mobile.selectmenu", $.extend( {
 	options: {
 		theme: null,
 		disabled: false,
-		icon: "arrow-d",
+		icon: "carat-d",
 		iconpos: "right",
 		inline: false,
 		corners: true,
