d5ba012556e373720e23989b34a8f954938cbbc6
Alexander Schmitz <arschmitz@gmail.com>
2013-07-11 00:54:35 -0400

    Core: add ui core

--- js/jquery.mobile.helpers.js
+++ js/jquery.mobile.helpers.js
@@ -5,7 +5,7 @@
 //>>css.structure: ../css/structure/jquery.mobile.core.css
 //>>css.theme: ../css/themes/default/jquery.mobile.theme.css
 
-define( [ "jquery", "./jquery.mobile.ns", "json!../package.json" ], function( jQuery ) {
+define( [ "jquery", "./jquery.ui.core", "json!../package.json" ], function( jQuery ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, window, undefined ) {
 
