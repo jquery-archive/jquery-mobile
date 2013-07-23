c20d6994ab1affb79b9fe44653e851af9d990b4b
Jasper de Groot <mail@ugomobi.com>
2013-07-23 21:00:03 +0200

    Support: No SVG for Opera except Opera Next. Removed hasFeature() check because we don't suppport Firefox 3.6.

--- js/jquery.mobile.support.js
+++ js/jquery.mobile.support.js
@@ -65,7 +65,7 @@ function validStyle( prop, value, check_vend ) {
 function inlineSVG() {
 	// Thanks Modernizr & Erik Dahlstrom
 	var w = window,
-		svg = !!w.document.createElementNS && !!w.document.createElementNS("http://www.w3.org/2000/svg", "svg").createSVGRect && !!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1"),
+		svg = !!w.document.createElementNS && !!w.document.createElementNS( "http://www.w3.org/2000/svg", "svg" ).createSVGRect && !( w.opera && navigator.userAgent.indexOf( "Chrome" ) === -1 ),
 		support = function( data ) {
 			if ( !( data && svg ) ) {
 				$( "html" ).addClass( "ui-nosvg" );
