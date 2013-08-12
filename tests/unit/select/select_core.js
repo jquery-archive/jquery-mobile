74192aec9af34810d2c983a643a2c0212f4b5f55
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-08-12 11:47:24 +0300

    Unit tests: Select: Make sure selectmenu widget button based on native select that does not have an ID does itself have an ID.

--- tests/unit/select/select_core.js
+++ tests/unit/select/select_core.js
@@ -10,4 +10,10 @@
 		deepEqual( $( "#role-test-listbox a:first" ).attr( "role" ), "button", "The close button for a multiple choice select popup has the " + '"' + "role='button'" + '"' + " set" );
 	});
 
+	module( "Native select" );
+
+	test( "Select menu ID", function() {
+		ok( $( ".no-id-test" ).closest( ".ui-btn" ).attr( "id" ) !== "undefined-button", "Select menu without an ID does not result in the button having name 'undefined-button'" );
+	});
+
 })(jQuery);
