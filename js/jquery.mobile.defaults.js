536f153b1472d240eedec574e76c0d87f1033ed9
Alexander Schmitz <arschmitz@gmail.com>
2013-08-13 08:52:03 -0400

    MinScrollBack: change default to 0 and deprecate

--- js/jquery.mobile.defaults.js
+++ js/jquery.mobile.defaults.js
@@ -51,7 +51,8 @@ define( [ "jquery", "./jquery.mobile.ns", "json!../package.json" ], function( jQ
 		maxTransitionWidth: false,
 
 		// Minimum scroll distance that will be remembered when returning to a page
-		minScrollBack: 250,
+		// Deprecated remove in 1.5 
+		minScrollBack: 0,
 
 		// DEPRECATED: the following property is no longer in use, but defined until 2.0 to prevent conflicts
 		touchOverflowEnabled: false,
