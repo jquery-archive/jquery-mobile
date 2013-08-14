245df8b916d3356edefdecfdc5fcaad4abf3455b
Jasper de Groot <mail@ugomobi.com>
2013-08-14 19:37:32 +0200

    Demos: Filterable inside custom select demo custom dialog style.

--- demos/examples/filterable/filter-inside-selectmenu.php
+++ demos/examples/filterable/filter-inside-selectmenu.php
@@ -67,16 +67,24 @@
 		});
 	</script>
 	<style>
-		.ui-selectmenu .ui-input-search {
+		.ui-selectmenu.ui-popup .ui-input-search {
 			margin-left: .5em;
 			margin-right: .5em;
+		}
+		.ui-selectmenu.ui-dialog .ui-content {
+			padding-top: 0;
+		}
+		.ui-selectmenu.ui-dialog .ui-selectmenu-list {
+			margin-top: 0;
 		}
 		.ui-selectmenu.ui-popup .ui-selectmenu-list li.ui-first-child .ui-btn {
 			border-top-width: 1px;
 			-webkit-border-radius: 0;
 			border-radius: 0;
 		}
-	
+		.ui-selectmenu.ui-dialog .ui-header {
+			border-bottom-width: 1px;
+		}
 	</style>
 </head>
 <body>
