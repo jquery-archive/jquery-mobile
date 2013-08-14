189168bf97f5bbcf6c5889d29b773f5245d8b4f2
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-08-15 00:07:11 +0300

    optionDemultiplexer: Removed.

--- js/index.php
+++ js/index.php
@@ -44,7 +44,6 @@ $files = array(
 	'transitions/visuals/flow.js',
 	'transitions/visuals/turn.js',
 	'jquery.mobile.degradeInputs.js',
-	'widgets/optionDemultiplexer.js',
 	'widgets/dialog.js',
 	'widgets/collapsible.js',
 	'widgets/addFirstLastClasses.js',
