d949a4f873b4f0f16689ae945d96721bbb0656b3
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-08-15 19:14:46 +0300

    Unit tests: individual-modules: Stop using initSelector to instantiate button widgets and string them up with a class instead.

--- tests/unit/individual-modules/button_core.js
+++ tests/unit/individual-modules/button_core.js
@@ -1,5 +1,5 @@
 test( "Button widget works correctly", function() {
-	$( $.mobile.button.prototype.initSelector, "body" ).button();
+	$( ".make-a-button", "body" ).button();
 	deepEqual( $( ":mobile-button" ).length, 3, "Three button widgets were instantiated" );
 	$( "input" ).parent().each( function() {
 		ok( $( this ).hasClass( "ui-btn" ), "Wrapper has class 'ui-btn'" );
