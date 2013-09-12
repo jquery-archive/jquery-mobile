1c01593cefc70fa5058f7731a20d380bfdbfcd58
Anne-Gaelle Colom <coloma@wmin.ac.uk>
2013-09-12 23:30:21 +0100

    jquery-mobile: fixed typos in comments

--- js/jquery.mobile.degradeInputs.js
+++ js/jquery.mobile.degradeInputs.js
@@ -1,5 +1,5 @@
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
-//>>description: Changes input type to another after custom enhancements are made (ex. range > numberic).
+//>>description: Changes input type to another after custom enhancements are made (ex. range > numeric).
 //>>label: Degrade Inputs
 //>>group: Utilities
 
