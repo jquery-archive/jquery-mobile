4647ce2756cf9a505afc877f7a2a5d104c1b273f
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-09-25 11:51:35 +0300

    Nojs: Needs to depend on jquery.mobile.ns.

--- js/jquery.mobile.nojs.js
+++ js/jquery.mobile.nojs.js
@@ -3,7 +3,7 @@
 //>>label: “nojs” Classes
 //>>group: Utilities
 
-define( [ "jquery" ], function( jQuery ) {
+define( [ "jquery", "jquery.mobile.ns" ], function( jQuery ) {
 //>>excludeEnd("jqmBuildExclude");
 (function( $, undefined ) {
 
