bb98ba63bb617c5a62e73d6e614530a5d9b3e23e
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-09-26 00:57:00 +0300

    Demos: Collapsible list items: Simplifying examples.

--- demos/listview-collapsible-item/index.php
+++ demos/listview-collapsible-item/index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Listviews - jQuery Mobile Demos</title>
 	<link rel="shortcut icon" href="../favicon.ico">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<link rel="stylesheet" href="../../css/themes/default/jquery.mobile.css">
 	<link rel="stylesheet" href="../_assets/css/jqm-demos.css">
 	<script src="../../js/jquery.js"></script>
@@ -14,30 +14,31 @@
 	<style id="collapsible-list-item-style">
 /* Basic settings */
 .ui-li-static.ui-collapsible {
-	padding: 0;
+  padding: 0;
 }
 
 .ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview,
 .ui-li-static.ui-collapsible > .ui-collapsible-heading {
-	margin: 0;
+  margin: 0;
 }
 
 .ui-li-static.ui-collapsible > .ui-collapsible-content {
-	padding-top: 0;
-	padding-bottom: 0;
-	padding-right: 0;
-	border-bottom-width: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  padding-right: 0;
+  border-bottom-width: 0;
 }
 
 /* collapse vertical borders */
 .ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview > li.ui-last-child,
 .ui-li-static.ui-collapsible.ui-collapsible-collapsed > .ui-collapsible-heading > a.ui-btn {
-	border-bottom-width: 0;
+  border-bottom-width: 0;
 }
 
 .ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview > li.ui-first-child,
+.ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview > li.ui-first-child > a.ui-btn,
 .ui-li-static.ui-collapsible > .ui-collapsible-heading > a.ui-btn {
-	border-top-width: 0;
+  border-top-width: 0;
 }
 
 /* Remove right borders */
@@ -45,7 +46,7 @@
 .ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview > .ui-li-static,
 .ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview > li > a.ui-btn,
 .ui-li-static.ui-collapsible > .ui-collapsible-content {
-	border-right-width: 0;
+  border-right-width: 0;
 }
 
 /* Remove left borders */
@@ -53,22 +54,22 @@
 .ui-listview-outer > .ui-li-static.ui-collapsible .ui-li-static.ui-collapsible.ui-collapsible,
 .ui-listview-outer > .ui-li-static.ui-collapsible > .ui-collapsible-heading > a.ui-btn,
 .ui-li-static.ui-collapsible > .ui-collapsible-content {
-	border-left-width: 0;
+  border-left-width: 0;
 }
 	</style>
 	<style id="flat-sublist-style">
 /* Additional rules for flat sublists */
 /* Remove some more left borders and paddings for flat sublists */
 .ui-listview-flat-sublists .ui-li-static.ui-collapsible > .ui-collapsible-content {
-	padding-left: 0;
+  padding-left: 0;
 }
 
 .ui-listview-outer.ui-listview-flat-sublists .ui-li-static.ui-collapsible > .ui-collapsible-heading > a.ui-btn,
 .ui-listview-outer.ui-listview-flat-sublists .ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview > .ui-li-static,
 .ui-listview-outer.ui-listview-flat-sublists .ui-li-static.ui-collapsible > .ui-collapsible-content > .ui-listview > li > a.ui-btn {
-	border-left-width: 0;
+  border-left-width: 0;
 }
-	</style>
+  </style>
 </head>
 <body>
 <div data-role="page" class="jqm-demos" data-quicklinks="true">
@@ -82,157 +83,110 @@
 
 	<div data-role="content" class="jqm-content">
 
-	<h1>Collapsible list item</h1>
+	<h1>Collapsible list items</h1>
 
-	<p>You can render list items collapsible by instantiating collapsible widgets on them and adding some custom CSS to collapse borders. In all the examples below, the outermost listview has class <code>ui-listview-outer</code> to identify it as the outermost listview in the tree structure of indented lists.</p>
+	<p>You can render list items collapsible by instantiating collapsible widgets on them and adding some custom CSS to collapse borders and padding. In all the examples below, the outermost listview has class <code>ui-listview-outer</code> to identify it as the outermost listview in the tree structure of indented lists.</p>
 
-	<h2>Regular istview</h2>
+	<h2>Regular listview</h2>
 
 	<div data-demo-html="true" data-demo-css="#collapsible-list-item-style">
     <ul data-role="listview" class="ui-listview-outer">
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Sublist title</h2>
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Birds</h2>
         <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Some Text</li>
-          <li><a href="#">Some Clickable Text</a></li>
-          <li><a href="#">Some More Clickable Text</a></li>
-          <li>Some More Text</li>
+          <li>Condor</li>
+          <li><a href="#">Eagle</a></li>
+          <li>Sparrow</li>
         </ul>
       </li>
-      <li><a href="#">Outer Clickable Text</a></li>
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Sublist title<p>Small text</p></h2>
+      <li><a href="#">Humans</a></li>
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Fish</h2>
         <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Some Text</li>
-          <li><a href="#">Some Clickable Text</a></li>
-          <li><a href="#">Some More Clickable Text</a></li>
-          <li>Some More Text</li>
-        </ul>
-      </li>
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Another Sublist</h2>
-        <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Another Sublist Item</li>
-          <li><a href="#">Clickable Sublist Item</a></li>
-          <li><a href="#">Another Clickable Sublist Item</a></li>
-          <li>Second Item In Another Sublist</li>
-          <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-            <h2>A Sub-sublist</h2>
-            <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-              <li>A Sub-sublist Item</li>
-            	<li><a href="#">Clickable Sub-sublist Item</a></li>
-              <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-                <h2>A Sub-sub-sublist</h2>
-                <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-                  <li>A sub-sub-sublist Item</li>
-                </ul>
-              </li>
-              <li><a href="#">Another Clickable Sub-sublist Item</a></li>
-              <li>Another Sub-sublist Item</li>
-            </ul>
-          </li>
+          <li><a href="#">Salmon</a></li>
+          <li>Pollock</li>
+          <li>Trout</li>
         </ul>
       </li>
     </ul>
-  </div>
+	</div>
 
-	<h2>Inset istview</h2>
+	<h2>Inset listview</h2>
 
 	<div data-demo-html="true" data-demo-css="#collapsible-list-item-style">
-    <ul data-role="listview" data-inset="true" class="ui-listview-outer">
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Sublist title</h2>
+    <ul data-role="listview" class="ui-listview-outer" data-inset="true">
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Birds</h2>
         <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Some Text</li>
-          <li><a href="#">Some Clickable Text</a></li>
-          <li><a href="#">Some More Clickable Text</a></li>
-          <li>Some More Text</li>
+          <li>Condor</li>
+          <li><a href="#">Eagle</a></li>
+          <li>Sparrow</li>
         </ul>
       </li>
-      <li><a href="#">Outer Clickable Text</a></li>
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Sublist title<p>Small text</p></h2>
+      <li><a href="#">Humans</a></li>
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Fish</h2>
         <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Some Text</li>
-          <li><a href="#">Some Clickable Text</a></li>
-          <li><a href="#">Some More Clickable Text</a></li>
-          <li>Some More Text</li>
-        </ul>
-      </li>
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Another Sublist</h2>
-        <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Another Sublist Item</li>
-          <li><a href="#">Clickable Sublist Item</a></li>
-          <li><a href="#">Another Clickable Sublist Item</a></li>
-          <li>Second Item In Another Sublist</li>
-          <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-            <h2>A Sub-sublist</h2>
-            <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-              <li>A Sub-sublist Item</li>
-            	<li><a href="#">Clickable Sub-sublist Item</a></li>
-              <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-                <h2>A Sub-sub-sublist</h2>
-                <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-                  <li>A sub-sub-sublist Item</li>
-                </ul>
-              </li>
-              <li><a href="#">Another Clickable Sub-sublist Item</a></li>
-              <li>Another Sub-sublist Item</li>
-            </ul>
-          </li>
+          <li><a href="#">Salmon</a></li>
+          <li>Pollock</li>
+          <li>Trout</li>
         </ul>
       </li>
     </ul>
-  </div>
+	</div>
 
 	<h2>Flat sublists</h2>
-	<p>With a few more rules you can specify that sublists are not to be indented. In the example below sublist indentation is on by default. You flatten sublists by adding the class <code>ui-flat-sublists</code> to the parent listview.</p>
+	<p>With a little more CSS, you can create class <code>ui-listview-flat-sublists</code> that will cause all the sublists of a listview to which it is applied to display without indentation. In the example below, the first list item contains a list that has indented sublists. The second list item contains a list that has flat sublists.</p>
 
 	<div data-demo-html="true" data-demo-css="#collapsible-list-item-style,#flat-sublist-style">
-    <ul data-role="listview" class="ui-listview-outer">
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Sublist title</h2>
+    <ul data-role="listview" class="ui-listview-outer" data-inset="true" data-shadow="false">
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Indented Sublists</h2>
+
+    <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Birds</h2>
         <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Some Text</li>
-          <li><a href="#">Some Clickable Text</a></li>
-          <li><a href="#">Some More Clickable Text</a></li>
-          <li>Some More Text</li>
+          <li>Condor</li>
+          <li><a href="#">Eagle</a></li>
+          <li>Sparrow</li>
         </ul>
       </li>
-      <li><a href="#">Outer Clickable Text</a></li>
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Sublist title<p>Small text</p></h2>
+      <li><a href="#">Humans</a></li>
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Fish</h2>
         <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Some Text</li>
-          <li><a href="#">Some Clickable Text</a></li>
-          <li><a href="#">Some More Clickable Text</a></li>
-          <li>Some More Text</li>
+          <li><a href="#">Salmon</a></li>
+          <li>Pollock</li>
+          <li>Trout</li>
         </ul>
       </li>
-      <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-        <h2>Another Sublist</h2>
+    </ul>
+
+      </li>
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Flat Sublists</h2>
+
+    <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false" class="ui-listview-flat-sublists">
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Birds</h2>
         <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-          <li>Another Sublist Item</li>
-          <li><a href="#">Clickable Sublist Item</a></li>
-          <li><a href="#">Another Clickable Sublist Item</a></li>
-          <li>Second Item In Another Sublist</li>
-          <li data-role="collapsible" class="ui-listview-flat-sublists" data-iconpos="right" data-collapsed="false" data-shadow="false" data-corners="false">
-            <h2>A Sub-sublist (with flat sublists)</h2>
-            <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-              <li>A Sub-sublist Item</li>
-            	<li><a href="#">Clickable Sub-sublist Item</a></li>
-              <li data-role="collapsible" data-iconpos="right" data-collapsed="false" data-role="listview" data-shadow="false" data-inset="true" data-corners="false" class="ui-listview-indented-sublists">
-                <h2>A Sub-sub-sublist</h2>
-                <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
-                  <li>A sub-sub-sublist Item</li>
-                </ul>
-              </li>
-              <li><a href="#">Another Clickable Sub-sublist Item</a></li>
-              <li>Another Sub-sublist Item</li>
-            </ul>
-          </li>
+          <li>Condor</li>
+          <li><a href="#">Eagle</a></li>
+          <li>Sparrow</li>
         </ul>
+      </li>
+      <li><a href="#">Humans</a></li>
+      <li data-role="collapsible" data-iconpos="right" data-shadow="false" data-corners="false">
+        <h2>Fish</h2>
+        <ul data-role="listview" data-shadow="false" data-inset="true" data-corners="false">
+          <li><a href="#">Salmon</a></li>
+          <li>Pollock</li>
+          <li>Trout</li>
+        </ul>
+      </li>
+    </ul>
+
       </li>
     </ul>
 	</div>
