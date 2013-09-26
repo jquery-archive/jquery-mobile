573359f5e00e39e5b62cf18fb8494d4818ce8dc0
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-09-26 11:42:42 +0300

    Demos: External toolbar: Ajax -> AJAX.

--- demos/toolbar-external/index.php
+++ demos/toolbar-external/index.php
@@ -44,11 +44,11 @@
 
 		<p>Because these toolbars are not within the page they will not auto initalize. You must call the toolbar plugin yourself on document ready.</p>
 
-		<h2>Ajax Navigation</h2>
+		<h2>AJAX Navigation</h2>
 
 		<p>Because these toolbars are not within the page they will remain in the DOM until manually removed. However they will automatically hide if you navigate to a page containing a toolbar to allow the pages own toolbars to take their place. They will automatically show again if you navigate to a page containing no toolbars.</p>
 
-		<p>Toolbars not within a page will not be pulled into the DOM during Ajax navigation.</p>
+		<p>Toolbars not within a page will not be pulled into the DOM during AJAX navigation.</p>
 
 		<h2>Fixed External Toolbars</h2>
 
