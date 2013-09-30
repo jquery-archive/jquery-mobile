1d456e533c35553f75cc5c1e771ad9fed83f5b32
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-09-30 11:36:20 +0300

    Pagecontainer: Separate emission of non-deprecated "pagecontainerload" from deprecated "pageload", move "pagecontainerload" in front of DOM insertion and page enhancement, and for both events bail if the default has been prevented.

--- js/widgets/pagecontainer.js
+++ js/widgets/pagecontainer.js
@@ -523,6 +523,23 @@ define( [
 
 				this._setLoadedTitle( content, html );
 
+				// Add the content reference and xhr to our triggerData.
+				triggerData.xhr = xhr;
+				triggerData.textStatus = textStatus;
+
+				// DEPRECATED
+				triggerData.page = content;
+
+				triggerData.content = content;
+
+				// If the default behavior is prevented, stop here!
+				// Note that it is the responsibility of the listener/handler
+				// that called preventDefault(), to resolve/reject the
+				// deferred object within the triggerData.
+				if ( !this._trigger( "load", undefined, triggerData ) ) {
+					return;
+				}
+
 				// rewrite src and href attrs to use a base url if the base tag won't work
 				if ( this._isRewritableBaseTag() && content ) {
 					this._getBase().rewrite( fileUrl, content );
@@ -542,17 +559,10 @@ define( [
 					this._hideLoading();
 				}
 
-				// Add the content reference and xhr to our triggerData.
-				triggerData.xhr = xhr;
-				triggerData.textStatus = textStatus;
-
-				// DEPRECATED
-				triggerData.page = content;
-
-				triggerData.content = content;
-
+				// BEGIN DEPRECATED ---------------------------------------------------
 				// Let listeners know the content loaded successfully.
-				this._triggerWithDeprecated( "load", triggerData );
+				this.element.trigger( "pageload" );
+				// END DEPRECATED -----------------------------------------------------
 
 				deferred.resolve( absUrl, settings, content );
 			}, this);
