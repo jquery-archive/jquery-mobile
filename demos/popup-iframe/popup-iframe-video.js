85b37070c003dc2404cd40fb7086eb64495b7891
Jasper de Groot <mail@ugomobi.com>
2013-10-04 16:26:31 +0200

    Demos: Updated popup demos.

--- demos/popup-iframe/popup-iframe-video.js
+++ demos/popup-iframe/popup-iframe-video.js
@@ -1,13 +1,7 @@
 // popup examples
 $( document ).on( "pagecreate", function() {
 
-	$( ".photopopup" ).on({
-		popupbeforeposition: function() {
-			var maxHeight = $( window ).height() - 60 + "px";
-			$( ".photopopup img" ).css( "max-height", maxHeight );
-		}
-	});
-
+	// The window width and height are decreased by 30 to take the tolerance of 15 pixels at each side into account
 	function scale( width, height, padding, border ) {
 		var scrWidth = $( window ).width() - 30,
 			scrHeight = $( window ).height() - 30,
@@ -55,44 +49,4 @@ $( document ).on( "pagecreate", function() {
 				.attr( "height", 0 );	
 		}
 	});
-
-	$( "#popupMap iframe" ).contents().find( "#map_canvas" )
-		.css( { "width" : 0, "height" : 0 } );
-	 		 
-	$( "#popupMap" ).on({
-		popupbeforeposition: function() {
-			var size = scale( 480, 320, 0, 1 ),
-				w = size.width,
-				h = size.height;
-
-			$( "#popupMap iframe" )
-				.attr( "width", w )
-				.attr( "height", h );
-					 
-			$( "#popupMap iframe" ).contents().find( "#map_canvas" )
-				.css( { "width": w, "height" : h } );
-		},
-		popupafterclose: function() {
-			$( "#popupMap iframe" )
-				.attr( "width", 0 )
-				.attr( "height", 0 );
-					 
-			$( "#popupMap iframe" ).contents().find( "#map_canvas" )
-				.css( { "width": 0, "height" : 0 } );
-		}
-	});
-      
-	$( "#popupPanel" ).on({
-		popupbeforeposition: function() {
-			var h = $( window ).height();
-			
-			$( "#popupPanel" )
-				.css( "height", h );
-		}
-	});
-		 
-	$( "#popupPanel button" ).on( "click", function() {	
-		$( "#popupPanel" ).popup('close');
-	});
-
 });
--- demos/popup-image-scaling/index.php
+++ demos/popup-image-scaling/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>jQuery Mobile - Popup images</title>
+	<title>Popup image scaling - jQuery Mobile Demos</title>
 	<link rel="shortcut icon" href="../favicon.ico">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<link rel="stylesheet" href="../../css/themes/default/jquery.mobile.css">
@@ -11,8 +11,7 @@
 	<script src="../../js/jquery.js"></script>
 	<script src="../_assets/js/"></script>
 	<script src="../../js/"></script>
-	<script src="popup-examples.js"></script>
-	<link rel="stylesheet" href="popup-examples.css">
+	<script src="popup-image-scaling.js" id="script"></script>
 </head>
 <body>
 <div data-role="page" class="jqm-demos">
@@ -30,32 +29,20 @@
 
 		<p>The framework CSS contains rules that make images that are immediate children of the popup scale to fit the screen. Because of the absolute positioning of the popup container and screen, the height is not adjusted to screen height on all browsers. You can prevent vertical scrolling with a simple script that sets the <code>max-height</code> of the image.</p>
 
-		<p>In the two examples below the divs with <code>data-role="popup"</code> have a class of <code>photopopup</code>.</p>
-
-		<a href="#popupPhotoLandscape" data-rel="popup" data-position-to="window" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-inline">Photo landscape</a>
-		<a href="#popupPhotoPortrait" data-rel="popup" data-position-to="window" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="fade">Photo portrait</a>
-
-		<div data-role="popup" id="popupPhotoLandscape" class="photopopup" data-overlay-theme="a" data-corners="false" data-tolerance="30,15" >
-			<a href="#" data-rel="back" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a><img src="../_assets/img/photo-landscape.jpg" alt="Photo landscape">
-		</div>
-
-		<div data-role="popup" id="popupPhotoPortrait" class="photopopup" data-overlay-theme="a" data-corners="false" data-tolerance="30,15" >
-			<a href="#" data-rel="back" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a><img src="../_assets/img/photo-portrait.jpg" alt="Photo portrait">
-		</div>
-
 		<p>The handler is bound to the <code>popupbeforeposition</code> event, which ensures the image is not only scaled before it is shown but also when the window is resized (e.g. orientation change). In this code example the height is reduced by 60 to take a top and bottom tolerance of 30 pixels into account.</p>
 
-<pre><code>
-$( document ).on( "pagecreate", function() {
-    $( ".photopopup" ).on({
-        popupbeforeposition: function() {
-            var maxHeight = $( window ).height() - 60 + "px";
-            $( ".photopopup img" ).css( "max-height", maxHeight );
-        }
-    });
-});
+		<div data-demo-html="true" data-demo-js="#script">
+			<a href="#popupPhotoLandscape" data-rel="popup" data-position-to="window" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-inline">Photo landscape</a>
+			<a href="#popupPhotoPortrait" data-rel="popup" data-position-to="window" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-inline" data-transition="fade">Photo portrait</a>
 
-</code></pre>
+			<div data-role="popup" id="popupPhotoLandscape" class="photopopup" data-overlay-theme="a" data-corners="false" data-tolerance="30,15">
+				<a href="#" data-rel="back" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a><img src="../_assets/img/photo-landscape.jpg" alt="Photo landscape">
+			</div>
+
+			<div data-role="popup" id="popupPhotoPortrait" class="photopopup" data-overlay-theme="a" data-corners="false" data-tolerance="30,15">
+				<a href="#" data-rel="back" role="button" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a><img src="../_assets/img/photo-portrait.jpg" alt="Photo portrait">
+			</div>
+		</div><!--/demo-html -->
 
 	</div><!-- /content -->
 
