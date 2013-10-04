7d5de8a951dfcd8d4ae7cb420712c6a274686ed2
Jasper de Groot <mail@ugomobi.com>
2013-10-04 21:52:47 +0200

    Demos: Changes in external toolbar demos.

--- demos/toolbar-fixed-external/index.php
+++ demos/toolbar-fixed-external/index.php
@@ -18,7 +18,7 @@
 	</script>
 </head>
 <body>
-	<div data-role="header" data-theme="a">
+	<div data-role="header" data-position="fixed" data-theme="a">
 			<a href="../toolbar/" data-rel="back" class="ui-btn ui-btn-left ui-alt-icon ui-nodisc-icon ui-corner-all ui-btn-icon-notext ui-icon-carat-l">Back</a>
 		<h1>External fixed header</h1>
 	</div><!-- /header -->
@@ -27,21 +27,35 @@
 
 		<div data-role="content" class="jqm-content">
 
-			<h1>Fixed external headers</h1>
+			<h1>Fixed external toolbars</h1>
 
 			<p>The header and footer on this page are outside of the page within the body. They have been set to position fixed.</p>
 
+			<h2>Auto init</h2>
+
+			<p>Because these toolbars are not within the page they will not auto initalize. You must call the toolbar plugin yourself.</p>
+
+<pre><code>
+$(function(){
+	$( "[data-role='header'], [data-role='footer']" ).toolbar();
+});
+</code></pre>
+
+			<h2>Theme</h2>
+
+			<p>Since external toolbars are outside the page they don't inherit a theme from the page. This means you always have to set a theme for them. You can use the <code>data-theme</code> attribute for this or set the <code>theme</code> option when you call the plugin:
+
 			<h2>True persistant toolbars</h2>
 
-			<p>Because External toolbars are outside of the page they are not effected by transition and make truely persistant toolbars possible.</p>
+			<p>Because External toolbars are outside of the page they are not effected by transition and make truly persistant toolbars possible.</p>
 
-			<p><a href="../toolbar-fixed-persistent/" data-ajax="false">Persistant footer example</a></p>
+			<p><a href="../toolbar-fixed-persistent/" data-ajax="false">Persistant toolbar demo</a></p>
 
 		</div><!-- /content -->
 
 	</div><!-- /page -->
 
-	<div data-role="footer" data-theme="a">
+	<div data-role="footer" data-position="fixed" data-theme="a">
 		<h1>External fixed footer</h1>
 	</div><!-- /footer -->
 
