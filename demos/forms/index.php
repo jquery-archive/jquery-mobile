fb159116abd07b101a654dd9a390a7c2eb2e805b
Jasper de Groot <mail@ugomobi.com>
2013-10-16 12:34:23 +0200

    Demos: Reorganized form demo pages.

--- demos/forms/index.php
+++ demos/forms/index.php
@@ -35,7 +35,7 @@
 
         <h2>Buttons</h2>
 
-		<p>Buttons are used within a wide range of other plugins. The button markup is flexible and can be created from links or form buttons. Learn more about <a href="../button-markup/">button markup</a> and <a href="../button-markup/">input buttons</a>.</p>
+		<p>Buttons are used within a wide range of other plugins. The button markup is flexible and can be created from links or form buttons. Learn more about <a href="../button-markup/">button markup</a> and <a href="../button/">input buttons</a>.</p>
         <div data-demo-html="true">
             <a href="#" class="ui-shadow ui-btn ui-corner-all">Link button</a>
             <button class="ui-shadow ui-btn ui-corner-all">Button element</button>
@@ -79,7 +79,7 @@
 
         <h2>Range slider</h2>
 
-		<p>Range sliders offer two handles to set a min and max value along a numeric continuum. Learn more about <a href="../sliders/rangeslider.php" data-ajax="false">range sliders</a>.</p>
+		<p>Range sliders offer two handles to set a min and max value along a numeric continuum. Learn more about <a href="../rangeslider/" data-ajax="false">range sliders</a>.</p>
 
         <div data-demo-html="true">
         <form>
@@ -247,7 +247,7 @@
 
         <h2>Text inputs & Textareas</h2>
 
-		<p>Text inputs and textareas are coded with standard HTML elements, then enhanced by jQuery Mobile to make them more attractive and useable on a mobile device. Learn more about <a href="../textinputs/">text inputs and textareas</a>.</p>
+		<p>Text inputs and textareas are coded with standard HTML elements, then enhanced by jQuery Mobile to make them more attractive and useable on a mobile device. Learn more about <a href="../textinput/">text inputs and textareas</a>.</p>
 
         <div data-demo-html="true">
              <label for="text-basic">Text input:</label>
@@ -293,7 +293,7 @@
 
 		<h2>Accessibly hidden labels</h2>
 
-		<p>For the sake of accessibility, jQuery Mobile requires that all form elements be paired with a meaningful <code>label</code>. To hide labels in a way that leaves them visible to assistive technologies &mdash; for example, when letting an element's <code>placeholder</code> attribute serve as a label &mdash; apply the helper class <code>ui-hidden-accessible</code> to the label. View more examples of <a href="form-label-hidden.php">accessibly hidden labels</a>. In case you use a field container you can use class <code>ui-hide-label</code> on the element with <code>class="ui-field-contain"</code> to <a href="form-hide-label.php">hide the label or legend</a>.</p>
+		<p>For the sake of accessibility, jQuery Mobile requires that all form elements be paired with a meaningful <code>label</code>. To hide labels in a way that leaves them visible to assistive technologies &mdash; for example, when letting an element's <code>placeholder</code> attribute serve as a label &mdash; apply the helper class <code>ui-hidden-accessible</code> to the label. View more examples of <a href="../forms-label-hidden-accessible/">accessibly hidden labels</a>.</p>
 
 		<div data-demo-html="true">
 			<label for="textinput-hide" class="ui-hidden-accessible">Text Input:</label>
@@ -302,7 +302,7 @@
 
 		<h2>Disabling form elements</h2>
 
-		<p>All jQuery Mobile widgets can be disabled in the markup by adding the standard <code>disabled</code> attribute to the element, just like you would with native controls. To dynamically disable or enable them, each form widget also has standard <code>disable</code> and <code>enable</code> methods that are documented with each form widget. View more examples of <a href="form-disabled.php" >disabled form elements</a>.</p>
+		<p>All jQuery Mobile widgets can be disabled in the markup by adding the standard <code>disabled</code> attribute to the element, just like you would with native controls. To dynamically disable or enable them, each form widget also has standard <code>disable</code> and <code>enable</code> methods that are documented with each form widget. View more examples of <a href="../forms-disabled/" >disabled form elements</a>.</p>
 
 		<div data-demo-html="true">
 			<div class="ui-field-contain">
@@ -315,7 +315,7 @@
 
 		<h2>Fieldcontainer groupings</h2>
 
-		<p>To improve the styling of labels and form elements on wider screens, wrap a <code>div</code> or <code>fieldset </code>with the <code>class="ui-field-contain"</code> attribute around each label/form element. This framework aligns the input and associated label side-by-side, and breaks to stacked block-level elements below ~448px. The framework will also add a thin bottom border to act as a field separator. See more examples of <a href="form-fieldcontain.php">fieldcontainer groupings</a>.</p>
+		<p>To improve the styling of labels and form elements on wider screens, wrap a <code>div</code> or <code>fieldset </code>with the <code>class="ui-field-contain"</code> attribute around each label/form element. This framework aligns the input and associated label side-by-side, and breaks to stacked block-level elements below ~448px. The framework will also add a thin bottom border to act as a field separator. See more examples of <a href="../forms-field-contain/">fieldcontainer groupings</a>.</p>
 
 		<div data-demo-html="true">
 			<div class="ui-field-contain">
