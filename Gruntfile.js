05db267194fb2ac4bf59223ed1c4fdae09736c6f
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-10-22 00:34:39 +0300

    Gruntfile.js: Re-order modules specified via the --modules option such that their order is the same as the order of modules in js/jquery.mobile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,65 @@ module.exports = function( grunt ) {
 	"use strict";
 
 	var _ = grunt.util._,
+
+		// Ensure that modules specified via the --modules option are in the same
+		// order as the one in which they appear in js/jquery.mobile.js. To achieve
+		// this, we parse js/jquery.mobile.js and reconstruct the array of
+		// dependencies listed therein.
+		makeModulesList = function( modules ) {
+			var start, end, index,
+				modulesHash = {},
+				fixedModules = [],
+				jsFile = grunt.file.read( path.join( "js", "jquery.mobile.js" ) );
+
+			modules = modules.split( "," );
+
+			// This is highly dependent on the contents of js/jquery.mobile.js
+			if ( jsFile ) {
+				start = jsFile.indexOf( "[" );
+				if ( start > -1 ) {
+					start++;
+					end = jsFile.indexOf( "]" );
+					if ( start < jsFile.length &&
+						end > -1 && end < jsFile.length && end > start ) {
+
+						// Convert list of desired modules to a hash
+						for ( index = 0 ; index < modules.length ; index++ ) {
+							modulesHash[ modules[ index ] ] = true;
+						}
+
+						// Split list of modules from js/jquery.mobile.js into an array
+						jsFile = jsFile
+							.slice( start, end )
+							.match( /"[^"]*"/gm );
+
+						// Add each desired module to the fixed list of modules in the
+						// correct order
+						for ( index = 0 ; index < jsFile.length ; index++ ) {
+
+							// First we need to touch up each module from js/jquery.mobile.js
+							jsFile[ index ] = jsFile[ index ]
+								.replace( /"/g, "" )
+								.replace( /^.\//, "" );
+
+							// Then, if it's in the hash of desired modules, add it to the
+							// list containing the desired modules in the correct order
+							if ( modulesHash[ jsFile[ index ] ] ) {
+								fixedModules.push( jsFile[ index ] );
+							}
+						}
+
+						// If we've found all the desired modules, we re-create the comma-
+						// separated list and return it.
+						if ( fixedModules.length === modules.length ) {
+							modules = fixedModules;
+						}
+					}
+				}
+			}
+
+			return modules;
+		},
 		path = require( "path" ),
 		httpPort =  Math.floor( 9000 + Math.random()*1000 ),
 		name = "jquery.mobile",
@@ -186,7 +245,9 @@ module.exports = function( grunt ) {
 
 					mainConfigFile: "js/requirejs.config.js",
 
-					include: ( grunt.option( "modules" ) || "jquery.mobile" ).split( "," ),
+					include: ( grunt.option( "modules" ) ?
+						makeModulesList( grunt.option( "modules" ) ) :
+						[ "jquery.mobile" ] ),
 
 					exclude: [
 						"jquery",
