4df822f45cbda1261149c9884313b3f87e21b402
Jasper de Groot <mail@ugomobi.com>
2013-10-21 09:41:12 +0200

    Demos: Replaced deprecated code in panel swipe open demo.

--- demos/panel-swipe-open/index.php
+++ demos/panel-swipe-open/index.php
@@ -18,8 +18,8 @@
 				// We check if there is no open panel on the page because otherwise
 				// a swipe to close the left panel would also open the right panel (and v.v.).
 				// We do this by checking the data that the framework stores on the page element (panel: open).
-				if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
-					if ( e.type === "swipeleft"  ) {
+				if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+					if ( e.type === "swipeleft" ) {
 						$( "#right-panel" ).panel( "open" );
 					} else if ( e.type === "swiperight" ) {
 						$( "#left-panel" ).panel( "open" );
