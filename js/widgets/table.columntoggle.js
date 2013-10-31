cee4204136151226084c116e6472638c1a62cdd8
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-10-31 19:56:26 +0200

    Columntoggle table: Use delegation to attach events to the checkboxes
    Fixes #6683

--- js/widgets/table.columntoggle.js
+++ js/widgets/table.columntoggle.js
@@ -41,7 +41,6 @@ $.widget( "mobile.table", $.mobile.table, {
 		if ( this.options.enhanced ) {
 			this._menu = $( this.document[ 0 ].getElementById( this._id() + "-popup" ) ).children().first();
 			this._addToggles( this._menu, true );
-			this._bindToggles( this._menu );
 		} else {
 			this._menu = this._enhanceColToggle();
 			this.element.addClass( this.options.classes.columnToggleTable );
@@ -61,17 +60,8 @@ $.widget( "mobile.table", $.mobile.table, {
 		// so it can be called on refresh, too
 
 		// update column toggles on resize
-		this._on( this.window, {
-			throttledresize: "_setToggleState"
-		});
-	},
-
-	_bindToggles: function( menu ) {
-		var inputs = menu.find( "input" );
-
-		this._on( inputs, {
-			change: "_menuInputChange"
-		});
+		this._on( this.window, { throttledresize: "_setToggleState" } );
+		this._on( this._menu, { "change input": "_menuInputChange" } );
 	},
 
 	_addToggles: function( menu, keep ) {
@@ -113,7 +103,6 @@ $.widget( "mobile.table", $.mobile.table, {
 		// set bindings here
 		if ( !keep ) {
 			menu.controlgroup( "refresh" );
-			this._bindToggles( menu );
 		}
 	},
 
