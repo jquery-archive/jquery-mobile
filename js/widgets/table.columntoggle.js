f61d376398ba81f7f7fa3d6f84cb9812ebfc255a
Kangsik Kim <allsteady@gmail.com>
2013-11-05 09:43:28 +0900

    Table: fix the bug of refresh method in table widget
    Closes gh-6698
    Fixes gh-7090

--- js/widgets/table.columntoggle.js
+++ js/widgets/table.columntoggle.js
@@ -178,7 +178,7 @@ $.widget( "mobile.table", $.mobile.table, {
 
 		if ( !create && this.options.mode === "columntoggle" ) {
 			// columns not being replaced must be cleared from input toggle-locks
-			this._unlockCells( this.allHeaders );
+			this._unlockCells( this.element.find( ".ui-table-cell-hidden, .ui-table-cell-visible" ) );
 
 			// update columntoggles and cells
 			this._addToggles( this._menu, create );
