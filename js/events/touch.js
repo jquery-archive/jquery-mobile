46dfbb839da833398e4972af7c54e191a724522c
Dimitri Benin <semtexx@web.de>
2013-11-11 01:17:53 +0100

    fixes the problem that touchend events were stopped (preventing vmouseup-s) if taphold is set to prevent a following tap event

--- js/events/touch.js
+++ js/events/touch.js
@@ -113,7 +113,7 @@ define( [ "jquery", "../jquery.mobile.vmouse", "../jquery.mobile.support.touch" 
 					if ( !isTaphold && origTarget === event.target ) {
 						triggerCustomEvent( thisObject, "tap", event );
 					} else if ( isTaphold ) {
-						event.stopPropagation();
+						event.preventDefault();
 					}
 				}
 
