41cd7b4863527ef81d609e9345c5fccdff779318
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-11-14 14:42:01 +0200

    Demos: Flipswitch: Add examples using custom indentation and custom width. Fixes #6729 - Demo flipswitch with custom label

--- demos/flipswitch/index.php
+++ demos/flipswitch/index.php
@@ -11,6 +11,41 @@
 	<script src="../../js/jquery.js"></script>
 	<script src="../_assets/js/"></script>
 	<script src="../../js/"></script>
+	<style id="custom-label-flipswitch">
+/* Custom indentations are needed because the length of custom labels differs from
+   the length of the standard labels */
+.custom-label-flipswitch.ui-flipswitch .ui-btn.ui-flipswitch-on {
+	text-indent: -3.4em;
+}
+.custom-label-flipswitch.ui-flipswitch .ui-flipswitch-off {
+	text-indent: 0.5em;
+}
+	</style>
+	<style id="custom-size-flipswitch">
+/* Custom indentations are needed because the length of custom labels differs from
+   the length of the standard labels */
+.custom-size-flipswitch.ui-flipswitch .ui-btn.ui-flipswitch-on {
+	text-indent: -5.9em;
+}
+.custom-size-flipswitch.ui-flipswitch .ui-flipswitch-off {
+	text-indent: 0.5em;
+}
+/* Custom widths are needed because the length of custom labels differs from
+   the length of the standard labels */
+.custom-size-flipswitch.ui-flipswitch {
+	width: 8.875em;
+}
+.custom-size-flipswitch.ui-flipswitch.ui-flipswitch-active {
+	padding-left: 7em;
+	width: 1.875em;
+}
+@media (min-width: 28em) {
+	// Repeated from rule .ui-flipswitch above
+	.ui-field-contain > label + .custom-size-flipswitch.ui-flipswitch {
+		width: 1.875em;
+	}
+}
+	</style>
 </head>
 <body>
 <div data-role="page" class="jqm-demos" data-quicklinks="true">
@@ -35,6 +70,24 @@
 				<form>
                     <label for="flip-checkbox">Flip toggle switch checkbox:</label>
                     <input type="checkbox" data-role="flipswitch" name="flip-checkbox" id="flip-checkbox">
+				</form>
+                </div><!-- /demo-html -->
+
+			<h2>Basic checkbox switch with custom labels</h2>
+
+                <div data-demo-html="true" data-demo-css="#custom-label-flipswitch">
+				<form>
+                    <label for="flip-checkbox">Flip toggle switch checkbox:</label>
+                    <input type="checkbox" data-role="flipswitch" name="flip-checkbox" id="flip-checkbox" data-on-text="Light" data-off-text="Dark" data-wrapper-class="custom-label-flipswitch">
+				</form>
+                </div><!-- /demo-html -->
+
+			<h2>Basic checkbox switch with custom labels and custom size</h2>
+
+                <div data-demo-html="true" data-demo-css="#custom-size-flipswitch">
+				<form>
+                    <label for="flip-checkbox">Flip toggle switch checkbox:</label>
+                    <input type="checkbox" data-role="flipswitch" name="flip-checkbox" id="flip-checkbox" data-on-text="Assured" data-off-text="Uncertain" data-wrapper-class="custom-size-flipswitch">
 				</form>
                 </div><!-- /demo-html -->
 
