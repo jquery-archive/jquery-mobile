362e3fce79faa436674d07bd92dfb0950e933e86
Ghislain Seguin <ghislain@gmail.com>
2013-12-27 14:47:43 -0700

    Removed "use strict" from client code
    As per http://bugs.jquery.com/ticket/13335

--- js/jquery.mobile.media.js
+++ js/jquery.mobile.media.js
@@ -10,8 +10,6 @@ define( [ "jquery", "./jquery.mobile.core" ], function( jQuery ) {
 	/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
 	window.matchMedia = window.matchMedia || (function( doc, undefined ) {
 
-		"use strict";
-
 		var bool,
 			docElem = doc.documentElement,
 			refNode = docElem.firstElementChild || docElem.firstChild,
