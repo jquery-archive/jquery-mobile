95c067703f025d57e8c4d91bab1da0eac4e09d34
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-01-02 21:18:49 +0200

    Filterable backcompat: Avoid having to listen to own event by overriding _trigger()

--- js/widgets/filterable.backcompat.js
+++ js/widgets/filterable.backcompat.js
@@ -65,19 +65,20 @@ $.widget( "mobile.filterable", $.mobile.filterable, {
 		this._setWidget( this.element.data( "mobile-" + evt.type.substring( 0, evt.type.length - 6 ) ) );
 	},
 
-	_triggerListviewBeforeFilter: function( event ) {
-		if ( event.target === this.element[ 0 ] ) {
-			this.element.trigger( "listviewbeforefilter" );
+	_trigger: function( type, event, data ) {
+		if ( this._widget && this._widget.widgetFullName === "mobile-listview" &&
+			type === "beforefilter" ) {
+
+			// Also trigger listviewbeforefilter if this widget is also a listview
+			this._widget._trigger( "beforefilter", event, data );
 		}
+		this._super( type, event, data );
 	},
 
 	_setWidget: function( widget ) {
 		if ( !this._widget && widget ) {
 			this._widget = widget;
 			this._widget._setOptions = replaceSetOptions( this, this._widget._setOptions );
-			if ( widget.widgetFullName === "mobile-listview" ) {
-				this._on({ "filterablebeforefilter": "_triggerListviewBeforeFilter" });
-			}
 		}
 
 		if ( !!this._widget ) {
