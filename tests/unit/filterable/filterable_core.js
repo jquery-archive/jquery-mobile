57c20a9c6660c9af265fdb8041bbd32571115092
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-03-13 09:57:37 +0200

    Filterable: Make sure default is prevented on ENTER

--- tests/unit/filterable/filterable_core.js
+++ tests/unit/filterable/filterable_core.js
@@ -10,4 +10,14 @@ test( "Listview with filter has hideDividers option set to true", function() {
 	deepEqual( $( "#hidedividers-option-test" ).listview( "option", "hideDividers" ), true );
 });
 
+test( "Filterable input prevents default on ENTER", function() {
+	var event = $.Event( "keydown" );
+
+	event.keyCode = $.ui.keyCode.ENTER;
+
+	$( "#test-input-preventDefault" ).trigger( event );
+
+	deepEqual( event.isDefaultPrevented(), true, "Keydown for ENTER default is prevented" );
+});
+
 })( jQuery );
