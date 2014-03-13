5b8a14f4b1ea2021ab569c9696f35c9d0e5c7f27
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-03-13 09:25:19 +0200

    Filterable: Prevent form submission when user presses ENTER
    Intercept ENTER on keydown and keypress, preventing default
    
    Fixes gh-7240

--- tests/unit/filterable/filterable_core.js
+++ tests/unit/filterable/filterable_core.js
@@ -10,4 +10,21 @@ test( "Listview with filter has hideDividers option set to true", function() {
 	deepEqual( $( "#hidedividers-option-test" ).listview( "option", "hideDividers" ), true );
 });
 
+test( "Filterable input prevents default on ENTER", function() {
+	var event = $.Event( "keydown" ),
+		input = $( "#test-input-preventDefault" );
+
+	event.keyCode = $.ui.keyCode.ENTER;
+
+	input.trigger( event );
+
+	deepEqual( event.isDefaultPrevented(), true, "keydown for ENTER default is prevented" );
+
+	event = $.Event( "keypress" );
+
+	input.trigger( event );
+
+	deepEqual( event.isDefaultPrevented(), true, "Subsequent keypress default is also prevented" );
+});
+
 })( jQuery );
