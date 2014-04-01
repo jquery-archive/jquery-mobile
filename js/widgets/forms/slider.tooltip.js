3d726c3c8226afca3348f8f27a2a23c0fee74980
Cory Gackenheimer <cory.gack@gmail.com>
2014-04-01 12:54:48 -0400

    Slider: value on button not updated
    Fixes gh-7137

--- js/widgets/forms/slider.tooltip.js
+++ js/widgets/forms/slider.tooltip.js
@@ -93,7 +93,7 @@ $.widget( "mobile.slider", $.mobile.slider, {
 		if ( o.popupEnabled && this._popup ) {
 			this._positionPopup();
 			this._popup.html( newValue );
-		} 
+		}
 
 		if ( o.showValue && !this.options.mini ) {
 			this.handle.html( newValue );
