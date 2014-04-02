d9f5d211f7584a7f096158a3e8d0218589933c4f
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-04-02 20:10:54 +0300

    Checkboxradio: Correctly retrieve label
    This moves label-finding code into one function to help render the code
    unit-testable. The new code also relies more on native means of getting the
    label, which improves performance a great deal when the .labels property is
    supported, and also improves performance when such a property is not supported
    (such as in FF27).
    
    http://jsperf.com/checkboxradio-label-retrieval-non-nested/5
    
    Closes gh-7293
    Fixes gh-7292

---
+++ tests/unit/checkboxradio/find_label_tests_core.js
@@ -0,0 +1,51 @@
+var pairs = [
+		{
+			label: "#separate-label-outside-form-label",
+			input: "#separate-label-outside-form",
+			isParent: false
+		},
+		{
+			label: "#separate-label-in-form-label",
+			input: "#separate-label-in-form",
+			isParent: false
+		},
+		{
+			label: "#nested-input-inside-form-label",
+			input: "#nested-input-inside-form",
+			isParent: true
+		},
+		{
+			label: "#separate-label-input-outside-form-label",
+			input: "#separate-label-input-outside-form",
+			isParent: false
+		},
+		{
+			label: "#nested-input-outside-form-label",
+			input: "#nested-input-outside-form",
+			isParent: true
+		}
+	];
+
+test( "_findLabel() works correctly", function() {
+	var index, pair, result,
+		findLabel = $.mobile.checkboxradio.prototype._findLabel;
+
+	for ( index in pairs ) {
+		pair = $.extend( {}, pairs[ index ] );
+		pair.label = $( pair.label );
+		pair.input = $( pair.input );
+
+		result = findLabel.call({
+			element: pair.input,
+			document: $( document )
+		});
+
+		deepEqual( result.element.length > 0, true,
+			pair.input.attr( "id" ) + ": a label was found" );
+		deepEqual( result.element[ 0 ], pair.label[ 0 ],
+			pair.input.attr( "id" ) + ": the right label was found" );
+		deepEqual( result.isParent, pair.isParent,
+			pair.input.attr( "id" ) +
+				": the label was correctly identified as (not?) the parent" );
+	}
+});
