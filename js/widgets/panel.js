9ff30f222131da40fd4dfdcbec2e3faea5268b2a
Ben Harrison <ben.onthewater@gmail.com>
2014-04-10 16:31:38 +0200

    Remove inline height from .ui-panel-dismiss on _closePanel
    Not removing the height from .ui-panel-dismiss breaks the layout if the
    page height is changed dynamically once the panel has open and closed
    once.

--- js/widgets/panel.js
+++ js/widgets/panel.js
@@ -397,7 +397,8 @@ $.widget( "mobile.panel", {
 					}
 
 					if ( self._modal ) {
-						self._modal.removeClass( self._modalOpenClasses );
+						self._modal.removeClass( self._modalOpenClasses )
+						.height('');
 					}
 				},
 				complete = function() {
