f2c2128f803741a758ce3364c28cfc581cf1e3ee
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-04-10 23:43:43 +0300

    Filterable: Instantiate on data-filter="true" listview for backcompat
    Closes gh-7311
    Fixes gh-6911

--- tests/unit/filterable/filterable_core.js
+++ tests/unit/filterable/filterable_core.js
@@ -4,6 +4,23 @@
 
 ( function( $ ){
 
+module( "Filterable tests" );
+
+asyncTest( "filterReveal filterable shows all items when all items match filter text", function() {
+	var input = $( "#test-filter-reveal-show-all-input" ),
+		list = $( "#test-filter-reveal-show-all-list" );
+
+	expect( 1 );
+
+	input.val( "Test" ).trigger( "change" );
+
+	setTimeout( function() {
+		deepEqual( list.children( ".ui-screen-hidden" ).length, 0,
+			"All items visible when search value matches them all" );
+		start();
+	}, 500 );
+});
+
 module( "Backwards compatibility tests" );
 
 test( "Listview with filter has hideDividers option set to true", function() {
@@ -27,17 +44,19 @@ test( "Filterable input prevents default on ENTER", function() {
 	deepEqual( event.isDefaultPrevented(), true, "Subsequent keypress default is also prevented" );
 });
 
-asyncTest( "filterReveal filterable shows all items when all items match filter text", function() {
-	var input = $( "#test-filter-reveal-show-all-input" ),
-		list = $( "#test-filter-reveal-show-all-list" );
+asyncTest( "Working filterable is instantiated on dynamic listview when data-filter='true'", function() {
+	var list = $( "<ul data-nstest-filter='true'><li>Chicago</li><li>Berlin</li><li>Windsor</li></ul>" )
+			.appendTo( "#content" )
+			.listview(),
+		input = list.prev().find( "input" );
 
-	expect( 1 );
+	expect( 3 );
 
-	input.val( "Test" ).trigger( "change" );
-
+	deepEqual( !!list.data( "mobile-filterable" ), true, "Filterable widget is present on listview" );
+	input.val( "o" ).trigger( "change" );
 	setTimeout( function() {
-		deepEqual( list.children( ".ui-screen-hidden" ).length, 0,
-			"All items visible when search value matches them all" );
+		deepEqual( list.children( ".ui-screen-hidden" ).length, 1, "One child was hidden" );
+		deepEqual( list.children( ".ui-screen-hidden" ).text(), "Berlin", "'Berlin' was hidden" );
 		start();
 	}, 500 );
 });
