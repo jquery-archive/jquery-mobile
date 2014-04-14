2eb11cb21f8b730f1415b40e7af45df43d12c729
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-04-14 21:22:35 +0300

    Popup: Ignore resize events right after a click
    (cherry picked from commit 5342dfd16fcce0e35eb5ce666ebab6f5bd2e95f6)
    
    Closes gh-7323
    Fixes gh-6961

--- js/widgets/popup.js
+++ js/widgets/popup.js
@@ -75,6 +75,14 @@ $.widget( "mobile.popup", {
 		history: !$.mobile.browser.oldIE
 	},
 
+	// When the user depresses the mouse/finger on an element inside the popup while the popup is
+	// open, we ignore resize events for a short while. This prevents #6961.
+	_handleDocumentVmousedown: function( theEvent ) {
+		if ( this._isOpen && $.contains( this._ui.container[ 0 ], theEvent.target ) ) {
+			this._ignoreResizeEvents();
+		}
+	},
+
 	_create: function() {
 		var theElement = this.element,
 			myId = theElement.attr( "id" ),
@@ -84,6 +92,10 @@ $.widget( "mobile.popup", {
 		// We can't do it in the option declarations because those are run before
 		// it is determined whether there shall be AJAX nav.
 		currentOptions.history = currentOptions.history && $.mobile.ajaxEnabled && $.mobile.hashListeningEnabled;
+
+		this._on( this.document, {
+			"vmousedown": "_handleDocumentVmousedown"
+		});
 
 		// Define instance variables
 		$.extend( this, {
