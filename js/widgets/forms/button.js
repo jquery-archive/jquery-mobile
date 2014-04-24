fe849f70ae6844d7d1625ea7fc00b9531c626916
allsteady <allsteady@gmail.com>
2014-04-24 21:00:59 +0900

    Button : Revert the js.

--- js/widgets/forms/button.js
+++ js/widgets/forms/button.js
@@ -18,8 +18,8 @@ $.widget( "mobile.button", {
 		icon: null,
 		iconpos: "left",
 		iconshadow: false, /* TODO: Deprecated in 1.4, remove in 1.5. */
-		corners: false,
-		shadow: false,
+		corners: true,
+		shadow: true,
 		inline: null,
 		mini: null,
 		wrapperClass: null,
@@ -27,6 +27,7 @@ $.widget( "mobile.button", {
 	},
 
 	_create: function() {
+
 		if ( this.element.is( ":disabled" ) ) {
 			this.options.disabled = true;
 		}
@@ -54,39 +55,19 @@ $.widget( "mobile.button", {
 
 	_enhance: function() {
 		this.element.wrap( this._button() );
-
-	},
-
-	// For backward compatability.
-	// $( selector ).button.("options", "XXX" )
-	_parseClasses : function ( clazz ) {
-		var opt = this.options,
-			classes,
-			index = 0;
-		if ( clazz ) {
-			classes = clazz.split( " " );
-			for ( ; index < classes.length ; index++ ) {
-				if ( classes[ index ] === "ui-corner-all" ) {
-					opt.corners = true;
-				} else if ( classes[ index ] === "ui-shadow" ) {
-					opt.shadow = true;
-				} else if ( classes[ index ] === "ui-btn-inline" ) {
-					opt.inline = true;
-				} else if ( classes[ index ] === "ui-mini" ) {
-					opt.mini = true;
-				}
-			}
-		}
 	},
 
 	_button: function() {
 		var options = this.options,
 			iconClasses = this._getIconClasses( this.options );
 
-		this._parseClasses( options.wrapperClass );
 		return $("<div class='ui-btn ui-input-btn" +
 			( options.wrapperClass ? " " + options.wrapperClass : "" ) +
 			( options.theme ? " ui-btn-" + options.theme : "" ) +
+			( options.corners ? " ui-corner-all" : "" ) +
+			( options.shadow ? " ui-shadow" : "" ) +
+			( options.inline ? " ui-btn-inline" : "" ) +
+			( options.mini ? " ui-mini" : "" ) +
 			( options.disabled ? " ui-state-disabled" : "" ) +
 			( iconClasses ? ( " " + iconClasses ) : "" ) +
 			"' >" + this.element.val() + "</div>" );
@@ -109,6 +90,7 @@ $.widget( "mobile.button", {
 
 	_setOptions: function( options ) {
 		var outer = this.widget();
+
 		if ( options.theme !== undefined ) {
 			outer
 				.removeClass( this.options.theme )
@@ -146,6 +128,7 @@ $.widget( "mobile.button", {
 	refresh: function( create ) {
 		var originalElement,
 			isDisabled = this.element.prop( "disabled" );
+
 		if ( this.options.icon && this.options.iconpos === "notext" && this.element.attr( "title" ) ) {
 			this.element.attr( "title", this.element.val() );
 		}
