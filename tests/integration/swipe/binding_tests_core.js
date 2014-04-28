0eba6e4c752072d8d7e19429db6afd7f634811cd
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-04-28 23:05:24 +0300

    Swipe: Keep swiperight when tearing down swipeleft and vice versa
    Closes gh-7353
    Fixes gh-7351

---
+++ tests/integration/swipe/binding_tests_core.js
@@ -0,0 +1,39 @@
+test( "Unbinding swipeleft leaves swiperight handler alone", function() {
+	var	dummy = function() {},
+		swipeLength = function() {
+			var swipe = $._data( document, "events" ).swipe;
+
+			return swipe ? swipe.length : 0;
+		},
+		initialSwipeLength = swipeLength();
+
+	$( document ).on( "swipeleft swiperight", ".ui-page", dummy );
+
+	deepEqual( swipeLength(), initialSwipeLength + 2,
+		"Two swipe handlers are present after attaching swipeleft and swiperight" );
+
+	$( document ).off( "swipeleft", ".ui-page", dummy );
+
+	deepEqual( swipeLength(), initialSwipeLength + 1,
+		"One swipe handler is present after detaching swipeleft" );
+
+	$( document ).on( "swipeleft", ".ui-page", dummy );
+
+	deepEqual( swipeLength(), initialSwipeLength + 2,
+		"Two swipe handlers are present after reattaching swipeleft" );
+
+	$( document ).off( "swiperight", ".ui-page", dummy );
+
+	deepEqual( swipeLength(), initialSwipeLength + 1,
+		"One swipe handler is present after detaching swiperight" );
+
+	$( document ).on( "swiperight", ".ui-page", dummy );
+
+	deepEqual( swipeLength(), initialSwipeLength + 2,
+		"Two swipe handlers are present after reattaching swiperight" );
+
+	$( document ).off( "swipeleft swiperight", ".ui-page", dummy );
+
+	deepEqual( swipeLength(), initialSwipeLength,
+		"No swipe handlers are present after detaching both swipeleft and swiperight" );
+});
