15ef3cbcbcc9c87a55fe7e4bd695933f6fbd6f58
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-05-19 14:32:25 +0300

    Table: Simplify and speed up tests

---
+++ tests/unit/table/basic_table_core.js
@@ -0,0 +1,23 @@
+test( "Basic table is enhanced correctly", function() {
+	var basicTable = $( "#table-enhance-test" );
+
+	deepEqual( basicTable.hasClass( "ui-table" ), true,
+		"Enhanced table has class 'ui-table'" );
+	deepEqual( !!basicTable.data( "mobile-table" ), true,
+		"Enhanced table has object at key 'mobile-table'" );
+	deepEqual( basicTable.data( "mobile-table" ).headers.length, 5,
+		"Enhanced table widget has member 'headers' of correct length (5)" );
+});
+
+test( "Basic table is disabled/enabled correctly", function() {
+
+	var table = $( "#table-disable-test" );
+
+	table.table( "option", "disabled", true );
+	deepEqual( table.hasClass( "ui-state-disabled" ), true,
+		"Table has the ui-state-disabled class when disabled" );
+
+	table.table( "option", "disabled", false );
+	deepEqual( table.hasClass( "ui-state-disabled" ), false,
+		"Table does not have the ui-state-disabled class when enabled" );
+});
