dae90c2152f8c3d1b128c6186ac34c3a50ba1cba
Cory Gackenheimer <cory.gack@gmail.com>
2014-05-22 23:37:54 -0400

    Navbar: Implement Classes Option
    Fixes gh-7705
    Closes gh-8297

--- js/jquery.mobile.js
+++ js/jquery.mobile.js
@@ -32,6 +32,8 @@
 			"./widgets/collapsibleSet",
 			"./grid",
 			"./widgets/navbar",
+			"./widgets/navbar.backcompat",
+			"./widgets/navbar.morebutton",
 			"./widgets/listview",
 			"./widgets/listview.backcompat",
 			"./widgets/listview.autodividers",
