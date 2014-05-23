59ba98e37e52606be3e40bee1e152d33dfb63922
Cory Gackenheimer <cory.gack@gmail.com>
2014-05-23 15:25:04 -0400

    navbar: replace delegate with _on + formatting

--- js/widgets/navbar.js
+++ js/widgets/navbar.js
@@ -37,24 +37,26 @@ $.widget( "mobile.navbar", {
 			self._createNavRows();
 		}
 
+		// Deprecated in 1.5
+		self._on( self.element, {
+			"vclick a": function( event ) {
+				var activeBtn = $( event.target );
 
-		$navbar.delegate( "a", "vclick", function( /* event */ ) {
-			var activeBtn = $( this );
+				if ( !( activeBtn.hasClass( "ui-state-disabled" ) ||
+					activeBtn.hasClass( $.mobile.activeBtnClass ) ) ) {
 
-			if ( !( activeBtn.hasClass( "ui-state-disabled" ) ||
-				activeBtn.hasClass( $.mobile.activeBtnClass ) ) ) {
+					$navbtns.removeClass( $.mobile.activeBtnClass );
+					activeBtn.addClass( $.mobile.activeBtnClass );
 
-				$navbtns.removeClass( $.mobile.activeBtnClass );
-				activeBtn.addClass( $.mobile.activeBtnClass );
-
-				// The code below is a workaround to fix #1181
-				$( document ).one( "pagehide", function() {
-					activeBtn.removeClass( $.mobile.activeBtnClass );
-				});
+					// The code below is a workaround to fix #1181
+					$( document ).one( "pagehide", function() {
+						activeBtn.removeClass( $.mobile.activeBtnClass );
+					});
+				}
 			}
 		});
 
-		// DEPRECATE
+		// Deprecated in 1.5
 		// Buttons in the navbar with ui-state-persist class should regain their active state before page show
 		$navbar.closest( ".ui-page" ).bind( "pagebeforeshow", function() {
 			$navbtns.filter( ".ui-state-persist" ).addClass( $.mobile.activeBtnClass );
@@ -62,43 +64,43 @@ $.widget( "mobile.navbar", {
 	},
 
 	_createNavRows: function(){
-		var $navbar = this.element,
-			$navbtns = $navbar.find( "a" ),
-			$navitems = $navbar.find( "li" ),
-			numbuttons = $navbtns.length,
-			maxbutton = this.options.maxbutton,
-			iconpos = this.option.iconpos,
-			numrows,
-			row,
-			pos,
-			btn,
-			overflowNav;
-		numrows = (numbuttons % maxbutton) === 0 ?
-						(numbuttons / maxbutton) :
-						Math.floor( numbuttons / maxbutton ) + 1;
-			// prep for new rows
-			for ( pos = 1; pos < numrows; pos++ ) {
-				$("<ul>")
-					.addClass("ui-navbar-row ui-navbar-row-" + pos)
-					.appendTo($navbar);
+		var rowCount, row, pos, buttonItem, overflowNav,
+			$navbar = this.element,
+			$navButtons = $navbar.find( "a" ),
+			$navItems = $navbar.find( "li" ),
+			buttonCount = $navButtons.length,
+			maxButton = this.options.maxbutton,
+			iconpos = this.options.iconpos;
+
+		rowCount = (buttonCount % maxButton) === 0 ?
+						(buttonCount / maxButton) :
+						Math.floor( buttonCount / maxButton ) + 1;
+
+		// prep for new rows
+		for ( pos = 1; pos < rowCount ; pos++ ) {
+			$( "<ul>" )
+				.addClass( "ui-navbar-row ui-navbar-row-" + pos )
+				.appendTo($navbar);
+		}
+
+		// enhance buttons and move to new rows
+		for( pos = 0; pos < buttonCount ; pos++ ) {
+			buttonItem = $navItems.eq(pos);
+			this._makeNavButton( buttonItem.find( "a" ), iconpos );
+			if ( pos + 1 > maxButton) {
+				buttonItem.detach();
+				row = ((pos + 1) % maxButton) === 0 ?
+						Math.floor((pos) / maxButton) :
+						Math.floor((pos + 1) / maxButton);
+				overflowNav = "ul.ui-navbar-row-" + row;
+				$navbar.find(overflowNav).append(buttonItem);
 			}
-			// enhance buttons and move to new rows
-			for( pos = 0; pos < numbuttons; pos++ ) {
-				btn = $navitems.eq(pos);
-				this._makeNavButton(btn.find("a"), iconpos);
-				if ( pos + 1 > maxbutton) {
-					btn.detach();
-					row = ((pos + 1) % maxbutton) === 0 ?
-							Math.floor((pos) / maxbutton) :
-							Math.floor((pos + 1) / maxbutton);
-					overflowNav ="ul.ui-navbar-row-" + row;
-					$navbar.find(overflowNav).append(btn);
-				}
-			}
-		},
-	_makeNavButton: function(btn, iconpos) {
-		var icon = $.mobile.getAttribute( btn, "icon" ),
-			theme = $.mobile.getAttribute( btn, "theme" ),
+		}
+	},
+
+	_makeNavButton: function(button, iconpos) {
+		var icon = $.mobile.getAttribute( button, "icon" ),
+			theme = $.mobile.getAttribute( button, "theme" ),
 			classes = "ui-btn";
 
 		if ( theme ) {
@@ -107,7 +109,7 @@ $.widget( "mobile.navbar", {
 		if ( icon ) {
 			classes += " ui-icon-" + icon + " ui-btn-icon-" + iconpos;
 		}
-		$( btn ).addClass( classes );
+		$( button ).addClass( classes );
 	}
 });
 
