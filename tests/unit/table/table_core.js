14d23435b5a64c438883a1ff07c00e52b01224ac
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-05-27 23:00:45 +0300

    Table: Copy entire contents of reflow headers into reflow bold tags
    (cherry picked from commit a9472ce35b951e6e2156dc15ecbbebfcec9d6c39)
    
    Closes gh-7437
    Fixes gh-7415

--- tests/unit/table/table_core.js
+++ tests/unit/table/table_core.js
@@ -92,6 +92,9 @@
 	asyncTest( "The page should be enhanced correctly" , function(){
 		setTimeout(function() {
 			ok($('#reflow-table-test .ui-table-reflow').length, ".ui-table-reflow class added to table element");
+			deepEqual(
+				$( "#reflow-table-test .ui-table-reflow > tbody span.make-it-red" ).length, 1,
+					"span was copied from table header" );
 			start();
 		}, 800);
 	});
