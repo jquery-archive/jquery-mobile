c65731fada1862a6e83716689770d1b5309ada2b
cgack <cory.gack@gmail.com>
2014-05-28 21:30:40 -0400

    navbar: remove grid tests - fix depracated ui-disabled test

--- tests/unit/navbar/navbar_core.js
+++ tests/unit/navbar/navbar_core.js
@@ -3,32 +3,19 @@
  */
 (function($){
 	test( "navbar button gets active button class when clicked", function() {
-		var link = $("#disabled-btn-click a:not(.ui-disabled)").first();
+		var link = $("#disabled-btn-click a:not(.ui-state-disabled)").first();
 
 		link.click();
 		ok( link.hasClass($.mobile.activeBtnClass), "link has active button class" );
 	});
 
 	test( "disabled navbar button doesn't add active button class when clicked", function() {
-		var link = $("#disabled-btn-click a.ui-disabled").first();
+		var link = $("#disabled-btn-click a.ui-state-disabled").first();
 
 		link.click();
 		ok( !link.hasClass($.mobile.activeBtnClass), "link doesn't have active button class" );
 	});
-
-	test( "grids inside an ignored container do not enhance", function() {
-		var $ignored = $( "#ignored-grid" ), $enhanced = $( "#enhanced-grid" );
-
-		$.mobile.ignoreContentEnabled = true;
-
-		$("#foo").trigger( "create" );
-
-		deepEqual( $ignored.attr( "class" ), undefined, "ignored list doesn't have the grid theme" );
-		deepEqual( $enhanced.attr( "class" ).indexOf("ui-grid"), 0, "enhanced list has the grid theme" );
-
-		$.mobile.ignoreContentEnabled = false;
-	});
-
+	
 	test( "classes are correctly assigned", function() {
 		var $ul = $('#enhanced-classes'),
 			r = $ul.find("li").eq(0).find("a"),
