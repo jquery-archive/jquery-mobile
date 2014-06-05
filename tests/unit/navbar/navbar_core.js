351bc2f5c80f7d2d0a1c27a110a51fc7545819ad
Cory Gackenheimer <cory.gack@gmail.com>
2014-06-05 10:32:09 -0400

    navbar: add tests for maxbutton, refresh, and destroy

--- tests/unit/navbar/navbar_core.js
+++ tests/unit/navbar/navbar_core.js
@@ -2,28 +2,94 @@
  * mobile navbar unit tests
  */
 (function($){
-	test( "navbar button gets active button class when clicked", function() {
-		var link = $("#disabled-btn-click a:not(.ui-state-disabled)").first();
+    test( "navbar button gets active button class when clicked", function() {
+        var link = $("#disabled-btn-click a:not(.ui-state-disabled)").first();
 
-		link.click();
-		ok( link.hasClass($.mobile.activeBtnClass), "link has active button class" );
-	});
+        link.click();
+        ok( link.hasClass($.mobile.activeBtnClass), "link has active button class" );
+    });
 
-	test( "disabled navbar button doesn't add active button class when clicked", function() {
-		var link = $("#disabled-btn-click a.ui-state-disabled").first();
+    test( "disabled navbar button doesn't add active button class when clicked", function() {
+        var link = $("#disabled-btn-click a.ui-state-disabled").first();
 
-		link.click();
-		ok( !link.hasClass($.mobile.activeBtnClass), "link doesn't have active button class" );
-	});
-	
-	test( "classes are correctly assigned", function() {
-		var $ul = $('#enhanced-classes'),
-			r = $ul.find("li").eq(0).find("a"),
-			d = $ul.find("li").eq(1).find("a"),
-			u = $ul.find("li").eq(2).find("a");
+        link.click();
+        ok( !link.hasClass($.mobile.activeBtnClass), "link doesn't have active button class" );
+    });
+    
+    test( "classes are correctly assigned", function() {
+        var $ul = $('#enhanced-classes'),
+            r = $ul.find("li").eq(0).find("a"),
+            d = $ul.find("li").eq(1).find("a"),
+            u = $ul.find("li").eq(2).find("a");
 
-		ok(r.hasClass("ui-icon-arrow-r") && !r.hasClass("ui-icon-arrow-d") && !r.hasClass("ui-icon-arrow-u"),"first item only has class of arrow-r");
-		ok(!d.hasClass("ui-icon-arrow-r") && d.hasClass("ui-icon-arrow-d") && !d.hasClass("ui-icon-arrow-u"),"second item only has class of arrow-d");
-		ok(!u.hasClass("ui-icon-arrow-r") && !u.hasClass("ui-icon-arrow-d") && u.hasClass("ui-icon-arrow-u"),"third item only has class of arrow-u");
-	});
+        ok(r.hasClass("ui-icon-arrow-r") && !r.hasClass("ui-icon-arrow-d") && !r.hasClass("ui-icon-arrow-u"),"first item only has class of arrow-r");
+        ok(!d.hasClass("ui-icon-arrow-r") && d.hasClass("ui-icon-arrow-d") && !d.hasClass("ui-icon-arrow-u"),"second item only has class of arrow-d");
+        ok(!u.hasClass("ui-icon-arrow-r") && !u.hasClass("ui-icon-arrow-d") && u.hasClass("ui-icon-arrow-u"),"third item only has class of arrow-u");
+    });
+
+    module( "navbar exceed maxbutton, without moreButton" );
+
+    test( "exceeding default maxbutton creates a new row", function() {
+        var navbar = $( "#default-maxbutton-overflow" ),
+            navRows = navbar.children( "ul" ),
+            navRowCount = navRows.length;
+
+        ok( navRowCount === 2, "six items overflows to two lists" );
+        ok( navRows.eq(1).hasClass("ui-navbar-row"),
+            "overflow list gets appropriate classname" );
+        ok( navRows.eq(1).children( "li" ).length === 1,
+            "overflow row holds sixth nav item" );
+    });
+
+    test( "override the default maxbutton works", function() {
+        var navbar = $( "#default-maxbutton-override" ),
+            navRows = navbar.children( "ul" ),
+            navRowCount = navRows.length;
+
+        ok( navRowCount === 1, "six items should fit on a single row when maxbutton = 6" );
+        ok( navRows.children( "li" ).length === 6, "there should be six items on the navbar row" );
+    });
+
+    module( "navbar appending items and refresh, without moreButton" , {
+        setup: function() {
+            this.addNav = $( "#add-items-navbar" );
+            this.navbarRow = this.addNav.find( "ul" );
+            this.navbarRow.append( "<li><a href='#'>four</a></li>" );
+            this.navbarRow.append( "<li><a href='#'>five</a></li>" );
+            this.navbarRow.append( "<li><a href='#'>six</a></li>" );
+            this.addNav.navbar( "refresh" );
+        }
+    });
+
+    test( "adding items to navbar", function() {
+        var navRows = this.addNav.children( "ul" ),
+            navRowCount = navRows.length;
+
+        ok( this.navbarRow.find( "li:last-child > a" ).hasClass ( "ui-btn" ),
+            "added items get ui-btn class after refresh" );
+        ok( navRowCount === 2, "six items overflows to two lists" );
+        ok( navRows.eq(1).hasClass("ui-navbar-row"),
+            "overflow list gets appropriate classname" );
+        ok( navRows.eq(1).children( "li" ).length === 1,
+            "overflow row holds sixth nav item" );
+    });
+
+    module( "navbar destroy, without moreButton", {
+        setup: function() {
+            this.navbar = $( "#default-maxbutton-overflow-destroy" );
+            this.navbar.navbar( "destroy" );
+        }
+    } );
+
+    test( "destroy navbar ", function() {
+        var navRows = this.navbar.children( "ul" ),
+            navRowCount = navRows.length;
+
+        ok( navRowCount === 1, "destroyed navbars revert to one ul" );
+        navRows.find( "li" ).each( function() {
+            ok( !$(this).find("a").hasClass( "ui-btn" ), "ui-btn class removed on item" );
+        });
+    });
+
+
 })(jQuery);
