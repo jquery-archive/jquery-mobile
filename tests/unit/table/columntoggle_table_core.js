3420aa00b5194f72bc52d17bdb3a523e23cf5e9e
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-06-17 23:16:16 +0300

    Table: Add columntoggle test for generated widget ID

--- tests/unit/table/columntoggle_table_core.js
+++ tests/unit/table/columntoggle_table_core.js
@@ -386,3 +386,17 @@ test( "setColumnVisibility() correctly resolves column from index/cell", functio
 	deepEqual( input.prop( "checked" ), true,
 		"Turning on visibility by tbody cell affects only the expected cells" );
 });
+
+test( "Columntoggle table with generated id works correctly", function() {
+	var container = $( "#generated-id-test-container" ),
+		link = container.find( "a:first" ),
+		linkId = link.attr( "id" ),
+		popupId = ( linkId || "" ).replace( /-button$/, "-popup" ),
+		popup = $( "#" + popupId + ":data('mobilePopup')" );
+
+	deepEqual( !!linkId, true, "Anchor has an ID" );
+	deepEqual( link.attr( "href" ), "#" + popupId,
+		"Anchor's href points to an element with an ID sharing the link ID's prefix, but -popup" );
+	deepEqual( popup.length, 1,
+		"There is exactly one element enhanced as a popup with the id to which the anchor links" );
+});
