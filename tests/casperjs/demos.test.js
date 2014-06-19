962f5658d485cff56fd9eb19882395a6b750b17d
Alexander Schmitz <arschmitz@gmail.com>
2014-06-19 09:51:15 -0400

    Demos: Make casper tests check for JS errors and fail if any are found

--- tests/casperjs/demos.test.js
+++ tests/casperjs/demos.test.js
@@ -3,9 +3,18 @@
 var host = casper.cli.get( "host" ) || "localhost",
 	port = casper.cli.get( "port" ) || 80,
 	path = casper.cli.get( "path" ) || "",
+	errors = [],
 	mouse = require("mouse").create(casper);
 
 casper.options.viewportSize = {width: 1024, height: 768};
+
+casper.on("page.error", function(msg, trace) {
+	this.echo("Error:    " + msg, "ERROR");
+	this.echo("file:     " + trace[0].file, "WARNING");
+	this.echo("line:     " + trace[0].line, "WARNING");
+	this.echo("function: " + trace[0]["function"], "WARNING");
+	errors.push(msg);
+});
 
 casper.test.begin("Root index should redirect to demos/", 2, function suite( test ) {
 	casper.start( "http://" + host + ":" + port + path + "/" );
@@ -31,6 +40,17 @@ casper.test.begin("demos/ should render properly", 2, function suite( test ) {
 		);
 	});
 
+	casper.run(function() {
+		test.done();
+	});
+});
+
+casper.test.begin("swipe page demo should render properly", 1, function suite( test ) {
+	casper.start( "http://" + host + ":" + port + path + "/demos/swipe-page/newyork.html" );
+
+	casper.then( function() {
+		test.assertHttpStatus( 200 );
+	});
 	casper.run(function() {
 		test.done();
 	});
@@ -70,6 +90,17 @@ casper.test.begin("Checkboxradio widget should render properly", 2, function sui
 	});
 
 	casper.run(function() {
+		test.done();
+	});
+});
+casper.test.begin( "There are no errors or warnings during other tests", 1, function suite( test ) {
+	casper.run(function(){
+		test.assert( errors.length === 0, "No JS errors found" );
+		if (errors.length > 0) {
+			this.echo(errors.length + ' Javascript errors found', "WARNING");
+		} else {
+			this.echo(errors.length + ' Javascript errors found', "INFO");
+		}
 		test.done();
 	});
 });
