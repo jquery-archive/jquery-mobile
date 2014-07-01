d97f5e8c90fef20b1cf6c51e0de45c969731857b
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-07-01 18:40:16 +0300

    Table: Correct grouped tests

--- tests/integration/table/grouped_core.js
+++ tests/integration/table/grouped_core.js
@@ -1,43 +1,36 @@
-function runTests( prefix, table, expectedColstart ) {
-	test( "Grouped headers have correct data", function() {
+function runAssertions( prefix, table, expectedColstart ) {
+	var expectedCells = table.find( "[data-column]" ),
+		header = table.find( ".test-column-header" );
 
-		var expectedCells = table.find( "[data-column]" ),
-			header = table.find( ".test-column-header" );
+	deepEqual( expectedCells.is( header.data( $.camelCase( $.mobile.ns + "cells" ) ) ), true,
+		prefix + "header's 'cells' data item contains the right cells." );
 
-		deepEqual( expectedCells.is( header.data( $.camelCase( $.mobile.ns + "cells" ) ) ), true,
-			"header's 'cells' data item contains the right cells." );
-	});
+	deepEqual( table.find( ".test-column-header" ).jqmData( "colstart" ), expectedColstart,
+		prefix + "header's 'colstart' attribute points to the right column" );
+	deepEqual(
+		table.find( "[data-top-label]" ).is( function() {
+			var cell = $( this );
 
-	test( "Reflow attributes and classes are correct", function() {
-		deepEqual( table.find( ".test-column-header" ).jqmData( "colstart" ), expectedColstart,
-			"header's 'colstart' attribute points to the right column" );
-		deepEqual(
-			table.find( "[data-top-label]" ).is( function() {
-				var cell = $( this );
-
-				return !(
-					cell
-						.children( "b.ui-table-cell-label:not(.ui-table-cell-label-top)" )
-							.length === 1 &&
-					cell
-						.children( "b.ui-table-cell-label.ui-table-cell-label-top" )
-							.length === 1 );
-			}),
-			false,
-			"Cells in the first of a group of columns have both group and column reflow labels" );
-	});
+			return !(
+				cell
+					.children( "b.ui-table-cell-label:not(.ui-table-cell-label-top)" )
+						.length === 1 &&
+				cell
+					.children( "b.ui-table-cell-label.ui-table-cell-label-top" )
+						.length === 1 );
+		}),
+		false,
+		prefix + "Cells in the first of a group of columns have both group and column reflow labels" );
 }
 
+
+
 function beforeAndAfterRefresh( prefix, table, expectedColStart ) {
-	module( prefix + "Initially" );
-
-	runTests( prefix, table, expectedColStart );
-
-	table.table( "refresh" );
-
-	module( prefix + "After refresh" );
-
-	runTests( prefix, table, expectedColStart );
+	test( prefix + "correct rendering", function() {
+		runAssertions( "initially: ", table, expectedColStart );
+		table.table( "refresh" );
+		runAssertions( "after refresh(): ", table, expectedColStart );
+	});
 }
 
 beforeAndAfterRefresh( "With colspan: ", $( "#grouped-test-table" ), 5 );
