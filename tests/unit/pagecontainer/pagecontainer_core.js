ba428b092ad5a94906405738c193af772febe5f9
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-07-02 22:52:48 +0300

    Pagecontainer: Correctly identify the current page as a dialog
    Closes gh-7541
    Fixes gh-7538

--- tests/unit/pagecontainer/pagecontainer_core.js
+++ tests/unit/pagecontainer/pagecontainer_core.js
@@ -26,4 +26,26 @@ test( "load does not trigger an error when called withput a second param", funct
 	ok( "no error triggered when load method called without options" );
 });
 
+module( "_handleDialog()" );
+
+test( "A dialog is recognized via presence of the data key, not the ui-dialog class", function() {
+	var getActiveHistoryCalled = false;
+
+	deepEqual( $.mobile.pagecontainer.prototype._handleDialog.call({
+			getActivePage: function() {
+				return $( "<div class='ui-dialog'></div>" );
+			},
+			_getActiveHistory: function() {
+				getActiveHistoryCalled = true;
+				return {};
+			},
+			back: $.noop,
+			forward: $.noop,
+		}, {}, {
+			pageUrl: "xyzzy.html"
+		}), false, "page is recognized as page even when the ui-dialog class is present" );
+
+	deepEqual( getActiveHistoryCalled, false, "_getActiveHistory() should not have been called" );
+});
+
 })();
