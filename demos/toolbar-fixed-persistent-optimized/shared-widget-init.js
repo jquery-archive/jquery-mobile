d39fbe5398edc86380df9a29bdd350e1573c6b41
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-09-09 10:27:37 +0300

    Toolbar: activate navbar links upon navigation
    Fixes gh-7087

--- demos/toolbar-fixed-persistent-optimized/shared-widget-init.js
+++ demos/toolbar-fixed-persistent-optimized/shared-widget-init.js
@@ -4,8 +4,23 @@
    page ends up being your application's startup page, the shared widgets will always be functional
    and correctly enhanced. */
 $( function() {
+	function updateActivePage( href ) {
+		$( "a.ui-btn" )
+			.removeClass( "ui-btn-active" )
+			.filter( "[href='" + href + "']" )
+				.addClass( "ui-btn-active" );
+	}
+
 	$( "[data-role='navbar']" ).navbar();
 	$( "[data-role='header'], [data-role='footer']" ).toolbar();
 	$( "#nav-menu" ).popup();
 	$( "#nav-menu-links" ).listview();
+
+	/* Since the navbar we use is outside the page, it does not update itself to indicate the currently
+	   active page. We manually update the navbar whenever the page changes. */
+	$( document ).on( "pagecontainerchange", function( event, data ) {
+		updateActivePage( $.mobile.path.parseUrl( data.toPage.jqmData( "url" ) ).filename );
+	});
+
+	updateActivePage( $.mobile.path.parseLocation().filename );
 });
