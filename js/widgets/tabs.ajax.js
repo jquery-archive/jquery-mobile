c74be57eea71f09b64271bc8b4cfc7b7566698af
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-09-29 17:12:48 +0300

    Tabs: Override _isLocal() to account for Ajax nav
    We cannot compare hrefs to location.href
    
    Fixes gh-7169
    Fixes gh-7725
    Closes gh-7727

---
+++ js/widgets/tabs.ajax.js
@@ -0,0 +1,39 @@
+//>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
+//>>description: Extension to make Tabs widget aware of jQuery Mobile's navigation
+//>>label: Tabs
+//>>group: Widgets
+
+define( [
+	"jquery",
+	"../defaults",
+	"../navigation/path",
+	"../navigation/base",
+	"jquery-ui/tabs" ], function( jQuery ) {
+
+//>>excludeEnd("jqmBuildExclude");
+
+( function( $, undefined ) {
+
+$.widget( "ui.tabs", $.ui.tabs, {
+	_isLocal: function( anchor ) {
+		var path, baseUrl, absUrl;
+
+		if ( $.mobile.ajaxEnabled ) {
+			path = $.mobile.path;
+			baseUrl = path.parseUrl( $.mobile.base.element().attr( "href" ) );
+			absUrl = path.parseUrl( path.makeUrlAbsolute( anchor.getAttribute( "href" ),
+				baseUrl ) );
+
+			return ( path.isSameDomain( absUrl.href, baseUrl.href ) &&
+				absUrl.pathname === baseUrl.pathname );
+		}
+
+		return this._superApply( arguments );
+	}
+});
+
+})( jQuery );
+
+//>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
+});
+//>>excludeEnd("jqmBuildExclude");
