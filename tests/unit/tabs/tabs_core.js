ec6dfa2c61455080edc960c89c9c860344be093d
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-09-29 17:12:48 +0300

    Tabs: Override _isLocal() to account for Ajax nav
    We cannot compare hrefs to location.href
    
    Fixes gh-7169
    Fixes gh-7725
    Closes gh-7727

---
+++ tests/unit/tabs/tabs_core.js
@@ -0,0 +1,32 @@
+( function() {
+
+var originalBase,
+	originalBaseSupport,
+	phonyDirectory = "/foo/bar/baz/";
+
+module( "Tabs extension", {
+	setup: function() {
+		originalBaseSupport = $.support.dynamicBaseTag;
+		$.support.dynamicBaseTag = true;
+		originalBase = $.mobile.base.element.attr( "href" );
+		$.mobile.base.set( phonyDirectory );
+	},
+	teardown: function() {
+		$.support.dynamicBaseTag = originalBaseSupport;
+		$.mobile.base.set( originalBase );
+	}
+});
+
+test( "_isLocal() correctly identifies URLs as local/non-local", function() {
+
+	var _isLocal = $.ui.tabs.prototype._isLocal;
+
+	deepEqual( _isLocal( $( "<a href='#some-id'></a>" )[ 0 ] ), true,
+		"'#some-id' identified as local" );
+
+	deepEqual( _isLocal( $( "<a href='" + phonyDirectory + "#some-other-id'></a>" )[ 0 ] ), true,
+		"'" + phonyDirectory + "#some-other-id' defined as local" );
+
+});
+
+})();
