bde66a30768bfdcdb6328b8e0eebae7e43adc961
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-10-09 09:21:47 +0300

    Loader: Add individual unit test
    Re gh-5987

---
+++ tests/unit/individual-modules/loader_core.js
@@ -0,0 +1,10 @@
+test( "Loader attaches to DOM when running individually", function() {
+	var loader = $.mobile.loading( "show" );
+
+	deepEqual( $.contains( document, loader[ 0 ] ), true,
+		"Document contains the loader after it is shown" );
+
+	deepEqual( loader.is( ":visible" ), true, "Loader is visible when shown" );
+
+	$.mobile.loading( "hide" );
+});
