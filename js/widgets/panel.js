c66e97af832323e399cc571bbd0f716679f5a496
Lisa Seacat DeLuca <ldeluca@us.ibm.com>
2014-11-03 17:18:08 -0500

    Panel: Android 2.3.7 not scrolling to top when panel closes
    Fixes #6767

--- js/widgets/panel.js
+++ js/widgets/panel.js
@@ -410,6 +410,9 @@ $.widget( "mobile.panel", {
 
 					self.element.addClass( o.classes.panelClosed );
 
+					//scroll to the top
+					self._positionPanel( true );
+
 					if ( o.display !== "overlay" ) {
 						self._page().parent().removeClass( o.classes.pageContainer );
 						self._wrapper.removeClass( o.classes.pageContentPrefix + "-open" );
