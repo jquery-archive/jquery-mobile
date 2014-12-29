de1b61c0fff366786ff436965c001b3ef48780ea
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-12-29 16:06:02 +0200

    Demos: Move external widgets demo panel content to popup and vice versa

--- demos/external-widgets/page-c.php
+++ demos/external-widgets/page-c.php
@@ -1,7 +1,17 @@
-<?php if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') { ?>
+<?php if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest' || isset( $_GET['source'])) { ?>
 <!DOCTYPE html>
 <html>
-	<head>
+<!-- This is an example of a page equipped to handle the startup of an application. It contains
+     a <head> section common to all pages of the application, and its body contains the markup
+	 for all widgets that will be shared across pages. The body for each page of the application
+	 contains such markup. -->
+<head>
+
+	<!-- The various documents reachable from within your navigation system must all have the
+	     necessary header information to be able to launch your application. Nevertheless, the
+		 server only needs to send this header information with the first request by the user. On
+		 Ajax requests by the application, this information can be discarded server-side in order
+		 to save bandwidth and to improve the time it takes to display a page. -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Ajax optimized persistent toolbars - jQuery Mobile Demos</title>
@@ -12,36 +22,43 @@
 	<script src="../../external/jquery/jquery.js"></script>
 	<script src="../_assets/js/"></script>
 	<script src="../../js/"></script>
-	<script src="shared-widget-init.js"></script>
+
+	<!-- This script contains the code that is shared between all the documents of your
+	     application. It is responsible for enhancing the shared widgets during your application's
+		 startup. -->
+	<script id="shared-widget-init" src="shared-widget-init.js"></script>
 </head>
 <body>
 	<!-- The following panel is shared across all pages of the application, and must therefore be
 	     copied to all the documents containing the application's pages. It will only be loaded
 		 once with the first page. On subsequent page loads the existing widget will be reused. -->
-	<div id="shared-panel" data-role="panel" data-theme="a">
-		<form>
-			<label for="login-field">Login:</label>
-			<input id="login-field" name="login">
-			<label for="password-field">Password:</label>
-			<input type="password" id="password-field" name="password">
-		</form>
+	<div id="shared-panel" data-role="panel" data-theme="a" data-position="right">
+		<ul id="nav-menu-links" data-role="listview">
+			<li data-icon="lock"><a href="#nav-menu" data-rel="popup">Login</a></li>
+			<li><a href="info.php" data-prefetch="true" data-transition="none">Info</a></li>
+			<li><a href="page-b.php" data-prefetch="true" data-transition="flip">Friends</a></li>
+			<li><a href="page-c.php" data-prefetch="true" data-transition="turn">Albums</a></li>
+			<li><a href="page-d.php" data-prefetch="true" data-transition="slide">Emails</a></li>
+		</ul>
 	</div>
-    <div data-role="header" data-position="fixed" data-theme="a">
+    <div id="shared-header" data-role="header" data-position="fixed" data-theme="a">
+		<!-- Shared header markup must be added to all documents of the demo to ensure any of them
+		     can serve as the start page. The server can be instructed to omit sending this portion
+			 of the data whenever the request for the document is made via Ajax. -->
 		<a href="../toolbar/" data-rel="back" class="ui-btn ui-btn-left ui-alt-icon ui-nodisc-icon ui-corner-all ui-btn-icon-notext ui-icon-carat-l">Back</a>
-		<a href="#nav-menu" data-rel="popup" class="ui-btn ui-btn-right ui-alt-icon ui-nodisc-icon ui-corner-all ui-btn-icon-right ui-icon-navigation">Navigation</a>
+		<a href="#shared-panel" data-rel="panel" class="ui-btn ui-btn-right ui-alt-icon ui-nodisc-icon ui-corner-all ui-btn-icon-right ui-icon-navigation">Navigation</a>
 		<div data-role="popup" id="nav-menu" data-theme="a">
-			<ul id="nav-menu-links">
-				<li data-icon="lock"><a href="#shared-panel">Login</a></li>
-				<li><a href="info.php" data-prefetch="true" data-transition="none">Info</a></li>
-				<li><a href="page-b.php" data-prefetch="true" data-transition="flip">Friends</a></li>
-				<li><a href="page-c.php" data-prefetch="true" data-transition="turn">Albums</a></li>
-				<li><a href="page-d.php" data-prefetch="true" data-transition="slide">Emails</a></li>
-			</ul>
+			<form class="ui-content">
+				<label for="login-field">Login:</label>
+				<input id="login-field" name="login">
+				<label for="password-field">Password:</label>
+				<input type="password" id="password-field" name="password">
+			</form>
 		</div>
         <h1>Fixed external header</h1>
     </div><!-- /header -->
 <?php } ?>
-
+	<!-- This is the actual page. It will always be sent to the client. -->
 	<div data-role="page">
 
     	<div class="ui-content" role="main">
@@ -127,8 +144,11 @@
 
 	</div><!-- /page -->
 
-<?php if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest') { ?>
-	<div data-role="footer" data-position="fixed" data-theme="a">
+<?php if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest' || isset( $_GET['source'])) { ?>
+	<div id="shared-navbar" data-role="footer" data-position="fixed" data-theme="a">
+		<!-- Shared navbar markup must be added to all pages of the demo to ensure any of them can
+		     serve as the start page. The server can be instructed to omit sending this portion of
+			 the data whenever the request for the document is made via Ajax. -->
 		<div data-role="navbar">
 			<ul>
 				<li><a href="info.php" data-prefetch="true" data-transition="none">Info</a></li>
