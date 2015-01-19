79b8797c065f48f864ab681633d452b842777951
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-01-19 10:58:57 +0200

    Init: resetActivePageHeight() gets called from "pageshow" handler async
    Do not assume that calling $.mobile.initializePage() results in a synchronous
    call to resetActivePageHeight(), because resetActivePageHeight() gets called
    from a pageshow handler, which may be asynchronous, depending on the
    implementation of the transition handler for the "none" transition.

--- tests/unit/init/no_autoinit_page_core.js
+++ tests/unit/init/no_autoinit_page_core.js
@@ -1,6 +1,8 @@
 asyncTest( "resetActivePageHeight() will be called when page is initialized late", function() {
 	var resetActivePageHeightCallCount = 0;
 
+	expect( 1 );
+
 	$( document ).on( "mobileinit", function() {
 		$.mobile.autoInitializePage = false;
 
@@ -13,13 +15,19 @@ asyncTest( "resetActivePageHeight() will be called when page is initialized late
 	});
 
 	require([ "jquery", "./init" ], function() {
-		setTimeout( function() {
-			$.mobile.initializePage();
-
-			deepEqual( resetActivePageHeightCallCount, 1,
-				"$.mobile.resetActivePageHeight() was called from delayed initializePage()" );
-			start();
-		}, 5000 );
+		$.testHelper.detailedEventCascade([
+			function() {
+				$.mobile.initializePage();
+			},
+			{
+				pagecontainershow: { src: $( "body" ), event: "pagecontainershow.noAutoinit1" }
+			},
+			function() {
+				deepEqual( resetActivePageHeightCallCount, 1,
+					"$.mobile.resetActivePageHeight() was called from delayed initializePage()" );
+				start();
+			}
+		]);
 	});
 
 });
