012989b461d94ccda94522e1ea0d59dd5f9b1e73
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-01-27 12:03:10 +0200

    Flipswitch: Prevent default when label is clicked
    Fixes gh-6661
    Closes gh-7942

--- tests/integration/flipswitch/flipswitch_core.js
+++ tests/integration/flipswitch/flipswitch_core.js
@@ -33,4 +33,25 @@
 			testFocusTransfer( $( "#flip-checkbox" ) );
 		});
 
+	asyncTest( "Default is prevented on label click, but click is sent to element", function() {
+		var eventNs = ".preventDefaultAndPropagateClick",
+			label = $( "label[for='test-select-label']" ),
+			select = $( "#test-select-label" );
+
+		$.testHelper.detailedEventCascade([
+			function() {
+				var event = $.Event( "click" );
+
+				label.trigger( event );
+				deepEqual( event.isDefaultPrevented(), true, "Click-on-label default prevented" );
+			},
+			{
+				click: { src: select, event: "click" + eventNs + "1" }
+			},
+			function( result ) {
+				deepEqual( result.click.timedOut, false, "Select received a click" );
+				start();
+			}
+		]);
+	});
 })( jQuery );
