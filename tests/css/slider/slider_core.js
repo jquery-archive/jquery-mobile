0eaa8e3de6e7aa7f5525bd92312c8100a02f3b4f
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-03-11 11:19:58 +0200

    Slider: Test style and fix tooltip inconsistencies

---
+++ tests/css/slider/slider_core.js
@@ -0,0 +1,28 @@
+function testDisjoint( slider ) {
+	var inputOffset = slider.offset(),
+		inputRectangle = $.extend( {}, inputOffset, {
+			right: inputOffset.left + slider.width(),
+			bottom: inputOffset.top + slider.height()
+		} ),
+		track = slider.siblings( ".ui-slider-track" ),
+		trackOffset = track.offset(),
+		trackRectangle = $.extend( {}, trackOffset, {
+			right: trackOffset.left + track.width(),
+			bottom: trackOffset.top + track.height()
+		} );
+
+	deepEqual(
+		inputRectangle.left > trackRectangle.right ||
+		trackRectangle.left > inputRectangle.right ||
+		inputRectangle.top > trackRectangle.bottom ||
+		trackRectangle.top > inputRectangle.bottom, true,
+			"input and track rectangles are disjoint" );
+}
+
+test( "mini slider track does not overlap with input", function() {
+	testDisjoint( $( "#test-slider-mini" ) );
+} );
+
+test( "normal slider track does not overlap with input", function() {
+	testDisjoint( $( "#test-slider-normal" ) );
+} );
