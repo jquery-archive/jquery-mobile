f65f1c91786c1c339154a3934e5461d84dcc1395
Alexander Schmitz <arschmitz@gmail.com>
2015-03-17 21:31:27 -0400

    Media: Style Fixes

--- js/media.js
+++ js/media.js
@@ -5,46 +5,46 @@
 
 define( [ "jquery", "./core" ], function( jQuery ) {
 //>>excludeEnd("jqmBuildExclude");
-(function( $, undefined ) {
+( function( $, undefined ) {
 
 	/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
-	window.matchMedia = window.matchMedia || (function( doc, undefined ) {
+	window.matchMedia = window.matchMedia || ( function( doc, undefined ) {
 
-		var bool,
-			docElem = doc.documentElement,
-			refNode = docElem.firstElementChild || docElem.firstChild,
-			// fakeBody required for <FF4 when executed in <head>
-			fakeBody = doc.createElement( "body" ),
-			div = doc.createElement( "div" );
+			var bool,
+				docElem = doc.documentElement,
+				refNode = docElem.firstElementChild || docElem.firstChild,
+				// fakeBody required for <FF4 when executed in <head>
+				fakeBody = doc.createElement( "body" ),
+				div = doc.createElement( "div" );
 
-		div.id = "mq-test-1";
-		div.style.cssText = "position:absolute;top:-100em";
-		fakeBody.style.background = "none";
-		fakeBody.appendChild(div);
+			div.id = "mq-test-1";
+			div.style.cssText = "position:absolute;top:-100em";
+			fakeBody.style.background = "none";
+			fakeBody.appendChild( div );
 
-		return function(q){
+			return function( q ) {
 
-			div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
+				div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
 
-			docElem.insertBefore( fakeBody, refNode );
-			bool = div.offsetWidth === 42;
-			docElem.removeChild( fakeBody );
+				docElem.insertBefore( fakeBody, refNode );
+				bool = div.offsetWidth === 42;
+				docElem.removeChild( fakeBody );
 
-			return {
-				matches: bool,
-				media: q
+				return {
+					matches: bool,
+					media: q
+				};
+
 			};
 
-		};
-
-	}( document ));
+		}( document ) );
 
 	// $.mobile.media uses matchMedia to return a boolean.
 	$.mobile.media = function( q ) {
 		return window.matchMedia( q ).matches;
 	};
 
-})(jQuery);
+} )( jQuery );
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
-});
+} );
 //>>excludeEnd("jqmBuildExclude");
--- tests/unit/media/media_core.js
+++ tests/unit/media/media_core.js
@@ -2,21 +2,21 @@
  * mobile media unit tests
  */
 
-(function($){
+( function( $ ) {
 
-	test( "$.mobile.media function returns same boolean result as window.matchMedia", function(){
-		deepEqual($.mobile.media("screen"), window.matchMedia("screen").matches);
-	});
+test( "$.mobile.media function returns same boolean result as window.matchMedia", function() {
+	deepEqual( $.mobile.media( "screen" ), window.matchMedia( "screen" ).matches );
+} );
 
-	test( "$.mobile.media function returns boolean result", function(){
-		deepEqual( typeof $.mobile.media("screen"), "boolean" );
-	});
+test( "$.mobile.media function returns boolean result", function() {
+	deepEqual( typeof $.mobile.media( "screen" ), "boolean" );
+} );
 
-	test( "$.mobile.media function returns false result for inapplicable media", function(){
-		deepEqual( $.mobile.media("foo"), false );
-	});
-
-
+test( "$.mobile.media function returns false result for inapplicable media", function() {
+	deepEqual( $.mobile.media( "foo" ), false );
+} );
 
 
-})(jQuery);
+
+
+} )( jQuery );
