f05212aa6ed275d5b94b043898462e66cc3007db
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 10:39:00 -0400

    clearButton: Style fixes powered by esformatter

--- js/widgets/forms/clearButton.js
+++ js/widgets/forms/clearButton.js
@@ -27,129 +27,129 @@
 		// Browser globals
 		factory( jQuery );
 	}
-})( function( $ ) {
+} )( function( $ ) {
 
-	return $.widget( "mobile.textinput", $.mobile.textinput, {
-		options: {
-			clearBtn: false,
-			clearBtnText: "Clear text"
-		},
+return $.widget( "mobile.textinput", $.mobile.textinput, {
+	options: {
+		clearBtn: false,
+		clearBtnText: "Clear text"
+	},
 
-		_create: function() {
-			this._super();
+	_create: function() {
+		this._super();
 
-			if ( this.isSearch ) {
-				this.options.clearBtn = true;
-			}
+		if ( this.isSearch ) {
+			this.options.clearBtn = true;
+		}
 
-			if ( !!this.options.clearBtn && this.inputNeedsWrap ) {
-				this._addClearBtn();
-			}
-		},
+		if ( !!this.options.clearBtn && this.inputNeedsWrap ) {
+			this._addClearBtn();
+		}
+	},
 
-		clearButton: function() {
-			return $( "<a href='#' tabindex='-1' aria-hidden='true' " +
-				"class='ui-input-clear ui-button ui-icon-delete ui-button-icon-only ui-corner-all'>" +
-				"</a>" )
-					.attr( "title", this.options.clearBtnText )
-					.text( this.options.clearBtnText );
-		},
+	clearButton: function() {
+		return $( "<a href='#' tabindex='-1' aria-hidden='true' " +
+			"class='ui-input-clear ui-button ui-icon-delete ui-button-icon-only ui-corner-all'>" +
+			"</a>" )
+			.attr( "title", this.options.clearBtnText )
+			.text( this.options.clearBtnText );
+	},
 
-		_clearBtnClick: function( event ) {
-			this.element.val( "" )
-					.focus()
-					.trigger( "change" );
+	_clearBtnClick: function( event ) {
+		this.element.val( "" )
+			.focus()
+			.trigger( "change" );
 
-			this._clearBtn.addClass( "ui-input-clear-hidden" );
-			event.preventDefault();
-		},
+		this._clearBtn.addClass( "ui-input-clear-hidden" );
+		event.preventDefault();
+	},
 
-		_addClearBtn: function() {
+	_addClearBtn: function() {
 
-			if ( !this.options.enhanced ) {
-				this._enhanceClear();
-			}
+		if ( !this.options.enhanced ) {
+			this._enhanceClear();
+		}
 
-			$.extend( this, {
-				_clearBtn: this.widget().find("a.ui-input-clear")
-			});
+		$.extend( this, {
+			_clearBtn: this.widget().find( "a.ui-input-clear" )
+		} );
 
-			this._bindClearEvents();
+		this._bindClearEvents();
 
-			this._toggleClear();
+		this._toggleClear();
 
-		},
+	},
 
-		_enhanceClear: function() {
+	_enhanceClear: function() {
 
-			this.clearButton().appendTo( this.widget() );
-			this.widget().addClass( "ui-input-has-clear" );
+		this.clearButton().appendTo( this.widget() );
+		this.widget().addClass( "ui-input-has-clear" );
 
-		},
+	},
 
-		_bindClearEvents: function() {
+	_bindClearEvents: function() {
 
-			this._on( this._clearBtn, {
-				"click": "_clearBtnClick"
-			});
+		this._on( this._clearBtn, {
+			"click": "_clearBtnClick"
+		} );
 
-			this._on({
-				"keyup": "_toggleClear",
-				"change": "_toggleClear",
-				"input": "_toggleClear",
-				"focus": "_toggleClear",
-				"blur": "_toggleClear",
-				"cut": "_toggleClear",
-				"paste": "_toggleClear"
+		this._on( {
+			"keyup": "_toggleClear",
+			"change": "_toggleClear",
+			"input": "_toggleClear",
+			"focus": "_toggleClear",
+			"blur": "_toggleClear",
+			"cut": "_toggleClear",
+			"paste": "_toggleClear"
 
-			});
+		} );
 
-		},
+	},
 
-		_unbindClear: function() {
-			this._off( this._clearBtn, "click");
-			this._off( this.element, "keyup change input focus blur cut paste" );
-		},
+	_unbindClear: function() {
+		this._off( this._clearBtn, "click" );
+		this._off( this.element, "keyup change input focus blur cut paste" );
+	},
 
-		_setOptions: function( options ) {
-			this._super( options );
+	_setOptions: function( options ) {
+		this._super( options );
 
-			if ( options.clearBtn !== undefined &&
+		if ( options.clearBtn !== undefined &&
 				!this.element.is( "textarea, :jqmData(type='range')" ) ) {
-				if ( options.clearBtn ) {
-					this._addClearBtn();
-				} else {
-					this._destroyClear();
-				}
-			}
-
-			if ( options.clearBtnText !== undefined && this._clearBtn !== undefined ) {
-				this._clearBtn.text( options.clearBtnText )
-					.attr("title", options.clearBtnText);
-			}
-		},
-
-		_toggleClear: function() {
-			this._delay( "_toggleClearClass", 0 );
-		},
-
-		_toggleClearClass: function() {
-			this._clearBtn.toggleClass( "ui-input-clear-hidden", !this.element.val() );
-		},
-
-		_destroyClear: function() {
-			this.widget().removeClass( "ui-input-has-clear" );
-			this._unbindClear();
-			this._clearBtn.remove();
-		},
-
-		_destroy: function() {
-			this._super();
-			if ( this.options.clearBtn ) {
+			if ( options.clearBtn ) {
+				this._addClearBtn();
+			} else {
 				this._destroyClear();
 			}
 		}
 
-	});
+		if ( options.clearBtnText !== undefined && this._clearBtn !== undefined ) {
+			this._clearBtn.text( options.clearBtnText )
+				.attr( "title", options.clearBtnText );
+		}
+	},
 
-});
+	_toggleClear: function() {
+		this._delay( "_toggleClearClass", 0 );
+	},
+
+	_toggleClearClass: function() {
+		this._clearBtn.toggleClass( "ui-input-clear-hidden", !this.element.val() );
+	},
+
+	_destroyClear: function() {
+		this.widget().removeClass( "ui-input-has-clear" );
+		this._unbindClear();
+		this._clearBtn.remove();
+	},
+
+	_destroy: function() {
+		this._super();
+		if ( this.options.clearBtn ) {
+			this._destroyClear();
+		}
+	}
+
+} );
+
+} );
