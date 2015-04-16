65bbd636616256ffa0407528feca75e35bc8ed77
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 10:36:02 -0400

    Navigation: Style fixes powered by esformatter
    Closes gh-8097

--- tests/integration/navigation/event/event_core.js
+++ tests/integration/navigation/event/event_core.js
@@ -1,158 +1,158 @@
 $.testHelper.setPushState();
 
-(function( $ ) {
-	module( "navigate", {
-		setup: function() {
-			location.hash = "";
-		},
+( function( $ ) {
+module( "navigate", {
+	setup: function() {
+		location.hash = "";
+	},
 
-		teardown: function() {
-			$( window ).unbind( "navigate" );
+	teardown: function() {
+		$( window ).unbind( "navigate" );
+	}
+} );
+
+asyncTest( "changes to the url trigger a navigate", function() {
+	$( window ).one( "navigate", function( event, data ) {
+		ok( true, "navigate called" );
+		start();
+	} );
+
+	location.hash = "foo";
+} );
+
+asyncTest( "traversing history back fires a navigate", function() {
+	expect( 2 );
+
+	$( window ).one( "navigate", function( event, data ) {
+		ok( true, "navigate called on hash change" );
+
+		$( window ).one( "navigate", function( event, data ) {
+			ok( true, "navigate called on back button" );
+
+			start();
+		} );
+
+		window.history.back();
+	} );
+
+	location.hash = "foo";
+} );
+
+asyncTest( "navigation events are marked", function() {
+	$( window ).one( "navigate", function( event, data ) {
+		equal( event.originalEvent.type, $.support.pushState ? "popstate" : "hashchange", "tagged as popstate" );
+		start();
+	} );
+
+	location.hash = "foo";
+} );
+
+asyncTest( "navigation events can be disabled with prevent default on beforenavigate", function() {
+	// Failure in this test would be 3 test assertions
+	expect( 2 );
+
+	$( window ).one( "beforenavigate", function( e ) {
+		ok( true, "beforenavigate fired" );
+		e.preventDefault();
+	} );
+
+	$( window ).one( "navigate", function() {
+		ok( true, "navigate fired" );
+		start();
+	} );
+
+	// fire a navigate that will be prevented
+	location.hash = "foo";
+
+	// fire another to absorb the navigate binding
+	location.hash = "bar";
+} );
+
+asyncTest( "beforenavigate should carry the original navigation function", function() {
+
+	$( window ).one( "beforenavigate", function( event ) {
+		var type = event.originalEvent.type;
+
+		if ( $.support.pushState ) {
+			equal( type, "popstate", "the original event type is popstate" );
+		} else {
+			equal( type, "hashchange", "the original event type is hashchange" );
 		}
-	});
 
-	asyncTest( "changes to the url trigger a navigate", function() {
-		$( window ).one( "navigate", function( event, data ) {
-			ok( true, "navigate called" );
-			start();
-		});
+		start();
+	} );
 
-		location.hash = "foo";
-	});
+	location.hash = "beforenavigate";
+} );
 
-	asyncTest( "traversing history back fires a navigate", function() {
-		expect( 2 );
+if ( $.support.pushState ) {
+	asyncTest( "popstate navigation events contain pushed state", function() {
+		$.testHelper.eventTarget = $( window );
 
-		$( window ).one( "navigate", function( event, data ) {
-			ok( true, "navigate called on hash change" );
+		$.testHelper.eventSequence( "navigate", [
+			function() {
+				window.history.replaceState( { foo: "bar" }, document.title, location.href.replace( /#.*/, "" ) + "#foo" );
+				location.hash = "#foo2";
+			},
 
-			$( window ).one( "navigate", function( event, data ) {
-				ok( true, "navigate called on back button" );
+			function() {
+				window.history.back();
+			},
 
-				start();
-			});
-
-			window.history.back();
-		});
-
-		location.hash = "foo";
-	});
-
-	asyncTest( "navigation events are marked", function() {
-		$( window ).one( "navigate", function( event, data ) {
-			equal( event.originalEvent.type, $.support.pushState ? "popstate" : "hashchange", "tagged as popstate" );
-			start();
-		});
-
-		location.hash = "foo";
-	});
-
-	asyncTest( "navigation events can be disabled with prevent default on beforenavigate", function() {
-		// Failure in this test would be 3 test assertions
-		expect( 2 );
-
-		$( window ).one( "beforenavigate", function( e ) {
-			ok( true, "beforenavigate fired" );
-			e.preventDefault();
-		});
-
-		$( window ).one( "navigate", function() {
-			ok( true, "navigate fired" );
-			start();
-		});
-
-		// fire a navigate that will be prevented
-		location.hash = "foo";
-
-		// fire another to absorb the navigate binding
-		location.hash = "bar";
-	});
-
-	asyncTest( "beforenavigate should carry the original navigation function", function() {
-
-		$( window ).one( "beforenavigate", function(event) {
-			var type = event.originalEvent.type;
-
-			if( $.support.pushState ) {
-				equal( type, "popstate", "the original event type is popstate" );
-			} else {
-				equal( type, "hashchange", "the original event type is hashchange" );
-			}
-
-			start();
-		});
-
-		location.hash = "beforenavigate";
-	});
-
-	if( $.support.pushState ) {
-		asyncTest( "popstate navigation events contain pushed state", function() {
-			$.testHelper.eventTarget = $( window );
-
-			$.testHelper.eventSequence( "navigate", [
-				function() {
-					window.history.replaceState({ foo: "bar" }, document.title, location.href.replace(/#.*/, "" ) + "#foo");
-					location.hash = "#foo2";
-				},
-
-				function() {
-					window.history.back();
-				},
-
-				function( timedOut, data ) {
-					equal( data.state.foo, "bar", "state provided properly" );
-					start();
-				}
-			]);
-		});
-
-		// relies on having the early popstate handler defined in early_popstate_handler.js
-		asyncTest( "Default-prevented popstate does not trigger a navigate event",
-			function( assert ) {
-				var eventNs = ".defaultPreventedPopstate";
-
-				assert.expect( 2 );
-
-				$.testHelper.detailedEventCascade( [
-					function() {
-						window.history.replaceState( { foo: "bar" }, document.title,
-							location.href.replace(/#.*/, "" ) + "#foo" );
-						location.hash = "#foo2";
-					},
-					{
-						navigate: { src: $( window ), event: "navigate" + eventNs + "1" }
-					},
-					function( result ) {
-						assert.deepEqual( result.navigate.timedOut, false,
-							"Received navigate event going forward" );
-						window.preventDefaultForNextPopstate = true;
-						window.history.back();
-					},
-					{
-						navigate: { src: $( window ), event: "navigate" + eventNs + "2" }
-					},
-					function( result ) {
-						assert.deepEqual( result.navigate.timedOut, true,
-							"Received no navigate event from a default-prevented popstate" );
-						delete window.preventDefaultForNextPopstate;
-						start();
-					}
-				] );
-			} );
-
-	} else {
-		asyncTest( "hashchange navigation provides for data added in a later binding", function() {
-			$( window ).one( "beforenavigate", function( event, data ) {
-				event.originalEvent.hashchangeState = { foo: "bar" };
-			});
-
-			$( window ).one( "navigate", function( event, data ) {
-				equal( event.originalEvent.type, "hashchange", "event triggered by a hashchange" );
+			function( timedOut, data ) {
 				equal( data.state.foo, "bar", "state provided properly" );
 				start();
-			});
+			}
+		] );
+	} );
 
-			location.hash = "#foo2";
-		});
-	}
-})( jQuery );
+	// relies on having the early popstate handler defined in early_popstate_handler.js
+	asyncTest( "Default-prevented popstate does not trigger a navigate event",
+		function( assert ) {
+			var eventNs = ".defaultPreventedPopstate";
+
+			assert.expect( 2 );
+
+			$.testHelper.detailedEventCascade( [
+				function() {
+					window.history.replaceState( { foo: "bar" }, document.title,
+						location.href.replace( /#.*/, "" ) + "#foo" );
+					location.hash = "#foo2";
+				},
+				{
+					navigate: { src: $( window ), event: "navigate" + eventNs + "1" }
+				},
+				function( result ) {
+					assert.deepEqual( result.navigate.timedOut, false,
+						"Received navigate event going forward" );
+					window.preventDefaultForNextPopstate = true;
+					window.history.back();
+				},
+				{
+					navigate: { src: $( window ), event: "navigate" + eventNs + "2" }
+				},
+				function( result ) {
+					assert.deepEqual( result.navigate.timedOut, true,
+						"Received no navigate event from a default-prevented popstate" );
+					delete window.preventDefaultForNextPopstate;
+					start();
+				}
+			] );
+		} );
+
+} else {
+	asyncTest( "hashchange navigation provides for data added in a later binding", function() {
+		$( window ).one( "beforenavigate", function( event, data ) {
+			event.originalEvent.hashchangeState = { foo: "bar" };
+		} );
+
+		$( window ).one( "navigate", function( event, data ) {
+			equal( event.originalEvent.type, "hashchange", "event triggered by a hashchange" );
+			equal( data.state.foo, "bar", "state provided properly" );
+			start();
+		} );
+
+		location.hash = "#foo2";
+	} );
+}
+} )( jQuery );
--- tests/integration/navigation/navigation_base.js
+++ tests/integration/navigation/navigation_base.js
@@ -1,246 +1,246 @@
 /*
  * mobile navigation base tag unit tests
  */
-(function($){
-	var baseDir = $.mobile.path.parseUrl($("base").attr("href")).directory,
-		contentDir = $.mobile.path.makePathAbsolute("../content/", baseDir),
-		home = location.pathname + location.search,
-		baseTagEnabled = $.mobile.dynamicBaseTagEnabled,
-		baseTagSupported = $.support.dynamicBaseTag;
+( function( $ ) {
+var baseDir = $.mobile.path.parseUrl( $( "base" ).attr( "href" ) ).directory,
+	contentDir = $.mobile.path.makePathAbsolute( "../content/", baseDir ),
+	home = location.pathname + location.search,
+	baseTagEnabled = $.mobile.dynamicBaseTagEnabled,
+	baseTagSupported = $.support.dynamicBaseTag;
 
-	module('jquery.mobile.navigation.js - base tag', {
-		setup: function(){
-			$.mobile.navigate.history.stack = [];
-			$.mobile.navigate.history.activeIndex = 0;
-			$.testHelper.navReset( home );
+module( 'jquery.mobile.navigation.js - base tag', {
+	setup: function() {
+		$.mobile.navigate.history.stack = [];
+		$.mobile.navigate.history.activeIndex = 0;
+		$.testHelper.navReset( home );
+	}
+} );
+
+asyncTest( "can navigate between internal and external pages", function() {
+	$.testHelper.pageSequence( [
+		function() {
+			// Navigate from default internal page to another internal page.
+			$.testHelper.openPage( "#internal-page-2" );
+		},
+
+		function() {
+			// Verify that we are on the 2nd internal page.
+			$.testHelper.assertUrlLocation( {
+				push: home + "#internal-page-2",
+				hash: "internal-page-2",
+				report: "navigate to internal page"
+			} );
+
+			// Navigate to a page that is in the base directory. Note that the application
+			// document and this new page are *NOT* in the same directory.
+			$( "#internal-page-2 .bp1" ).click();
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: baseDir + "base-page-1.html",
+				report: "navigate from internal page to page in base directory"
+			} );
+
+			// Navigate to another page in the same directory as the current page.
+			$( "#base-page-1 .bp2" ).click();
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: baseDir + "base-page-2.html",
+				report: "navigate from base directory page to another base directory page"
+			} );
+
+			// Navigate to another page in a directory that is the sibling of the base.
+			$( "#base-page-2 .cp1" ).click();
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: contentDir + "content-page-1.html",
+				report: "navigate from base directory page to a page in a different directory hierarchy"
+			} );
+
+			// Navigate to another page in a directory that is the sibling of the base.
+			$( "#content-page-1 .cp2" ).click();
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: contentDir + "content-page-2.html",
+				report: "navigate to another page within the same non-base directory hierarchy"
+			} );
+
+			// Navigate to an internal page.
+			$( "#content-page-2 .ip1" ).click();
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			// the hash based nav result (hash:) is dictate by the fact that #internal-page-1
+			// is the original root page element
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: home,
+				report: "navigate from a page in a non-base directory to an internal page"
+			} );
+
+			// Try calling changePage() directly with a relative path.
+			$.mobile.changePage( "base-page-1.html" );
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: baseDir + "base-page-1.html",
+				report: "call changePage() with a filename (no path)"
+			} );
+
+			// Try calling changePage() directly with a relative path.
+			$.mobile.changePage( "../content/content-page-1.html" );
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: contentDir + "content-page-1.html",
+				report: "call changePage() with a relative path containing up-level references"
+			} );
+
+			// Try calling changePage() with an id
+			$.mobile.changePage( "content-page-2.html" );
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: contentDir + "content-page-2.html",
+				report: "call changePage() with a relative path should resolve relative to current page"
+			} );
+
+			// test that an internal page works
+			$( "a.ip2" ).click();
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hash: "internal-page-2",
+				push: home + "#internal-page-2",
+				report: "call changePage() with a page id"
+			} );
+
+			// Try calling changePage() with an id
+			$.mobile.changePage( "internal-page-1" );
+		},
+
+		function() {
+			// Verify that we are on the expected page.
+			$.testHelper.assertUrlLocation( {
+				hash: "internal-page-2",
+				push: home + "#internal-page-2",
+				report: "calling changePage() with a page id that is not prefixed with '#' should not change page"
+			} );
+
+			// Previous load should have failed and left us on internal-page-2.
+			start();
 		}
-	});
+	] );
+} );
 
-	asyncTest( "can navigate between internal and external pages", function(){
-		$.testHelper.pageSequence([
-			function(){
-				// Navigate from default internal page to another internal page.
-				$.testHelper.openPage( "#internal-page-2" );
-			},
+asyncTest( "internal form with no action submits to document URL", function() {
+	$.testHelper.pageSequence( [
+		// open our test page
+		function() {
+			$.testHelper.openPage( "#internal-no-action-form-page" );
+		},
 
-			function(){
-				// Verify that we are on the 2nd internal page.
-				$.testHelper.assertUrlLocation({
-					push: home + "#internal-page-2",
-					hash: "internal-page-2",
-					report: "navigate to internal page"
-				});
+		function() {
+			$( "#internal-no-action-form-page form" ).eq( 0 ).submit();
+		},
 
-				// Navigate to a page that is in the base directory. Note that the application
- 				// document and this new page are *NOT* in the same directory.
- 				$("#internal-page-2 .bp1").click();
-			},
+		function() {
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: location.pathname + "?foo=1&bar=2",
+				report: "hash should match document url and not base url"
+			} );
 
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: baseDir + "base-page-1.html",
-					report: "navigate from internal page to page in base directory"
-				});
+			start();
+		}
+	] );
+} );
 
-				// Navigate to another page in the same directory as the current page.
-				$("#base-page-1 .bp2").click();
-			},
+asyncTest( "external page form with no action submits to external page URL", function() {
+	$.testHelper.pageSequence( [
+		function() {
+			// Go to an external page that has a form.
+			$( "#internal-page-1 .cp1" ).click();
+		},
 
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: baseDir + "base-page-2.html",
-					report: "navigate from base directory page to another base directory page"
-				});
+		function() {
+			// Make sure we actually navigated to the external page.
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: contentDir + "content-page-1.html",
+				report: "should be on content-page-1.html"
+			} );
 
-				// Navigate to another page in a directory that is the sibling of the base.
-				$("#base-page-2 .cp1").click();
-			},
+			// Now submit the form in the external page.
+			$( "#content-page-1 form" ).eq( 0 ).submit();
+		},
 
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: contentDir + "content-page-1.html",
-					report: "navigate from base directory page to a page in a different directory hierarchy"
-				});
+		function() {
+			$.testHelper.assertUrlLocation( {
+				hashOrPush: contentDir + "content-page-1.html?foo=1&bar=2",
+				report: "hash should match page url and not document url"
+			} );
 
-				// Navigate to another page in a directory that is the sibling of the base.
-				$("#content-page-1 .cp2").click();
-			},
+			start();
+		}
+	] );
+} );
 
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: contentDir + "content-page-2.html",
-					report: "navigate to another page within the same non-base directory hierarchy"
-				});
+var testBaseTagAlteration = function( assertions ) {
+	$.testHelper.pageSequence( [
+		function() {
+			$.mobile.changePage( "../../base-change.html" );
+		},
 
-				// Navigate to an internal page.
-				$("#content-page-2 .ip1").click();
-			},
+		function() {
+			assertions();
+			window.history.back();
+		},
 
-			function(){
-				// Verify that we are on the expected page.
-				// the hash based nav result (hash:) is dictate by the fact that #internal-page-1
-				// is the original root page element
-				$.testHelper.assertUrlLocation({
-					hashOrPush: home,
-					report: "navigate from a page in a non-base directory to an internal page"
-				});
+		function() {
+			start();
+		}
+	] );
 
-				// Try calling changePage() directly with a relative path.
-				$.mobile.changePage("base-page-1.html");
-			},
+};
 
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: baseDir + "base-page-1.html",
-					report: "call changePage() with a filename (no path)"
-				});
+asyncTest( "disabling base tag changes should prevent base href value changes", function() {
+	var baseHref = $( "base" ).attr( "href" );
+	$.mobile.dynamicBaseEnabled = false;
 
-				// Try calling changePage() directly with a relative path.
-				$.mobile.changePage("../content/content-page-1.html");
-			},
+	testBaseTagAlteration( function() {
+		if ( $.support.dynamicBaseTag ) {
+			equal( baseHref, $( "base" ).attr( "href" ), "the base href value should be unchanged" );
+		} else {
+			equal( $.mobile.activePage.find( "#base-change-link" ).attr( "href" ), "foo", "the link href's remain unchanged" );
+		}
+	} );
+} );
 
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: contentDir + "content-page-1.html",
-					report: "call changePage() with a relative path containing up-level references"
-				});
+asyncTest( "enabling base tag changes should enable base href value changes", function() {
+	var baseHref = $( "base" ).attr( "href" );
+	$.mobile.dynamicBaseEnabled = true;
+	$.support.dynamicBaseTag = true;
 
-				// Try calling changePage() with an id
-				$.mobile.changePage("content-page-2.html");
-			},
-
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: contentDir + "content-page-2.html",
-					report: "call changePage() with a relative path should resolve relative to current page"
-				});
-
-				// test that an internal page works
-				$("a.ip2").click();
-			},
-
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hash:  "internal-page-2",
-					push: home + "#internal-page-2",
-					report: "call changePage() with a page id"
-				});
-
-				// Try calling changePage() with an id
-				$.mobile.changePage("internal-page-1");
-			},
-
-			function(){
-				// Verify that we are on the expected page.
-				$.testHelper.assertUrlLocation({
-					hash:  "internal-page-2",
-					push: home + "#internal-page-2",
-					report: "calling changePage() with a page id that is not prefixed with '#' should not change page"
-				});
-
-				// Previous load should have failed and left us on internal-page-2.
-				start();
-			}
-		]);
-	});
-
-	asyncTest( "internal form with no action submits to document URL", function(){
-		$.testHelper.pageSequence([
-			// open our test page
-			function(){
-				$.testHelper.openPage( "#internal-no-action-form-page" );
-			},
-
-			function(){
-				$( "#internal-no-action-form-page form" ).eq( 0 ).submit();
-			},
-
-			function(){
-				$.testHelper.assertUrlLocation({
-					hashOrPush: location.pathname + "?foo=1&bar=2",
-					report: "hash should match document url and not base url"
-				});
-
-				start();
-			}
-		]);
-	});
-
-	asyncTest( "external page form with no action submits to external page URL", function(){
-		$.testHelper.pageSequence([
-			function(){
-				// Go to an external page that has a form.
-				$("#internal-page-1 .cp1").click();
-			},
-
-			function(){
-				// Make sure we actually navigated to the external page.
-				$.testHelper.assertUrlLocation({
-					hashOrPush: contentDir + "content-page-1.html",
-					report: "should be on content-page-1.html"
-				});
-
-				// Now submit the form in the external page.
-				$("#content-page-1 form").eq(0).submit();
-			},
-
-			function(){
-				$.testHelper.assertUrlLocation({
-					hashOrPush: contentDir + "content-page-1.html?foo=1&bar=2",
-					report: "hash should match page url and not document url"
-				});
-
-				start();
-			}
-		]);
-	});
-
-	var testBaseTagAlteration = function( assertions ) {
-		$.testHelper.pageSequence([
-			function(){
-				$.mobile.changePage( "../../base-change.html" );
-			},
-
-			function(){
-				assertions();
-				window.history.back();
-			},
-
-			function() {
-				start();
-			}
-		]);
-
-	};
-
-	asyncTest( "disabling base tag changes should prevent base href value changes", function() {
-		var baseHref = $( "base" ).attr( "href" );
-		$.mobile.dynamicBaseEnabled = false;
-
-		testBaseTagAlteration(function() {
-				if ( $.support.dynamicBaseTag ) {
-					equal( baseHref, $( "base" ).attr( "href" ), "the base href value should be unchanged" );
-				} else {
-					equal( $.mobile.activePage.find( "#base-change-link" ).attr( "href" ), "foo", "the link href's remain unchanged" );
-				}
-		});
-	});
-
-	asyncTest( "enabling base tag changes should enable base href value changes", function() {
-		var baseHref = $( "base" ).attr( "href" );
-		$.mobile.dynamicBaseEnabled = true;
-		$.support.dynamicBaseTag = true;
-
-		testBaseTagAlteration(function() {
-			ok( baseHref !== $( "base" ).attr( "href" ), "the base href value should be changed" );
-		});
-	});
-})(jQuery);
+	testBaseTagAlteration( function() {
+		ok( baseHref !== $( "base" ).attr( "href" ), "the base href value should be changed" );
+	} );
+} );
+} )( jQuery );
