fe847618398b41e3ef118e6f3b66265053a9d344
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 10:36:02 -0400

    Navigation: Style fixes powered by esformatter
    Closes gh-8097

--- js/navigation/base.js
+++ js/navigation/base.js
@@ -24,75 +24,75 @@
 		// Browser globals
 		factory( jQuery );
 	}
-})( function( $ ) {
+} )( function( $ ) {
 
-	var base,
+var base,
 
-		// Existing base tag?
-		baseElement = $( "head" ).children( "base" ),
+	// Existing base tag?
+	baseElement = $( "head" ).children( "base" ),
+
+	// DEPRECATED as of 1.5.0 and will be removed in 1.6.0. As of 1.6.0 only
+	// base.dynamicBaseEnabled will be checked
+	getDynamicEnabled = function() {
+
+		// If a value has been set at the old, deprecated location, we return that value.
+		// Otherwise we return the value from the new location. We check explicitly for
+		// undefined because true and false are both valid values for dynamicBaseEnabled.
+		if ( $.mobile.dynamicBaseEnabled !== undefined ) {
+			return $.mobile.dynamicBaseEnabled;
+		}
+		return base.dynamicBaseEnabled;
+	};
+
+// base element management, defined depending on dynamic base tag support
+// TODO move to external widget
+base = {
+
+	// Disable the alteration of the dynamic base tag or links
+	dynamicBaseEnabled: true,
+
+	// Make sure base element is defined, for use in routing asset urls that are referenced
+	// in Ajax-requested markup
+	element: function() {
+		if ( !( baseElement && baseElement.length ) ) {
+			baseElement = $( "<base>", { href: $.mobile.path.documentBase.hrefNoSearch } )
+				.prependTo( $( "head" ) );
+		}
+
+		return baseElement;
+	},
+
+	// set the generated BASE element's href to a new page's base path
+	set: function( href ) {
+
+		// We should do nothing if the user wants to manage their url base manually.
+		// Note: Our method of ascertaining whether the user wants to manager their url base
+		// manually is DEPRECATED as of 1.5.0 and will be removed in 1.6.0. As of 1.6.0 the
+		// flag base.dynamicBaseEnabled will be checked, so the function getDynamicEnabled()
+		// will be removed.
+		if ( !getDynamicEnabled() ) {
+			return;
+		}
+
+		// we should use the base tag if we can manipulate it dynamically
+		base.element().attr( "href",
+			$.mobile.path.makeUrlAbsolute( href, $.mobile.path.documentBase ) );
+	},
+
+	// set the generated BASE element's href to a new page's base path
+	reset: function( /* href */ ) {
 
 		// DEPRECATED as of 1.5.0 and will be removed in 1.6.0. As of 1.6.0 only
 		// base.dynamicBaseEnabled will be checked
-		getDynamicEnabled = function() {
-
-			// If a value has been set at the old, deprecated location, we return that value.
-			// Otherwise we return the value from the new location. We check explicitly for
-			// undefined because true and false are both valid values for dynamicBaseEnabled.
-			if ( $.mobile.dynamicBaseEnabled !== undefined ) {
-				return $.mobile.dynamicBaseEnabled;
-			}
-			return base.dynamicBaseEnabled;
-		};
-
-	// base element management, defined depending on dynamic base tag support
-	// TODO move to external widget
-	base = {
-
-		// Disable the alteration of the dynamic base tag or links
-		dynamicBaseEnabled: true,
-
-		// Make sure base element is defined, for use in routing asset urls that are referenced
-		// in Ajax-requested markup
-		element: function() {
-			if ( !( baseElement && baseElement.length ) ) {
-				baseElement = $( "<base>", { href: $.mobile.path.documentBase.hrefNoSearch } )
-					.prependTo( $( "head" ) );
-			}
-
-			return baseElement;
-		},
-
-		// set the generated BASE element's href to a new page's base path
-		set: function( href ) {
-
-			// We should do nothing if the user wants to manage their url base manually.
-			// Note: Our method of ascertaining whether the user wants to manager their url base
-			// manually is DEPRECATED as of 1.5.0 and will be removed in 1.6.0. As of 1.6.0 the
-			// flag base.dynamicBaseEnabled will be checked, so the function getDynamicEnabled()
-			// will be removed.
-			if ( !getDynamicEnabled() ) {
-				return;
-			}
-
-			// we should use the base tag if we can manipulate it dynamically
-			base.element().attr( "href",
-				$.mobile.path.makeUrlAbsolute( href, $.mobile.path.documentBase ) );
-		},
-
-		// set the generated BASE element's href to a new page's base path
-		reset: function(/* href */) {
-
-			// DEPRECATED as of 1.5.0 and will be removed in 1.6.0. As of 1.6.0 only
-			// base.dynamicBaseEnabled will be checked
-			if ( !getDynamicEnabled() ) {
-				return;
-			}
-
-			base.element().attr( "href", $.mobile.path.documentBase.hrefNoSearch );
+		if ( !getDynamicEnabled() ) {
+			return;
 		}
-	};
 
-	$.mobile.base = base;
+		base.element().attr( "href", $.mobile.path.documentBase.hrefNoSearch );
+	}
+};
 
-	return base;
-});
+$.mobile.base = base;
+
+return base;
+} );
--- tests/integration/navigation/dynamic_base_disabled_core.js
+++ tests/integration/navigation/dynamic_base_disabled_core.js
@@ -1,31 +1,31 @@
 test( "Base tag inserted only if dynamic base tag support is enabled", function() {
-	var docBaseUrl = $.mobile.path.documentBase.hrefNoSearch;
+var docBaseUrl = $.mobile.path.documentBase.hrefNoSearch;
 
-	deepEqual( $( "head" ).children( "base" ).length, 0, "Initially, no base tag is present" );
+deepEqual( $( "head" ).children( "base" ).length, 0, "Initially, no base tag is present" );
 
-	$.mobile.base.dynamicBaseEnabled = false;
-	$.mobile.base.set( "http://example.com/" );
-	deepEqual( $( "head" ).children( "base" ).length, 0,
-		"Calling set() while the flag is disabled causes no base tag to be inserted" );
+$.mobile.base.dynamicBaseEnabled = false;
+$.mobile.base.set( "http://example.com/" );
+deepEqual( $( "head" ).children( "base" ).length, 0,
+	"Calling set() while the flag is disabled causes no base tag to be inserted" );
 
-	$.mobile.base.dynamicBaseEnabled = true;
-	$.mobile.base.set( "http://example.com/" );
-	deepEqual( $( "head" ).children( "base" ).length, 1,
-		"Calling set() while the flag is enabled causes one base tag to be inserted" );
-	deepEqual( $( "head" ).children( "base" ).attr( "href" ), "http://example.com/",
-		"The base URL is correctly set after calling set() while the flag is enabled" );
+$.mobile.base.dynamicBaseEnabled = true;
+$.mobile.base.set( "http://example.com/" );
+deepEqual( $( "head" ).children( "base" ).length, 1,
+	"Calling set() while the flag is enabled causes one base tag to be inserted" );
+deepEqual( $( "head" ).children( "base" ).attr( "href" ), "http://example.com/",
+	"The base URL is correctly set after calling set() while the flag is enabled" );
 
-	$.mobile.base.dynamicBaseEnabled = false;
-	$.mobile.base.reset();
-	deepEqual( $( "head" ).children( "base" ).length, 1,
-		"Calling reset() while the flag is disabled causes no more base tags to be inserted" );
-	deepEqual( $( "head" ).children( "base" ).attr( "href" ), "http://example.com/",
-		"The base remains correct after calling reset() with the flag unset" );
+$.mobile.base.dynamicBaseEnabled = false;
+$.mobile.base.reset();
+deepEqual( $( "head" ).children( "base" ).length, 1,
+	"Calling reset() while the flag is disabled causes no more base tags to be inserted" );
+deepEqual( $( "head" ).children( "base" ).attr( "href" ), "http://example.com/",
+	"The base remains correct after calling reset() with the flag unset" );
 
-	$.mobile.base.dynamicBaseEnabled = true;
-	$.mobile.base.reset();
-	deepEqual( $( "head" ).children( "base" ).length, 1,
-		"Calling reset() while the flag is enabled causes no more base tags to be inserted" );
-	deepEqual( $( "head" ).children( "base" ).attr( "href" ), docBaseUrl,
-		"Calling reset() while the flag is enabled correctly resets the base URL" );
-});
+$.mobile.base.dynamicBaseEnabled = true;
+$.mobile.base.reset();
+deepEqual( $( "head" ).children( "base" ).length, 1,
+	"Calling reset() while the flag is enabled causes no more base tags to be inserted" );
+deepEqual( $( "head" ).children( "base" ).attr( "href" ), docBaseUrl,
+	"Calling reset() while the flag is enabled correctly resets the base URL" );
+} );
