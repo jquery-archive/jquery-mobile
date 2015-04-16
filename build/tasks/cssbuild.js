77935ae4f6d92687535a3d88118400ae759ab46c
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 09:46:37 -0400

    Build: Add esformatter and format build files
    Closes gh-8079

--- build/tasks/cssbuild.js
+++ build/tasks/cssbuild.js
@@ -1,63 +1,63 @@
 module.exports = function( grunt ) {
-	var requirejs = require( "requirejs" ),
-		async = grunt.util.async;
+var requirejs = require( "requirejs" ),
+	async = grunt.util.async;
 
-	requirejs.define( "node/print", [], function() {
-		return function print( msg ) {
-			if ( msg.substring( 0, 5 ) === "Error" ) {
-				grunt.log.errorlns( msg );
-				grunt.fail.warn( "RequireJS failed." );
-			} else {
-				grunt.log.oklns( msg );
-			}
-		};
-	} );
+requirejs.define( "node/print", [], function() {
+	return function print( msg ) {
+		if ( msg.substring( 0, 5 ) === "Error" ) {
+			grunt.log.errorlns( msg );
+			grunt.fail.warn( "RequireJS failed." );
+		} else {
+			grunt.log.oklns( msg );
+		}
+	};
+} );
 
-	function expandFiles( files ) {
-		return grunt.util._.pluck( grunt.file.expandMapping( files ), "src" );
-	}
+function expandFiles( files ) {
+	return grunt.util._.pluck( grunt.file.expandMapping( files ), "src" );
+}
 
-	grunt.registerMultiTask( "cssbuild", "Resolve CSS @imports and concat files", function() {
-		var done = this.async(),
-			_ = grunt.util._,
-			options = _.clone( this.options( {
-				banner: ""
-			} ) ),
-			banner = options.banner;
+grunt.registerMultiTask( "cssbuild", "Resolve CSS @imports and concat files", function() {
+	var done = this.async(),
+		_ = grunt.util._,
+		options = _.clone( this.options( {
+			banner: ""
+		} ) ),
+		banner = options.banner;
 
-		delete options.banner;
+	delete options.banner;
 
-		async.forEach( this.files,
-			function( file, callback ) {
-				var src = grunt.template.process( file.orig.src[0] ),
-					dest = file.dest;
+	async.forEach( this.files,
+		function( file, callback ) {
+			var src = grunt.template.process( file.orig.src[ 0 ] ),
+				dest = file.dest;
 
-				grunt.log.debug( "Building '" + src + "' -> '" + dest + "'" );
+			grunt.log.debug( "Building '" + src + "' -> '" + dest + "'" );
 
-				async.series( [
-					function( next ) {
+			async.series( [
+				function( next ) {
 
-						// pull the includes together using require js
-						requirejs.optimize(
-							_.extend( {
-								cssIn: src,
-								out: dest
-							}, options
+					// pull the includes together using require js
+					requirejs.optimize(
+						_.extend( {
+							cssIn: src,
+							out: dest
+						}, options
 						), function( response ) {
 							next();
 						} );
-					},
-					function( next ) {
-						var contents = grunt.file.read( dest );
-						if ( banner ) {
-							contents = banner + contents;
-						}
-						grunt.file.write( dest, contents );
-						grunt.log.writeln( "File '" + dest + "' written." );
-						next();
+				},
+				function( next ) {
+					var contents = grunt.file.read( dest );
+					if ( banner ) {
+						contents = banner + contents;
 					}
-				], callback );
-			}, done
-		);
-	} );
+					grunt.file.write( dest, contents );
+					grunt.log.writeln( "File '" + dest + "' written." );
+					next();
+				}
+			], callback );
+		}, done
+	);
+} );
 };
--- build/tasks/options/bowercopy.js
+++ build/tasks/options/bowercopy.js
@@ -1,57 +1,57 @@
 module.exports = function( grunt ) {
-	return {
-		options: {
+return {
+	options: {
 
-			// Bower components folder will be removed afterwards
-			clean: true,
-			destPrefix: "external"
-		},
-		tests: {
-			files: {
-				"qunit/qunit.js": "qunit/qunit/qunit.js",
-				"qunit/qunit.css": "qunit/qunit/qunit.css",
-				"qunit/LICENSE.txt": "qunit/LICENSE.txt",
-				"qunit-assert-classes/qunit-assert-classes.js":
-					"qunit-assert-classes/qunit-assert-classes.js",
-				"qunit-assert-classes/LICENSE.txt": "qunit-assert-classes/LICENSE",
-				"jshint/jshint.js": "jshint/dist/jshint.js"
-			}
-		},
-		requirejs: {
-			files: {
-				"requirejs/require.js": "requirejs/require.js"
-			}
-		},
-		jquery: {
-			files: {
-				"jquery/": "jquery/dist/"
-			}
-		},
-		"jquery-ui": {
-			options: {
-				copyOptions: {
-					process: function( content ) {
-						var version = grunt
-							.file
-							.readJSON( "bower.json" )
-							.dependencies[ "jquery-ui" ];
-						if ( /#/.test( version ) ) {
-							version = version.split( "#" )[ 1 ];
-						}
-						return content.replace( /@VERSION/g, version );
-					}
-				}
-			},
-			files: {
-				"jquery-ui/core.js": "jquery-ui/ui/core.js",
-				"jquery-ui/widget.js": "jquery-ui/ui/widget.js",
-				"jquery-ui/accordion.js": "jquery-ui/ui/accordion.js",
-				"jquery-ui/tabs.js": "jquery-ui/ui/tabs.js",
-				"jquery-ui/button.js": "jquery-ui/ui/button.js",
-				"jquery-ui/checkboxradio.js": "jquery-ui/ui/checkboxradio.js",
-				"jquery-ui/controlgroup.js": "jquery-ui/ui/controlgroup.js",
-				"jquery-ui/LICENSE.txt": "jquery-ui/LICENSE.txt"
-			}
+		// Bower components folder will be removed afterwards
+		clean: true,
+		destPrefix: "external"
+	},
+	tests: {
+		files: {
+			"qunit/qunit.js": "qunit/qunit/qunit.js",
+			"qunit/qunit.css": "qunit/qunit/qunit.css",
+			"qunit/LICENSE.txt": "qunit/LICENSE.txt",
+			"qunit-assert-classes/qunit-assert-classes.js":
+			"qunit-assert-classes/qunit-assert-classes.js",
+			"qunit-assert-classes/LICENSE.txt": "qunit-assert-classes/LICENSE",
+			"jshint/jshint.js": "jshint/dist/jshint.js"
 		}
-	};
+	},
+	requirejs: {
+		files: {
+			"requirejs/require.js": "requirejs/require.js"
+		}
+	},
+	jquery: {
+		files: {
+			"jquery/": "jquery/dist/"
+		}
+	},
+	"jquery-ui": {
+		options: {
+			copyOptions: {
+				process: function( content ) {
+					var version = grunt
+						.file
+						.readJSON( "bower.json" )
+						.dependencies[ "jquery-ui" ];
+					if ( /#/.test( version ) ) {
+						version = version.split( "#" )[ 1 ];
+					}
+					return content.replace( /@VERSION/g, version );
+				}
+			}
+		},
+		files: {
+			"jquery-ui/core.js": "jquery-ui/ui/core.js",
+			"jquery-ui/widget.js": "jquery-ui/ui/widget.js",
+			"jquery-ui/accordion.js": "jquery-ui/ui/accordion.js",
+			"jquery-ui/tabs.js": "jquery-ui/ui/tabs.js",
+			"jquery-ui/button.js": "jquery-ui/ui/button.js",
+			"jquery-ui/checkboxradio.js": "jquery-ui/ui/checkboxradio.js",
+			"jquery-ui/controlgroup.js": "jquery-ui/ui/controlgroup.js",
+			"jquery-ui/LICENSE.txt": "jquery-ui/LICENSE.txt"
+		}
+	}
+};
 };
