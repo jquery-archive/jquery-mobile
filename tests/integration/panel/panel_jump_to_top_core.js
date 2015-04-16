e7a52eb80ff4e227bb29b3a0643cde844ee8ee14
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 10:34:12 -0400

    Panel: Style fixes powered by esformatter
    Closes gh-8105

--- tests/integration/panel/panel_jump_to_top_core.js
+++ tests/integration/panel/panel_jump_to_top_core.js
@@ -5,32 +5,32 @@ module( "Panel positioning", {
 	teardown: function() {
 		$( "#body-extender" ).remove();
 	}
-});
+} );
 
 asyncTest( "Panel must not jump to top upon throttledresize", function() {
-	var eventNs = ".panelMustNotJumpToTop";
+var eventNs = ".panelMustNotJumpToTop";
 
-	$.testHelper.detailedEventCascade([
-		function() {
-			$( "#scroll-to-top-test-link" ).click();
-		},
-		{
-			panelopen: { src: $( "#scroll-to-top-test" ), event: "panelopen" + eventNs + "1" }
-		},
-		function( result ) {
-			deepEqual( result.panelopen.timedOut, false, "Panel did open" );
-			window.scrollTo( 0, screen.height );
-			$( window ).trigger( "throttledresize" );
-			deepEqual( $( window ).scrollTop(), screen.height,
-				"Triggering throttledresize on panel did not cause it to jump to top" );
-			$( "#scroll-to-top-test" ).panel( "close" );
-		},
-		{
-			panelclose: { src: $( "#scroll-to-top-test" ), event: "panelclose" + eventNs + "2" }
-		},
-		function( result ) {
-			deepEqual( result.panelclose.timedOut, false, "Panel did close" );
-			start();
-		}
-	]);
-});
+$.testHelper.detailedEventCascade( [
+	function() {
+		$( "#scroll-to-top-test-link" ).click();
+	},
+	{
+		panelopen: { src: $( "#scroll-to-top-test" ), event: "panelopen" + eventNs + "1" }
+	},
+	function( result ) {
+		deepEqual( result.panelopen.timedOut, false, "Panel did open" );
+		window.scrollTo( 0, screen.height );
+		$( window ).trigger( "throttledresize" );
+		deepEqual( $( window ).scrollTop(), screen.height,
+			"Triggering throttledresize on panel did not cause it to jump to top" );
+		$( "#scroll-to-top-test" ).panel( "close" );
+	},
+	{
+		panelclose: { src: $( "#scroll-to-top-test" ), event: "panelclose" + eventNs + "2" }
+	},
+	function( result ) {
+		deepEqual( result.panelclose.timedOut, false, "Panel did close" );
+		start();
+	}
+] );
+} );
