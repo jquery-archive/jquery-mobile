21aa1583838e36066bcb9a0079301c97c906c13c
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 10:33:11 -0400

    Select: Style fixes powered by esformatter
    Closes gh-8108

--- tests/unit/select/select_core.js
+++ tests/unit/select/select_core.js
@@ -2,75 +2,75 @@
  * mobile select unit tests
  */
 
-(function($){
+( function( $ ) {
 
-	module( "Native select" );
+module( "Native select" );
 
-	test( "Native select does not blur synchronously in response to change", function() {
-		var selectmenu = $( "#blur-test" );
+test( "Native select does not blur synchronously in response to change", function() {
+	var selectmenu = $( "#blur-test" );
 
-		selectmenu.focus();
+	selectmenu.focus();
 
-		selectmenu.trigger( "change" );
+	selectmenu.trigger( "change" );
 
-		deepEqual( selectmenu.parent().hasClass( "ui-focus" ), true,
-			"Native select is focused after triggering 'change'" );
-	});
+	deepEqual( selectmenu.parent().hasClass( "ui-focus" ), true,
+		"Native select is focused after triggering 'change'" );
+} );
 
-	module( "Custom select" );
+module( "Custom select" );
 
-	test( "Custom select is enhanced correctly", function() {
-		var popup = $( "#enhance-test-listbox" );
+test( "Custom select is enhanced correctly", function() {
+	var popup = $( "#enhance-test-listbox" );
 
-		deepEqual( $( "#enhance-test-listbox a:first" ).attr( "role" ), "button", "The close button for a multiple choice select popup has the " + '"' + "role='button'" + '"' + " set" );
-		deepEqual( popup.popup( "option", "overlayTheme" ), "b", "Popup has overlayTheme b" );
-		deepEqual( popup.popup( "option", "theme" ), "x", "Popup has theme x" );
+	deepEqual( $( "#enhance-test-listbox a:first" ).attr( "role" ), "button", "The close button for a multiple choice select popup has the " + '"' + "role='button'" + '"' + " set" );
+	deepEqual( popup.popup( "option", "overlayTheme" ), "b", "Popup has overlayTheme b" );
+	deepEqual( popup.popup( "option", "theme" ), "x", "Popup has theme x" );
 
-	});
+} );
 
-	module( "Custom select Multiple" );
+module( "Custom select Multiple" );
 
-	test( "Custom select multiple is cleared correctly", function() {
-		var popup = $( "#enhance-test-listbox" );
-		$( "#enhance-test" )
+test( "Custom select multiple is cleared correctly", function() {
+	var popup = $( "#enhance-test-listbox" );
+	$( "#enhance-test" )
+		.find( "option" )
+			.attr( "selected", false )
+			.prop( "selected", false )
+		.end()
+		.selectmenu( "refresh" );
+	deepEqual( popup.find( ".ui-checkbox-on" ).length, 0,
+		"Checkboxes should not have ui-checkbox-on class" );
+} );
+
+module( "Custom select Multiple Inline" ) ;
+
+test( "Custom select multiple inline width is adjusted correctly", function() {
+	var selectMenu = $( "#width-test" ),
+		parent = selectMenu.parent(),
+		widths = [ 100, 250, 500, 1000 ],
+		finalResult = 0,
+		result = 0;
+
+	$.each( widths, function( index, width ) {
+		parent.css( { "width": width + "px" } );
+		selectMenu
 			.find( "option" )
-				.attr( "selected", false )
-				.prop( "selected", false )
-			.end()
-			.selectmenu( "refresh" );
-		deepEqual( popup.find( ".ui-checkbox-on" ).length, 0,
-			"Checkboxes should not have ui-checkbox-on class" );
-	});
+				.prop( "selected", true )
+			.end();
+		selectMenu.selectmenu( "refresh" );
+		result = selectMenu.width() < parent.width() ? 1 : 0;
+		if ( result === 1 ) {
+			finalResult += 1;
+		}
+	} );
 
-	module( "Custom select Multiple Inline" ) ;
+	strictEqual( finalResult, 4,
+		"select menu width should not exceed parent's width" );
+} );
 
-	test( "Custom select multiple inline width is adjusted correctly", function() {
-		var selectMenu = $( "#width-test" ),
-			parent = selectMenu.parent(),
-			widths = [ 100, 250, 500, 1000 ],
-			finalResult = 0,
-			result = 0;
+module( "Native select" );
 
-		$.each( widths, function( index, width ){
-			parent.css( {"width": width+"px"} );
-			selectMenu
-				.find( "option" )
-					.prop( "selected", true )
-				.end();
-			selectMenu.selectmenu( "refresh" );
-			result = selectMenu.width() < parent.width() ? 1 : 0;
-			if ( result === 1 ) {
-				finalResult += 1;
-			}
-		});
-
-		strictEqual( finalResult, 4, 
-			"select menu width should not exceed parent's width" );
-	});
-
-	module( "Native select" );
-
-	test( "Select menu ID", function() {
-		ok( $( ".no-id-test" ).closest( ".ui-button" ).attr( "id" ) !== "undefined-button", "Select menu without an ID does not result in the button having name 'undefined-button'" );
-	});
-})(jQuery);
+test( "Select menu ID", function() {
+	ok( $( ".no-id-test" ).closest( ".ui-button" ).attr( "id" ) !== "undefined-button", "Select menu without an ID does not result in the button having name 'undefined-button'" );
+} );
+} )( jQuery );
