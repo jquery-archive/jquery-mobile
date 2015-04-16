58bdf257455dc72fad502844e49cbbabb7c9db50
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 10:34:20 -0400

    page: Style fixes powered by esformatter

--- tests/unit/page-sections/page_core.js
+++ tests/unit/page-sections/page_core.js
@@ -1,46 +1,47 @@
 /*
  * mobile page unit tests
  */
-(function($){
-	var libName = 'jquery.mobile.page.sections';
+( function( $ ) {
+var libName = 'jquery.mobile.page.sections';
 
-	module(libName);
+module( libName );
 
-	test( "nested header anchors aren't altered", function(){
-		ok(!$('.ui-header > div > a').hasClass('ui-button'));
-	});
+test( "nested header anchors aren't altered", function() {
+	ok( !$( '.ui-header > div > a' ).hasClass( 'ui-button' ) );
+} );
 
-	test( "nested footer anchors aren't altered", function(){
-		ok(!$('.ui-footer > div > a').hasClass('ui-button'));
-	});
+test( "nested footer anchors aren't altered", function() {
+	ok( !$( '.ui-footer > div > a' ).hasClass( 'ui-button' ) );
+} );
 
-	test( "nested bar anchors aren't styled", function(){
-		ok(!$('.ui-bar > div > a').hasClass('ui-button'));
-	});
+test( "nested bar anchors aren't styled", function() {
+	ok( !$( '.ui-bar > div > a' ).hasClass( 'ui-button' ) );
+} );
 
-	test( "no auto-generated back button exists on first page", function(){
-		ok( !$(".ui-header > :jqmData(rel='back')").length );
-	});
+test( "no auto-generated back button exists on first page", function() {
+	ok( !$( ".ui-header > :jqmData(rel='back')" ).length );
+} );
 
-	test( "sections inside an ignored container do not enhance", function() {
-		var $ignored = $( "#ignored-header" ),  $enhanced = $( "#enhanced-header" );
+test( "sections inside an ignored container do not enhance", function() {
+	var $ignored = $( "#ignored-header" ),
+		$enhanced = $( "#enhanced-header" );
 
-		$.mobile.ignoreContentEnabled = true;
+	$.mobile.ignoreContentEnabled = true;
 
-		$ignored
-			.parent()
+	$ignored
+		.parent()
 			.attr( "data-" + $.mobile.ns + "role", "page" )
 			.page()
 			.trigger( "pagecreate" );
-		ok( !$ignored.hasClass( "ui-header" ), "ignored header has no class" );
+	ok( !$ignored.hasClass( "ui-header" ), "ignored header has no class" );
 
-		$enhanced
-			.parent()
+	$enhanced
+		.parent()
 			.attr( "data-" + $.mobile.ns + "role", "page" )
 			.page()
 			.trigger( "pagecreate" );
-		ok( $enhanced.hasClass( "ui-header" ), "enhanced header has classes" );
+	ok( $enhanced.hasClass( "ui-header" ), "enhanced header has classes" );
 
-		$.mobile.ignoreContentEnabled = false;
-	});
-})(jQuery);
+	$.mobile.ignoreContentEnabled = false;
+} );
+} )( jQuery );
--- tests/unit/pagecontainer/pagecontainer_core.js
+++ tests/unit/pagecontainer/pagecontainer_core.js
@@ -1,22 +1,22 @@
 test( "_find() can handle weird data-url attributes", function() {
-	deepEqual(
-		$.mobile.pagecontainer.prototype._find.call({
-			_createFileUrl: $.mobile.pagecontainer.prototype._createFileUrl,
-			_createDataUrl: $.mobile.pagecontainer.prototype._createDataUrl,
-			_getInitialContent: $.mobile.pagecontainer.prototype._getInitialContent,
-			element: $( "body" ),
-			_getNs: $.mobile.pagecontainer.prototype._getNs,
+deepEqual(
+	$.mobile.pagecontainer.prototype._find.call( {
+		_createFileUrl: $.mobile.pagecontainer.prototype._createFileUrl,
+		_createDataUrl: $.mobile.pagecontainer.prototype._createDataUrl,
+		_getInitialContent: $.mobile.pagecontainer.prototype._getInitialContent,
+		element: $( "body" ),
+		_getNs: $.mobile.pagecontainer.prototype._getNs,
 
-		}, "Raison d'être.html" )[ 0 ],
-		$( ".weird-data-url" )[ 0 ],
-		"Correct element is retrieved when the file name is weird" );
-});
+	}, "Raison d'être.html" )[ 0 ],
+	$( ".weird-data-url" )[ 0 ],
+	"Correct element is retrieved when the file name is weird" );
+} );
 
 ( function() {
 var triggerData,
 	originalLoad = $.mobile.pagecontainer.prototype._triggerWithDeprecated;
 module( "load method", {
-	setup: function(){
+	setup: function() {
 		$.mobile.pagecontainer.prototype._triggerWithDeprecated = function( eventName, data ) {
 			triggerData = data;
 			return {
@@ -31,17 +31,17 @@ module( "load method", {
 			}
 		}
 	},
-	teardown: function(){
+	teardown: function() {
 		triggerData = null;
 		$.mobile.pagecontainer.prototype._triggerWithDeprecated = originalLoad;
 	}
-});
-test( "load does not trigger an error when called withput a second param", function(){
+} );
+test( "load does not trigger an error when called withput a second param", function() {
 	var pagecontainer = $( ":mobile-pagecontainer" );
 
 	pagecontainer.pagecontainer( "load", "stuff.html" );
 	ok( "no error triggered when load method called without options" );
-});
+} );
 
 test( "Options 'reload' and 'reloadPage' both work, and 'reload' takes precedence", function() {
 	var pagecontainer = $( ":mobile-pagecontainer" );
@@ -49,40 +49,40 @@ test( "Options 'reload' and 'reloadPage' both work, and 'reload' takes precedenc
 	pagecontainer.pagecontainer( "load", "stuff.html", {
 		reload: true,
 		reloadPage: false
-	});
+	} );
 
 	deepEqual( triggerData.options.reload, true,
 		"The value of option 'reload' is not affected by the value of option 'reloadPage'" );
 
 	pagecontainer.pagecontainer( "load", "stuff.html", {
 		reloadPage: true
-	});
+	} );
 
 	deepEqual( triggerData.options.reload, true,
 		"The value of option 'reloadPage' is copied to the value of option 'reload' if the " +
 		"latter is absent from the options hash" );
-});
+} );
 
 module( "_handleDialog()" );
 
 test( "A dialog is recognized via presence of the data key, not the ui-dialog class", function() {
 	var getActiveHistoryCalled = false;
 
-	deepEqual( $.mobile.pagecontainer.prototype._handleDialog.call({
-			getActivePage: function() {
-				return $( "<div class='ui-dialog'></div>" );
-			},
-			_getActiveHistory: function() {
-				getActiveHistoryCalled = true;
-				return {};
-			},
-			back: $.noop,
-			forward: $.noop,
-		}, {}, {
-			pageUrl: "xyzzy.html"
-		}), false, "page is recognized as page even when the ui-dialog class is present" );
+	deepEqual( $.mobile.pagecontainer.prototype._handleDialog.call( {
+		getActivePage: function() {
+			return $( "<div class='ui-dialog'></div>" );
+		},
+		_getActiveHistory: function() {
+			getActiveHistoryCalled = true;
+			return {};
+		},
+		back: $.noop,
+		forward: $.noop,
+	}, {}, {
+		pageUrl: "xyzzy.html"
+	} ), false, "page is recognized as page even when the ui-dialog class is present" );
 
 	deepEqual( getActiveHistoryCalled, false, "_getActiveHistory() should not have been called" );
-});
+} );
 
-})();
+} )();
