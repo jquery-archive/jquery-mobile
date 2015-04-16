a368fb555d61c402ab8446667519661c360b3f06
Alexander Schmitz <arschmitz@gmail.com>
2015-04-16 10:36:02 -0400

    navigation: Style fixes powered by esformatter

--- tests/integration/navigation/click_handler_core.js
+++ tests/integration/navigation/click_handler_core.js
@@ -1,14 +1,14 @@
 module( "Click handler" );
 
 asyncTest( "Active class is removed from reset button", function() {
-	var button = $( "#reset-button" ).click();
+var button = $( "#reset-button" ).click();
 
-	expect( 2 );
+expect( 2 );
 
-	deepEqual( button.hasClass( $.mobile.activeBtnClass ), true, "When clicked, reset button gets active class" );
+deepEqual( button.hasClass( $.mobile.activeBtnClass ), true, "When clicked, reset button gets active class" );
 
-	setTimeout( function() {
-		deepEqual( button.hasClass( $.mobile.activeBtnClass ), false, "Active class is removed after a while" );
-		start();
-	}, 700 );
-});
+setTimeout( function() {
+	deepEqual( button.hasClass( $.mobile.activeBtnClass ), false, "Active class is removed after a while" );
+	start();
+}, 700 );
+} );
--- tests/integration/navigation/event/event_beforenavigate.js
+++ tests/integration/navigation/event/event_beforenavigate.js
@@ -2,17 +2,17 @@ $.testHelper.setPushState();
 
 asyncTest( "changes to the url trigger a beforenavigate", function() {
 
-	$.testHelper.detailedEventCascade([
-		function() {
-			location.hash = "foo";
-		},
-		{
-			beforenavigate: { src: $( window ), event: "beforenavigate.changesToUrlTrigger1" }
-		},
-		function( result ) {
-			deepEqual( result.beforenavigate.timedOut, false,
-				"beforenavigate event was triggered" );
-			start();
-		}
-	]);
-});
+$.testHelper.detailedEventCascade( [
+	function() {
+		location.hash = "foo";
+	},
+	{
+		beforenavigate: { src: $( window ), event: "beforenavigate.changesToUrlTrigger1" }
+	},
+	function( result ) {
+		deepEqual( result.beforenavigate.timedOut, false,
+			"beforenavigate event was triggered" );
+		start();
+	}
+] );
+} );
