a066147910f4960689109e7441eb02e49d71f7eb
Cory Gackenheimer <cory.gack@gmail.com>
2015-04-17 12:49:11 -0400

    Flipswitch: Implement Classes Option
    Fixes gh-7715
    Closes gh-8118

---
+++ tests/unit/flipswitch/flipswitch_backcompat.js
@@ -0,0 +1,21 @@
+/*
+ * mobile flipswitch unit tests
+ */
+( function( $ ) {
+test( "checkbox based flipswitch backcompat tests", function( assert ) {
+	assert.hasClasses( $( "#flip-checkbox-mini" ).parent(), "ui-mini", "is mini" );
+	assert.lacksClasses( $( "#flip-checkbox-corners" ).parent(), "ui-corner-all",
+		"does not get ui-corner-all when set to false" );
+	assert.hasClasses( $( "#flip-checkbox-wrapperclass" ).parent(), "checkbox-custom-class",
+		"has wrapper class" );
+} );
+
+test( "select based flipswitch backcompat tests", function( assert ) {
+	assert.hasClasses( $( "#flip-select-mini" ).parent(), "ui-mini", "is mini" );
+	assert.lacksClasses( $( "#flip-select-corners" ).parent(), "ui-corner-all",
+		"does not get ui-corner-all when set to false" );
+	assert.hasClasses( $( "#flip-select-wrapperclass" ).parent(), "select-custom-class",
+		"has wrapper class" );
+} );
+
+} )( jQuery );
