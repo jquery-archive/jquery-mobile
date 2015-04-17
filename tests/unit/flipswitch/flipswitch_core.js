41dd71ef77af861f25b3d405bed5be2619593423
Cory Gackenheimer <cory.gack@gmail.com>
2015-04-17 13:45:43 -0400

    Flipswitch: Implement Classes Option
    assert classes in test
    
    Fixes gh-7715

--- tests/unit/flipswitch/flipswitch_core.js
+++ tests/unit/flipswitch/flipswitch_core.js
@@ -3,60 +3,60 @@
  */
 ( function( $ ) {
 
-test( "checkbox based flipswitch is enhanced", function() {
-	ok( $( "#flip-checkbox" ).parent().hasClass( "ui-flipswitch" ), "should be enhanced" );
+test( "checkbox based flipswitch is enhanced", function( assert ) {
+	assert.hasClasses( $( "#flip-checkbox" ).parent(), "ui-flipswitch", "should be enhanced" );
 } );
-test( "select based flipswitch is enhanced", function() {
-	ok( $( "#flip-select" ).parent().hasClass( "ui-flipswitch" ), "should be enhanced" );
+test( "select based flipswitch is enhanced", function( assert ) {
+	assert.hasClasses( $( "#flip-select" ).parent(), "ui-flipswitch", "should be enhanced" );
 } );
-test( "checkbox based flipswitch is disabled", function() {
-	ok( $( "#flip-checkbox-disabled" ).parent().hasClass( "ui-state-disabled" ), "should be disabled" );
+test( "checkbox based flipswitch is disabled", function( assert ) {
+	assert.hasClasses( $( "#flip-checkbox-disabled" ).parent(), "ui-state-disabled", "should be disabled" );
 } );
-test( "select based flipswitch is disabled", function() {
-	ok( $( "#flip-select-disabled" ).parent().hasClass( "ui-state-disabled" ), "should be disabled" );
+test( "select based flipswitch is disabled", function( assert ) {
+	assert.hasClasses( $( "#flip-select-disabled" ).parent(), "ui-state-disabled", "should be disabled" );
 } );
-test( "checkbox based flipswitch is active", function() {
-	ok( $( "#flip-checkbox-active" ).parent().hasClass( "ui-flipswitch-active" ), "should be active" );
+test( "checkbox based flipswitch is active", function( assert ) {
+	assert.hasClasses( $( "#flip-checkbox-active" ).parent(), "ui-flipswitch-active", "should be active" );
 } );
-test( "select based flipswitch is active", function() {
-	ok( $( "#flip-select-second-option" ).parent().hasClass( "ui-flipswitch-active" ), "should be active" );
+test( "select based flipswitch is active", function( assert ) {
+	assert.hasClasses( $( "#flip-select-second-option" ).parent(), "ui-flipswitch-active", "should be active" );
 } );
-test( "checkbox based flipswitch is mini", function() {
-	ok( $( "#flip-checkbox-mini" ).parent().hasClass( "ui-mini" ), "should be mini" );
+test( "checkbox based flipswitch is mini", function( assert ) {
+	assert.hasClasses( $( "#flip-checkbox-mini" ).parent(), "ui-mini", "should be mini" );
 } );
-test( "select based flipswitch is mini", function() {
-	ok( $( "#flip-select-mini" ).parent().hasClass( "ui-mini" ), "should be mini" );
+test( "select based flipswitch is mini", function( assert ) {
+	assert.hasClasses( $( "#flip-select-mini" ).parent(), "ui-mini", "should be mini" );
 } );
-test( "checkbox based flipswitch should have theme inherit", function() {
-	ok( $( "#flip-checkbox-active" ).parent().hasClass( "ui-bar-inherit" ), "should be inherit theme" );
+test( "checkbox based flipswitch should have theme inherit", function( assert ) {
+	assert.hasClasses( $( "#flip-checkbox-active" ).parent(), "ui-bar-inherit", "should be inherit theme" );
 } );
-test( "select based flipswitch should have theme inherit", function() {
-	ok( $( "#flip-select-second-option" ).parent().hasClass( "ui-bar-inherit" ), "should be inherit theme" );
+test( "select based flipswitch should have theme inherit", function( assert ) {
+	assert.hasClasses( $( "#flip-select-second-option" ).parent(), "ui-bar-inherit", "should be inherit theme" );
 } );
-test( "checkbox based flipswitch is toggled on click", function() {
-	ok( !$( "#flip-checkbox" ).parent().hasClass( "ui-flipswitch-active" ), "should not be active" );
+test( "checkbox based flipswitch is toggled on click", function( assert ) {
+	assert.lacksClasses($( "#flip-checkbox" ).parent(), "ui-flipswitch-active", "should not be active" );
 	$( "#flip-checkbox" ).parent().click()
-	ok( $( "#flip-checkbox" ).parent().hasClass( "ui-flipswitch-active" ), "should be active" );
+	assert.hasClasses( $( "#flip-checkbox" ).parent(), "ui-flipswitch-active", "should be active" );
 } );
-test( "select based flipswitch is toggled on click", function() {
+test( "select based flipswitch is toggled on click", function( assert ) {
 	$( "#flip-select" ).click();
-	ok( $( "#flip-select" ).parent().hasClass( "ui-flipswitch-active" ), "should be active" );
+	assert.hasClasses( $( "#flip-select" ).parent(), "ui-flipswitch-active", "should be active" );
 } );
-test( "checkbox based flipswitch is not active after left swipe", function() {
+test( "checkbox based flipswitch is not active after left swipe", function( assert ) {
 	$( "#flip-checkbox" ).trigger( "swipeleft" );
-	ok( !$( "#flip-checkbox" ).parent().hasClass( "ui-flipswitch-active" ), "should be active" );
+	assert.lacksClasses( $( "#flip-checkbox" ).parent(), "ui-flipswitch-active", "should be active" );
 } );
-test( "select based flipswitch is not active after left swipe", function() {
+test( "select based flipswitch is not active after left swipe", function( assert ) {
 	$( "#flip-select" ).trigger( "swipeleft" );
-	ok( !$( "#flip-select" ).parent().hasClass( "ui-flipswitch-active" ), "should be active" );
+	assert.lacksClasses( $( "#flip-select" ).parent(), "ui-flipswitch-active", "should be active" );
 } );
-test( "checkbox based flipswitch is active after right swipe", function() {
+test( "checkbox based flipswitch is active after right swipe", function( assert ) {
 	$( "#flip-checkbox" ).trigger( "swiperight" );
-	ok( $( "#flip-checkbox" ).parent().hasClass( "ui-flipswitch-active" ), "should not be active" );
+	assert.hasClasses( $( "#flip-checkbox" ).parent(), "ui-flipswitch-active", "should not be active" );
 } );
-test( "select based flipswitch is active after right swipe", function() {
+test( "select based flipswitch is active after right swipe", function( assert ) {
 	$( "#flip-select" ).trigger( "swiperight" );
-	ok( $( "#flip-select" ).parent().hasClass( "ui-flipswitch-active" ), "should not be active" );
+	assert.hasClasses( $( "#flip-select" ).parent(), "ui-flipswitch-active", "should not be active" );
 } );
 test( "checkbox based flipswitch is untabbable", function() {
 	deepEqual( parseInt( $( "#flip-checkbox" ).attr( "tabindex" ) ), -1,
