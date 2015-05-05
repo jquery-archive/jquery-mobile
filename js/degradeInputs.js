5d5bcf1e07fb53f368245b75f8534c9ac75a6ec6
Alexander Schmitz <arschmitz@gmail.com>
2015-05-05 09:19:10 -0400

    DegradeInputs: Remove code supporting an IE8 bug that is no longer
    reproducible with current code.

--- js/degradeInputs.js
+++ js/degradeInputs.js
@@ -9,7 +9,7 @@
 
 //>>label: Degrade Inputs
 //>>group: Utilities
-//>>description: Changes input type to another after custom enhancements are made (ex. range > numeric).
+//>>description: Degrades HTM5 input types to compatible HTML4 ones.
 //>>docs: http://api.jquerymobile.com/jQuery.mobile.degradeInputsWithin/
 
 ( function( factory ) {
@@ -27,19 +27,8 @@
 } )( function( $ ) {
 
 $.mobile.degradeInputs = {
-	color: false,
-	date: false,
-	datetime: false,
-	"datetime-local": false,
-	email: false,
-	month: false,
-	number: false,
 	range: "number",
-	search: "text",
-	tel: false,
-	time: false,
-	url: false,
-	week: false
+	search: "text"
 };
 
 // Auto self-init widgets
@@ -48,7 +37,7 @@ $.mobile.degradeInputsWithin = function( target ) {
 
 	// Degrade inputs to avoid poorly implemented native functionality
 	target.find( "input" ).not( $.mobile.keepNative ).each( function() {
-		var html, hasType, findstr, repstr,
+		var html, findstr, repstr,
 			element = $( this ),
 			type = this.getAttribute( "type" ),
 			optType = $.mobile.degradeInputs[ type ] || "text";
@@ -56,12 +45,8 @@ $.mobile.degradeInputsWithin = function( target ) {
 		if ( $.mobile.degradeInputs[ type ] ) {
 			html = $( "<div>" ).html( element.clone() ).html();
 
-			// In IE browsers, the type sometimes doesn't exist in the cloned markup,
-			// so we replace the closing tag instead
-			hasType = html.indexOf( " type=" ) > -1;
-			findstr = hasType ? /\s+type=["']?\w+['"]?/ : /\/?>/;
-			repstr = " type=\"" + optType + "\" data-" + $.mobile.ns +
-				"type=\"" + type + "\"" + ( hasType ? "" : ">" );
+			findstr = /\s+type=["']?\w+['"]?/;
+			repstr = " type=\"" + optType + "\" data-" + $.mobile.ns + "type=\"" + type + "\"";
 
 			element.replaceWith( html.replace( findstr, repstr ) );
 		}
