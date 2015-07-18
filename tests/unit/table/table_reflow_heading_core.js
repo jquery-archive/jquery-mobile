69aadc8fe03d6c9598e6c34f7c7130147ff2a6b1
Amanpreet Singh <apsdehal@gmail.com>
2015-07-18 02:00:58 -0700

    Table: Cleanup deprecated QUnit code
    Closes gh-8224

--- tests/unit/table/table_reflow_heading_core.js
+++ tests/unit/table/table_reflow_heading_core.js
@@ -1,5 +1,9 @@
-test( "Heading label added for empty heading", function() {
-$( "#test-reflow-headings" ).table();
-deepEqual( $( "#interesting-cell" ).children( "b.ui-table-cell-label" ).length, 1,
-	"Cell beneath empty heading has a copy of the heading" );
-} );
+( function( QUnit, $ ) {
+
+QUnit.test( "Heading label added for empty heading", function( assert ) {
+	$( "#test-reflow-headings" ).table();
+	assert.deepEqual( $( "#interesting-cell" ).children( "b.ui-table-cell-label" ).length, 1,
+		"Cell beneath empty heading has a copy of the heading" );
+});
+
+} )( QUnit, jQuery );
