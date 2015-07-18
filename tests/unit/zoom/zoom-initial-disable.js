648f63473e0bcfd61dfce09559ac67d05321b0df
Amanpreet Singh <apsdehal@gmail.com>
2015-07-18 02:13:20 -0700

    Zoom: Cleanup deprecated QUnit code

--- tests/unit/zoom/zoom-initial-disable.js
+++ tests/unit/zoom/zoom-initial-disable.js
@@ -1,9 +1,11 @@
 /*
  * mobile zoom
  */
-( function( $ ) {
-test( "User zooming will not enable when calling enable() method if zooming was disabled in page source", function() {
+( function( QUnit, $ ) {
+
+QUnit.test( "User zooming will not enable when calling enable() method if zooming was disabled in page source", function( assert ) {
 	$.mobile.zoom.enable();
-	ok( !$.mobile.zoom.enabled );
+	assert.ok( !$.mobile.zoom.enabled );
 } );
-} )( jQuery );
+
+} )( QUnit, jQuery );
