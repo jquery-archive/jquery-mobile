5f7ff95d02113fc08a224004e7c07fe6ee69b39b
Amanpreet Singh <apsdehal@gmail.com>
2015-07-18 00:30:56 -0700

    Flipswitch: Clean deprecated QUnit code
    Closes gh-8166

--- tests/unit/flipswitch/flipswitch_backcompat.js
+++ tests/unit/flipswitch/flipswitch_backcompat.js
@@ -1,8 +1,8 @@
 /*
  * mobile flipswitch unit tests
  */
-( function( $ ) {
-test( "checkbox based flipswitch backcompat tests", function( assert ) {
+( function( QUnit, $ ) {
+QUnit.test( "checkbox based flipswitch backcompat tests", function( assert ) {
 	assert.hasClasses( $( "#flip-checkbox-mini" ).parent(), "ui-mini", "is mini" );
 	assert.lacksClasses( $( "#flip-checkbox-corners" ).parent(), "ui-corner-all",
 		"does not get ui-corner-all when set to false" );
@@ -10,7 +10,7 @@ test( "checkbox based flipswitch backcompat tests", function( assert ) {
 		"has wrapper class" );
 } );
 
-test( "select based flipswitch backcompat tests", function( assert ) {
+QUnit.test( "select based flipswitch backcompat tests", function( assert ) {
 	assert.hasClasses( $( "#flip-select-mini" ).parent(), "ui-mini", "is mini" );
 	assert.lacksClasses( $( "#flip-select-corners" ).parent(), "ui-corner-all",
 		"does not get ui-corner-all when set to false" );
@@ -18,4 +18,4 @@ test( "select based flipswitch backcompat tests", function( assert ) {
 		"has wrapper class" );
 } );
 
-} )( jQuery );
+} )( QUnit, jQuery );
