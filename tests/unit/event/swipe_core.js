c0ebe9a88856a71c869b281120b1ba37c7eea5a5
Amanpreet Singh <apsdehal@gmail.com>
2015-07-22 13:47:06 -0700

    Event: Fix swipe core tests
    Closes gh-8206

--- tests/unit/event/swipe_core.js
+++ tests/unit/event/swipe_core.js
@@ -1,1 +1,77 @@
-undefined
+( function( QUnit, $ ) {
+
+QUnit.asyncTest( "Swipe does not trigger when motion is default-prevented", function( assert ) {
+	var target = $( "#qunit" ),
+		swipeTriggered = false,
+		pointer = $.mobile.support.touch ?
+			{ down: "touchstart", move: "touchmove", up: "touchend" } :
+			{ down: "mousedown", move: "mousemove", up: "mouseup" };
+		recordSwipe = function() {
+			swipeTriggered = true;
+		};
+
+	target.one( "swipe", recordSwipe );
+
+	$.testHelper.sequence([
+		function() {
+			target.trigger( $.extend( $.Event( pointer.down ), {
+				originalEvent: {
+					touches: false
+				},
+				pageX: 206,
+				pageY: 190,
+				clientX: 206,
+				clientY: 190
+			}));
+		},
+		function() {
+			target.trigger( ( function( event ) {
+					event.preventDefault();
+					return event;
+				} )( $.extend( $.Event( pointer.move ), {
+					originalEvent: {
+						touches: false
+					},
+					pageX: 206,
+					pageY: 190,
+					clientX: 206,
+					clientY: 190
+				})));
+		},
+		function() {
+			target.trigger( ( function( event ) {
+					event.preventDefault();
+					return event;
+				} )( $.extend( $.Event( pointer.move ), {
+				originalEvent: {
+					touches: false
+				},
+				pageX: 170,
+				pageY: 190,
+				clientX: 170,
+				clientY: 190
+			})));
+		},
+		function() {
+			target.trigger( ( function( event ) {
+					event.preventDefault();
+					return event;
+				} )( $.extend( $.Event( pointer.up ), {
+				originalEvent: {
+					touches: false
+				},
+				pageX: 170,
+				pageY: 190,
+				clientX: 170,
+				clientY: 190
+			})));
+		},
+		function() {
+			assert.deepEqual( swipeTriggered, false, "Swipe was not triggered" );
+			target.off( "swipe", recordSwipe );
+			QUnit.start();
+		}
+	], 100 );
+});
+
+} )( QUnit, jQuery );
