3db5c23a9c8ef9658e367da3e1e03f987769fd25
Amanpreet Singh <apsdehal@gmail.com>
2015-07-24 15:18:20 -0700

    Toolbar: Shift to use bootstrap

--- tests/unit/toolbar/fixed_toolbar_core.js
+++ tests/unit/toolbar/fixed_toolbar_core.js
@@ -1,7 +1,7 @@
 // Establishing the page container for a toolbar reduces performance and is only needed for fixed
 // toolbars (https://github.com/jquery/jquery-mobile/issues/7839)
 
-( function( QUnit, $ ) {
+define( [ "qunit", "jquery" ], function( QUnit, $ ) {
 
 QUnit.test( "Static toolbar does not have 'pagecontainer' instance variable",
 	function( assert ) {
@@ -10,4 +10,3 @@ QUnit.test( "Static toolbar does not have 'pagecontainer' instance variable",
 	assert.deepEqual( !!$( "#testFooter" ).data( "mobile-toolbar" ).pagecontainer, true,
 		"Fixed toolbar has 'pagecontainer' instance variable" );
 } );
-} )( QUnit, jQuery );
