e66456c70d49e03bab198963ac05bc7d758c017a
Amanpreet Singh <apsdehal@gmail.com>
2015-07-26 00:38:22 -0700

    Zoom: Shift to use bootstrap

--- tests/unit/zoom/zoom-initial-disable.js
+++ tests/unit/zoom/zoom-initial-disable.js
@@ -1,11 +1,11 @@
 /*
  * mobile zoom
  */
-( function( QUnit, $ ) {
+define( [ "qunit", "jquery" ], function( QUnit, $ ) {
 
 QUnit.test( "User zooming will not enable when calling enable() method if zooming was disabled in page source", function( assert ) {
 	$.mobile.zoom.enable();
 	assert.ok( !$.mobile.zoom.enabled );
 } );
 
-} )( QUnit, jQuery );
+} );
