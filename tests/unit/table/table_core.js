65c235c922ed6eec0145f4f9d8e0a3fd7f949055
Alexander Schmitz <arschmitz@gmail.com>
2015-08-07 17:29:23 -0400

    Table: remove use of $.mobile.pageContainer in tests

--- tests/unit/table/table_core.js
+++ tests/unit/table/table_core.js
@@ -15,7 +15,7 @@ QUnit.module( "Basic Table", {
 				QUnit.start();
 			} );
 
-			$.mobile.pageContainer.pagecontainer( "change", hash );
+			$( ".ui-pagecontainer" ).pagecontainer( "change", hash );
 		}
 	},
 
@@ -81,7 +81,7 @@ QUnit.module( "Reflow Mode", {
 				QUnit.start();
 			} );
 
-			$.mobile.pageContainer.pagecontainer( "change", hash );
+			$( ".ui-pagecontainer" ).pagecontainer( "change", hash );
 		}
 	},
 
@@ -154,7 +154,7 @@ QUnit.module( "Column toggle table Mode", {
 				QUnit.start();
 			} );
 
-			$.mobile.pageContainer.pagecontainer( "change", hash );
+			$( ".ui-pagecontainer" ).pagecontainer( "change", hash );
 		}
 	},
 
