0149b9a8c25f39a86a529738561114b8d2678ea6
Sarah Frisk <sarah@sarahfrisk.com>
2015-08-20 14:55:28 -0400

    Accordion: add icons

--- demos/accordion/index.php
+++ demos/accordion/index.php
@@ -192,8 +192,47 @@
 			</div>
 		</div><!--/demo-html -->
 
-		<h2>Icons</h2>
+		<h2>Custon Icons</h2>
 		<p>You can add or remove icons with the  <code>data-icons</code> option, which accepts classes for the header's default and active (open) state. Use any class from jQuery Mobile, or create custom classes with background images</p>
+
+		<h3>Icons</h3>
+		<div data-demo-html="true">
+
+			<div data-role="accordion" data-icons='{ "header": "ui-icon-arrow-d-r", "activeHeader": "ui-icon-star"}'>
+				<h3>Section 1</h3>
+				<div>
+					<p>
+						Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+					</p>
+				</div>
+				<h3>Section 2</h3>
+				<div>
+					<p>
+						Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna.
+					</p>
+				</div>
+				<h3>Section 3</h3>
+				<div>
+					<p>
+						Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+					</p>
+					<ul>
+						<li>List item one</li>
+						<li>List item two</li>
+						<li>List item three</li>
+					</ul>
+				</div>
+				<h3>Section 4</h3>
+				<div>
+					<p>
+						Cras dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia mauris vel est.
+					</p>
+					<p>
+						Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+					</p>
+				</div>
+			</div>
+		</div><!--/demo-html -->
 
 		<h3>No Icons</h3>
 		<div data-demo-html="true">
