a3d20ee3fc214ba868f5446add6dce6934d2c7fb
Jasper de Groot <mail@ugomobi.com>
2015-11-11 09:04:27 +0100

    Demos: Update navmenu panel markup

--- demos/jqm-navmenu.php
+++ demos/jqm-navmenu.php
@@ -1,5 +1,6 @@
 	    <div data-role="panel" class="jqm-navmenu-panel" data-position="left" data-display="overlay" data-theme="a">
-	    	<ul class="jqm-list ui-alt-icon ui-nodisc-icon">
+	    	<ul class="ui-alt-icon ui-nodisc-icon">
+				<li data-role='list-divider'>Menu</li>
 		     	<?php include( 'jqm-contents.php' ); ?>
 		     </ul>
 		</div><!-- /panel -->
