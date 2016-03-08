9b23a6e95f767e488a15643f2205f4399fffb1f2
Stephen Moon <stephen@logicalmoon.com>
2016-03-08 13:01:52 +0000

    Fix to text about datepicker.
    Paragraph (originally) was difficult to understand. Hopefully this makes a little more sense!

--- demos/datepicker/index.php
+++ demos/datepicker/index.php
@@ -33,7 +33,7 @@
 
         <p>This demo uses the jQuery UI Datepicker widget combined with a 3rd party wrapper to make this work with jQuery Mobile. This widget has all the same options and methods as the jQuery UI widget.</p>
 
-        <p>For documentation on the Datepicker widget please see jQuery UI API docs <a href="http://api.jqueryui.com/datepicker/">http://api.jqueryui.com/datepicker/</a>.</p>
+        <p>For documentation on the Datepicker widget, please see jQuery UI API docs <a href="http://api.jqueryui.com/datepicker/">http://api.jqueryui.com/datepicker/</a>.</p>
 
         <p>For information and support on the jQuery Mobile Wrapper please see <a href="https://github.com/arschmitz/jquery-mobile-datepicker-wrapper">https://github.com/arschmitz/jquery-mobile-datepicker-wrapper</a>.</p>
 
@@ -43,7 +43,9 @@
             <input type="text" data-role="date">
         </div>
 
-        <p> The Popup does not always position well for use on small screens and mobile devices there for the wrapper adds an inline option this option makes the calendar for the datepicker show up inline after the input which it is called on avoiding the issues related to popups</p>
+        <p> The popup does not always position itself appropriately when used on small screens and mobile devices. 
+        Therefore, the wrapper adds an *inline* option which makes the calendar appear inline after the input. 
+        This should hopefully help avoid issues related to popups that can occur in these situations.</p>
 
         <h2>Inline Datepicker</h2>
 
