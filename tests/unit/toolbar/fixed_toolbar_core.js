a877a975775ca9f2cc4c9d44fbf1b72b18c61d8b
Amanpreet Singh <apsdehal@gmail.com>
2016-03-26 11:58:57 +0530

    Toolbar: Cleanup deprecated QUnit code
    Closes #8221

--- tests/unit/toolbar/fixed_toolbar_core.js
+++ tests/unit/toolbar/fixed_toolbar_core.js
@@ -1,8 +1,13 @@
 // Establishing the page container for a toolbar reduces performance and is only needed for fixed
 // toolbars (https://github.com/jquery/jquery-mobile/issues/7839)
-test( "Static toolbar does not have 'pagecontainer' instance variable", function() {
-deepEqual( !!$( "#testHeader" ).data( "mobile-toolbar" ).pagecontainer, false,
-	"Static toolbar does not have 'pagecontainer' instance variable" );
-deepEqual( !!$( "#testFooter" ).data( "mobile-toolbar" ).pagecontainer, true,
-	"Fixed toolbar has 'pagecontainer' instance variable" );
+
+( function( QUnit, $ ) {
+
+QUnit.test( "Static toolbar does not have 'pagecontainer' instance variable",
+	function( assert ) {
+	assert.deepEqual( !!$( "#testHeader" ).data( "mobile-toolbar" ).pagecontainer, false,
+		"Static toolbar does not have 'pagecontainer' instance variable" );
+	assert.deepEqual( !!$( "#testFooter" ).data( "mobile-toolbar" ).pagecontainer, true,
+		"Fixed toolbar has 'pagecontainer' instance variable" );
 } );
+} )( QUnit, jQuery );
--- tests/unit/toolbar/toolbar_core.js
+++ tests/unit/toolbar/toolbar_core.js
@@ -1,97 +1,102 @@
 /*
- * mobile Fixed Toolbar unit tests
+ * Mobile Fixed Toolbar unit tests
  */
-( function( $ ) {
-    module( "toolbar" );
+( function( QUnit, $ ) {
+	QUnit.config.autostart = false;
 
-    test( "markup structure", function( assert ) {
-        $( "body" ).pagecontainer( "change", "#default" );
-        var toolbar = $( "#testHeaderClasses" ),
-            toolbarH1 = toolbar.find( "h1" );
+	QUnit.module( "toolbar" );
 
-        assert.hasClasses( toolbar, "ui-toolbar-header",
-            "toolbar gets ui-toolbar-header after init" );
-        assert.hasClasses( toolbarH1, "ui-toolbar-title", "toolbar title assigned correctly" );
-        ok( toolbar.attr( "role" ) === "banner", "header gets banner role assigned" );
-        ok( toolbarH1.attr( "role" ) === "heading", "heading role properly assigned" );
-    } );
+	QUnit.test( "markup structure", function( assert ) {
+		$( "body" ).pagecontainer( "change", "#default" );
+		var toolbar = $( "#testHeaderClasses" ),
+			toolbarH1 = toolbar.find( "h1" );
 
-    test( "footer classes and roles assigned correctly", function( assert ) {
-        $( ":mobile-pagecontainer" ).pagecontainer( "change", "#default" );
-        var toolbar = $( "#testFooterClasses" ),
-            toolbarH1 = toolbar.find( "h1" );
+		assert.hasClasses( toolbar, "ui-toolbar-header",
+			"toolbar gets ui-toolbar-header after init" );
+		assert.hasClasses( toolbarH1, "ui-toolbar-title", "toolbar title assigned correctly" );
+		assert.ok( toolbar.attr( "role" ) === "banner", "header gets banner role assigned" );
+		assert.ok( toolbarH1.attr( "role" ) === "heading", "heading role properly assigned" );
+	} );
 
-        assert.hasClasses( toolbar, "ui-toolbar-footer",
-            "toolbar gets ui-toolbar-footer after init" );
-        assert.hasClasses( toolbarH1, "ui-toolbar-title", "toolbar title assigned correctly" );
-        ok( toolbar.attr( "role" ) === "contentinfo", "footer gets contentinfo role assigned" );
-        ok( toolbarH1.attr( "role" ) === "heading", "heading role properly assigned" );
-    } );
+	QUnit.test( "footer classes and roles assigned correctly", function( assert ) {
+		$( ":mobile-pagecontainer" ).pagecontainer( "change", "#default" );
+		var toolbar = $( "#testFooterClasses" ),
+			toolbarH1 = toolbar.find( "h1" );
 
-    asyncTest( "manual init works properly", function( assert ) {
-        $( "body" ).pagecontainer( "change", "#default" );
-        expect( 3 );
+		assert.hasClasses( toolbar, "ui-toolbar-footer",
+			"toolbar gets ui-toolbar-footer after init" );
+		assert.hasClasses( toolbarH1, "ui-toolbar-title", "toolbar title assigned correctly" );
+		assert.ok( toolbar.attr( "role" ) === "contentinfo",
+			"footer gets contentinfo role assigned" );
+		assert.ok( toolbarH1.attr( "role" ) === "heading", "heading role properly assigned" );
+	} );
 
-        var toolbar = $( "#testInit" ).toolbar( {
-            create: function( event, ui ) {
-                ok( true, "create event fired" );
-                start();
-            }
-        } );
+	QUnit.asyncTest( "manual init works properly", function( assert ) {
+		$( "body" ).pagecontainer( "change", "#default" );
+		assert.expect( 3 );
 
-        assert.hasClasses( toolbar, "ui-toolbar", "manual init gets base toolbar class" );
-        assert.hasClasses( toolbar.find( "h1" ), "ui-toolbar-title", "ui-toolbar-title assigned" );
-    } );
+		var toolbar = $( "#testInit" ).toolbar( {
+			create: function( event, ui ) {
+				assert.ok( true, "create event fired" );
+				QUnit.start();
+			}
+		} );
 
-    asyncTest( "external headers are created properly", function( assert ) {
-        $( "body" ).pagecontainer( "change", "#default" );
-        expect( 3 );
+		assert.hasClasses( toolbar, "ui-toolbar", "manual init gets base toolbar class" );
+		assert.hasClasses( toolbar.find( "h1" ), "ui-toolbar-title", "ui-toolbar-title assigned" );
+	} );
 
-        var toolbar = $( "#testExternalHeader" ).toolbar( {
-            create: function( event, ui ) {
-                ok( true, "external toolbar create event" );
-                start();
-            }
-        } );
+	QUnit.asyncTest( "external headers are created properly", function( assert ) {
+		$( "body" ).pagecontainer( "change", "#default" );
+		assert.expect( 3 );
 
-        assert.hasClasses( toolbar, "ui-toolbar-header",
-            "external toolbar gets ui-toolbar-header class" );
-        assert.hasClasses( toolbar.find( "h1" ), "ui-toolbar-title",
-            "ui-toolbar-title assigned properly" );
-    } );
+		var toolbar = $( "#testExternalHeader" ).toolbar( {
+			create: function( event, ui ) {
+				assert.ok( true, "external toolbar create event" );
+				QUnit.start();
+			}
+		} );
 
-     asyncTest( "external footers are created properly", function( assert ) {
-        $( "body" ).pagecontainer( "change", "#default" );
-        expect( 3 );
+		assert.hasClasses( toolbar, "ui-toolbar-header",
+			"external toolbar gets ui-toolbar-header class" );
+		assert.hasClasses( toolbar.find( "h1" ), "ui-toolbar-title",
+			"ui-toolbar-title assigned properly" );
+	} );
 
-        var toolbar = $( "#testExternalFooter" ).toolbar( {
-            create: function( event, ui ) {
-                ok( true, "external toolbar create event" );
-                start();
-            }
-        } );
+	 QUnit.asyncTest( "external footers are created properly", function( assert ) {
+		$( "body" ).pagecontainer( "change", "#default" );
+		assert.expect( 3 );
 
-        assert.hasClasses( toolbar, "ui-toolbar-footer",
-            "external toolbar gets ui-toolbar-footer class" );
-        assert.hasClasses( toolbar.find( "h1" ), "ui-toolbar-title",
-            "ui-toolbar-title assigned properly" );
-    } );
+		var toolbar = $( "#testExternalFooter" ).toolbar( {
+			create: function( event, ui ) {
+				assert.ok( true, "external toolbar create event" );
+				QUnit.start();
+			}
+		} );
 
-    test( "destroy preserves original structure", function() {
-        $( "body" ).pagecontainer( "change", "#destroyPage" );
+		assert.hasClasses( toolbar, "ui-toolbar-footer",
+			"external toolbar gets ui-toolbar-footer class" );
+		assert.hasClasses( toolbar.find( "h1" ), "ui-toolbar-title",
+			"ui-toolbar-title assigned properly" );
+	} );
 
-        var unEnhanced = $( "#testDestroy" ).clone(),
-            destroyed = $( "#testDestroy" ).toolbar().toolbar( "destroy" ),
-            unEnhancedHeader = $( "<div data-" + ( $.mobile.ns || "" ) + "role='toolbar' data-" +
-                            ( $.mobile.ns || "" ) + "type='header'>" +
-                            "<h1>title</h1>" +
-                            "</div>" ),
-            headerClone = unEnhancedHeader.clone();
-        $( "#destroyPage" ).append( headerClone );
-        headerClone.toolbar().toolbar( "destroy" );
+	QUnit.test( "destroy preserves original structure", function( assert ) {
+		$( "body" ).pagecontainer( "change", "#destroyPage" );
 
-        ok( $.testHelper.domEqual( destroyed, unEnhanced ), "unEnhanced footer equals destroyed" );
-        ok( $.testHelper.domEqual( headerClone, unEnhancedHeader ), "headers match after destroy" );
-     } );
+		var unEnhanced = $( "#testDestroy" ).clone(),
+			destroyed = $( "#testDestroy" ).toolbar().toolbar( "destroy" ),
+			unEnhancedHeader = $( "<div data-" + ( $.mobile.ns || "" ) + "role='toolbar' data-" +
+							( $.mobile.ns || "" ) + "type='header'>" +
+							"<h1>title</h1>" +
+							"</div>" ),
+			headerClone = unEnhancedHeader.clone();
+		$( "#destroyPage" ).append( headerClone );
+		headerClone.toolbar().toolbar( "destroy" );
 
-} )( jQuery );
+		assert.ok( $.testHelper.domEqual( destroyed, unEnhanced ),
+			"unEnhanced footer equals destroyed" );
+		assert.ok( $.testHelper.domEqual( headerClone, unEnhancedHeader ),
+			"headers match after destroy" );
+	 } );
+
+} )( QUnit, jQuery );
