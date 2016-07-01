da5b42165d9c8025749618b44a7de8882a6b3916
Alexander Schmitz <arschmitz@gmail.com>
2016-07-01 09:34:55 -0400

    Build: Ignore accordion api until it exists

--- build/tasks/options/spider.js
+++ build/tasks/options/spider.js
@@ -3,14 +3,16 @@ module.exports = {
 		options: {
 			url: "http://localhost:" + "<%= phpPort %>",
 			ignore:
-			"notapage|intel|api\\.jquerymobile\\.com/[0-9]\\.[0-9]|packt|twitter\\.com/jquery"
+			"api\\.jquerymobile\\.com/accordion|notapage|intel|" +
+			"api\\.jquerymobile\\.com/[0-9]\\.[0-9]|packt|twitter\\.com/jquery"
 		}
 	},
 	"dist": {
 		options: {
 			url: "http://localhost:" + "<%= phpPort %>" + "/dist/",
 			ignore:
-			"notapage|intel|api\\.jquerymobile\\.com/[0-9]\\.[0-9]|packt|twitter\\.com/jquery"
+			"api\\.jquerymobile\\.com/accordion|notapage|intel|" +
+			"api\\.jquerymobile\\.com/[0-9]\\.[0-9]|packt|twitter\\.com/jquery"
 		}
 	}
 };
